e61217dc37aac83d9a31c774018e9c0582f1c35e
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2001-01-10 23:54:59 +0000

    MWE:   * Fixed make clean   + moved allunits from exe to unit, skipping link stage
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@111 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/allunits.pp
+++ lcl/allunits.pp
@@ -1,12 +1,31 @@
-program allunits;
+{  $Id$  }
+{
+ /***************************************************************************
+                               allunits.pp
 
+                      dummy unit to compile all units 
+
+ /***************************************************************************
+}
+unit allunits;
+interface
 uses
   interfacebase, interfaces,
 	buttons,    extctrls,        registry,     vclglobals, 
 	clipbrd,    filectrl,        forms,        lcllinux,   spin, 
 	comctrls,   graphics,        lmessages,    stdctrls, 
 	controls,   imglist,         menus,        toolwin, 
-	dialogs,    messages,     utrace;
-	
-begin
+	dialogs,    messages,        utrace;
+
+implementation
 end.
+
+{ =============================================================================
+
+  $Log$
+  Revision 1.3  2001/01/10 23:54:59  lazarus
+  MWE:
+    * Fixed make clean
+    + moved allunits from exe to unit, skipping link stage
+
+}
