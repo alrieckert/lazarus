27faf9ad773fbda2ae14359ecb69ee2bfc936749
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2001-01-28 16:16:11 +0000

    MWE:   + Added synedit to the components
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@142 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/synedit/allunits.pp
@@ -0,0 +1,28 @@
+{  $Id$  }
+{
+ /***************************************************************************
+                               allunits.pp
+
+                      dummy unit to compile all units 
+
+ /***************************************************************************
+}
+unit allunits;
+interface
+uses
+  synedit, syneditautocomplete, synedithighlighter, syneditkeycmds,
+  syneditmiscclasses, syneditmiscprocs, syneditsearch, syneditstrconst,
+  synedittextbuffer, synedittypes, synhighlighterpas, syntextdrawer;
+	
+implementation
+end.
+
+{ =============================================================================
+
+  $Log$
+  Revision 1.1  2001/01/28 16:16:11  lazarus
+  MWE:
+    + Added synedit to the components
+
+
+}
