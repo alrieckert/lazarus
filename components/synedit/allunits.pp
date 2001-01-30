a9424c6bae49e642c38628644254623c99ebb44e
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2001-01-30 22:55:00 +0000

    MWE:   + Added $mode objfpc directive
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@149 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/allunits.pp
+++ components/synedit/allunits.pp
@@ -8,6 +8,7 @@
  /***************************************************************************
 }
 unit allunits;
+{$mode objfpc}
 interface
 uses
   synedit, syneditautocomplete, synedithighlighter, syneditkeycmds,
@@ -20,6 +21,10 @@ end.
 { =============================================================================
 
   $Log$
+  Revision 1.2  2001/01/30 22:55:00  lazarus
+  MWE:
+    + Added $mode objfpc directive
+
   Revision 1.1  2001/01/28 16:16:11  lazarus
   MWE:
     + Added synedit to the components
