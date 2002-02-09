fe78ea8762982e48abbbe0b57b8ea3a4241078ca
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2002-02-09 01:48:11 +0000

    MG: updated licenses
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@1264 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/win32/win32def.pp
+++ lcl/interfaces/win32/win32def.pp
@@ -9,15 +9,19 @@
 
  This unit contains type definitions needed in the Windows <-> LCL interface
 
-/*************************************************************************** 
- *                                                                         * 
- *   This program is free software; you can redistribute it and/or modify  * 
- *   it under the terms of the GNU General Public License as published by  * 
- *   the Free Software Foundation; either version 2 of the License, or     * 
- *   (at your option) any later version.                                   * 
- *                                                                         * 
- ***************************************************************************/ 
- }
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.LCL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
 
 Unit Win32Def;
 
@@ -116,6 +120,9 @@ End.
 { =============================================================================
 
   $Log$
+  Revision 1.4  2002/05/10 07:43:48  lazarus
+  MG: updated licenses
+
   Revision 1.3  2002/01/05 13:16:09  lazarus
   MG: win32 interface update from Keith Bowes
 
