451317d4099e78733d91b01757eb2198ced242e0
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2002-02-09 01:47:10 +0000

    MG: changed license to LGPL
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@867 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/gtk/gtkint.pp
+++ lcl/interfaces/gtk/gtkint.pp
@@ -8,15 +8,19 @@
  
  ***************************************************************************/ 
  
-/*************************************************************************** 
- *                                                                         * 
- *   This program is free software; you can redistribute it and/or modify  * 
- *   it under the terms of the GNU General Public License as published by  * 
- *   the Free Software Foundation; either version 2 of the License, or     * 
- *   (at your option) any later version.                                   * 
- *                                                                         * 
- ***************************************************************************/ 
- } 
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.LCL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+ }
  
 unit GtkInt;
  
@@ -357,6 +361,9 @@ end.
 { =============================================================================
 
   $Log$
+  Revision 1.38  2002/05/10 06:05:56  lazarus
+  MG: changed license to LGPL
+
   Revision 1.37  2002/05/09 12:41:29  lazarus
   MG: further clientrect bugfixes
 
