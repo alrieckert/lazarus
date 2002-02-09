b871241f0493883cef2ba76957e64b7c1c5a8350
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2002-02-09 01:48:10 +0000

    Removed FOCUSIN messages Removed Bitbtn created message Shane
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@1249 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/gtk/gtkwinapiwindow.pp
+++ lcl/interfaces/gtk/gtkwinapiwindow.pp
@@ -213,10 +213,6 @@ function GTKAPIWidgetClient_FocusIn(Widget: PGTKWidget;
   Event: PGdkEventFocus): gint; cdecl;
 begin
 //  Assert(False, 'Trace:[GTKAPIWidgetClient_FocusIn]');
-Writeln('------------------------');
-Writeln('FOCUSIN');
-Writeln('------------------------');
-
   gtk_widget_set_flags(Widget, GTK_HAS_FOCUS);
   GTKAPIWidgetClient_DrawCaret(PGTKAPIWidgetClient(Widget));
   Result := gtk_False;
@@ -226,9 +222,6 @@ function GTKAPIWidgetClient_FocusOut(Widget: PGTKWidget;
   Event: PGdkEventFocus): gint; cdecl;
 begin
 //  Assert(False, 'Trace:[GTKAPIWidgetClient_FocusOut]');
-Writeln('------------------------');
-Writeln('FOCUSOUT');
-Writeln('------------------------');
   gtk_widget_unset_flags(Widget, GTK_HAS_FOCUS);
   GTKAPIWidgetClient_DrawCaret(PGTKAPIWidgetClient(Widget));
   Result := gtk_False;
@@ -725,6 +718,11 @@ end.
 { =============================================================================
 
   $Log$
+  Revision 1.23  2002/03/15 13:15:24  lazarus
+  Removed FOCUSIN messages
+  Removed Bitbtn created message
+  Shane
+
   Revision 1.22  2002/03/14 20:28:50  lazarus
   Bug fix for Mattias.
   Fixed spinedit so you can now get the value and set the value.
