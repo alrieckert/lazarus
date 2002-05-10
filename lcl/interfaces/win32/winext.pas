3da32afc188a8ff143fe7edccd046b6debe986d0
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2002-05-10 07:43:49 +0000

    MG: updated licenses
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@1671 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/win32/winext.pas
+++ lcl/interfaces/win32/winext.pas
@@ -2,15 +2,18 @@
   Extra Win32 code that's not in the RTL.
   Copyright (C) 2001, 2002 Keith Bowes.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.LCL, included in this distribution,                 *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
 }
 
 Unit WinExt;
