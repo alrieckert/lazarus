b790a13fb6703e55f63cca48ffaa4bc3a7c87b69
lazarus <lazarus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2002-09-13 14:06:19 +0000

    Fix designer initialization.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@3335 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -271,10 +271,6 @@ resourcestring
    +'along with this program; if not, write to the Free Software '
    +'Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. ';
 
-  // component editors commands
-  liscAdd = '&Add';
-  liscDelete = '&Delete';
-  
 implementation
 
 end.
