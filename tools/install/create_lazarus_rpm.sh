e76e71cd2321cfd21f5aa703c0ae521d6f374647
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2003-03-16 16:37:31 +0000

    added fully automatic lazarus rpm script
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@3941 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ tools/install/create_lazarus_rpm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#set -x
+set -e
+
+# get date of day
+Year=`date +%y`
+Month=`date +%m`
+Day=`date +%d`
+
+# get installed fpc version
+FPCRPM=`rpm -qa | egrep '^fpc-'`
+if [ "x$FPCRPM" = "x" ]; then
+  echo ERROR: fpc rpm not installed
+  exit
+fi
+
+Date=20$Year$Month$Day
+LazVersion=0.8.5.1
+LazRelease=$FPCRPM
+LazRelease=`echo $FPCRPM | sed -e 's/-/_/g'`
+SrcTGZ=lazarus-$Date.tgz
+TmpDir=/tmp/lazarus$LazVersion
+SpecFile=lazarus-$LazVersion-$LazRelease.spec
+
+# download lazarus cvs if necessary
+if [ ! -f $SrcTGZ ]; then
+  ./create_lazarus_export_tgz.sh $SrcTGZ
+fi
+
+# put src tgz into rpm build directory
+cp $SrcTGZ /usr/src/redhat/SOURCES/
+
+# create spec file
+cat lazarus.spec | \
+  sed -e "s/LAZVERSION/$LazVersion/g" \
+      -e "s/LAZRELEASE/$LazRelease/" \
+      -e "s/LAZSOURCE/$SrcTGZ/" \
+  > $SpecFile
+
+# build rpm
+rpm -ba $SpecFile || rpmbuild -ba $SpecFile
+
+# end.
+
