1e01a573ab8b383511ffb465b113dda806fbb39a
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2003-04-21 16:22:57 +0000

    implemented package synedit
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@4089 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ packager/registersynedit.pas
@@ -0,0 +1,135 @@
+{  $Id$  }
+{
+ /***************************************************************************
+                           registersynedit.pas
+                           -------------------
+
+
+ ***************************************************************************/
+
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+
+  Author: Mattias Gaertner
+
+  Abstract:
+    Registration of the SynEdit components.
+}
+unit RegisterSynEdit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  SynEditLazDsgn, SynEdit, SynCompletion, SynExportHTML, SynMacroRecorder,
+  SynMemo, SynHighlighterPas, SynHighlighterCPP, SynHighlighterJava,
+  SynHighlighterPerl, SynHighlighterHTML, SynHighlighterXML,
+  SynHighlighterLFM, SynHighlighterMulti,
+  LazarusPackageIntf, LazarusIDEStrConsts;
+
+procedure Register;
+
+implementation
+
+procedure RegisterSynEditUnit;
+begin
+  RegisterComponents('SynEdit',[TSynEdit]);
+end;
+
+procedure RegisterSynCompletion;
+begin
+  RegisterComponents('SynEdit',[TSynAutoComplete]);
+end;
+
+procedure RegisterSynExportHTML;
+begin
+  RegisterComponents('SynEdit',[TSynExporterHTML]);
+end;
+
+procedure RegisterSynMacroRecorder;
+begin
+  RegisterComponents('SynEdit',[TSynMacroRecorder]);
+end;
+
+procedure RegisterSynMemo;
+begin
+  RegisterComponents('SynEdit',[TSynMemo]);
+end;
+
+procedure RegisterSynHighlighterPas;
+begin
+  RegisterComponents('SynEdit',[TSynPasSyn]);
+end;
+
+procedure RegisterSynHighlighterJava;
+begin
+  RegisterComponents('SynEdit',[TSynJavaSyn]);
+end;
+
+procedure RegisterSynHighlighterCPP;
+begin
+  RegisterComponents('SynEdit',[TSynCPPSyn]);
+end;
+
+procedure RegisterSynHighlighterPerl;
+begin
+  RegisterComponents('SynEdit',[TSynPerlSyn]);
+end;
+
+procedure RegisterSynHighlighterHTML;
+begin
+  RegisterComponents('SynEdit',[TSynHTMLSyn]);
+end;
+
+procedure RegisterSynHighlighterXML;
+begin
+  RegisterComponents('SynEdit',[TSynXMLSyn]);
+end;
+
+procedure RegisterSynHighlighterLFM;
+begin
+  RegisterComponents('SynEdit',[TSynLFMSyn]);
+end;
+
+procedure RegisterSynHighlighterMulti;
+begin
+  RegisterComponents('SynEdit',[TSynMultiSyn]);
+end;
+
+procedure Register;
+begin
+  RegisterUnit('SynEdit',@RegisterSynEditUnit);
+  RegisterUnit('SynCompletion',@RegisterSynCompletion);
+  RegisterUnit('SynExportHTML',@RegisterSynExportHTML);
+  RegisterUnit('SynMacroRecorder',@RegisterSynMacroRecorder);
+  RegisterUnit('SynMemo',@RegisterSynMemo);
+  RegisterUnit('SynHighlighterPas',@RegisterSynHighlighterPas);
+  RegisterUnit('SynHighlighterCPP',@RegisterSynHighlighterCPP);
+  RegisterUnit('SynHighlighterJava',@RegisterSynHighlighterJava);
+  RegisterUnit('SynHighlighterPerl',@RegisterSynHighlighterPerl);
+  RegisterUnit('SynHighlighterHTML',@RegisterSynHighlighterHTML);
+  RegisterUnit('SynHighlighterXML',@RegisterSynHighlighterXML);
+  RegisterUnit('SynHighlighterLFM',@RegisterSynHighlighterLFM);
+  RegisterUnit('SynHighlighterMulti',@RegisterSynHighlighterMulti);
+end;
+
+end.
+
