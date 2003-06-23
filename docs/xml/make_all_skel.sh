3d48612f9522a9be03eb60f0d66a88927a94e045
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2003-06-23 21:40:17 +0000

    added fpdoc sceletons for LCL and gtk interface
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@4299 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ docs/xml/make_all_skel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Author: Mattias Gaertner
+#
+# This script calls makeskel on every unit in the current directory and creates
+# an fpdoc xml file for every unit.
+
+set -x
+set -e
+
+MakeSkel=$1
+if [ -z $MakeSkel ]; then
+  MakeSkel=makeskel
+fi
+
+# create unit list
+UnitList=`echo *.pp *.pas`
+
+for unit in $UnitList; do
+  OutFile=`echo $unit | sed -e 's/\.pp\b/\.xml/g' -e 's/\.pas\b/\.xml/g'`
+  $MakeSkel --input=$unit' -Fiinclude' --package=lcl --output=$OutFile
+done
+
+# end.
+
