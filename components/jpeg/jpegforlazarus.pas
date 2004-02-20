a94c44a9298dc887911fb6d0aca1cacbc0601d76
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2004-02-20 14:44:40 +0000

    added package JPEGForLazarus
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@5210 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/jpeg/jpegforlazarus.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install
+  the package JPEGForLazarus 1.0.1.
+}
+
+unit JPEGForLazarus; 
+
+interface
+
+uses
+  LazJPEG, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('JPEGForLazarus', @Register)
+end.
