a22a4b1aaeeddb1fb7d08c4d6785d10e61726a01
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2004-10-04 22:00:52 +0000

    Use -XX option for building lazarus to prevent problems on win95.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@6114 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/win32/build-lazarus.bat
+++ tools/install/win32/build-lazarus.bat
@@ -6,7 +6,7 @@ SET COMPILER=%BUILDDIR%\pp\bin\win32\ppc386.exe
 %BUILDDRIVE%
 cd %BUILDDIR%
 %MAKEEXE% clean PP=%COMPILER% >> %LOGFILE%
-%MAKEEXE% all OPT="-gl -Xs" PP=%COMPILER% >> %LOGFILE%
+%MAKEEXE% all OPT="-gl -Xs -XX" PP=%COMPILER% >> %LOGFILE%
 %FPCBINDIR%\strip.exe lazarus.exe
 
 %FPCBINDIR%\strip.exe startlazarus.exe
