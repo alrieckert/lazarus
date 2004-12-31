d366495e9eb6e544a0ea15035ef79788dfdc4135
micha <micha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2004-12-31 16:41:26 +0000

    speed up creation a bit
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@6443 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/groupboxnested.pas
+++ examples/groupboxnested.pas
@@ -42,28 +42,29 @@ begin
   inherited Create(TheOwner);
   Name:='Form1';
   Caption:='Nested Groupbox demo';
-  SetBounds(100,50,300,250);
   
   GroupBox1:=TGroupBox.Create(Self);
   with GroupBox1 do begin
     Name:='GroupBox1';
-    Parent:=Self;
+    SetBounds(0,0,350,350);
     Align:=alClient;
+    Parent:=Self;
   end;
 
   GroupBox2:=TGroupBox.Create(Self);
   with GroupBox2 do begin
     Name:='GroupBox2';
-    Parent:=GroupBox1;
     Align:=alClient;
+    Parent:=GroupBox1;
   end;
 
   GroupBox3:=TGroupBox.Create(Self);
   with GroupBox3 do begin
     Name:='GroupBox3';
-    Parent:=GroupBox2;
     Align:=alClient;
+    Parent:=GroupBox2;
   end;
+  SetBounds(100,50,300,250);
 end;
 
 var Form1 : TForm1;
