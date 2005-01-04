2bd743a238d5b3d9daad7b50b006cb105eb4778c
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2005-01-04 08:49:22 +0000

    set version to 0.9.5
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@6477 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/win32/create_installer.bat
+++ tools/install/win32/create_installer.bat
@@ -1,5 +1,5 @@
 :: This setting need to change for every build
-SET LAZVERSION=0.9.4
+SET LAZVERSION=0.9.5
 
 :: These settings are dependent on the configuration of the build machine
 :: Path to the Inno Setup Compiler
