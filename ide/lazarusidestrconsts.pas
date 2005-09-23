44e71ccd5e4352cce8db933ad0d1c4ba8a1ff97a
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2005-09-23 21:48:30 +0000

    fixed Insert GPL
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@7812 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -624,20 +624,21 @@ resourcestring
   lisGPLNotice =
     'Copyright (C) <year> <name of author>'
    +'%s'
-   +'This program is free software; you can redistribute it and/or modify '
-   +'it under the terms of the GNU General Public License as published by '
-   +'the Free Software Foundation; either version 2 of the License, or '
-   +'(at your option) any later version. '
+   +'This source is free software; you can redistribute it and/or modify'
+   +'it under the terms of the GNU General Public License as published by'
+   +'the Free Software Foundation; either version 2 of the License, or'
+   +'(at your option) any later version.'
    +'%s'
-   +'This program is distributed in the hope that it will be useful, '
-   +'but WITHOUT ANY WARRANTY; without even the implied warranty of '
-   +'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the '
-   +'GNU Library General Public License for more details. '
+   +'This code is distributed in the hope that it will be useful, but'
+   +'WITHOUT ANY WARRANTY; without even the implied warranty of'
+   +'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU'
+   +'General Public License for more details.'
    +'%s'
-   +'You should have received a copy of the GNU General Public License '
-   +'along with this program; if not, write to the Free Software '
-   +'Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
-   
+   +'A copy of the GNU General Public License is available on the World'
+   +'Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also'
+   +'obtain it by writing to the Free Software Foundation,'
+   +'Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
+
   lisLGPLNotice =
     'Copyright (C) <year> <name of author>'
    +'%s'
