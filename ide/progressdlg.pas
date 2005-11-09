414c7855bbd2feb70ee0e236e0d1d2d5ce0455b7
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2005-11-09 21:29:23 +0000

    implemented IDE progress dialog
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8121 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/progressdlg.pas
+++ ide/progressdlg.pas
@@ -42,18 +42,86 @@ type
     DescriptionLabel: TLabel;
     ProgressBar: TProgressBar;
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
   public
     procedure ApplicationIdle(Sender: TObject);
   end; 
+  
+var
+  IDEProgressDialog: TIDEProgressDialog = nil;
 
 function ShowProgress(const SomeText: string;
                       Step, MaxStep: integer): boolean;
 
 implementation
 
-function ShowProgress(const SomeText: string; Step, MaxStep: integer): boolean;
+type
+
+  { TProgressWait }
+
+  { TProgressWait }
+
+  TProgressWait = class
+  public
+    StartTime: TDateTime;
+    StartTimeValid: boolean;
+    constructor Create;
+    procedure ApplicationIdle(Sender: TObject);
+  end;
+
+{ TProgressWait }
+
+constructor TProgressWait.Create;
 begin
+  Application.AddOnIdleHandler(@ApplicationIdle);
+end;
+
+procedure TProgressWait.ApplicationIdle(Sender: TObject);
+begin
+  StartTimeValid:=false;
+end;
+  
+var
+  ProgressWait: TProgressWait = nil;
+
+function ShowProgress(const SomeText: string; Step, MaxStep: integer): boolean;
+const
+  Delay = 1.0/86400;
+  
+  procedure InitDlg;
+  begin
+    IDEProgressDialog.DescriptionLabel.Caption:=SomeText;
+    if (Step>=0) and (MaxStep>Step) then begin
+      IDEProgressDialog.ProgressBar.Visible:=true;
+      IDEProgressDialog.ProgressBar.Max:=MaxStep;
+      IDEProgressDialog.ProgressBar.Position:=Step;
+    end else begin
+      IDEProgressDialog.ProgressBar.Visible:=false;
+    end;
+  end;
+  
+begin
+  if IDEProgressDialog<>nil then begin
+    // there is already a TIDEProgressDialog
+    InitDlg;
+  end else begin
+    // there is no TIDEProgressDialog yet
+    // create one after 1 second
+    if ProgressWait=nil then
+      ProgressWait:=TProgressWait.Create;
+    if ProgressWait.StartTimeValid then begin
+      if Now-ProgressWait.StartTime>Delay then begin
+        // one second waited
+        // => create a TIDEProgressDialog and show it modal
+        IDEProgressDialog:=TIDEProgressDialog.Create(Application);
+        InitDlg;
+        IDEProgressDialog.Show;
+      end;
+    end else begin
+      ProgressWait.StartTime:=Now;
+    end;
+  end;
   Result:=true;
 end;
 
@@ -68,13 +136,28 @@ begin
   Application.AddOnIdleHandler(@ApplicationIdle);
 end;
 
+procedure TIDEProgressDialog.FormDestroy(Sender: TObject);
+begin
+  Application.RemoveOnIdleHandler(@ApplicationIdle);
+end;
+
 procedure TIDEProgressDialog.ApplicationIdle(Sender: TObject);
 begin
-  ModalResult:=mrOk;
+  // IDE got idle => progress dialog is not used anymore
+  if Screen.FormIndex(Self)>=0 then begin
+    // let the LCL close it on end of message loop
+    Close;
+  end else begin
+    // unused and invisible -> free it
+    FreeAndNil(IDEProgressDialog);
+  end;
 end;
 
 initialization
   {$I progressdlg.lrs}
+  
+finalization
+  FreeAndNil(ProgressWait);
 
 end.
 
