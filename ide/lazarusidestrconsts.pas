c48abc0d0d9b50b78f7c6da11bd45d285feb633d
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2005-11-19 10:32:11 +0000

    fixed gpl insertion  from Graeme Geldenhuys
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8190 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -625,21 +625,21 @@ resourcestring
   lisHintStepOver = 'Step Over';
   
   lisGPLNotice =
-    'Copyright (C) <year> <name of author>'
+    'Copyright (C) <year> <name of author> '
    +'%s'
-   +'This source is free software; you can redistribute it and/or modify'
-   +'it under the terms of the GNU General Public License as published by'
-   +'the Free Software Foundation; either version 2 of the License, or'
-   +'(at your option) any later version.'
+   +'This source is free software; you can redistribute it and/or modify '
+   +'it under the terms of the GNU General Public License as published by '
+   +'the Free Software Foundation; either version 2 of the License, or '
+   +'(at your option) any later version. '
    +'%s'
-   +'This code is distributed in the hope that it will be useful, but'
-   +'WITHOUT ANY WARRANTY; without even the implied warranty of'
-   +'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU'
-   +'General Public License for more details.'
+   +'This code is distributed in the hope that it will be useful, but '
+   +'WITHOUT ANY WARRANTY; without even the implied warranty of '
+   +'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU '
+   +'General Public License for more details. '
    +'%s'
-   +'A copy of the GNU General Public License is available on the World'
-   +'Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also'
-   +'obtain it by writing to the Free Software Foundation,'
+   +'A copy of the GNU General Public License is available on the World '
+   +'Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also '
+   +'obtain it by writing to the Free Software Foundation, '
    +'Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
 
   lisLGPLNotice =
