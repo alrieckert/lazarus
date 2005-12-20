59b7b7f7e36053bbd459ef6dd73512180d1b10ca
andrew <andrew@4005530d-fff6-0310-9dd1-cebe43e6787f>
2005-12-20 03:28:40 +0000

    Updated paslzx license info - fixed typo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8347 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/chmhelp/packages/chm/paslzx.pas
+++ components/chmhelp/packages/chm/paslzx.pas
@@ -1,4 +1,4 @@
-{ Copyright (C) <2005> <Andrew Haines> chmspecialparser.pas
+{ Copyright (C) <2005> <Andrew Haines> paslzx.pas
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
