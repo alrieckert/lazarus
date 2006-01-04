6361c0608e3c5b6b03c18b3510bbd4ea67561485
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-01-04 17:36:00 +0000

    updated license note
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8443 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/sqlite/registersqlite.pas
+++ components/sqlite/registersqlite.pas
@@ -1,3 +1,15 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL, included in this distribution,        *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
 unit registersqlite;
 
 {$Mode ObjFpc}
