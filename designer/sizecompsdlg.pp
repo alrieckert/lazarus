6b315f0cf65c88925dd072478f1e9b55bc76c1c5
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-01-10 14:33:16 +0000

    converted SizeCompsDlg to lfm  from Darius
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8486 4005530d-fff6-0310-9dd1-cebe43e6787f

--- designer/sizecompsdlg.pp
+++ designer/sizecompsdlg.pp
@@ -34,19 +34,18 @@ uses
   LResources;
 
 type
+  { TSizeComponentsDialog }
   TSizeComponentsDialog = class(TForm)
+    OKButton: TBitBtn;
+    CancelButton: TBitBtn;
     WidthRadioGroup: TRadioGroup;
-    WidthEdit: TEdit;
     HeightRadioGroup: TRadioGroup;
+    WidthEdit: TEdit;
     HeightEdit: TEdit;
-    OkButton: TButton;
-    CancelButton: TButton;
-    procedure OkButtonClick(Sender: TObject);
-    procedure CancelButtonClick(Sender: TObject);
+    procedure FormResize(Sender: TObject);
   public
     constructor Create(AOwner: TComponent);  override;
   end;
-
 
 function ShowSizeComponentsDialog(var HorizSizingID, FixedWidth,
   VertSizingID, FixedHeight: integer): TModalResult;
@@ -59,9 +58,6 @@ var SizeComponentsDialog: TSizeComponentsDialog;
 begin
   SizeComponentsDialog:=TSizeComponentsDialog.Create(nil);
   with SizeComponentsDialog do begin
-    SetBounds((Screen.Width-365) div 2,(Screen.Height-175) div 2,355,165);
-    WidthRadioGroup.ItemIndex:=0;
-    HeightRadioGroup.ItemIndex:=0;
     Result:=ShowModal;
     HorizSizingID:=SizeComponentsDialog.WidthRadioGroup.ItemIndex;
     FixedWidth:=StrToIntDef(SizeComponentsDialog.WidthEdit.Text,0);
@@ -70,114 +66,54 @@ begin
   end;
 end;
 
-
 { TSizeComponentsDialog }
+
+procedure TSizeComponentsDialog.FormResize(Sender: TObject);
+begin
+  WidthRadioGroup.Width := ClientWidth div 2 - 9;
+  HeightRadioGroup.Left := WidthRadioGroup.Width + 12;
+  HeightRadioGroup.Width := ClientWidth div 2 - 9;
+end;
 
 constructor TSizeComponentsDialog.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
-  if LazarusResources.Find(Classname)=nil then begin
-    SetBounds((Screen.Width-365) div 2,(Screen.Height-175) div 2,355,165);
-    Caption:='Size';
 
-    WidthRadioGroup:=TRadioGroup.Create(Self);
-    with WidthRadioGroup do begin
-      Name:='WidthRadioGroup';
-      Parent:=Self;
-      Left:=5;
-      Top:=5;
-      Width:=170;
-      Height:=115;
-      Caption:='Width:';
-      with Items do begin
-        BeginUpdate;
-        Add('No change');
-        Add('Shrink to smallest');
-        Add('Grow to Largest');
-        Add('Width:');
-        EndUpdate;
-      end;
-      Show;
-    end;
+  Caption:='Size';
 
-    WidthEdit:=TEdit.Create(Self);
-    with WidthEdit do begin
-      Name:='WidthEdit';
-      Parent:=Self;
-      Left:=87;
-      Top:=90;
-      Width:=60;
-      Text:='';
-      Show;
+  with WidthRadioGroup do begin
+    Caption:='Width:';
+    with Items do begin
+      BeginUpdate;
+      Add('No change');
+      Add('Shrink to smallest');
+      Add('Grow to Largest');
+      Add('Width:');
+      EndUpdate;
     end;
-
-    HeightRadioGroup:=TRadioGroup.Create(Self);
-    with HeightRadioGroup do begin
-      Name:='HeightRadioGroup';
-      Parent:=Self;
-      Left:=180;
-      Top:=5;
-      Width:=170;
-      Height:=115;
-      Caption:='Height:';
-      with Items do begin
-        BeginUpdate;
-        Add('No change');
-        Add('Shrink to smallest');
-        Add('Grow to Largest');
-        Add('Height:');
-        EndUpdate;
-      end;
-      Show;
-    end;
-
-    HeightEdit:=TEdit.Create(Self);
-    with HeightEdit do begin
-      Name:='HeightEdit';
-      Parent:=Self;
-      Left:=262;
-      Top:=90;
-      Width:=60;
-      Text:='';
-      Show;
-    end;
-
-    OkButton:=TButton.Create(Self);
-    with OkButton do begin
-      Name:='OkButton';
-      Parent:=Self;
-      Left:=145;
-      Top:=WidthRadioGroup.Top+WidthRadioGroup.Height+10;
-      Width:=75;
-      Height:=25;
-      Caption:='Ok';
-      OnClick:=@OkButtonClick;
-      Show;
-    end;
-
-    CancelButton:=TButton.Create(Self);
-    with CancelButton do begin
-      Name:='CancelButton';
-      Parent:=Self;
-      Left:=235;
-      Top:=OkButton.Top;
-      Width:=75;
-      Height:=25;
-      Caption:='Cancel';
-      OnClick:=@CancelButtonClick;
-      Show;
-    end;
+    ItemIndex:=0;
   end;
+
+  with HeightRadioGroup do begin
+    Caption:='Height:';
+    with Items do begin
+      BeginUpdate;
+      Add('No change');
+      Add('Shrink to smallest');
+      Add('Grow to Largest');
+      Add('Height:');
+      EndUpdate;
+    end;
+    ItemIndex:=0;
+  end;
+
+  WidthEdit.Text:='';
+  HeightEdit.Text:='';
+
+  OkButton.Caption:='Ok';
 end;
 
-procedure TSizeComponentsDialog.OkButtonClick(Sender: TObject);
-begin
-  ModalResult:=mrOk;
-end;
-
-procedure TSizeComponentsDialog.CancelButtonClick(Sender: TObject);
-begin
-  ModalResult:=mrCancel;
-end;
+initialization
+  {$i sizecompsdlg.lrs}
 
 end.
