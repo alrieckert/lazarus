bea4aeb65ae45f7557547ebe4515b2cbcfb589da
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-01-19 20:34:05 +0000

    mapped shift + F3 to find previous (issue 1647)
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8566 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/keymapping.pp
+++ ide/keymapping.pp
@@ -345,7 +345,7 @@ begin
   ecMatchBracket: SetResult(VK_UNKNOWN,[],VK_UNKNOWN,[]);
   ecFind: SetResult2(VK_Q,[SSCtrl],VK_F,[],VK_F,[SSCtrl],VK_UNKNOWN,[]);
   ecFindNext: SetResult2(VK_F3,[],VK_UNKNOWN,[],VK_L,[SSCtrl],VK_UNKNOWN,[]);
-  ecFindPrevious: SetResult(VK_UNKNOWN,[],VK_UNKNOWN,[]);
+  ecFindPrevious: SetResult(VK_F3,[ssShift],VK_UNKNOWN,[]);
   ecFindInFiles: SetResult(VK_UNKNOWN,[],VK_UNKNOWN,[]);
   ecReplace: SetResult2(VK_R,[SSCtrl],VK_UNKNOWN,[],  VK_Q,[SSCtrl],VK_A,[]);
   ecIncrementalFind: SetResult(VK_E,[SSCtrl],VK_UNKNOWN,[]);
