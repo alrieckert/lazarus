1e48b1043fd423c924cb4abbab53ae1918f57453
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-01-21 19:08:57 +0000

    FilePath and TableName property editors for Tdbf
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8596 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tdbf/registerdbf.pas
+++ components/tdbf/registerdbf.pas
@@ -22,15 +22,46 @@ unit RegisterDBF;
 interface
 
 uses
-  Classes, SysUtils, LResources, Dbf, LazarusPackageIntf;
+  Classes, SysUtils, LResources, Dbf, LazarusPackageIntf, PropEdits;
 
+resourcestring
+  dbfsAllDbasefiles = 'DBase Files';
+  
 procedure Register;
 
 implementation
 
+type
+
+  { TDbfFileNamePropertyEditor }
+
+  TDbfFileNamePropertyEditor=class(TFileNamePropertyEditor)
+  protected
+    function GetFilter: String; override;
+    function GetInitialDirectory: string; override;
+  end;
+
+{ TDbfFileNamePropertyEditor }
+
+function TDbfFileNamePropertyEditor.GetFilter: String;
+begin
+  Result := dbfsAllDbaseFiles+' (*.dbf)|*.dbf;*.DBF';
+  Result:= Result+ '|'+ inherited GetFilter;
+end;
+
+function TDbfFileNamePropertyEditor.GetInitialDirectory: string;
+begin
+  Result:= (GetComponent(0) as TDBF).FilePath;
+end;
+
 procedure RegisterUnitDBF;
 begin
   RegisterComponents('Data Access',[TDbf]);
+  
+  RegisterPropertyEditor(TypeInfo(AnsiString),
+    Tdbf, 'FilePath', TDirectoryPropertyEditor);
+  RegisterPropertyEditor(TypeInfo(AnsiString),
+    Tdbf, 'TableName', TDbfFileNamePropertyEditor);
 end;
 
 procedure Register;
