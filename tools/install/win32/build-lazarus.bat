8cb903543927c27b5febdff51ee9d08950485586
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-02-01 19:11:42 +0000

    use -CX to compile the LCL
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8682 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/win32/build-lazarus.bat
+++ tools/install/win32/build-lazarus.bat
@@ -6,8 +6,9 @@ SET COMPILER=%BUILDDIR%\pp\bin\i386-win32\ppc386.exe
 %BUILDDRIVE%
 cd %BUILDDIR%
 %MAKEEXE% clean PP=%COMPILER% >> %LOGFILE%
-%MAKEEXE% lcl OPT="-gl -Xs -XX -Ur" PP=%COMPILER% >> %LOGFILE%
-%MAKEEXE% all OPT="-gl -Xs -XX" PP=%COMPILER% >> %LOGFILE%
+%MAKEEXE% lcl OPT="-gl -Xs -CX -XX -Ur" PP=%COMPILER% >> %LOGFILE%
+%MAKEEXE% lcl OPT="-gl -Xs -CX -XX -Ur" PP=%COMPILER% LCL_PLATFORM=gtk2 >> %LOGFILE%
+%MAKEEXE% bigide OPT="-gl -Xs -XX" PP=%COMPILER% >> %LOGFILE%
 %FPCBINDIR%\strip.exe lazarus.exe
 
 %FPCBINDIR%\strip.exe startlazarus.exe
