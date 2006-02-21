3936f5a6f8df19caaa825281e219cd5b749593d7
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-02-21 19:13:21 +0000

    added multithreading example
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8790 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ examples/multithreading/mainunit.pas
@@ -0,0 +1,108 @@
+{
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+}
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs; 
+
+type
+
+  { TMyThread }
+
+  TMyThread = class(TThread)
+  private
+    fStatusText: string;
+    procedure ShowStatus;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(CreateSuspended: boolean);
+  end;
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+  public
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  MyThread : TMyThread;
+begin
+   MyThread := TMyThread.Create(True); // This way it doesn't start automatically
+   if Assigned(MyThread.FatalException) then
+     raise MyThread.FatalException;
+      
+   // Here the code initialises anything required before the threads starts executing
+
+   MyThread.Resume;
+end;
+
+{ TMyThread }
+
+procedure TMyThread.ShowStatus;
+begin
+  Form1.Caption := fStatusText;
+end;
+
+procedure TMyThread.Execute;
+var
+  newStatus : string;
+begin
+  fStatusText := 'Starting...';
+  Synchronize(@Showstatus);
+  fStatusText := 'Running...';
+  while (not Terminated) and (true {any condition required}) do begin
+
+    //here goes the code of the main thread loop
+    newStatus:='Time: '+FormatDateTime('YYYY-MM-DD HH:NN:SS',Now);
+    
+    if NewStatus <> fStatusText then begin
+      fStatusText := newStatus;
+      Synchronize(@Showstatus);
+    end;
+  end;
+end;
+
+constructor TMyThread.Create(CreateSuspended: boolean);
+begin
+  FreeOnTerminate := True;
+  inherited Create(CreateSuspended);
+end;
+
+initialization
+  {$I mainunit.lrs}
+
+end.
+
---
+++ examples/multithreading/multithreadingexample1.lpr
@@ -0,0 +1,18 @@
+program MultiThreadingExample1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms
+  { add your units here }, MainUnit;
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
