96ed8146b18c7dd8aac3f47dce4c404279d7a994
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-02-26 11:33:27 +0000

    added lpi for codetool example
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8828 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/examples/methodjumping.pas
+++ components/codetools/examples/methodjumping.pas
@@ -1,19 +1,23 @@
 { Copyright (C) 2005 Mattias Gaertner
 
-  This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Library General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-  for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
 
   Abstract:
     Example, how to setup the codetools, load a pascal unit and jump from
@@ -32,17 +36,15 @@ var
   NewCode: TCodeBuffer;
   NewX, NewY, NewTopLine: integer;
   RevertableJump: boolean;
-  //Tool: TCustomCodeTool;
 begin
   ExpandedFilename:=ExpandFileName('tgeneric2.pp');
   CodeBuf:=CodeToolBoss.LoadFile(ExpandedFilename,true,false);
   if CodeToolBoss.JumpToMethod(CodeBuf,3,15,NewCode,NewX,NewY,NewTopLine,
                             RevertableJump)
   then
-    writeln(NewCode.Filename,' ',NewX,',',NewY,' TopLine=',NewTopLine,' RevertableJump=',RevertableJump)
+    writeln(NewCode.Filename,' ',NewX,',',NewY,' TopLine=',NewTopLine,
+            ' RevertableJump=',RevertableJump)
   else
     writeln('Method body not found.');
-  //Tool:=CodeToolBoss.FindCodeToolForSource(CodeBuf);
-  //Tool.WriteDebugTreeReport;
 end.
 
