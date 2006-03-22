ded6891264c9469fef5bd5e42c3e82392f5ae7b8
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-03-22 15:23:24 +0000

    * adds some documentation to the TCustomCheckbox control. * added an example file for TCustomCheckbox * Improves the formatting of the tarrayexample.pas file * adds some documentation to the TWinControl patch by Graeme
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@8984 4005530d-fff6-0310-9dd1-cebe43e6787f

--- docs/xml/lcl/dynamicarray/tarrayexample.pas
+++ docs/xml/lcl/dynamicarray/tarrayexample.pas
@@ -6,68 +6,76 @@ uses
   {$IFDEF UNIX}{$IFDEF UseCThreads}
   cthreads,
   {$ENDIF}{$ENDIF}
-  Classes
-  { add your units here },strings,DynamicArray;
+  Classes,
+  strings,
+  DynamicArray;
   
 type
-Tarrayexampleclass=class
-private
-procedure doDestroyItem(Sender: Tobject; Col,Row: Integer;var Item: Pointer);
+  TArrayExampleClass = class
+  private
+    procedure doDestroyItem(Sender: Tobject; Col,Row: Integer;var Item: Pointer);
+  end;
 
-end;
-
-procedure Tarrayexampleclass.doDestroyItem(Sender: Tobject; Col,Row: Integer;
+procedure TArrayExampleClass.doDestroyItem(Sender: Tobject; Col,Row: Integer;
   var Item: Pointer);
 begin
-  strdispose(Item);
+  StrDispose(Item);
 end;
 
 
-var FCols: Tarray;
-var ex:Tarrayexampleclass;
+var
+  FCols: Tarray;
+  ex: TArrayExampleClass;
 
 begin
-FCols:= TArray.Create;
-ex:= Tarrayexampleclass.Create;
-FCols.OnDestroyItem:=@ex.doDestroyItem;
-FCols.SetLength(8,8);
-//FCols.OnNewItem:=@doNewItem;
-FCols.arr[0,0]:=StrNew('string1');
-FCols.arr[4,7]:=StrNew('string2');
-FCols.arr[4,3]:=StrNew('string3');
-writeln('0,0:'+Pchar(FCols.arr[0,0]));
-writeln('4,7:'+Pchar(FCols.arr[4,7]));
-FCols.MoveColRow(true,4,5);
-writeln('after moving column 4 to 5');
-writeln('5,7:'+Pchar(FCols.arr[5,7]));
-writeln('before exchanging row 7 and 3:');
-writeln('5,3:'+Pchar(FCols.arr[5,3]));
-writeln('5,7:'+Pchar(FCols.arr[5,7]));
-FCols.ExchangeColRow(false,7,3);
-writeln('after exchanging row 7 and 3:');
-writeln('5,3:'+Pchar(FCols.arr[5,3]));
-writeln('5,7:'+Pchar(FCols.arr[5,7]));
-FCols.DeleteColRow(true,5);
-writeln('after deleting column 5:');
-try
- writeln('5,3:'+Pchar(FCols.arr[5,3])); //this raises an exception
-except
- writeln ('An exception has taken place be because 5,3 does not exist.');
-end;
-try
- writeln('5,7:'+Pchar(FCols.arr[5,7])); //this raises an exception
-except
- writeln ('An exception has taken place be because 5,7 does not exist.');
-end;
-FCols.Clear; writeln('after clear:');
-try
- writeln('4,7:'+Pchar(FCols.arr[4,7])); //this raises an exception
-except
- writeln ('An exception has taken place be because 4,7 does not exist.');
-end;
-FCols.Destroy;
-ex.Destroy;
-readln;
-end.
+  FCols := TArray.Create;
+  ex    := TArrayExampleClass.Create;
+  FCols.OnDestroyItem := @ex.doDestroyItem;
 
+  FCols.SetLength(8,8);
+  FCols.arr[0,0] := StrNew('string1');
+  FCols.arr[4,7] := StrNew('string2');
+  FCols.arr[4,3] := StrNew('string3');
+
+  writeln('0,0:' + Pchar(FCols.arr[0,0]));
+  writeln('4,7:' + Pchar(FCols.arr[4,7]));
+
+  FCols.MoveColRow(True,4,5);
+  writeln('after moving column 4 to 5');
+  writeln('5,7:' + Pchar(FCols.arr[5,7]));
+  writeln('before exchanging row 7 and 3:');
+  writeln('5,3:' + Pchar(FCols.arr[5,3]));
+  writeln('5,7:' + Pchar(FCols.arr[5,7]));
+
+  FCols.ExchangeColRow(False,7,3);
+  writeln('after exchanging row 7 and 3:');
+  writeln('5,3:' + Pchar(FCols.arr[5,3]));
+  writeln('5,7:' + Pchar(FCols.arr[5,7]));
+
+  FCols.DeleteColRow(true,5);
+  writeln('after deleting column 5:');
+
+  try
+    writeln('5,3:' + Pchar(FCols.arr[5,3])); //this raises an exception
+  except
+    writeln ('An exception has taken place be because 5,3 does not exist.');
+  end;
+
+  try
+    writeln('5,7:' + Pchar(FCols.arr[5,7])); //this raises an exception
+  except
+    writeln ('An exception has taken place be because 5,7 does not exist.');
+  end;
+
+  FCols.Clear; writeln('after clear:');
+  try
+    writeln('4,7:' + Pchar(FCols.arr[4,7])); //this raises an exception
+  except
+    writeln ('An exception has taken place be because 4,7 does not exist.');
+  end;
+
+  FCols.Destroy;
+  ex.Destroy;
+  readln;
+end.
 
