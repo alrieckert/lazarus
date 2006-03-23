3b7f9d4eb6202129a9754464591ced778c310ff0
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-03-23 09:48:36 +0000

    span comment over mulitple lines
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9002 4005530d-fff6-0310-9dd1-cebe43e6787f

--- docs/xml/lcl/stdctrls/tcustomcheckbox_allowgrayed.pas
+++ docs/xml/lcl/stdctrls/tcustomcheckbox_allowgrayed.pas
@@ -1,4 +1,6 @@
-{ This example uses a check box on a form. When the application runs, the check box is initially checked. When the user clicks it, the check box is unchecked. Clicking it again grays the check box. }
+{ This example uses a check box on a form. When the application runs, 
+  the check box is initially checked. When the user clicks it,
+  the check box is unchecked. Clicking it again grays the check box. }
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   Checkbox1.AllowGrayed := True;
