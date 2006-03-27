9b49162b64fde2ba12684a0ad28030bd297a1a93
micha <micha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-03-27 20:38:46 +0000

    move debian to toplevel; easier for building packages
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9024 4005530d-fff6-0310-9dd1-cebe43e6787f

--- install/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lazarus package
-#
-0 4	* * *	root	lazarus_maintenance
--- install/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lazarus):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="lazarus" command="/usr/bin/lazarus"
