17e8503b4812ad2c0290b04581b175c3497249e3
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-05-13 13:29:47 +0000

    fixed compilation
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9276 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ lcl/helpintfs.pas
@@ -0,0 +1,45 @@
+{
+ /***************************************************************************
+                                 helpintfs.pas
+                                 -------------
+                             Component Library Code
+
+
+ ***************************************************************************/
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL, included in this distribution,        *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+ 
+  Author: Mattias Gaertner
+  
+  Abstract:
+    Interfaces to define the abstract HelpSystem.
+    You can create your own HelpSystem based on these interfaces
+    or use the LCL help system in lazhelpsystem.pas.
+    The THTMLHelpDatabase and THTMLBrowserHelpViewer in helphtml.pas use the
+    LCL help system.
+}
+unit HelpIntfs;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+  
+
+implementation
+
+end.
+
