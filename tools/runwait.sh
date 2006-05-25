983b0652096d91570bc6145249a02e267572abf4
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-05-25 18:50:05 +0000

    fixed waiting using sh   from Almindor
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9354 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/runwait.sh
+++ tools/runwait.sh
@@ -7,16 +7,17 @@ echo $CommandLine
 ext=${CommandLine#*.}
 if [ "$ext" = "exe" ]; then
   echo "Windows Executable detected. Attempting to use WINE..."
-  if [ -x $( which wine ) ]; then  
-    wine $CommandLine 
+  if [ -x "`which wine`" ]; then
+    wine $CommandLine
   else
     echo "WINE not found in path"
-  fi  
+  fi
 else
   $CommandLine
 fi
+
 echo "--------------------------------------------------"
 echo "Press enter"
-read
+read trash crash
 
 # end.
