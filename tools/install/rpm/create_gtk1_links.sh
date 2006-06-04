a4bf7811858b8fe5ca38ed56f922b0cc89eca6f7
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-06-04 18:06:28 +0000

    first rpm for suse and fc
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9401 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/rpm/create_gtk1_links.sh
+++ tools/install/rpm/create_gtk1_links.sh
@@ -6,10 +6,11 @@ mainlibpath="/usr/lib"
 libpaths="$mainlibpath /opt/gnome/lib /usr/local/lib"
 libs="glib-1.2.so.0 gdk-1.2.so.0 gtk-1.2.so.0 gdk_pixbuf.so.2"
 
+#echo "create_gtk1_links.sh running for $buildroot ..."
 for lib in $libs; do
     # create libname without version number
     ShortLib=$(echo $lib | sed -e 's/\.[0-9]\+$//g' -e 's/-.*.so/.so/g')
-    echo "searching lib$ShortLib ..."
+    #echo "searching lib$ShortLib ..."
     # search lib in all paths
     ShortLibFile=""
     for Path in $libpaths; do
@@ -29,6 +30,7 @@ for lib in $libs; do
 		#echo "trying "$File
 		if [ -f $File ]; then
 		    ShortLibFile=$mainlibpath/lib$ShortLib
+		    #echo ln -s $Path/lib$lib $ShortLibFile
 		    ln -s $Path/lib$lib $ShortLibFile
 		fi
 	    fi
