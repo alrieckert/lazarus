fd9a917bf76853e274f10e876ee1f8ed032b2cc1
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-06-06 18:16:19 +0000

    added script to check fpc build dependencies
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9409 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ tools/install/check_fpc_dependencies.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Check if fpc can be compiled
+# This checks a few common mistakes.
+
+set -x 
+set -e
+
+# try to compile a program
+echo Testing fpc ...
+TmpDir=/tmp/fpc
+rm -rf $TmpDir
+mkdir -p $TmpDir
+TestPas=$TmpDir/test.pas
+echo "program test;" >> $TestPas
+echo "{$linklib gpm}" >> $TestPas
+echo "begin end." >> $TestPas
+
+fpc $TestPas
+
+# end.
+
