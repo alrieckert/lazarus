285375d865cc30741a02db381b75f52299a374bb
micha <micha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-07-21 14:14:49 +0000

    release/snapshot building fixes
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9651 4005530d-fff6-0310-9dd1-cebe43e6787f

--- debian/move-usr-lib.sh
+++ debian/move-usr-lib.sh
@@ -1,19 +1,21 @@
 LAZDEBDIR=$1
-for i in `find -maxdepth 1 -not -name debian -and -not -name '.'`; do
-    cp -Rl $i $LAZDEBDIR/usr/share/lazarus
+LAZTARGET=lib
+for i in `find -maxdepth 1 -not -name debian -and -not -name install -and -not -name '.'`; do
+    cp -Rl $i $LAZDEBDIR/usr/$LAZTARGET/lazarus
 done
-rm $LAZDEBDIR/usr/share/lazarus/lcl/COPYING
-rm $LAZDEBDIR/usr/share/lazarus/COPYING.GPL
-rm $LAZDEBDIR/usr/share/lazarus/COPYING.LGPL
-rm $LAZDEBDIR/usr/share/lazarus/COPYING.modifiedLGPL
-rm $LAZDEBDIR/usr/share/lazarus/COPYING
-cd $LAZDEBDIR/usr/share/lazarus
-mv lazarus ../../lib/lazarus
-mv startlazarus ../../lib/lazarus
-mv tools/svn2revisioninc ../../lib/lazarus/tools/svn2revisioninc
-for i in `find -type d -name 'lib' -or -name 'units'`; do
-        d=`dirname $i`
-        mkdir -p ../../lib/lazarus/$d
-        mv $i ../../lib/lazarus/$d			  
-done
-
+rm $LAZDEBDIR/usr/$LAZTARGET/lazarus/lcl/COPYING
+rm $LAZDEBDIR/usr/$LAZTARGET/lazarus/COPYING.GPL
+rm $LAZDEBDIR/usr/$LAZTARGET/lazarus/COPYING.LGPL
+rm $LAZDEBDIR/usr/$LAZTARGET/lazarus/COPYING.modifiedLGPL
+rm $LAZDEBDIR/usr/$LAZTARGET/lazarus/COPYING
+if [ "$LAZTARGET" = "share" ]; then
+    cd $LAZDEBDIR/usr/share/lazarus
+    mv lazarus ../../lib/lazarus
+    mv startlazarus ../../lib/lazarus
+    mv tools/svn2revisioninc ../../lib/lazarus/tools/svn2revisioninc
+    for i in `find -type d -name 'lib' -or -name 'units'`; do
+            d=`dirname $i`
+            mkdir -p ../../lib/lazarus/$d
+            mv $i ../../lib/lazarus/$d			  
+    done
+fi
