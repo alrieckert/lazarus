7e4437491fbc80d705baf03816b5df70e1e9f448
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-09-12 17:45:44 +0000

    fixed get_lazarus_version.sh
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@9868 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/get_lazarus_version.sh
+++ tools/install/get_lazarus_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-grep 'LazarusVersionStr= ' ../../ide/aboutfrm.pas \
-  | sed -e 's/.*[^0-9 \.]\([0-9\.][0-9\.]*\)[^0-9\.].*/\1/'
+cat ../../ide/version.inc \
+  | sed -e 's/.*[^0-9 \.]\([0-9\.]\+\)[^0-9\.].*/\1/'
 
 # end.
 
