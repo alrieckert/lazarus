9d33b93d616afa122c50fe7847f74d2e80752e60
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-10-02 09:10:56 +0000

    chnaged wrong copied license headers in LCL units, they are modified LGPL not GPL, as mentioned in the Copying files
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10024 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/templates/template.pp
+++ lcl/templates/template.pp
@@ -8,14 +8,18 @@
 
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL, included in this distribution,        *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
 }
 
 {
