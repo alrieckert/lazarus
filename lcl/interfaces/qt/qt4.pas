b38b9e811de80d45b18b33e4e9c219de22eb62a9
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2006-10-16 14:08:52 +0000

    Added mac qt4 unit.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10081 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/qt/qt4.pas
+++ lcl/interfaces/qt/qt4.pas
@@ -1,9 +1,13 @@
-{$ifdef linux}
-{$I qt4linux.pas}
+{$ifdef darwin}
+{$I qt4mac.pas}
+{$else}
+{$ifdef Unix}
+{$I qt4x11.pas}
 {$else}
 {$ifdef windows}
 {$I qt4win.pas}
 {$else}
 {$FATAL no qt4 unit for this OS}
+{$endif}
 {$endif}
 {$endif}
