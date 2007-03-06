59d114cf1b215a6782f1cb01b421729b8d39f706
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-03-06 22:29:27 +0000

    test: made test program for 8437 suitable for automated testing
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10713 4005530d-fff6-0310-9dd1-cebe43e6787f

--- test/bugs/8437/unit1.pas
+++ test/bugs/8437/unit1.pas
@@ -13,11 +13,15 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    ApplicationProperties1: TApplicationProperties;
     Button2: TButton;
     Button3: TButton;
     PairSplitter1: TPairSplitter;
     PairSplitterSide1: TPairSplitterSide;
     PairSplitterSide2: TPairSplitterSide;
+    procedure ApplicationProperties1Idle(Sender: TObject; var Done: Boolean);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
     { private declarations }
@@ -39,6 +43,37 @@ begin
     Sides[0].Color := clRed;
     Sides[1].Color := clGreen;
   end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  PairSplitterSide1.Width := PairSplitterSide1.Width - 25;
+end;
+
+procedure TForm1.ApplicationProperties1Idle(Sender: TObject; var Done: Boolean);
+var
+  Side1Width, Side2Width : integer;
+begin
+  if ParamStr(1)='--runtest' then begin
+    Side1Width := PairSplitterSide1.Width;
+    Side2Width := PairSplitterSide2.Width;
+    writeln(format('Side 1: %d, Side 2: %d',
+              [PairSplitterSide1.Width, PairSplitterSide2.Width]));
+    // splitter is working, so descreasing width of Side1 should increase Side2
+    Button2Click(nil);
+    writeln(format('Side 1: %d, Side 2: %d',
+              [PairSplitterSide1.Width, PairSplitterSide2.Width]));
+    // increase width again
+    Button3Click(nil);
+    writeln(format('Side 1: %d, Side 2: %d',
+              [PairSplitterSide1.Width, PairSplitterSide2.Width]));
+    Close;
+  end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  PairSplitterSide1.Width := PairSplitterSide1.Width + 25;
 end;
 
 initialization
