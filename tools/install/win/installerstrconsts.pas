d3c24545290ecb58e9990f496968e22159d994a3
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-03-13 23:32:37 +0000

    win32 installer: added more localizable strings (bug 8516)
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10744 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/win/installerstrconsts.pas
+++ tools/install/win/installerstrconsts.pas
@@ -10,6 +10,15 @@ resourcestring
   wisOpenProject = '&Open project';
   wisOpenPackage = '&Open package';
 
+  wisAssociateFiles = 'Associate Lazarus files with Lazarus';
+  wisAssociateLfmFiles = 'Associate .lfm files with Lazarus';
+  wisAssociateLpkFiles = 'Associate .lpk files with Lazarus';
+  wisAssociateLpiFiles = 'Associate .lpi files with Lazarus';
+  wisAssociateLprFiles = 'Associate .lpr files with Lazarus';
+  wisAssociateIncFiles = 'Associate .inc files with Lazarus';
+  wisAssociatePasFiles = 'Associate .pas files with Lazarus';
+  wisAssociatePpFiles = 'Associate .pas files with Lazarus';
+
 implementation
 
 end.
