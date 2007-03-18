813af1bdb58cfd95ed80fa3a99f372a590b9c361
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-03-18 18:21:54 +0000

    - added missed carbon cursors - regenerated cursors.lrs
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10762 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/images/cursors/build.bat
+++ lcl/images/cursors/build.bat
@@ -1,1 +1,1 @@
-lazres cursors.lrs cur_12.cur cur_13.cur cur_14.cur cur_15.cur cur_16.cur cur_17.cur cur_21.cur
+lazres cursors.lrs cur_10.cur cur_12.cur cur_13.cur cur_14.cur cur_15.cur cur_16.cur cur_17.cur cur_18.cur cur_21.cur cur_22.cur
