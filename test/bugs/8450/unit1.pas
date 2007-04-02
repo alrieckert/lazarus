94f58f932951841973b96087ee0e0065bf9f997d
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-04-02 09:45:39 +0000

    LCL: fixed TComboBox.SelText (bug 8450) based on patch by Tom
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@10848 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ test/bugs/8450/unit1.pas
@@ -0,0 +1,56 @@
+unit Unit1; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, Buttons,
+  StdCtrls, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ApplicationProperties1: TApplicationProperties;
+    ComboBox1: TComboBox;
+    procedure ApplicationProperties1Idle(Sender: TObject; var Done: Boolean);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  Form1: TForm1; 
+
+implementation
+
+var
+  InTestRun: boolean;
+
+{ TForm1 }
+
+procedure TForm1.ApplicationProperties1Idle(Sender: TObject; var Done: Boolean);
+begin
+  ComboBox1.SelStart := 3;
+  ComboBox1.SelLength := 3;
+  if InTestRun then
+    writeln('ComboBox SelText: ', ComboBox1.SelText);
+  ComboBox1.SelText := '1234';
+
+  if InTestRun then begin
+    writeln('ComboBox.Text: ', ComboBox1.Text);
+    writeln('ComboBox.SelText: ', ComboBox1.SelText);
+    Close;
+  end;
+  ApplicationProperties1.OnIdle := nil;
+end;
+
+initialization
+  {$I unit1.lrs}
+  InTestRun :=  paramstr(1)='--runtest';
+
+end.
+
