46323c7af7bb23e3c21d4d782d86a3e2ce07a9a6
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-04-29 09:02:43 +0000

    codetools: aded example to list all classes of interface
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@11022 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/codetools/examples/listinterfaceclasses.pas
@@ -0,0 +1,57 @@
+{ Copyright (C) 2005 Mattias Gaertner
+
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+
+  Abstract:
+    Example, how to setup the codetools, load a pascal unit and list of classes
+    defined in the interface.
+}
+program ListInterfaceClasses;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, SysUtils, CodeToolManager, CodeCache, CustomCodeTool, CodeTree;
+  
+var
+  ExpandedFilename: String;
+  CodeBuf: TCodeBuffer;
+  Tool: TCodeTool;
+  CurClassName: String;
+  Node: TCodeTreeNode;
+begin
+  ExpandedFilename:=ExpandFileName('scanexamples/simpleunit1.pas');
+  CodeBuf:=CodeToolBoss.LoadFile(ExpandedFilename,true,false);
+  if CodeBuf=nil then
+    raise Exception.Create('failed loading '+ExpandedFilename);
+  if not CodeToolBoss.Explore(CodeBuf,Tool,false,true) then begin
+    writeln('error in code');
+    exit;
+  end;
+  Node:=Tool.Tree.Root;
+  while (Node<>nil) do begin
+    if Node.Desc=ctnClass then begin
+      CurClassName:=Tool.ExtractClassName(Node,false);
+      writeln(CurClassName);
+    end;
+    Node:=Node.Next;
+  end
+end.
+
