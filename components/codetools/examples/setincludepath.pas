9323e068dbb3fab7af8ed02e0e11b7ef6b7be932
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-04-29 09:37:57 +0000

    codetools: aded example to extend the include search path of a directory
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@11023 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/codetools/examples/setincludepath.pas
@@ -0,0 +1,59 @@
+{ Copyright (C) 2005 Mattias Gaertner
+
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+
+  Abstract:
+    Example, how to setup the codetools, load a pascal unit and
+    extend the include path of a directory.
+}
+program SetIncludePath;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, SysUtils, CodeToolManager, DefineTemplates;
+  
+var
+  Directory: String;
+  DirectoryTemplate: TDefineTemplate;
+  IncPathTemplate: TDefineTemplate;
+begin
+  // create a template for the current directory
+  // all child nodes of this templates are only valid for this directory.
+  Directory:=ExpandFileName(GetCurrentDir);
+  DirectoryTemplate:=TDefineTemplate.Create('Current working directory',
+    'Example template for current working directory','',Directory,da_Directory);
+
+  // add a sub template to extend the include search path #IncPath.
+  IncPathTemplate:=TDefineTemplate.Create('Add myincludes to the IncPath',
+    'Add myincludes to the include search path',
+    IncludePathMacroName,  // variable name: #IncPath
+    '$('+IncludePathMacroName+');myincludes' // new value: $(#IncPath);myincludes
+    ,da_Define
+    );
+  DirectoryTemplate.AddChild(IncPathTemplate);
+  
+  // add the directory template to the tree
+  CodeToolBoss.DefineTree.Add(DirectoryTemplate);
+
+  writeln('Directory="',Directory,'"',
+    ' IncPath="',CodeToolBoss.GetIncludePathForDirectory(Directory,true),'"');
+end.
+
