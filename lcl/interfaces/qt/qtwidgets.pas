70a0a37b145dabec78adc39b1546333d8395fde3
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-07-31 17:13:55 +0000

    (Qt): removed unneeded writeln()
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@11692 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/qt/qtwidgets.pas
+++ lcl/interfaces/qt/qtwidgets.pas
@@ -1080,7 +1080,6 @@ begin
     QEventMouseButtonRelease,
     QEventMouseButtonDblClick:
       begin
-        WriteLn('QEventMouseButtonDblClick');
         SlotMouse(Event);
         Result := StopMouseEventsProcessing;
       end;
