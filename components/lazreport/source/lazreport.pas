bd7d978feb279790cc33fc7bc89be2bcd11fb88e
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-10-21 23:07:49 +0000

    lazreport: fixed languages directory position
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@12548 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lazreport.pas
+++ components/lazreport/source/lazreport.pas
@@ -9,7 +9,7 @@ interface
 uses
   LR_Class, LR_Desgn, LR_Register, LR_Flds, LR_DBSet, LR_BarC, LR_BndEd, 
     LR_PGrid, LR_View, lr_expres, lr_funct_editor_unit, lr_funct_editor_unit1, 
-    LR_Prntr, LR_Edit, LR_Pars, LR_fmted, LR_Const, LR_pgopt, 
+    LR_Prntr, LR_Edit, LR_Pars, LR_fmted, LR_Const, LR_pgopt, LR_Dopt, 
     LazarusPackageIntf; 
 
 implementation
