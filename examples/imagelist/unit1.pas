4f452954be6fa9daff2e1f8edc3456da395d7b9c
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-11-09 04:09:30 +0000

    new imagelist state drawing example
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@12793 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ examples/imagelist/unit1.pas
@@ -0,0 +1,128 @@
+unit Unit1; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, GraphType, Dialogs, ExtCtrls,
+  StdCtrls, ExtDlgs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
+    GroupBox4: TGroupBox;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    ImageList1: TImageList;
+    ListBox1: TListBox;
+    OpenPictureDialog1: TOpenPictureDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    function GetImageMap(AEffect: TGraphicsDrawEffect): TImage;
+    procedure ListBox1SelectionChange(Sender: TObject; User: boolean);
+  private
+    { private declarations }
+  public
+    procedure ShowImages(AIndex: Integer);
+    procedure AddFile(AFileName: String);
+    property ImageMap[AEffect: TGraphicsDrawEffect]: TImage read GetImageMap;
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{ TForm1 }
+
+function LoadBitmapFromFile(AFileName: String): TBitmap;
+var
+  Stream: TStream;
+  GraphicClass: TGraphicClass;
+begin
+  Result := nil;
+  Stream := nil;
+  try
+    Stream := TFileStream.Create(AFileName, fmOpenRead or fmShareDenyNone);
+    GraphicClass := GetGraphicClassForFileExtension(ExtractFileExt(AFileName));
+    if (GraphicClass <> nil) and (GraphicClass.InheritsFrom(TBitmap)) then
+    begin
+      Result := TBitmap(GraphicClass.Create);
+      Result.LoadFromStream(Stream);
+    end;
+  finally
+    Stream.Free;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  AddFile(ExtractFilePath(ParamStr(0)) + 'images\edit-clear.png');
+  AddFile(ExtractFilePath(ParamStr(0)) + 'images\edit-find-replace.png');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if OpenPictureDialog1.Execute then
+    AddFile(OpenPictureDialog1.FileName);
+end;
+
+function TForm1.GetImageMap(AEffect: TGraphicsDrawEffect): TImage;
+begin
+  case AEffect of
+    gdeNormal: Result := Image1;
+    gdeDisabled: Result := Image2;
+    gdeHighlighted: Result := Image3;
+    gdeShadowed: Result := Image4;
+  end;
+end;
+
+procedure TForm1.ListBox1SelectionChange(Sender: TObject; User: boolean);
+begin
+  if ListBox1.ItemIndex <> -1 then
+    ShowImages(ListBox1.ItemIndex);
+end;
+
+procedure TForm1.ShowImages(AIndex: Integer);
+var
+  AEffect: TGraphicsDrawEffect;
+  Bmp: TBitmap;
+begin
+  for AEffect := Low(TGraphicsDrawEffect) to High(TGraphicsDrawEffect) do
+  begin
+    Bmp := TBitmap.Create;
+    ImageList1.GetBitmap(AIndex, Bmp, AEffect);
+    ImageMap[AEffect].Picture.Assign(Bmp);
+    Bmp.Free;
+  end;
+end;
+
+procedure TForm1.AddFile(AFileName: String);
+var
+  bmp: TBitmap;
+begin
+  bmp := LoadBitmapFromFile(AFileName);
+  if bmp <> nil then
+  begin
+    ImageList1.Add(bmp, nil);
+    ListBox1.Items.Add(ExtractFileName(AFileName));
+    if ListBox1.ItemIndex = -1 then
+      ListBox1.ItemIndex := 0;
+  end;
+  bmp.Free;
+end;
+
+initialization
+  {$I unit1.lrs}
+
+end.
+
