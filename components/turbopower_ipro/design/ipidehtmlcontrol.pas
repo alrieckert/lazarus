d145c65262ba7da994d2a29e4988d48d26800927
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2007-12-29 20:58:16 +0000

    added package components/turbopower_ipro/design/turbopoweriprodsgn.lpk
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@13530 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/turbopower_ipro/design/ipidehtmlcontrol.pas
@@ -0,0 +1,164 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL, included in this distribution,        *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+
+  Author: Mattias Gaertner
+
+  Abstract:
+    Installs a HTML control in the IDE using TIpHtmlPanel.
+}
+unit IPIDEHTMLControl;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LCLProc, Graphics, Controls, Dialogs,
+  IpHtml, IDEHelpIntf, LazHelpIntf;
+
+type
+  TSimpleIpHtml = class(TIpHtml)
+  public
+    property OnGetImageX;
+  end;
+
+  { TIPLazHtmlControl }
+
+  TIPLazHtmlControl = class(TIpHtmlPanel,TIDEHTMLControlIntf)
+  private
+    FProvider: TAbstractIDEHTMLProvider;
+    FURL: string;
+    procedure SetProvider(const AValue: TAbstractIDEHTMLProvider);
+    procedure HTMLGetImageX(Sender: TIpHtmlNode; const URL: string;
+                            var Picture: TPicture);
+  public
+    function GetURL: string;
+    procedure SetURL(const AValue: string);
+    property Provider: TAbstractIDEHTMLProvider read FProvider write SetProvider;
+  end;
+
+function IPCreateLazIDEHTMLControl(Owner: TComponent;
+  var Provider: TAbstractIDEHTMLProvider): TControl;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  CreateIDEHTMLControl:=@IPCreateLazIDEHTMLControl;
+end;
+
+function IPCreateLazIDEHTMLControl(Owner: TComponent;
+  var Provider: TAbstractIDEHTMLProvider): TControl;
+var
+  HTMLControl: TIPLazHtmlControl;
+begin
+  HTMLControl:=TIPLazHtmlControl.Create(Owner);
+  Result:=HTMLControl;
+  if Provider=nil then
+    Provider:=CreateIDEHTMLProvider(Owner);
+  Provider.ControlIntf:=HTMLControl;
+  HTMLControl.Provider:=Provider;
+end;
+
+{ TIPLazHtmlControl }
+
+procedure TIPLazHtmlControl.SetProvider(const AValue: TAbstractIDEHTMLProvider
+  );
+begin
+  if FProvider=AValue then exit;
+  FProvider:=AValue;
+end;
+
+procedure TIPLazHtmlControl.HTMLGetImageX(Sender: TIpHtmlNode;
+  const URL: string; var Picture: TPicture);
+var
+  URLType: string;
+  URLPath: string;
+  URLParams: string;
+  Filename: String;
+  Ext: String;
+  Stream: TStream;
+  NewURL: String;
+begin
+  //DebugLn(['TIPLazHtmlControl.HTMLGetImageX URL=',URL]);
+  if Provider=nil then exit;
+  NewURL:=Provider.BuildURL(Provider.BaseURL,URL);
+  //DebugLn(['TIPLazHtmlControl.HTMLGetImageX NewURL=',NewURL,' Provider.BaseURL=',Provider.BaseURL,' URL=',URL]);
+
+  Picture:=nil;
+  Stream:=nil;
+  try
+    try
+      SplitURL(NewURL,URLType,URLPath,URLParams);
+      if URLPath='' then
+        URLPath:=NewURL;
+      Filename:=URLPathToFilename(URLPath);
+      Ext:=ExtractFileExt(Filename);
+      //DebugLn(['TIPLazHtmlControl.HTMLGetImageX URLPath=',URLPath,' Filename=',Filename,' Ext=',Ext]);
+      Picture:=TPicture.Create;
+      // quick check if file format is supported
+      Picture.FindGraphicClassWithFileExt(Ext);
+      // get stream
+      Stream:=Provider.GetStream(NewURL);
+      // load picture
+      Picture.LoadFromStreamWithFileExt(Stream,Ext);
+    finally
+      if Stream<>nil then
+        Provider.ReleaseStream(NewURL);
+    end;
+  except
+    on E: Exception do begin
+      FreeAndNil(Picture);
+      DebugLn(['TIPLazHtmlControl.HTMLGetImageX ERROR: ',E.Message]);
+    end;
+  end;
+end;
+
+function TIPLazHtmlControl.GetURL: string;
+begin
+  Result:=FURL;
+end;
+
+procedure TIPLazHtmlControl.SetURL(const AValue: string);
+var
+  Stream: TStream;
+  NewHTML: TSimpleIpHtml;
+  NewURL: String;
+begin
+  if Provider=nil then raise Exception.Create('TIPLazHtmlControl.SetURL missing Provider');
+  if FURL=AValue then exit;
+  NewURL:=Provider.BuildURL(Provider.BaseURL,AValue);
+  if FURL=NewURL then exit;
+  FURL:=NewURL;
+  try
+    Stream:=Provider.GetStream(FURL);
+    try
+      NewHTML:=TSimpleIpHtml.Create; // Beware: Will be freed automatically
+      NewHTML.OnGetImageX:=@HTMLGetImageX;
+      NewHTML.LoadFromStream(Stream);
+    finally
+      Provider.ReleaseStream(FURL);
+    end;
+    SetHtml(NewHTML);
+  except
+    on E: Exception do begin
+      MessageDlg('Unable to open HTML file',
+        'HTML File: '+FURL+#13
+        +'Error: '+E.Message,mtError,[mbCancel],0);
+    end;
+  end;
+end;
+
+end.
+
---
+++ components/turbopower_ipro/design/turbopoweriprodsgn.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit TurboPowerIProDsgn; 
+
+interface
+
+uses
+  IPIDEHTMLControl, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('IPIDEHTMLControl', @IPIDEHTMLControl.Register); 
+end; 
+
+initialization
+  RegisterPackage('TurboPowerIProDsgn', @Register); 
+end.
