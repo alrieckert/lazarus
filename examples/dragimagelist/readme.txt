4a53c61c25bd4b51052f293570aa008562f247c1
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-01-15 10:18:20 +0000

    DragImageList example
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@13757 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ examples/dragimagelist/readme.txt
@@ -0,0 +1,6 @@
+This example demonstrates the usage of DragImageList.
+
+You will see Button image nearly cursor while drag. 
+This Button image is from the DragImageList. You can
+place any other image there to show it while drag 
+operation.
---
+++ examples/dragimagelist/unit1.pas
@@ -0,0 +1,110 @@
+unit Unit1; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    Panel1: TPanel;
+    procedure Button1StartDrag(Sender: TObject; var DragObject: TDragObject);
+    procedure Panel1DragDrop(Sender, Source: TObject; X, Y: Integer);
+    procedure Panel1DragOver(Sender, Source: TObject; X, Y: Integer;
+      State: TDragState; var Accept: Boolean);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+  
+  { TMyDragObject }
+
+  TMyDragObject = class(TDragControlObject)
+  private
+    FDragImages: TDragImageList;
+  protected
+    function GetDragImages: TDragImageList; override;
+  public
+    constructor Create(AControl: TControl); override;
+    destructor Destroy; override;
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{ TForm1 }
+
+procedure TForm1.Button1StartDrag(Sender: TObject; var DragObject: TDragObject);
+begin
+  DragObject := TMyDragObject.Create(Sender as TControl);
+end;
+
+procedure TForm1.Panel1DragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+  ShowMessage('Congratulations. You droped button on me :)')
+end;
+
+procedure TForm1.Panel1DragOver(Sender, Source: TObject; X, Y: Integer;
+  State: TDragState; var Accept: Boolean);
+var
+  Control: TControl;
+begin
+  if Source is TControl then
+    Control := Source as TControl
+  else
+  if Source is TDragControlObject then
+    Control := (Source as TDragControlObject).Control
+  else
+    Control := nil;
+  Accept := Control is TButton;
+end;
+
+{ TMyDragObject }
+
+function TMyDragObject.GetDragImages: TDragImageList;
+begin
+  Result := FDragImages;
+end;
+
+constructor TMyDragObject.Create(AControl: TControl);
+var
+  Bitmap: TBitmap;
+begin
+  inherited Create(AControl);
+  FDragImages := TDragImageList.Create(AControl);
+  AlwaysShowDragImages := True;
+
+  Bitmap := TBitmap.Create;
+  Bitmap.Width := AControl.Width;
+  Bitmap.Height := AControl.Height;
+  if AControl is TWinControl then
+    (AControl as TWinControl).PaintTo(Bitmap.Canvas, 0, 0);
+  FDragImages.Width := Bitmap.Width;
+  FDragImages.Height := Bitmap.Height;
+  FDragImages.Add(Bitmap, nil);
+  FDragImages.DragHotspot := Point(Bitmap.Width, Bitmap.Height);
+  Bitmap.Free;
+end;
+
+destructor TMyDragObject.Destroy;
+begin
+  FDragImages.Free;
+  inherited Destroy;
+end;
+
+initialization
+  {$I unit1.lrs}
+
+end.
+
