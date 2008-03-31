2388e340140eda1aa3ef5db1eb050026d554d85b
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-03-31 07:45:23 +0000

    save input history of evaluate/modify dialog in a better way (as it was designed)
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@14696 4005530d-fff6-0310-9dd1-cebe43e6787f

--- debugger/evaluatedlg.pp
+++ debugger/evaluatedlg.pp
@@ -136,7 +136,6 @@ procedure TEvaluateDlg.FormClose(Sender: TObject;
   var CloseAction: TCloseAction);
 begin
   IDEDialogLayoutList.SaveLayout(Self);
-  InputHistories.HistoryLists.GetList(ClassName, True).Assign(cmbExpression.Items);
 end;
 
 procedure TEvaluateDlg.FormShow(Sender: TObject);
@@ -156,6 +155,7 @@ var
   S, R: String;
 begin
   S := cmbExpression.Text;
+  InputHistories.HistoryLists.Add(ClassName, S);
   if DebugBoss.Evaluate(S, R)
   then begin
     if cmbExpression.Items.IndexOf(S) = -1
