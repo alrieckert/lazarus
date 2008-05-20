97cd9c1eeeadfbba4907b5e0f3a1b0be17fa157c
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-05-20 16:29:00 +0000

    LCL: added license header
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@15191 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/buttonpanel.pas
+++ lcl/buttonpanel.pas
@@ -1,3 +1,17 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL, included in this distribution,        *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
 unit ButtonPanel;
 
 {$mode objfpc}{$h+}
