7a36b08e41f09f6b81fc3defe7b3b49834baaf78
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-06-27 11:37:00 +0000

    codetools: ppu: showing cpu and target
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@15584 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/ppuparser.pas
+++ components/codetools/ppuparser.pas
@@ -144,6 +144,7 @@ const
   uf_has_exports = $100000;   { this module or a used unit has exports }
 
 const
+  PPU_ID = 'PPU';
   PPU_ID_Size = 3;
   PPU_Ver_Size = 3;
 
@@ -169,6 +170,11 @@ type
     nr   : byte;
   end;
   
+  { EPPUParserError }
+
+  EPPUParserError = class(Exception)
+  end;
+  
   { TPPU }
 
   TPPU = class
@@ -180,6 +186,7 @@ type
     procedure InitInput(s: TStream);
     procedure ReadBuf(var Buf; Count: longint);
     procedure ReadWord(out w: word);
+    procedure Error(const Msg: string);
   public
     constructor Create;
     destructor Destroy; override;
@@ -191,7 +198,141 @@ type
     property InputStream: TStream read FInputStream;
   end;
 
+function PPUTargetToStr(w: longint): string;
+function PPUCpuToStr(w: longint): string;
+
 implementation
+
+function PPUTargetToStr(w: longint): string;
+type
+       { taken from systems.pas }
+       ttarget =
+       (
+             target_none,               { 0 }
+             target_i386_GO32V1,        { 1 }
+             target_i386_GO32V2,        { 2 }
+             target_i386_linux,         { 3 }
+             target_i386_OS2,           { 4 }
+             target_i386_Win32,         { 5 }
+             target_i386_freebsd,       { 6 }
+             target_m68k_Amiga,         { 7 }
+             target_m68k_Atari,         { 8 }
+             target_m68k_Mac,           { 9 }
+             target_m68k_linux,         { 10 }
+             target_m68k_PalmOS,        { 11 }
+             target_alpha_linux,        { 12 }
+             target_powerpc_linux,      { 13 }
+             target_powerpc_macos,      { 14 }
+             target_i386_sunos,         { 15 }
+             target_i386_beos,          { 16 }
+             target_i386_netbsd,        { 17 }
+             target_m68k_netbsd,        { 18 }
+             target_i386_Netware,       { 19 }
+             target_i386_qnx,           { 20 }
+             target_i386_wdosx,         { 21 }
+             target_sparc_sunos,        { 22 }
+             target_sparc_linux,        { 23 }
+             target_i386_openbsd,       { 24 }
+             target_m68k_openbsd,       { 25 }
+             system_x86_64_linux,       { 26 }
+             system_powerpc_macosx,     { 27 }
+             target_i386_emx,           { 28 }
+             target_powerpc_netbsd,     { 29 }
+             target_powerpc_openbsd,    { 30 }
+             target_arm_linux,          { 31 }
+             target_i386_watcom,        { 32 }
+             target_powerpc_MorphOS,    { 33 }
+             target_x86_64_freebsd,     { 34 }
+             target_i386_netwlibc,      { 35 }
+             system_powerpc_Amiga,      { 36 }
+             system_x86_64_win64,       { 37 }
+             system_arm_wince,          { 38 }
+             system_ia64_win64,         { 39 }
+             system_i386_wince,         { 40 }
+             system_x86_6432_linux,     { 41 }
+             system_arm_gba,            { 42 }
+             system_arm_nds             { 43 }
+       );
+const
+  Targets : array[ttarget] of string[17]=(
+  { 0 }   'none',
+  { 1 }   'GO32V1',
+  { 2 }   'GO32V2',
+  { 3 }   'Linux-i386',
+  { 4 }   'OS/2',
+  { 5 }   'Win32',
+  { 6 }   'FreeBSD-i386',
+  { 7 }   'Amiga',
+  { 8 }   'Atari',
+  { 9 }   'MacOS-m68k',
+  { 10 }  'Linux-m68k',
+  { 11 }  'PalmOS-m68k',
+  { 12 }  'Linux-alpha',
+  { 13 }  'Linux-ppc',
+  { 14 }  'MacOS-ppc',
+  { 15 }  'Solaris-i386',
+  { 16 }  'BeOS-i386',
+  { 17 }  'NetBSD-i386',
+  { 18 }  'NetBSD-m68k',
+  { 19 }  'Netware-i386-clib',
+  { 20 }  'Qnx-i386',
+  { 21 }  'WDOSX-i386',
+  { 22 }  'Solaris-sparc',
+  { 23 }  'Linux-sparc',
+  { 24 }  'OpenBSD-i386',
+  { 25 }  'OpenBSD-m68k',
+  { 26 }  'Linux-x86-64',
+  { 27 }  'MacOSX-ppc',
+  { 28 }  'OS/2 via EMX',
+  { 29 }  'NetBSD-powerpc',
+  { 30 }  'OpenBSD-powerpc',
+  { 31 }  'Linux-arm',
+  { 32 }  'Watcom-i386',
+  { 33 }  'MorphOS-powerpc',
+  { 34 }  'FreeBSD-x86-64',
+  { 35 }  'Netware-i386-libc',
+  { 36 }  'Amiga-PowerPC',
+  { 37 }  'Win64-x64',
+  { 38 }  'WinCE-ARM',
+  { 39 }  'Win64-iA64',
+  { 40 }  'WinCE-i386',
+  { 41 }  'Linux-x64',
+  { 42 }  'GBA-arm',
+  { 43 }  'NDS-arm'
+  );
+begin
+  if w<=ord(high(ttarget)) then
+    Result:=Targets[ttarget(w)]
+  else
+    Result:='<!! Unknown target value '+IntToStr(w)+'>';
+end;
+
+function PPUCpuToStr(w:longint):string;
+type
+  { Copied from systems.pas }
+       tsystemcpu=
+       (
+             cpu_no,                       { 0 }
+             cpu_i386,                     { 1 }
+             cpu_m68k,                     { 2 }
+             cpu_alpha,                    { 3 }
+             cpu_powerpc,                  { 4 }
+             cpu_sparc,                    { 5 }
+             cpu_vm,                       { 6 }
+             cpu_iA64,                     { 7 }
+             cpu_x86_64,                   { 8 }
+             cpu_mips,                     { 9 }
+             cpu_arm                       { 10 }
+       );
+const
+  CpuTxt : array[tsystemcpu] of string[8]=
+    ('none','i386','m68k','alpha','powerpc','sparc','vis','ia64','x86_64','mips','arm');
+begin
+  if w<=ord(high(tsystemcpu)) then
+    Result:=CpuTxt[tsystemcpu(w)]
+  else
+    Result:='<!! Unknown cpu value '+IntToStr(w)+'>';
+end;
 
 { TPPU }
 
@@ -199,6 +340,8 @@ procedure TPPU.ReadHeader;
 begin
   // read ID
   ReadBuf(FHeader.id,PPU_ID_Size);
+  if String(FHeader.id)<>PPU_ID then
+    Error('This is not a PPU. Wrong ID.');
   // read version
   ReadBuf(FHeader.ver,PPU_Ver_Size);
   // read rest of header
@@ -233,6 +376,11 @@ begin
   FInputStream.Read(w,2);
   if fChangeEndian then
     swapendian(w);
+end;
+
+procedure TPPU.Error(const Msg: string);
+begin
+  raise EPPUParserError.Create(Msg);
 end;
 
 constructor TPPU.Create;
@@ -287,8 +435,8 @@ begin
   DebugLn([Prefix,'  ID=',String(FHeader.ID)]);
   DebugLn([Prefix,'  Ver=',String(FHeader.ver)]);
   DebugLn([Prefix,'  Compiler=',FHeader.compiler]);
-  DebugLn([Prefix,'  CPU=',FHeader.cpu]);
-  DebugLn([Prefix,'  Target=',FHeader.target]);
+  DebugLn([Prefix,'  CPU=',PPUCpuToStr(FHeader.cpu)]);
+  DebugLn([Prefix,'  Target=',PPUTargetToStr(FHeader.target)]);
   DebugLn([Prefix,'  Flags=',FHeader.flags]);
   DebugLn([Prefix,'  Size=',FHeader.size]);
   DebugLn([Prefix,'  Checksum=',FHeader.checksum]);
