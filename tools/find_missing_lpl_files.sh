53a95af309b8929a6ddf425d12f90797a56594b4
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-08-12 13:47:42 +0000

    added missing lpl
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@16037 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/find_missing_lpl_files.sh
+++ tools/find_missing_lpl_files.sh
@@ -5,12 +5,22 @@
 
 set -e
 
-LazDir=..
+if [ ! -e tools ]; then
+  echo "please cd to lazarus source directory before running this script"
+  exit
+fi
 
-LPKFiles=$(find $LazDir -name '*.lpk' | xargs)
+LPKFiles=$(find . -name '*.lpk' | xargs)
 for LPK in $LPKFiles; do
   LPKName=$(echo $LPK | sed -e 's/.*\///' -e 's/.lpk//')
-  ls $LazDir/packager/globallinks/${LPKName}-*.lpl 2>/dev/null > /dev/null || echo missing lpl for $LPK
+  Missing=
+  ls packager/globallinks/${LPKName}-*.lpl >/dev/null 2>/dev/null || Missing=1
+  if [ -n "$Missing" ]; then
+    echo missing lpl for $LPK
+    LPK=$(echo $LPK | sed -e 's/^\.\///')
+    LPLFilename=packager/globallinks/$LPKName-0.lpl
+    echo '$(LazarusDir)/'$LPK > $LPLFilename
+  fi
 done
 
 #end.
