0a265010666b8a8e94c68a538fb59edef409aaca
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-09-28 23:29:51 +0000

    rpm: removing i386 and x86_64 from fpc version
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@16776 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/create_lazarus_rpm.sh
+++ tools/install/create_lazarus_rpm.sh
@@ -15,15 +15,16 @@ if [ "x$FPCRPM" = "x" ]; then
   echo ERROR: fpc rpm not installed
   exit
 fi
-FPCRPMVersion=$(echo $FPCRPM | sed -e 's/fpc-//g')
+FPCRPMVersion=$(echo $FPCRPM | sed -e 's/fpc-//g' -e 's/\.i386\|\.x86_64//g')
 echo "installed fpc version: $FPCRPMVersion"
 FPCSRCRPMVersion=$(echo $FPCRPMVersion | cut -d- -f1)
 
 Date=$Year$Month$Day
 LazVersion=$(./get_lazarus_version.sh)
 LazRelease='0' # $(echo $FPCRPM | sed -e 's/-/_/g')
+RPMSrcDir=$(./rpm/get_rpm_source_dir.sh)
 Src=lazarus-$LazVersion-$LazRelease.tar.gz
-SrcTGZ=$(./rpm/get_rpm_source_dir.sh)/SOURCES/$Src
+SrcTGZ=$RPMSrcDir/SOURCES/$Src
 SpecFile=rpm/lazarus-$LazVersion-$LazRelease.spec
 
 Arch=$(rpm --eval "%{_arch}")
@@ -40,7 +41,7 @@ cat rpm/lazarus.spec.template | \
   sed -e "s/LAZVERSION/$LazVersion/g" \
       -e "s/LAZRELEASE/$LazRelease/g" \
       -e "s/LAZSOURCE/$Src/g" \
-      -e "s/FPCBUILDVERSION/2.0.0/g" \
+      -e "s/FPCBUILDVERSION/2.2.2/g" \
       -e "s/FPCVERSION/$FPCRPMVersion/g" \
       -e "s/FPCSRCVERSION/$FPCSRCRPMVersion/g" \
   > $SpecFile
@@ -49,7 +50,7 @@ cat rpm/lazarus.spec.template | \
 echo "building rpm ..."
 rpm -ba $SpecFile || rpmbuild -ba $SpecFile
 
-echo "The new rpm can be found at $(./rpm/get_rpm_source_dir.sh)/RPMS/$Arch/lazarus-$LazVersion-$LazRelease.$Arch.rpm"
+echo "The new rpm can be found at $RPMSrcDir/RPMS/$Arch/lazarus-$LazVersion-$LazRelease.$Arch.rpm"
 
 # end.
 
