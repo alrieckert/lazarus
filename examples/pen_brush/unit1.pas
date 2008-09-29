5d18382a610b2d7b2eed663f649d968f390f077b
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-09-29 05:15:17 +0000

    examples: add bitmap pattern brush to pen and brush example
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@16780 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/pen_brush/unit1.pas
+++ examples/pen_brush/unit1.pas
@@ -20,9 +20,11 @@ type
     BrushBox: TPaintBox;
     procedure BrushBoxPaint(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure PenBoxPaint(Sender: TObject);
   private
-    { private declarations }
+    FPattern: TBitmap;
   public
     { public declarations }
   end; 
@@ -42,20 +44,40 @@ begin
   Close;
 end;
 
+procedure TForm1.FormCreate(Sender: TObject);
+const
+  LineBitsDotted: array[0..7] of Word = ($55, $AA, $55, $AA, $55, $AA, $55, $AA);
+begin
+  FPattern := TBitmap.Create;
+  FPattern.SetHandles(CreateBitmap(8, 8, 1, 1, @LineBitsDotted), 0);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  FPattern.Free;
+end;
+
 procedure TForm1.BrushBoxPaint(Sender: TObject);
 var
   bs: TBrushStyle;
   y: integer;
 begin
+  y := 15;
   for bs := bsSolid to bsDiagCross do
   begin
-    y := 15 * (Ord(bs) + 1);
     BrushBox.Canvas.Brush.Color := clBtnFace;
     BrushBox.Canvas.TextOut(10, y - 7, GetEnumName(TypeInfo(TBrushStyle), Ord(bs)));
     BrushBox.Canvas.Brush.Color := clRed;
     BrushBox.Canvas.Brush.Style := bs;
     BrushBox.Canvas.FillRect(120, y - 5, PenBox.Width - 10, y + 5);
+    inc(y, 15);
   end;
+
+  BrushBox.Canvas.Brush.Color := clBtnFace;
+  BrushBox.Canvas.TextOut(10, y - 7, 'Pattern');
+  BrushBox.Canvas.Brush.Color := clRed;
+  BrushBox.Canvas.Brush.Bitmap := FPattern;
+  BrushBox.Canvas.FillRect(120, y - 5, PenBox.Width - 10, y + 5);
 end;
 
 procedure TForm1.PenBoxPaint(Sender: TObject);
