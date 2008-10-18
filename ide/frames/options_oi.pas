b89d0f8d183d48379ed2c025ffbd504040231bc5
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-10-18 17:16:29 +0000

    added options_oi unit to lpi
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@17043 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/frames/options_oi.pas
+++ ide/frames/options_oi.pas
@@ -1,4 +1,24 @@
-unit options_oi;
+{
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+}
+unit Options_OI;
 
 {$mode objfpc}{$H+}
 
@@ -33,7 +53,6 @@ type
     OIValueColorButton: TColorButton;
     OIValueColorLabel: TLabel;
   private
-    { private declarations }
   public
     procedure Setup;
     function GetTitle: String;
