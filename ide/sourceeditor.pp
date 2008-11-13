cbca159f08ceec136a424bec4ae121b3857a6105
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-11-13 12:10:01 +0000

    clean up
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@17368 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -5533,6 +5533,7 @@ end;
 Procedure TSourceNotebook.BookMarkSet(Value: Integer);
 var
   ActEdit, AnEdit: TSourceEditor;
+  Cmd: TIDEMenuCommand;
 Begin
   ActEdit:=GetActiveSE;
 
@@ -5542,8 +5543,8 @@ Begin
   end;
   ActEdit.EditorComponent.SetBookMark(Value,
      ActEdit.EditorComponent.CaretX,ActEdit.EditorComponent.CaretY);
-  (SrcEditSubMenuSetBookmarks[Value] as TIDEMenuCommand).Checked := true;
-  TIDEMenuCommand(SrcEditSubMenuGotoBookmarks[Value]).Checked:=true;
+  Cmd:=SrcEditSubMenuSetBookmarks[Value] as TIDEMenuCommand;
+  Cmd.Checked := true;
   if Project1<>nil then
     Project1.SessionModified:=true;
 end;
