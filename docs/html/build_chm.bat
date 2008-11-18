e6714e8acaab859d4cbb7189d13801d5e9e6f139
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-11-18 02:46:49 +0000

    docs: add build chm bat file
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@17419 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ docs/html/build_chm.bat
@@ -0,0 +1,11 @@
+REM
+REM Builds all Lazarus docs and pack them in one .chm file
+REM
+REM Notes:
+REM
+REM 1 - Please fix the path in this file for your installation
+REM 2 - Before running this file, first compile the project build_lcl_docs.lpi
+REM
+PATH=D:\programming\fpc\bin\i386-win32\
+build_lcl_docs.exe --outfmt chm --arg --make-searchable
+pause
