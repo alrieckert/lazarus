e14e384bff0258564fffdc1b1f92ce61b232ceb8
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2008-12-04 01:45:00 +0000

    lcl: fix memory leak in translations unit
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@17677 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/translations.pas
+++ lcl/translations.pas
@@ -4,7 +4,7 @@
  *                                                                           *
  *  This file is part of the Lazarus Component Library (LCL)                 *
  *                                                                           *
- *  See the file COPYING.modifiedLGPL.txt, included in this distribution,        *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
  *  for details about the copyright.                                         *
  *                                                                           *
  *  This program is distributed in the hope that it will be useful,          *
@@ -351,7 +351,7 @@ begin
   po:=nil;
   try
     po:=TPOFile.Create(AFilename);
-    result:=TranslateUnitResourceStrings(ResUnitName,TPOFile.Create(AFilename));
+    result:=TranslateUnitResourceStrings(ResUnitName,po);
   finally
     po.free;
   end;
