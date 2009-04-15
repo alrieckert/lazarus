e689adb1864df73e9721f8261f3f8a8a1c71070f
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-04-15 12:53:26 +0000

    nogui: make this widgetset linkable
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@19444 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ lcl/interfaces/nogui/noguiwsfactory.pas
@@ -0,0 +1,493 @@
+unit NoGUIWSFactory;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, WSLCLClasses;
+
+// imglist
+function RegisterCustomImageList: Boolean;
+// controls
+function RegisterDragImageList: Boolean;
+function RegisterControl: Boolean;
+function RegisterWinControl: Boolean;
+function RegisterGraphicControl: Boolean;
+function RegisterCustomControl: Boolean;
+// comctrls
+function RegisterStatusBar: Boolean;
+function RegisterTabSheet: Boolean;
+function RegisterPageControl: Boolean;
+function RegisterCustomListView: Boolean;
+function RegisterCustomProgressBar: Boolean;
+function RegisterCustomUpDown: Boolean;
+function RegisterCustomToolButton: Boolean;
+function RegisterToolBar: Boolean;
+function RegisterCustomTrackBar: Boolean;
+function RegisterCustomTreeView: Boolean;
+// calendar
+function RegisterCustomCalendar: Boolean;
+// dialogs
+function RegisterCommonDialog: Boolean;
+function RegisterFileDialog: Boolean;
+function RegisterOpenDialog: Boolean;
+function RegisterSaveDialog: Boolean;
+function RegisterSelectDirectoryDialog: Boolean;
+function RegisterColorDialog: Boolean;
+function RegisterColorButton: Boolean;
+function RegisterFontDialog: Boolean;
+// StdCtrls
+function RegisterCustomScrollBar: Boolean;
+function RegisterCustomGroupBox: Boolean;
+function RegisterCustomComboBox: Boolean;
+function RegisterCustomListBox: Boolean;
+function RegisterCustomEdit: Boolean;
+function RegisterCustomMemo: Boolean;
+function RegisterButtonControl: Boolean;
+function RegisterCustomButton: Boolean;
+function RegisterCustomCheckBox: Boolean;
+function RegisterToggleBox: Boolean;
+function RegisterRadioButton: Boolean;
+function RegisterCustomStaticText: Boolean;
+function RegisterCustomLabel: Boolean;
+// extctrls
+function RegisterCustomPage: Boolean;
+function RegisterCustomNotebook: Boolean;
+function RegisterShape: Boolean;
+function RegisterCustomSplitter: Boolean;
+function RegisterPaintBox: Boolean;
+function RegisterCustomImage: Boolean;
+function RegisterBevel: Boolean;
+function RegisterCustomRadioGroup: Boolean;
+function RegisterCustomCheckGroup: Boolean;
+function RegisterCustomLabeledEdit: Boolean;
+function RegisterCustomPanel: Boolean;
+function RegisterCustomTrayIcon: Boolean;
+//ExtDlgs
+function RegisterPreviewFileControl: Boolean;
+function RegisterPreviewFileDialog: Boolean;
+function RegisterOpenPictureDialog: Boolean;
+function RegisterSavePictureDialog: Boolean;
+function RegisterCalculatorDialog: Boolean;
+function RegisterCalculatorForm: Boolean;
+function RegisterCalendarDialog: Boolean;
+// Buttons
+function RegisterCustomBitBtn: Boolean;
+function RegisterCustomSpeedButton: Boolean;
+// Arrow
+function RegisterArrow: Boolean;
+// CheckLst
+function RegisterCustomCheckListBox: Boolean;
+// Forms
+function RegisterScrollingWinControl: Boolean;
+function RegisterScrollBox: Boolean;
+function RegisterCustomFrame: Boolean;
+function RegisterCustomForm: Boolean;
+function RegisterHintWindow: Boolean;
+function RegisterCustomGrid: Boolean;
+function RegisterMenuItem: Boolean;
+function RegisterMenu: Boolean;
+function RegisterMainMenu: Boolean;
+function RegisterPopupMenu: Boolean;
+function RegisterPairSplitterSide: Boolean;
+function RegisterCustomPairSplitter: Boolean;
+function RegisterCustomFloatSpinEdit: Boolean;
+function RegisterCustomRubberBand: Boolean;
+
+implementation
+
+// imglist
+function RegisterCustomImageList: Boolean; alias : 'WSRegisterCustomImageList';
+begin
+  Result := False;
+end;
+
+// controls
+function RegisterDragImageList: Boolean; alias : 'WSRegisterDragImageList';
+begin
+  Result := False;
+end;
+
+function RegisterControl: Boolean; alias : 'WSRegisterControl';
+begin
+  Result := False;
+end;
+
+function RegisterWinControl: Boolean; alias : 'WSRegisterWinControl';
+begin
+  Result := False;
+end;
+
+function RegisterGraphicControl: Boolean; alias : 'WSRegisterGraphicControl';
+begin
+  Result := False;
+end;
+
+function RegisterCustomControl: Boolean; alias : 'WSRegisterCustomControl';
+begin
+  Result := False;
+end;
+
+// comctrls
+function RegisterStatusBar: Boolean; alias : 'WSRegisterStatusBar';
+begin
+  Result := False;
+end;
+
+function RegisterTabSheet: Boolean; alias : 'WSRegisterTabSheet';
+begin
+  Result := False;
+end;
+
+function RegisterPageControl: Boolean; alias : 'WSRegisterPageControl';
+begin
+  Result := False;
+end;
+
+function RegisterCustomListView: Boolean; alias : 'WSRegisterCustomListView';
+begin
+  Result := False;
+end;
+
+function RegisterCustomProgressBar: Boolean; alias : 'WSRegisterCustomProgressBar';
+begin
+  Result := False;
+end;
+
+function RegisterCustomUpDown: Boolean; alias : 'WSRegisterCustomUpDown';
+begin
+  Result := False;
+end;
+
+function RegisterCustomToolButton: Boolean; alias : 'WSRegisterCustomToolButton';
+begin
+  Result := False;
+end;
+
+function RegisterToolBar: Boolean; alias : 'WSRegisterToolBar';
+begin
+  Result := False;
+end;
+
+function RegisterCustomTrackBar: Boolean; alias : 'WSRegisterCustomTrackBar';
+begin
+  Result := False;
+end;
+
+function RegisterCustomTreeView: Boolean; alias : 'WSRegisterCustomTreeView';
+begin
+  Result := False;
+end;
+
+// calendar
+function RegisterCustomCalendar: Boolean; alias : 'WSRegisterCustomCalendar';
+begin
+  Result := False;
+end;
+
+// dialogs
+function RegisterCommonDialog: Boolean; alias : 'WSRegisterCommonDialog';
+begin
+  Result := False;
+end;
+
+function RegisterFileDialog: Boolean; alias : 'WSRegisterFileDialog';
+begin
+  Result := False;
+end;
+
+function RegisterOpenDialog: Boolean; alias : 'WSRegisterOpenDialog';
+begin
+  Result := False;
+end;
+
+function RegisterSaveDialog: Boolean; alias : 'WSRegisterSaveDialog';
+begin
+  Result := False;
+end;
+
+function RegisterSelectDirectoryDialog: Boolean; alias : 'WSRegisterSelectDirectoryDialog';
+begin
+  Result := False;
+end;
+
+function RegisterColorDialog: Boolean; alias : 'WSRegisterColorDialog';
+begin
+  Result := False;
+end;
+
+function RegisterColorButton: Boolean; alias : 'WSRegisterColorButton';
+begin
+  Result := False;
+end;
+
+function RegisterFontDialog: Boolean; alias : 'WSRegisterFontDialog';
+begin
+  Result := False;
+end;
+
+// StdCtrls
+function RegisterCustomScrollBar: Boolean; alias : 'WSRegisterCustomScrollBar';
+begin
+  Result := False;
+end;
+
+function RegisterCustomGroupBox: Boolean; alias : 'WSRegisterCustomGroupBox';
+begin
+  Result := False;
+end;
+
+function RegisterCustomComboBox: Boolean; alias : 'WSRegisterCustomComboBox';
+begin
+  Result := False;
+end;
+
+function RegisterCustomListBox: Boolean; alias : 'WSRegisterCustomListBox';
+begin
+  Result := False;
+end;
+
+function RegisterCustomEdit: Boolean; alias : 'WSRegisterCustomEdit';
+begin
+  Result := False;
+end;
+
+function RegisterCustomMemo: Boolean; alias : 'WSRegisterCustomMemo';
+begin
+  Result := False;
+end;
+
+function RegisterButtonControl: Boolean; alias : 'WSRegisterButtonControl';
+begin
+  Result := False;
+end;
+
+function RegisterCustomButton: Boolean; alias : 'WSRegisterCustomButton';
+begin
+  Result := False;
+end;
+
+function RegisterCustomCheckBox: Boolean; alias : 'WSRegisterCustomCheckBox';
+begin
+  Result := False;
+end;
+
+function RegisterToggleBox: Boolean; alias : 'WSRegisterToggleBox';
+begin
+  Result := False;
+end;
+
+function RegisterRadioButton: Boolean; alias : 'WSRegisterRadioButton';
+begin
+  Result := False;
+end;
+
+function RegisterCustomStaticText: Boolean; alias : 'WSRegisterCustomStaticText';
+begin
+  Result := False;
+end;
+
+function RegisterCustomLabel: Boolean; alias : 'WSRegisterCustomLabel';
+begin
+  Result := False;
+end;
+
+// extctrls
+function RegisterCustomPage: Boolean; alias : 'WSRegisterCustomPage';
+begin
+  Result := False;
+end;
+
+function RegisterCustomNotebook: Boolean; alias : 'WSRegisterCustomNotebook';
+begin
+  Result := False;
+end;
+
+function RegisterShape: Boolean; alias : 'WSRegisterShape';
+begin
+  Result := False;
+end;
+
+function RegisterCustomSplitter: Boolean; alias : 'WSRegisterCustomSplitter';
+begin
+  Result := False;
+end;
+
+function RegisterPaintBox: Boolean; alias : 'WSRegisterPaintBox';
+begin
+  Result := False;
+end;
+
+function RegisterCustomImage: Boolean; alias : 'WSRegisterCustomImage';
+begin
+  Result := False;
+end;
+
+function RegisterBevel: Boolean; alias : 'WSRegisterBevel';
+begin
+  Result := False;
+end;
+
+function RegisterCustomRadioGroup: Boolean; alias : 'WSRegisterCustomRadioGroup';
+begin
+  Result := False;
+end;
+
+function RegisterCustomCheckGroup: Boolean; alias : 'WSRegisterCustomCheckGroup';
+begin
+  Result := False;
+end;
+
+function RegisterCustomLabeledEdit: Boolean; alias : 'WSRegisterCustomLabeledEdit';
+begin
+  Result := False;
+end;
+
+function RegisterCustomPanel: Boolean; alias : 'WSRegisterCustomPanel';
+begin
+  Result := False;
+end;
+
+function RegisterCustomTrayIcon: Boolean; alias : 'WSRegisterCustomTrayIcon';
+begin
+  Result := False;
+end;
+
+//ExtDlgs
+function RegisterPreviewFileControl: Boolean; alias : 'WSRegisterPreviewFileControl';
+begin
+  Result := False;
+end;
+
+function RegisterPreviewFileDialog: Boolean; alias : 'WSRegisterPreviewFileDialog';
+begin
+  Result := False;
+end;
+
+function RegisterOpenPictureDialog: Boolean; alias : 'WSRegisterOpenPictureDialog';
+begin
+  Result := False;
+end;
+
+function RegisterSavePictureDialog: Boolean; alias : 'WSRegisterSavePictureDialog';
+begin
+  Result := False;
+end;
+
+function RegisterCalculatorDialog: Boolean; alias : 'WSRegisterCalculatorDialog';
+begin
+  Result := False;
+end;
+
+function RegisterCalculatorForm: Boolean; alias : 'WSRegisterCalculatorForm';
+begin
+  Result := False;
+end;
+
+function RegisterCalendarDialog: Boolean; alias : 'WSRegisterCalendarDialog';
+begin
+  Result := False;
+end;
+
+// Buttons
+function RegisterCustomBitBtn: Boolean; alias : 'WSRegisterCustomBitBtn';
+begin
+  Result := False;
+end;
+
+function RegisterCustomSpeedButton: Boolean; alias : 'WSRegisterCustomSpeedButton';
+begin
+  Result := False;
+end;
+
+// Arrow
+function RegisterArrow: Boolean; alias : 'WSRegisterArrow';
+begin
+  Result := False;
+end;
+
+// CheckLst
+function RegisterCustomCheckListBox: Boolean; alias : 'WSRegisterCustomCheckListBox';
+begin
+  Result := False;
+end;
+
+// Forms
+function RegisterScrollingWinControl: Boolean; alias : 'WSRegisterScrollingWinControl';
+begin
+  Result := False;
+end;
+
+function RegisterScrollBox: Boolean; alias : 'WSRegisterScrollBox';
+begin
+  Result := False;
+end;
+
+function RegisterCustomFrame: Boolean; alias : 'WSRegisterCustomFrame';
+begin
+  Result := False;
+end;
+
+function RegisterCustomForm: Boolean; alias : 'WSRegisterCustomForm';
+begin
+  Result := False;
+end;
+
+function RegisterHintWindow: Boolean; alias : 'WSRegisterHintWindow';
+begin
+  Result := False;
+end;
+
+// Grids
+function RegisterCustomGrid: Boolean; alias : 'WSRegisterCustomGrid';
+begin
+  Result := False;
+end;
+
+// Menus
+function RegisterMenuItem: Boolean; alias : 'WSRegisterMenuItem';
+begin
+  Result := False;
+end;
+
+function RegisterMenu: Boolean; alias : 'WSRegisterMenu';
+begin
+  Result := False;
+end;
+
+function RegisterMainMenu: Boolean; alias : 'WSRegisterMainMenu';
+begin
+  Result := False;
+end;
+
+function RegisterPopupMenu: Boolean; alias : 'WSRegisterPopupMenu';
+begin
+  Result := False;
+end;
+
+function RegisterPairSplitterSide: Boolean; alias : 'WSRegisterPairSplitterSide';
+begin
+  Result := False;
+end;
+
+function RegisterCustomPairSplitter: Boolean; alias : 'WSRegisterCustomPairSplitter';
+begin
+  Result := False;
+end;
+
+// Spin
+function RegisterCustomFloatSpinEdit: Boolean; alias : 'WSRegisterCustomFloatSpinEdit';
+begin
+  Result := False;
+end;
+
+// RubberBand
+function RegisterCustomRubberBand: Boolean; alias : 'WSRegisterCustomRubberBand';
+begin
+  Result := False;
+end;
+
+
+end.
+
