7b446bc9ba4c96e6d61feb2ae79aa7f4b1fb3216
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-04-23 02:49:12 +0000

    ide: simplify goto form
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@19579 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/gotofrm.pas
+++ ide/gotofrm.pas
@@ -6,18 +6,16 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, ExtCtrls, Buttons, LazarusIDEStrConsts, LCLType;
+  StdCtrls, ExtCtrls, Buttons, LazarusIDEStrConsts, LCLType, ButtonPanel;
 
 type
 
   { TfrmGoto }
 
   TfrmGoto = class(TForm)
+    ButtonPanel1: TButtonPanel;
     Label1: TLabel;
     Edit1: TEdit;
-    btnOK: TBitbtn;
-    btnCancel: TBitBtn;
-    procedure Edit1KeyDown(Sender: TObject; var Key:Word; Shift:TShiftState);
   public
     constructor Create(AOwner: TComponent); override;
     procedure DoShow; override;
@@ -41,21 +39,6 @@ begin
   Edit1.SelectAll;
   Edit1.SetFocus;
   inherited DoShow;
-end;
-
-procedure TfrmGoto.Edit1KeyDown(Sender: TObject; var Key:Word;
-   Shift:TShiftState);
-begin
-  if (Key=VK_RETURN) then
-  begin
-    ModalResult:=mrOk;
-    Key := 0;
-  end;
-  if (Key=VK_ESCAPE) then
-  begin
-    ModalResult:=mrCancel;
-    Key := 0;
-  end;
 end;
 
 initialization
