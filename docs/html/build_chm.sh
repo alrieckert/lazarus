57270e6150120a4c6af6ec4a26fb0ba1abb06865
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-04-25 18:07:55 +0000

    docs: added chm builder script
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@19621 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ docs/html/build_chm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Builds all Lazarus docs and pack them in one .chm file
+#
+# Notes:
+#
+# Before running this file, first compile the project build_lcl_docs.lpi
+#
+./build_lcl_docs --outfmt chm --arg --make-searchable
+
+
