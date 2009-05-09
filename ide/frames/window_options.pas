a7bd8ced0061736fbe3434a8f2f2c358ab144b4c
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-05-09 14:46:34 +0000

    ide: window options: remove TIDEWindowSetupLayoutComponent and place controls in design-time instead
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@19885 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/frames/window_options.pas
+++ ide/frames/window_options.pas
@@ -25,67 +25,49 @@ unit window_options;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, StdCtrls,
-  EnvironmentOpts, LazarusIDEStrConsts, IDEOptionDefs, ObjectInspector, IDEOptionsIntf;
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, StdCtrls, ExtCtrls,
+  Spin, EnvironmentOpts, LazarusIDEStrConsts, IDEOptionDefs, ObjectInspector,
+  IDEOptionsIntf;
 
 type
-  TOnApplyWindowPos = procedure(Layout: TIDEWindowLayout) of object;
-
-  TIDEWindowSetupLayoutComponent = class(TGroupBox)
-    RestoreWindowGeometryRadioButton: TRadioButton;
-    DefaultRadioButton: TRadioButton;
-    CustomPositionRadioButton: TRadioButton;
-    LeftLabel: TLabel;
-    LeftEdit: TEdit;
-    TopLabel: TLabel;
-    TopEdit: TEdit;
-    WidthLabel: TLabel;
-    WidthEdit: TEdit;
-    HeightLabel: TLabel;
-    HeightEdit: TEdit;
-    UseWindowManagerSettingRadioButton: TRadioButton;
-    DockedRadioButton: TRadioButton;
-    ApplyButton: TButton;
-    GetWindowPositionButton: TButton;
-    procedure ApplyButtonClick(Sender: TObject);
-    procedure GetWindowPositionButtonClick(Sender: TObject);
-  private
-    fOnApplyWindowPos: TOnApplyWindowPos;
-    fLayout: TIDEWindowLayout;
-    fUpdateRadioButtons: boolean;
-  protected
-    function GetLayout: TIDEWindowLayout;
-    procedure SetLayout(const AValue: TIDEWindowLayout);
-    function GetPlacementRadioButtons(APlacement: TIDEWindowPlacement): TRadioButton;
-    procedure SetPlacementRadioButtons(APlacement: TIDEWindowPlacement;
-      const AValue: TRadioButton);
-    procedure LoadFrom(ALayout: TIDEWindowLayout);
-    procedure BoundsChanged; override;
-  public
-    constructor Create(TheOwner: TComponent); override;
-    procedure Save;
-    procedure SaveTo(AnLayout: TIDEWindowLayout);
-    property Layout: TIDEWindowLayout read GetLayout write SetLayout;
-    property PlacementRadioButtons[APlacement: TIDEWindowPlacement]: TRadioButton
-      read GetPlacementRadioButtons write SetPlacementRadioButtons;
-    property OnApplyWindowPos: TOnApplyWindowPos
-      read fOnApplyWindowPos write fOnApplyWindowPos;
-  end;
-
   { TWindowOptionsFrame }
 
   TWindowOptionsFrame = class(TAbstractIDEOptionsEditor)
+    Bevel1: TBevel;
+    Bevel2: TBevel;
+    GetWindowPositionButton: TButton;
+    ApplyButton: TButton;
     HideMessagesIconsCheckBox: TCheckBox;
+    LeftLabel: TLabel;
+    DockedRadioButton: TRadioButton;
+    TopLabel: TLabel;
+    WidthLabel: TLabel;
+    HeightLabel: TLabel;
+    lblWindowCaption: TLabel;
+    LeftEdit: TSpinEdit;
+    TopEdit: TSpinEdit;
+    WidthEdit: TSpinEdit;
+    HeightEdit: TSpinEdit;
+    UseWindowManagerSettingRadioButton: TRadioButton;
+    DefaultRadioButton: TRadioButton;
+    RestoreWindowGeometryRadioButton: TRadioButton;
+    CustomPositionRadioButton: TRadioButton;
     WindowPositionsGroupBox: TGroupBox;
     HideIDEOnRunCheckBox: TCheckBox;
     MinimizeAllOnMinimizeMainCheckBox: TCheckBox;
     WindowPositionsListBox: TListBox;
+    procedure ApplyButtonClick(Sender: TObject);
+    procedure GetWindowPositionButtonClick(Sender: TObject);
     procedure WindowPositionsListBoxSelectionChange(Sender: TObject; User: boolean);
   private
     FLayouts: TIDEWindowLayoutList;
-    WindowPositionsBox: TIDEWindowSetupLayoutComponent;
+    FLayout: TIDEWindowLayout;
+    function GetPlacementRadioButtons(APlacement: TIDEWindowPlacement): TRadioButton;
+    procedure SetLayout(const AValue: TIDEWindowLayout);
     procedure SetWindowPositionsItem(Index: integer);
+    procedure SaveLayout;
     function GetCaptionFor(AWindow: TNonModalIDEWindow): String;
+    property Layout: TIDEWindowLayout read FLayout write SetLayout;
   public
     function GetTitle: String; override;
     procedure Setup(ADialog: TAbstractOptionsEditorDialog); override;
@@ -122,15 +104,19 @@ begin
     Add(dlgObjInsp);
     EndUpdate;
   end;
-  WindowPositionsBox := TIDEWindowSetupLayoutComponent.Create(Self);
-  with WindowPositionsBox do
-  begin
-    Name:='WindowPositionsBox';
-    Parent:=WindowPositionsGroupBox;
-    BorderSpacing.Around:=6;
-    Align:=alBottom;
-    AnchorToNeighbour(akTop,6,WindowPositionsListBox);
-  end;
+
+  LeftLabel.Caption := dlgLeftPos;
+  TopLabel.Caption := dlgTopPos;
+  WidthLabel.Caption := dlgWidthPos;
+  HeightLabel.Caption := DlgHeightPos;
+  ApplyButton.Caption := dlgButApply;
+  GetWindowPositionButton.Caption := dlgGetPosition;
+
+  UseWindowManagerSettingRadioButton.Caption := rsiwpUseWindowManagerSetting;
+  DefaultRadioButton.Caption := rsiwpDefault;
+  RestoreWindowGeometryRadioButton.Caption := rsiwpRestoreWindowGeometry;
+  DockedRadioButton.Caption := rsiwpDocked;
+  CustomPositionRadioButton.Caption := rsiwpCustomPosition;
 end;
 
 procedure TWindowOptionsFrame.ReadSettings(AOptions: TAbstractIDEOptions);
@@ -151,7 +137,7 @@ procedure TWindowOptionsFrame.WriteSettings(AOptions: TAbstractIDEOptions);
 begin
   with AOptions as TEnvironmentOptions do
   begin
-    WindowPositionsBox.Save;
+    SaveLayout;
     // window minimizing
     MinimizeAllOnMinimizeMain:=MinimizeAllOnMinimizeMainCheckBox.Checked;
     HideIDEOnRun:=HideIDEOnRunCheckBox.Checked;
@@ -159,28 +145,137 @@ begin
   end;
 end;
 
-procedure TWindowOptionsFrame.WindowPositionsListBoxSelectionChange(
-  Sender: TObject; User: boolean);
+function TWindowOptionsFrame.GetPlacementRadioButtons(APlacement: TIDEWindowPlacement): TRadioButton;
+begin
+  case APlacement of
+    iwpRestoreWindowGeometry:   Result := RestoreWindowGeometryRadioButton;
+    iwpDefault:                 Result := DefaultRadioButton;
+    iwpCustomPosition:          Result := CustomPositionRadioButton;
+    iwpUseWindowManagerSetting: Result := UseWindowManagerSettingRadioButton;
+    iwpDocked:                  Result := DockedRadioButton;
+  else
+    Result := nil;
+  end;
+end;
+
+procedure TWindowOptionsFrame.SetLayout(const AValue: TIDEWindowLayout);
+var
+  APlacement: TIDEWindowPlacement;
+  RadioButton: TRadioButton;
+begin
+  FLayout := AValue;
+  if Layout=nil then Exit;
+
+  for APlacement := Low(TIDEWindowPlacement) to High(TIDEWindowPlacement) do
+  begin
+    RadioButton := GetPlacementRadioButtons(APlacement);
+    if APlacement in Layout.WindowPlacementsAllowed then
+    begin
+      RadioButton.Enabled := True;
+      RadioButton.Checked := (APlacement = Layout.WindowPlacement);
+
+      if APlacement = iwpCustomPosition then
+      begin
+        // custom window position
+        if Layout.CustomCoordinatesAreValid then
+        begin
+          LeftEdit.Value := Layout.Left;
+          TopEdit.Value := Layout.Top;
+          WidthEdit.Value := Layout.Width;
+          HeightEdit.Value := Layout.Height;
+        end
+        else
+        if Layout.Form <> nil then
+        begin
+          LeftEdit.Value := Layout.Form.Left;
+          TopEdit.Value := Layout.Form.Top;
+          WidthEdit.Value := Layout.Form.Width;
+          HeightEdit.Value := Layout.Form.Height;
+        end
+        else
+        begin
+          LeftEdit.Value := 0;
+          TopEdit.Value := 0;
+          WidthEdit.Value := 0;
+          HeightEdit.Value := 0;
+        end;
+      end;
+    end
+    else
+    if RadioButton <> nil then
+      RadioButton.Enabled := False;
+  end;
+
+  GetWindowPositionButton.Enabled :=
+    (iwpCustomPosition in Layout.WindowPlacementsAllowed) and
+    (Layout.Form <> nil);
+end;
+
+procedure TWindowOptionsFrame.WindowPositionsListBoxSelectionChange(Sender: TObject; User: boolean);
 begin
   if User then
     SetWindowPositionsItem(WindowPositionsListBox.ItemIndex);
 end;
 
+procedure TWindowOptionsFrame.ApplyButtonClick(Sender: TObject);
+begin
+  SaveLayout;
+  Layout.Apply;
+end;
+
+procedure TWindowOptionsFrame.GetWindowPositionButtonClick(Sender: TObject);
+begin
+  if Layout.Form <> nil then
+  begin
+    LeftEdit.Value := Layout.Form.Left;
+    TopEdit.Value := Layout.Form.Top;
+    WidthEdit.Value := Layout.Form.Width;
+    HeightEdit.Value := Layout.Form.Height;
+  end;
+end;
+
 procedure TWindowOptionsFrame.SetWindowPositionsItem(Index: integer);
 begin
-  if WindowPositionsBox.Layout <> nil then
-    WindowPositionsBox.Save;
+  SaveLayout;
   WindowPositionsListBox.ItemIndex := Index;
+
   if Index < Ord(High(TNonModalIDEWindow)) then
-    WindowPositionsBox.Layout := FLayouts.ItemByEnum(TNonModalIDEWindow(Index + 1))
+    Layout := FLayouts.ItemByEnum(TNonModalIDEWindow(Index + 1))
   else
   begin
     case Index - Ord(High(TNonModalIDEWindow)) of
-      0: WindowPositionsBox.Layout := FLayouts.ItemByFormID(DefaultObjectInspectorName);
+      0: Layout := FLayouts.ItemByFormID(DefaultObjectInspectorName);
     end;
   end;
+
   if Index >= 0 then
-    WindowPositionsBox.Caption := WindowPositionsListBox.Items[Index];
+    lblWindowCaption.Caption := WindowPositionsListBox.Items[Index];
+end;
+
+procedure TWindowOptionsFrame.SaveLayout;
+var
+  APlacement: TIDEWindowPlacement;
+  ARadioButton: TRadioButton;
+begin
+  if Layout = nil then
+   Exit;
+
+  for APlacement := Low(TIDEWindowPlacement) to High(TIDEWindowPlacement) do
+  begin
+    if APlacement in Layout.WindowPlacementsAllowed then
+    begin
+      ARadioButton := GetPlacementRadioButtons(APlacement);
+      if (ARadioButton <> nil) and ARadioButton.Enabled and ARadioButton.Checked then
+        Layout.WindowPlacement := APlacement;
+      if APlacement = iwpCustomPosition then
+      begin
+        Layout.Left := LeftEdit.Value;
+        Layout.Top := TopEdit.Value;
+        Layout.Width := WidthEdit.Value;
+        Layout.Height := HeightEdit.Value;
+      end;
+    end;
+  end;
 end;
 
 function TWindowOptionsFrame.GetCaptionFor(AWindow: TNonModalIDEWindow): String;
@@ -215,239 +310,6 @@ end;
 class function TWindowOptionsFrame.SupportedOptionsClass: TAbstractIDEOptionsClass;
 begin
   Result := TEnvironmentOptions;
-end;
-
-{ TIDEWindowSetupLayoutComponent }
-
-procedure TIDEWindowSetupLayoutComponent.BoundsChanged;
-begin
-  inherited BoundsChanged;
-  LoadFrom(fLayout);
-end;
-
-procedure TIDEWindowSetupLayoutComponent.LoadFrom(ALayout: TIDEWindowLayout);
-var
-  APlacement: TIDEWindowPlacement;
-  CurY: LongInt;
-  Col2X: Integer;
-  PreviousButton: TRadioButton;
-
-  procedure SetLabelAndEdit(var ALabel: TLabel;
-    var AnEdit: TEdit;  const ACaption: string; x, y: integer);
-  begin
-    if iwpCustomPosition in ALayout.WindowPlacementsAllowed then begin
-      if ALabel=nil then ALabel:=TLabel.Create(Self);
-      with ALabel do begin
-        Parent:=Self;
-        SetBounds(x,y,45,Height);
-        Caption:=ACaption;
-      end;
-      if AnEdit=nil then AnEdit:=TEdit.Create(Self);
-      with AnEdit do begin
-        Parent:=Self;
-        SetBounds(x+ALabel.Width+3,y,40,Height);
-        Text:='';
-      end;
-    end else begin
-      FreeAndNil(ALabel);
-      FreeAndNil(AnEdit);
-    end;
-  end;
-
-  function GetRadioBtnCaptions(aPos : TIDEWindowPlacement) : String;
-  begin
-    Result:='?';
-    Case aPos of
-      iwpUseWindowManagerSetting : Result:= rsiwpUseWindowManagerSetting;
-      iwpDefault                 : Result:= rsiwpDefault;
-      iwpRestoreWindowGeometry   : Result:= rsiwpRestoreWindowGeometry;
-      iwpDocked                  : Result:= rsiwpDocked;
-      iwpCustomPosition          : Result:= rsiwpCustomPosition;
-      iwpRestoreWindowSize       : Result:= rsiwpRestoreWindowSize;
-    end;
-  end;
-
-begin
-  if ALayout=nil then exit;
-  CurY:=5;
-  Col2X:=300;
-  PreviousButton:= nil;
-  for APlacement:=Low(TIDEWindowPlacement) to High(TIDEWindowPlacement) do
-  begin
-    if APlacement in ALayout.WindowPlacementsAllowed then
-    begin
-      if PlacementRadioButtons[APlacement]=nil then
-        PlacementRadioButtons[APlacement]:=TRadioButton.Create(Self);
-      with PlacementRadioButtons[APlacement] do
-      begin
-        Parent:=Self;
-        Left := 6;
-        if PreviousButton=nil then
-          Top := 6
-        else
-          AnchorToNeighbour(akTop,6,PreviousButton);
-        Caption:=GetRadioBtnCaptions(APlacement);
-        Checked:=(APlacement=ALayout.WindowPlacement);
-      end;
-      PreviousButton := PlacementRadioButtons[APlacement];
-
-      case APlacement of
-      iwpCustomPosition:
-        begin
-          // custom window position
-          SetLabelAndEdit(LeftLabel,LeftEdit,dlgLeftPos,Col2X,CurY);
-          SetLabelAndEdit(TopLabel,TopEdit,dlgTopPos,
-            LeftEdit.Left+LeftEdit.Width+15,CurY);
-          inc(CurY,LeftEdit.Height+6);
-          SetLabelAndEdit(WidthLabel,WidthEdit,dlgWidthPos,LeftLabel.Left,CurY);
-          SetLabelAndEdit(HeightLabel,HeightEdit,DlgHeightPos,
-            WidthEdit.Left+WidthEdit.Width+15,CurY);
-          inc(CurY,WidthEdit.Height+6);
-          if ALayout.CustomCoordinatesAreValid then begin
-            LeftEdit.Text:=IntToStr(ALayout.Left);
-            TopEdit.Text:=IntToStr(ALayout.Top);
-            WidthEdit.Text:=IntToStr(ALayout.Width);
-            HeightEdit.Text:=IntToStr(ALayout.Height);
-          end else if ALayout.Form<>nil then begin
-            LeftEdit.Text:=IntToStr(ALayout.Form.Left);
-            TopEdit.Text:=IntToStr(ALayout.Form.Top);
-            WidthEdit.Text:=IntToStr(ALayout.Form.Width);
-            HeightEdit.Text:=IntToStr(ALayout.Form.Height);
-          end;
-        end;
-      end;
-
-    end else begin
-      // window placement not allowed
-      if PlacementRadioButtons[APlacement]<>nil then
-      begin
-        PlacementRadioButtons[APlacement].Free;
-        PlacementRadioButtons[APlacement]:=nil;
-      end;
-    end;
-  end;
-
-  if ApplyButton=nil then
-    ApplyButton:=TButton.Create(Self);
-
-  with ApplyButton do
-  begin
-    Parent:=Self;
-    SetBounds(Col2X,CurY,Width,Height);
-    OnClick:=@ApplyButtonClick;
-    Caption:=dlgButApply;
-    AutoSize:=true;
-  end;
-
-  if iwpCustomPosition in ALayout.WindowPlacementsAllowed then
-  begin
-    if GetWindowPositionButton=nil then
-      GetWindowPositionButton:=TButton.Create(Self);
-    with GetWindowPositionButton do
-    begin
-      Parent:=Self;
-      OnClick:=@GetWindowPositionButtonClick;
-      Caption:=dlgGetPosition;
-      AutoSize:=true;
-      AnchorToNeighbour(akLeft,6,ApplyButton);
-      AnchorParallel(akTop,0,ApplyButton);
-      Enabled := ALayout.Form <> nil;
-    end;
-  end;
-  //inc(CurY,ApplyButton.Height+7);
-end;
-
-procedure TIDEWindowSetupLayoutComponent.SaveTo(AnLayout: TIDEWindowLayout);
-var
-  APlacement: TIDEWindowPlacement;
-  ARadioButton: TRadioButton;
-begin
-  if AnLayout=nil then exit;
-  if LeftEdit<>nil then
-    AnLayout.Left:=StrToIntDef(LeftEdit.Text,0);
-  if TopEdit<>nil then
-    AnLayout.Top:=StrToIntDef(TopEdit.Text,0);
-  if WidthEdit<>nil then
-    AnLayout.Width:=StrToIntDef(WidthEdit.Text,0);
-  if HeightEdit<>nil then
-    AnLayout.Height:=StrToIntDef(HeightEdit.Text,0);
-  for APlacement:=Low(TIDEWindowPlacement) to High(TIDEWindowPlacement) do
-  begin
-    ARadioButton:=GetPlacementRadioButtons(APlacement);
-    if (ARadioButton<>nil) and ARadioButton.Checked then
-      AnLayout.WindowPlacement:=APlacement;
-  end;
-end;
-
-constructor TIDEWindowSetupLayoutComponent.Create(TheOwner: TComponent);
-begin
-  inherited Create(TheOwner);
-  fUpdateRadioButtons:=false;
-end;
-
-function TIDEWindowSetupLayoutComponent.GetPlacementRadioButtons(
-  APlacement: TIDEWindowPlacement): TRadioButton;
-begin
-  case APlacement of
-  iwpRestoreWindowGeometry:   Result:=RestoreWindowGeometryRadioButton;
-  iwpDefault:                 Result:=DefaultRadioButton;
-  iwpCustomPosition:          Result:=CustomPositionRadioButton;
-  iwpUseWindowManagerSetting: Result:=UseWindowManagerSettingRadioButton;
-  iwpDocked:                  Result:=DockedRadioButton;
-  else
-    Result:=nil;
-  end;
-end;
-
-procedure TIDEWindowSetupLayoutComponent.SetPlacementRadioButtons(
-  APlacement: TIDEWindowPlacement; const AValue: TRadioButton);
-begin
-  case APlacement of
-  iwpRestoreWindowGeometry:   RestoreWindowGeometryRadioButton:=AValue;
-  iwpDefault:                 DefaultRadioButton:=AValue;
-  iwpCustomPosition:          CustomPositionRadioButton:=AValue;
-  iwpUseWindowManagerSetting: UseWindowManagerSettingRadioButton:=AValue;
-  iwpDocked:                  DockedRadioButton:=AValue;
-  end;
-end;
-
-procedure TIDEWindowSetupLayoutComponent.ApplyButtonClick(Sender: TObject);
-begin
-  Save;
-  if Assigned(OnApplyWindowPos) then OnApplyWindowPos(Layout);
-  Layout.Apply;
-end;
-
-procedure TIDEWindowSetupLayoutComponent.GetWindowPositionButtonClick(
-  Sender: TObject);
-begin
-  if Layout.Form<>nil then begin
-    if LeftEdit<>nil then
-      LeftEdit.Text:=IntToStr(Layout.Form.Left);
-    if TopEdit<>nil then
-      TopEdit.Text:=IntToStr(Layout.Form.Top);
-    if WidthEdit<>nil then
-      WidthEdit.Text:=IntToStr(Layout.Form.Width);
-    if HeightEdit<>nil then
-      HeightEdit.Text:=IntToStr(Layout.Form.Height);
-  end;
-end;
-
-function TIDEWindowSetupLayoutComponent.GetLayout: TIDEWindowLayout;
-begin
-  Result:=fLayout;
-end;
-
-procedure TIDEWindowSetupLayoutComponent.SetLayout(
-  const AValue: TIDEWindowLayout);
-begin
-  fLayout:=AValue;
-  LoadFrom(fLayout);
-end;
-
-procedure TIDEWindowSetupLayoutComponent.Save;
-begin
-  SaveTo(Layout);
 end;
 
 initialization
