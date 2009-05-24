9388764ddcab2e8ee0a3197c8d83ed02e178c12a
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-05-24 13:12:43 +0000

    TAChart: Use TUserDefinedChartSource in function demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@20173 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/func/main.pas
+++ components/tachart/demo/func/main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  TAGraph, TASeries;
+  TAGraph, TASeries, TASources;
 
 type
 
@@ -14,11 +14,16 @@ type
 
   TForm1 = class(TForm)
     Chart1: TChart;
+    Chart1BarSeries1: TBarSeries;
     Chart1FuncSeries1: TFuncSeries;
     Chart1YAxis: TLine;
     Chart1XAxis: TLine;
+    UserDefinedChartSource1: TUserDefinedChartSource;
     procedure Chart1FuncSeries1Calculate(const AX: Double; out AY: Double);
     procedure FormCreate(Sender: TObject);
+    procedure UserDefinedChartSource1GetChartDataItem(
+      ASource: TUserDefinedChartSource; AIndex: Integer;
+      var AItem: TChartDataItem);
   end;
 
 var
@@ -36,6 +41,15 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   Chart1FuncSeries1.OnCalculate := @Chart1FuncSeries1Calculate;
+  UserDefinedChartSource1.OnGetChartDataItem :=
+    @UserDefinedChartSource1GetChartDataItem;
+end;
+
+procedure TForm1.UserDefinedChartSource1GetChartDataItem(
+  ASource: TUserDefinedChartSource; AIndex: Integer; var AItem: TChartDataItem);
+begin
+  AItem.X := AIndex - ASource.PointsNumber / 2;
+  AItem.Y := Cos(AItem.X);
 end;
 
 initialization
