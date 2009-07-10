c4ac677050b96598fd3fd3b4c0db2c7becfeb2e1
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-07-10 19:49:49 +0000

    LazReport, fix inverted functionality for shadow width and curve in roundrect object, issue 14130
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@20820 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lazreport.pas
+++ components/lazreport/source/lazreport.pas
@@ -7,10 +7,10 @@ unit lazreport;
 interface
 
 uses
-  LR_Class, LR_Desgn, LR_Register, LR_Flds, LR_DBSet, LR_BarC, LR_BndEd,
+    LR_Class, LR_Desgn, LR_Register, LR_Flds, LR_DBSet, LR_BarC, LR_BndEd, 
   LR_PGrid, LR_View, lr_expres, lr_funct_editor_unit, lr_funct_editor_unit1, 
   LR_Prntr, LR_Edit, LR_Pars, LR_fmted, LR_Const, LR_pgopt, LR_Dopt, LR_GEdit, 
-  LR_Utils, LR_GrpEd, lr_propedit, LR_progr, LR_IFlds, SysUtilsAdds,
+  LR_Utils, LR_GrpEd, lr_propedit, LR_progr, LR_IFlds, SysUtilsAdds, LR_RRect, 
   LazarusPackageIntf;
 
 implementation
