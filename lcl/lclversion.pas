5f855b81d91eda5d17c731adb06038ad601cb402
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-08-20 09:53:39 +0000

    Version 0.9.29
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@21344 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -29,9 +29,9 @@ interface
 const
   lcl_major = 0;
   lcl_minor = 9;
-  lcl_release = 27;
+  lcl_release = 29;
   lcl_patch = 0;
-  lcl_version = '0.9.27';
+  lcl_version = '0.9.29';
 
 implementation
 
