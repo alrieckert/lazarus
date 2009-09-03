e4f1cd56651aff814011ac3a7e93994a67fb6ebf
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-09-03 14:43:48 +0000

    IDE: started build mode frame
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@21557 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ ide/frames/buildmodeseditor.pas
@@ -0,0 +1,42 @@
+{
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+}
+unit BuildModesEditor;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms; 
+
+type
+  TBuildModesEditorFrame = class(TFrame)
+  private
+  public
+  end;
+
+implementation
+
+initialization
+  {$I buildmodeseditor.lrs}
+
+end.
+
