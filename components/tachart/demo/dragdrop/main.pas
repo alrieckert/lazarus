f782be4249791fc8a6a4827e7f3b33a984b2669b
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-09-21 16:23:21 +0000

    TAChart: Update drag-and-drop demo to display point marks on mouse-over
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@21806 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/dragdrop/main.pas
+++ components/tachart/demo/dragdrop/main.pas
@@ -15,6 +15,8 @@ type
   TForm1 = class(TForm)
     Chart1: TChart;
     Chart1LineSeries1: TLineSeries;
+    procedure Chart1LineSeries1GetMark(out AFormattedMark: String;
+      AIndex: Integer);
     procedure Chart1MouseDown(
       Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
     procedure Chart1MouseMove(
@@ -24,6 +26,7 @@ type
     procedure FormCreate(Sender: TObject);
   private
     FDragIndex: Integer;
+    FNearestIndex: Integer;
   end;
 
 var
@@ -39,29 +42,46 @@ type
 
 { TForm1 }
 
+procedure TForm1.Chart1LineSeries1GetMark(
+  out AFormattedMark: String; AIndex: Integer);
+begin
+  if AIndex = FNearestIndex then
+    AFormattedMark := Chart1LineSeries1.DefaultFormattedMark(AIndex)
+  else
+    AFormattedMark := '';
+end;
+
 procedure TForm1.Chart1MouseDown(
   Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-var
-  pt, img: TPoint;
-  val: TDoublePoint;
 begin
   Unused(Button);
-  pt := Point(X, Y);
-  if
-    (Shift = [ssShift, ssLeft]) and
-      TLineSeriesAccess(Chart1LineSeries1).GetNearestPoint(
-        @PointDist, pt, FDragIndex, img, val) and
-    (PointDist(pt, img) <= Sqr(Chart1LineSeries1.Pointer.HorizSize))
-  then
-  else
-    FDragIndex := -1;
+  Unused(X, Y);
+  if Shift = [ssShift, ssLeft] then
+    FDragIndex := FNearestIndex;
 end;
 
 procedure TForm1.Chart1MouseMove(
   Sender: TObject; Shift: TShiftState; X, Y: Integer);
+var
+  newNearest: Integer;
+  pt, img: TPoint;
+  val: TDoublePoint;
 begin
   Unused(Shift);
-  Unused(X, Y);
+  pt := Point(X, Y);
+  if
+    TLineSeriesAccess(Chart1LineSeries1).
+      GetNearestPoint(@PointDist, pt, newNearest, img, val)
+  then begin
+    if PointDist(pt, img) <= Sqr(Chart1LineSeries1.Pointer.HorizSize) then begin
+      if newNearest <> FNearestIndex then begin
+        FNearestIndex := newNearest;
+        Chart1.Invalidate;
+      end;
+    end
+    else
+      FNearestIndex := -1;
+  end;
   if FDragIndex < 0 then exit;
   Chart1LineSeries1.SetXValue(FDragIndex, Chart1.XImageToGraph(X));
   Chart1LineSeries1.SetYValue(FDragIndex, Chart1.YImageToGraph(Y));
@@ -83,6 +103,7 @@ begin
   for i := 1 to 10 do
     Chart1LineSeries1.AddXY(i, Random(20) - 10);
   FDragIndex := -1;
+  FNearestIndex := -1;
 end;
 
 initialization
