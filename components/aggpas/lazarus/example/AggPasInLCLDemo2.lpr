ef53f27f225b5f0ce9b9512c602236b862bce14c
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-10-12 15:12:28 +0000

    aggpas: added example for comparing LCL/aggpas comparison
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@22117 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/aggpas/lazarus/example/AggPasInLCLDemo2.lpr
@@ -0,0 +1,18 @@
+program AggPasInLCLDemo1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Unit2, AggPasLCL
+  { you can add units after this };
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ components/aggpas/lazarus/example/unit2.pas
@@ -0,0 +1,84 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LCLProc, FileUtil, LResources, Forms, Controls, Graphics,
+  Dialogs, FPimage, agg_fpimage, Agg_LCL;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
+  private
+  public
+    AggLCLCanvas: TAggLCLCanvas;
+    Bitmap1: TBitmap;
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Bitmap1:=TBitmap.Create;
+  AggLCLCanvas:=TAggLCLCanvas.Create;
+  with AggLCLCanvas do begin
+    Image.PixelFormat:=afpimRGBA32;
+    Image.SetSize(250,250);
+  end;
+
+  // paint to agg canvas
+  with AggLCLCanvas do begin
+    // solid white background
+    Brush.Color:=clWhite;
+    FillRect(0,0,Width,Height);
+
+    Brush.Color:=clRed;
+    Pen.Color:=clBlue;
+    Pen.Width:=1;
+
+    Line(10,10,20,10);
+    Line(10,12,10,22);
+  end;
+
+  // convert to LCL native pixel format
+  Bitmap1.LoadFromIntfImage(AggLCLCanvas.Image.IntfImg);
+
+  // paint with widgetset to bitmap
+  with Bitmap1.Canvas do begin
+    Brush.Color:=clBlue;
+    Pen.Color:=clRed;
+    Pen.Width:=1;
+
+    Line(22,10,32,10);
+    Line(10,24,10,34);
+  end;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  AggLCLCanvas.Free;
+  Bitmap1.Free;
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+begin
+  Canvas.Draw(0,0,Bitmap1);
+end;
+
+initialization
+  {$I unit2.lrs}
+
+end.
+
