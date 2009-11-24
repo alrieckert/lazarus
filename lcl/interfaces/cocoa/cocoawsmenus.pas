87f4ac933141d9ac43d41afd029e9c990c5e1553
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-11-24 08:46:38 +0000

    cocoa: fix copyright header
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@22738 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/cocoa/cocoawsmenus.pas
+++ lcl/interfaces/cocoa/cocoawsmenus.pas
@@ -1,7 +1,7 @@
 { $Id: Cocoawsmenus.pp 15309 2008-06-04 22:12:59Z vincents $}
 {
  *****************************************************************************
- *                               CocoaWSMenus.pp                                *
+ *                               CocoaWSMenus.pp                             *
  *                               ------------                                * 
  *                                                                           *
  *                                                                           *
@@ -11,7 +11,7 @@
  *                                                                           *
  *  This file is part of the Lazarus Component Library (LCL)                 *
  *                                                                           *
- *  See the file COPYING.modifiedLGPL.txt, included in this distribution,        *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
  *  for details about the copyright.                                         *
  *                                                                           *
  *  This program is distributed in the hope that it will be useful,          *
