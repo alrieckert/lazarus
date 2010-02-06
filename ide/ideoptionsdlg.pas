85cecc3f8220b81cb3c6c2e3adcef36fd1a7bc5d
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-02-06 09:50:00 +0000

    ide: fix similar InheritsFrom issue which cause duplicate error after exception adding to the ignore list
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@23653 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/ideoptionsdlg.pas
+++ ide/ideoptionsdlg.pas
@@ -220,7 +220,7 @@ var
       if Node.Data <> nil then
         with TAbstractIDEOptionsEditor(Node.Data) do
           if ((ClassTypeForCompare = nil) and (SupportedOptionsClass = nil)) or
-             ClassTypeForCompare.InheritsFrom(SupportedOptionsClass) then
+             ((SupportedOptionsClass <> nil) and ClassTypeForCompare.InheritsFrom(SupportedOptionsClass)) then
             WriteSettings(AOptions);
       Traverse(Node.GetFirstChild);
       Traverse(Node.GetNextSibling);
