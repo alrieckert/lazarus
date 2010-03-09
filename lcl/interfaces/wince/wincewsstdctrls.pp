87cc92571c665f910af4ea57b92ed7dfcd8bc38b
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-03-09 21:52:33 +0000

    Fixes bug 15933 in WinCE TMemo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@23905 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/wince/wincewsstdctrls.pp
+++ lcl/interfaces/wince/wincewsstdctrls.pp
@@ -34,7 +34,7 @@ uses
   {$ifdef Win32}win32compat,{$endif}
   // RTL, FCL, LCL
   SysUtils, LCLType, Classes, StdCtrls, Controls, Graphics, Forms, WinCEProc,
-  InterfaceBase, LMessages, LCLMessageGlue,
+  InterfaceBase, LMessages, LCLMessageGlue, LCLProc,
   // Widgetset
   WSControls, WSStdCtrls, WSLCLClasses, WinCEInt, WinCEWSControls, WinCEExtra,
   WSProc;
