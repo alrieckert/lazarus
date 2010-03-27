da840dc502d392a198a6aa606275a9f366713b23
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-03-27 08:57:38 +0000

    Replacement property and type editor.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@24237 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ converter/replacenamesunit.pas
@@ -0,0 +1,136 @@
+unit ReplaceNamesUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  Grids, Buttons, ExtCtrls, CodeToolsStructs, LazarusIDEStrConsts;
+
+type
+
+  { TGridUpdater }
+
+  TGridUpdater = class
+  private
+    fGrid: TStringGrid;
+    fReplaceMap: TStringToStringTree;
+    fSeenName: TStringList;
+    i: Integer;
+  public
+    constructor Create(AGrid: TStringGrid; AReplaceMap: TStringToStringTree);
+    destructor Destroy; override;
+    procedure AddUnique(AOldIdent: string);
+  end;
+
+  { TReplaceNamesForm }
+
+  TReplaceNamesForm = class(TForm)
+    btnCancel: TBitBtn;
+    btnOK: TBitBtn;
+    BtnPanel: TPanel;
+    HelpButton: TBitBtn;
+    NamePairGrid: TStringGrid;
+    procedure btnOKClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end; 
+
+var
+  ReplaceNamesForm: TReplaceNamesForm;
+
+procedure CopyFromMapToGrid(AGrid: TStringGrid; AMap: TStringToStringTree);
+procedure CopyFromGridToMap(AGrid: TStringGrid; AMap: TStringToStringTree);
+
+
+implementation
+
+{$R *.lfm}
+
+procedure CopyFromMapToGrid(AGrid: TStringGrid; AMap: TStringToStringTree);
+var
+  OldIdent, NewIdent: string;
+  List: TStringList;
+  i: Integer;
+begin
+  // Collect (maybe edited) properties from StringGrid to NameReplacements.
+  List:=TStringList.Create;
+  try
+    AMap.GetNames(List);
+    for i:=0 to List.Count-1 do begin
+      OldIdent:=List[i];
+      NewIdent:=AMap[OldIdent];
+      if AGrid.RowCount<i+1 then
+        AGrid.RowCount:=i+1;
+      AGrid.Cells[0,i]:=OldIdent;
+      AGrid.Cells[1,i]:=NewIdent;
+    end;
+  finally
+    List.Free;
+  end;
+end;
+
+procedure CopyFromGridToMap(AGrid: TStringGrid; AMap: TStringToStringTree);
+var
+  OldIdent, NewIdent: string;
+  i: Integer;
+begin
+  // Collect (maybe edited) properties from StringGrid to NameReplacements.
+  for i:=1 to AGrid.RowCount-1 do begin // Skip the fixed row.
+    OldIdent:=AGrid.Cells[0,i];
+    NewIdent:=AGrid.Cells[1,i];
+    if NewIdent<>'' then
+      AMap[OldIdent]:=NewIdent;
+  end;
+end;
+
+{ TGridUpdater }
+
+constructor TGridUpdater.Create(AGrid: TStringGrid; AReplaceMap: TStringToStringTree);
+begin
+  fGrid:=AGrid;
+  fReplaceMap:=AReplaceMap;
+  i:=1;
+  fSeenName:=TStringList.Create;
+end;
+
+destructor TGridUpdater.Destroy;
+begin
+  fSeenName.Free;
+  inherited Destroy;
+end;
+
+procedure TGridUpdater.AddUnique(AOldIdent: string);
+var
+  NewIdent: string;
+begin
+  // Add only one instance of each property name.
+  if fSeenName.IndexOf(AOldIdent)<0 then begin
+    fSeenName.Append(AOldIdent);
+    NewIdent:=fReplaceMap[AOldIdent];
+    if fGrid.RowCount<i+1 then
+      fGrid.RowCount:=i+1;
+    fGrid.Cells[0,i]:=AOldIdent;
+    fGrid.Cells[1,i]:=NewIdent;
+    Inc(i);
+  end;
+end;
+
+{ TReplaceNamesForm }
+
+procedure TReplaceNamesForm.FormCreate(Sender: TObject);
+begin
+  Caption:=lisReplacementPropTypes;
+end;
+
+procedure TReplaceNamesForm.btnOKClick(Sender: TObject);
+begin
+  ModalResult:=mrOK;
+end;
+
+end.
+
