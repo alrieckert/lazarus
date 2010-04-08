b831d5cf2d1c2b6acabe9fe463d5039e38e6948f
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-04-08 13:54:58 +0000

    TAChart: Add "Logarithm transform" page to axisdemo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@24512 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/axis/main.pas
+++ components/tachart/demo/axis/main.pas
@@ -5,17 +5,20 @@ unit Main;
 interface
 
 uses
-  ComCtrls, Forms, TAGraph, TASeries, TASources, TATransformations;
+  ComCtrls, ExtCtrls, Forms, StdCtrls, TAGraph, TASeries, TASources,
+  TATransformations;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Chart1: TChart;
-    Chart1LineSeries1: TLineSeries;
+    ChartLog: TChart;
+    cfsLog: TFuncSeries;
+    cbLog: TCheckBox;
+    clsLogPoints: TLineSeries;
     ChartT: TChart;
-    ChartAxisTransformations1: TChartAxisTransformations;
+    catLog: TChartAxisTransformations;
     ChartAxisTransformations1LinearAxisTransform2: TLinearAxisTransform;
     ChartAxisTransformations1LogarithmAxisTransform1: TLogarithmAxisTransform;
     catT: TChartAxisTransformations;
@@ -24,14 +27,17 @@ type
     ChartCustomMarksBarSeries1: TBarSeries;
     ChartTSummer: TLineSeries;
     ChartTWinter: TLineSeries;
-    ListChartSource1: TListChartSource;
+    lcsMarks: TListChartSource;
     PageControl1: TPageControl;
-    RandomChartSource1: TRandomChartSource;
+    pnlLogControls: TPanel;
     rcsTSummer: TRandomChartSource;
     rcsTWinter: TRandomChartSource;
     lsLinear: TTabSheet;
-    tsMultiAxis: TTabSheet;
+    tsLog: TTabSheet;
     tsCustomMarks: TTabSheet;
+    procedure cbLogChange(Sender: TObject);
+    procedure ChartLogFuncSeries1Calculate(const AX: Double; out AY: Double);
+    procedure FormCreate(Sender: TObject);
     procedure TChartAxisList1MarkToText(var AText: String; AMark: Double);
   end;
 
@@ -40,9 +46,39 @@ var
 
 implementation
 
+uses
+  Math;
+
 {$R *.lfm}
 
+function MyFunc(AX: Double): Double;
+begin
+  Result := Power(10, AX) + 3;
+end;
+
 { TForm1 }
+
+procedure TForm1.cbLogChange(Sender: TObject);
+begin
+  ChartAxisTransformations1LogarithmAxisTransform1.Enabled := cbLog.Checked;
+end;
+
+procedure TForm1.ChartLogFuncSeries1Calculate(const AX: Double; out AY: Double);
+begin
+  AY := MyFunc(AX);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  i: Integer;
+  x: Double;
+begin
+  for i := 0 to 50 do begin
+    with cfsLog.Extent do
+      x := i / 50 * (XMax - XMin) + XMin;
+    clsLogPoints.AddXY(x + Random - 0.5, MyFunc(x) + Random - 0.5);
+  end;
+end;
 
 procedure TForm1.TChartAxisList1MarkToText(var AText: String; AMark: Double);
 begin
