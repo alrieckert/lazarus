ab855ac67a70857180f06c8ba185eee2a1c56a81
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-04-20 12:02:42 +0000

    LCL: added TConfigMemStorage: config storage for memory
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@24734 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lazconfigstorage.pas
+++ lcl/lazconfigstorage.pas
@@ -22,7 +22,7 @@ unit LazConfigStorage;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, AvgLvlTree;
   
 type
   { TConfigStorage }
@@ -76,8 +76,115 @@ type
   end;
   
   TConfigStorageClass = class of TConfigStorage;
-  
+
+
+  { TConfigMemStorageNode }
+
+  TConfigMemStorageNode = class
+  public
+    Name: string;
+    Value: string;
+    Parent: TConfigMemStorageNode;
+    Childs: TAvgLvlTree; // tree of TConfigMemStorageNode
+    procedure ClearChilds;
+    constructor Create(AParent: TConfigMemStorageNode; const AName: string);
+    destructor Destroy; override;
+  end;
+
+  TConfigMemStorageModification = (cmsmSet, cmsmGet, cmsmDelete, cmsmDeleteValue);
+
+  { TConfigMemStorage }
+
+  TConfigMemStorage = class(TConfigStorage)
+  private
+    procedure Modify(const APath: string; Mode: TConfigMemStorageModification;
+                     var AValue: string);
+  protected
+    procedure DeleteFullPath(const APath: string); override;
+    procedure DeleteFullPathValue(const APath: string); override;
+    function GetFullPathValue(const APath, ADefault: String): String; override;
+    function GetFullPathValue(const APath: String; ADefault: Boolean): Boolean;
+      override;
+    function GetFullPathValue(const APath: String; ADefault: Integer): Integer;
+      override;
+    procedure SetDeleteFullPathValue(const APath, AValue, DefValue: String);
+      override;
+    procedure SetDeleteFullPathValue(const APath: String; AValue, DefValue:
+      Boolean); override;
+    procedure SetDeleteFullPathValue(const APath: String; AValue, DefValue:
+      Integer); override;
+    procedure SetFullPathValue(const APath, AValue: String); override;
+    procedure SetFullPathValue(const APath: String; AValue: Boolean); override;
+    procedure SetFullPathValue(const APath: String; AValue: Integer); override;
+  public
+    Root: TConfigMemStorageNode;
+    function GetFilename: string; override;
+    procedure WriteToDisk; override;
+    destructor Destroy; override;
+  end;
+
+function CompareConfigMemStorageNames(p1, p2: PChar): integer;
+function CompareConfigMemStorageNodes(Node1, Node2: Pointer): integer;
+function ComparePCharWithConfigMemStorageNode(aPChar, ANode: Pointer): integer;
+
 implementation
+
+function CompareConfigMemStorageNames(p1, p2: PChar): integer;
+// compare strings till / or #0
+begin
+  if (p1=nil) then begin
+    if (p2=nil) or (p2^ in ['/',#0]) then begin
+      // both empty
+      Result:=0;
+    end else begin
+      // p1 shorter
+      Result:=-1;
+    end;
+  end else begin
+    if p2=nil then begin
+      // p2 shorter
+      Result:=1;
+    end else begin
+      repeat
+        if p1^ in ['/',#0] then begin
+          if p2^ in ['/',#0] then begin
+            // same
+            exit(0);
+          end else begin
+            // p1 shorter
+            exit(-1);
+          end;
+        end else begin
+          if p2^ in ['/',#0] then begin
+            // p2 shorter
+            exit(1);
+          end else if p1^=p2^ then begin
+            // continue
+          end else begin
+            // differ
+            exit(ord(p1^)-ord(p2^));
+          end;
+        end;
+        inc(p1);
+        inc(p2);
+      until false;
+    end;
+  end;
+end;
+
+function CompareConfigMemStorageNodes(Node1, Node2: Pointer): integer;
+var
+  CfgNode1: TConfigMemStorageNode absolute Node1;
+  CfgNode2: TConfigMemStorageNode absolute Node2;
+begin
+  Result:=CompareConfigMemStorageNames(PChar(CfgNode1.Name),PChar(CfgNode2.Name));
+end;
+
+function ComparePCharWithConfigMemStorageNode(aPChar, ANode: Pointer): integer;
+begin
+  Result:=CompareConfigMemStorageNames(PChar(aPChar),
+                                      PChar(TConfigMemStorageNode(ANode).Name));
+end;
 
 { TConfigStorage }
 
@@ -245,6 +352,219 @@ begin
     raise Exception.Create('TConfigStorage.UndoAppendBasePath');
   FCurrentBasePath:=FPathStack[FPathStack.Count-1];
   FPathStack.Delete(FPathStack.Count-1);
+end;
+
+{ TConfigMemStorage }
+
+procedure TConfigMemStorage.Modify(const APath: string;
+  Mode: TConfigMemStorageModification; var AValue: string);
+var
+  Node: TConfigMemStorageNode;
+  p: PChar;
+  StartPos: PChar;
+  ChildNode: TAvgLvlTreeNode;
+  Child: TConfigMemStorageNode;
+  NewName: string;
+begin
+  p:=PChar(APath);
+  if p=nil then begin
+    if Root<>nil then begin
+      if Mode in [cmsmDelete,cmsmDeleteValue] then
+        Root.Value:='';
+      if Mode=cmsmDelete then
+        Root.ClearChilds;
+    end;
+  end else begin
+    if Root=nil then begin
+      if Mode in [cmsmDelete,cmsmDeleteValue] then exit;
+      Root:=TConfigMemStorageNode.Create(nil,'');
+    end;
+    Node:=Root;
+    repeat
+      StartPos:=p;
+      while (not (p^ in ['/',#0])) do inc(p);
+      if p^='/' then begin
+        // child node
+        if Node.Childs=nil then begin
+          if Mode in [cmsmDelete,cmsmDeleteValue] then exit;
+          Node.Childs:=TAvgLvlTree.Create(@CompareConfigMemStorageNodes);
+        end;
+        ChildNode:=Node.Childs.FindKey(StartPos,@ComparePCharWithConfigMemStorageNode);
+        if ChildNode=nil then begin
+          if Mode in [cmsmDelete,cmsmDeleteValue] then exit;
+          SetLength(NewName,p-StartPos);
+          if NewName<>'' then
+            System.Move(StartPos^,NewName[1],p-StartPos);
+          Child:=TConfigMemStorageNode.Create(Node,NewName);
+          Node.Childs.Add(Child);
+        end else
+          Child:=TConfigMemStorageNode(ChildNode.Data);
+        // next level
+        Node:=Child;
+        inc(p);
+      end else begin
+        // end of path
+        case Mode of
+        cmsmSet: Node.Value:=AValue;
+        cmsmGet: AValue:=Node.Value;
+        cmsmDelete,cmsmDeleteValue:
+          begin
+            Node.Value:='';
+            while (Node.Childs=nil) or (Node.Childs.Count=0) do begin
+              Child:=Node;
+              Node:=Node.Parent;
+              Child.Free;
+            end;
+          end;
+        end;
+        exit;
+      end;
+    until false;
+  end;
+end;
+
+procedure TConfigMemStorage.DeleteFullPath(const APath: string);
+var
+  V: string;
+begin
+  V:='';
+  Modify(APath,cmsmDelete,V);
+end;
+
+procedure TConfigMemStorage.DeleteFullPathValue(const APath: string);
+var
+  V: string;
+begin
+  V:='';
+  Modify(APath,cmsmDeleteValue,V);
+end;
+
+function TConfigMemStorage.GetFullPathValue(const APath, ADefault: String
+  ): String;
+begin
+  Result:=ADefault;
+  Modify(APath,cmsmGet,Result);
+end;
+
+function TConfigMemStorage.GetFullPathValue(const APath: String; ADefault:
+  Boolean): Boolean;
+var
+  s: string;
+begin
+  if ADefault then
+    s := 'True'
+  else
+    s := 'False';
+
+  s := GetFullPathValue(APath, s);
+
+  if CompareText(s,'TRUE')=0 then
+    Result := True
+  else if CompareText(s,'FALSE')=0 then
+    Result := False
+  else
+    Result := ADefault;
+end;
+
+function TConfigMemStorage.GetFullPathValue(const APath: String; ADefault:
+  Integer): Integer;
+begin
+  Result := StrToIntDef(GetFullPathValue(APath, IntToStr(ADefault)),ADefault);
+end;
+
+procedure TConfigMemStorage.SetDeleteFullPathValue(const APath, AValue, DefValue
+  : String);
+begin
+  if AValue=DefValue then
+    DeleteFullPathValue(APath)
+  else
+    SetFullPathValue(APath,AValue);
+end;
+
+procedure TConfigMemStorage.SetDeleteFullPathValue(const APath: String; AValue,
+  DefValue: Boolean);
+begin
+  if AValue=DefValue then
+    DeleteFullPath(APath)
+  else
+    SetFullPathValue(APath,AValue);
+end;
+
+procedure TConfigMemStorage.SetDeleteFullPathValue(const APath: String; AValue,
+  DefValue: Integer);
+begin
+  if AValue=DefValue then
+    DeleteFullPath(APath)
+  else
+    SetFullPathValue(APath,AValue);
+end;
+
+procedure TConfigMemStorage.SetFullPathValue(const APath, AValue: String);
+var
+  V: String;
+begin
+  V:=AValue;
+  Modify(APath,cmsmSet,V);
+end;
+
+procedure TConfigMemStorage.SetFullPathValue(const APath: String; AValue:
+  Boolean);
+begin
+  if AValue then
+    SetFullPathValue(APath, 'True')
+  else
+    SetFullPathValue(APath, 'False');
+end;
+
+procedure TConfigMemStorage.SetFullPathValue(const APath: String; AValue:
+  Integer);
+begin
+  SetFullPathValue(APath,IntToStr(AValue));
+end;
+
+function TConfigMemStorage.GetFilename: string;
+begin
+  Result:='';
+end;
+
+procedure TConfigMemStorage.WriteToDisk;
+begin
+  raise Exception.Create('TConfigMemStorage.WriteToDisk invalid operation');
+end;
+
+destructor TConfigMemStorage.Destroy;
+begin
+  FreeAndNil(Root);
+  inherited Destroy;
+end;
+
+{ TConfigMemStorageNode }
+
+procedure TConfigMemStorageNode.ClearChilds;
+var
+  OldChilds: TAvgLvlTree;
+begin
+  if Childs<>nil then begin
+    OldChilds:=Childs;
+    Childs:=nil;
+    OldChilds.FreeAndClear;
+    FreeAndNil(OldChilds);
+  end;
+end;
+
+constructor TConfigMemStorageNode.Create(AParent: TConfigMemStorageNode;
+  const AName: string);
+begin
+  Parent:=AParent;
+  Name:=AName;
+end;
+
+destructor TConfigMemStorageNode.Destroy;
+begin
+  ClearChilds;
+  if (Parent<>nil) and (Parent.Childs<>nil) then
+    Parent.Childs.Remove(Self);
+  inherited Destroy;
 end;
 
 end.
