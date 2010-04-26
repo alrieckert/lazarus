eec25bbe1fd7d4c85bcc09544e846c2484d42e77
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-04-26 08:52:17 +0000

    Fixes wince compilation for fpc 2.4.x
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@24958 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/alllclunits.pp
+++ lcl/alllclunits.pp
@@ -22,7 +22,7 @@ unit AllLCLUnits;
 
 { At least 2.4.0 is required, except for wince which supports fpc 2.2.4 too }
 {$ifdef Wince}
-  {$if defined(ver1) or (defined(ver2) and (fpc_patch<4))}
+  {$if defined(ver1) or (defined(ver2) and (fpc_release<2)) or (defined(ver2_2) and (fpc_patch<4))}
     {$fatal Lazarus for WinCE requires at least FPC 2.2.4}
   {$endif}
 {$else}
