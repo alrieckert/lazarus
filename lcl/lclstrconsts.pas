3dbf70fa85b8846eafd9c08ba9daf085ca425aa7
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-05-22 15:05:08 +0000

    Qt: implemented TQtWidgetSet.GetCmdLineParamDescForInterface. Fixes #16536
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25582 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lclstrconsts.pas
+++ lcl/lclstrconsts.pas
@@ -146,7 +146,68 @@ resourceString
     +'class of a program is the program name with the initial character '
     +'capitalized. For example, the classname for gimp is "Gimp". If --class '
     +'is specified, the class of the program will be set to "classname".';
-     
+
+  // qt interface
+  rsqtOptionNoGrab = '-nograb, tells Qt that it must never grab '
+    +'the mouse or the keyboard.Need QT_DEBUG.';
+  rsqtOptionDoGrab = '-dograb (only under X11), running under a debugger can '
+    +'cause an implicit -nograb, use -dograb to override.Need QT_DEBUG.';
+  rsqtOptionSync = '-sync (only under X11), switches to synchronous mode '
+    +'for debugging.';
+  rsqtOptionStyle = '-style style or -style=style, sets the application GUI '
+    +'style.Possible values are motif, windows, and platinum. If you compiled '
+    +'Qt with additional styles or have additional styles as plugins these '
+    +'will be available to the -style  command line option. NOTE: Not all '
+    +'styles are available on all platforms. If style param does not exist '
+    +'Qt will start an application with default common style (windows).';
+  rsqtOptionStyleSheet = '-stylesheet stylesheet or -stylesheet=stylesheet, '
+    +'sets the application styleSheet. '
+    +'The value must be a path to a file that contains the Style Sheet. '
+    +'Note: Relative URLs in the Style Sheet file are relative '
+    +'to the Style Sheet file''s path.';
+  rsqtOptionGraphicsStyle = '-graphicssystem param, sets the backend to be '
+   +'used for on-screen widgets and QPixmaps.'
+   +'Available options are native,raster and opengl.OpenGL is still unstable.';
+  rsqtOptionSession = '-session session, restores the application from an '
+    +'earlier session.';
+  rsqtOptionWidgetCount = '-widgetcount, prints debug message at the end about '
+    +'number of widgets left undestroyed and maximum number of widgets existed '
+    +'at the same time.';
+  rsqtOptionReverse = '-reverse, sets the application''s layout direction '
+    +'to Qt::RightToLeft';
+  // qt X11 options
+  rsqtOptionX11Display = '-display display, sets the X display '
+    +'(default is $DISPLAY).';
+  rsqtOptionX11Geometry = '-geometry geometry, sets the client geometry of '
+    +'the first window that is shown.';
+  rsqtOptionX11Font = '-fn or -font font, defines the application font. The '
+    +'font should be specified using an X logical font description.';
+  rsqtOptionX11BgColor = '-bg or -background color, sets the default '
+    +'background color and an application palette (light and dark '
+    +'shades are calculated).';
+  rsqtOptionX11FgColor = '-fg or -foreground color, sets the default '
+    +'foreground color.';
+  rsqtOptionX11BtnColor = '-btn or -button color, sets the default button '
+    +'color.';
+  rsqtOptionX11Name = '-name name, sets the application name.';
+  rsqtOptionX11Title = '-title title, sets the application title.';
+  rsqtOptionX11Visual = '-visual TrueColor, forces the application to use a '
+    +'TrueColor visual on an 8-bit display.';
+  rsqtOptionX11NCols = '-ncols count, limits the number of colors allocated '
+    +'in the color cube on an 8-bit display, if the application is using the '
+    +'QApplication::ManyColor color specification. If count is 216 then a '
+    +'6x6x6 color cube is used (i.e. 6 levels of red, 6 of green, and 6 of '
+    +'blue); for other values, a cube approximately proportional to a 2x3x1 '
+    +'cube is used.';
+  rsqtOptionX11CMap = '-cmap, causes the application to install a private '
+    +'color map on an 8-bit display.';
+  rsqtOptionX11IM = '-im, sets the input method server (equivalent to setting '
+    +'the XMODIFIERS environment variable)';
+  rsqtOptionX11InputStyle = '-inputstyle, defines how the input is inserted '
+    +'into the given widget, e.g., onTheSpot makes the input appear directly '
+    +'in the widget, while overTheSpot makes the input appear in a box '
+    +'floating over the widget and is not inserted until the editing is done.';
+
   // win32 interface
   rsWin32Warning = 'Warning:';
   rsWin32Error = 'Error:';
