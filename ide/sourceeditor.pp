96dbf718904dc49f4b1c527f747b0c69900a340e
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-05-26 23:08:22 +0000

    SourceEditor: Instruct SourceLog to write directly to SynEdit (skip stringlist)
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25677 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -2025,7 +2025,7 @@ begin
         if assigned(SharedEditors[i].FEditPlugin) then
           SharedEditors[i].FEditPlugin.Enabled := False;
       SynEditor.BeginUpdate;
-      FCodeBuffer.AssignTo(SynEditor.Lines,true);
+      FCodeBuffer.AssignTo(SynEditor.Lines, False);
       FEditorStampCommitedToCodetools:=(SynEditor.Lines as TSynEditLines).TextChangeStamp;
       SynEditor.EndUpdate;
       for i := 0 to FSharedEditorList.Count - 1 do
