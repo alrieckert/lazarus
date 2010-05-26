a91a29d86b72410bdf9fe31d62cfb9ae365e7995
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-05-26 08:24:40 +0000

    IDE: reduce some overhead
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25657 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -6574,8 +6574,8 @@ Begin
   {$IFDEF IDE_DEBUG}
   writeln('[TSourceNotebook.NewFile] A ');
   {$ENDIF}
-  //DisableAutoSizing{$IFDEF DebugDisableAutoSizing}('TSourceNotebook.NewFile'){$ENDIF};
-  //try
+  DisableAutoSizing{$IFDEF DebugDisableAutoSizing}('TSourceNotebook.NewFile'){$ENDIF};
+  try
     IDEWindowCreators.ShowForm(Self,false);
     Result := NewSE(-1, -1, AShareEditor);
     {$IFDEF IDE_DEBUG}
@@ -6589,9 +6589,9 @@ Begin
     UpdatePageNames;
     UpdateProjectFiles;
     UpdateStatusBar;
-  //finally
-  //  EnableAutoSizing{$IFDEF DebugDisableAutoSizing}('TSourceNotebook.NewFile'){$ENDIF};
-  //end;
+  finally
+    EnableAutoSizing{$IFDEF DebugDisableAutoSizing}('TSourceNotebook.NewFile'){$ENDIF};
+  end;
   if FocusIt then FocusEditor;
   {$IFDEF IDE_DEBUG}
   writeln('[TSourceNotebook.NewFile] end');
