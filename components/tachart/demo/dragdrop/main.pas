ec0597ffe30293f6edb717bec68ca37a66c4de03
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-05-30 08:15:48 +0000

    TAChart: Use TDataPointDragTool in dragdrop demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25762 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/dragdrop/main.pas
+++ components/tachart/demo/dragdrop/main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, ExtCtrls, SysUtils, FileUtil, LResources, Forms, Controls, Graphics,
-  Dialogs, TAGraph, TASeries;
+  Dialogs, TAGraph, TASeries, TATools;
 
 type
 
@@ -15,14 +15,10 @@ type
   TForm1 = class(TForm)
     Chart1: TChart;
     Chart1LineSeries1: TLineSeries;
+    ChartToolset1: TChartToolset;
+    ChartToolset1DataPointDragTool1: TDataPointDragTool;
     procedure Chart1LineSeries1GetMark(out AFormattedMark: String;
       AIndex: Integer);
-    procedure Chart1MouseDown(
-      Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-    procedure Chart1MouseMove(
-      Sender: TObject; Shift: TShiftState; X, Y: Integer);
-    procedure Chart1MouseUp(
-      Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
     procedure FormCreate(Sender: TObject);
   private
     FDragIndex: Integer;
@@ -39,59 +35,16 @@ implementation
 uses
   TAChartUtils;
 
-type
-  TLineSeriesAccess = class(TLineSeries);
-
 { TForm1 }
 
 procedure TForm1.Chart1LineSeries1GetMark(
   out AFormattedMark: String; AIndex: Integer);
 begin
-  if AIndex = FNearestIndex then
-    AFormattedMark := Chart1LineSeries1.FormattedMark(AIndex)
+  if AIndex = ChartToolset1DataPointDragTool1.PointIndex then
+    with Chart1LineSeries1 do
+      AFormattedMark := Source.FormatItem(Marks.Format, AIndex)
   else
     AFormattedMark := '';
-end;
-
-procedure TForm1.Chart1MouseDown(
-  Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  Unused(Button);
-  Unused(X, Y);
-  if Shift = [ssShift, ssLeft] then
-    FDragIndex := FNearestIndex;
-end;
-
-procedure TForm1.Chart1MouseMove(
-  Sender: TObject; Shift: TShiftState; X, Y: Integer);
-var
-  newNearest: Integer;
-  pt, img: TPoint;
-  val: TDoublePoint;
-begin
-  Unused(Shift);
-  pt := Point(X, Y);
-  if
-    not TLineSeriesAccess(Chart1LineSeries1).
-      GetNearestPoint(@PointDist, pt, newNearest, img, val) or
-    (PointDist(pt, img) > Sqr(Chart1LineSeries1.Pointer.HorizSize))
-  then
-    newNearest := -1;
-  if newNearest <> FNearestIndex then begin
-    FNearestIndex := newNearest;
-    Chart1.Invalidate;
-  end;
-  if FDragIndex < 0 then exit;
-  Chart1LineSeries1.SetXValue(FDragIndex, Chart1.XImageToGraph(X));
-  Chart1LineSeries1.SetYValue(FDragIndex, Chart1.YImageToGraph(Y));
-end;
-
-procedure TForm1.Chart1MouseUp(
-  Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  Unused(Button, Shift);
-  Unused(X, Y);
-  FDragIndex := -1;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
