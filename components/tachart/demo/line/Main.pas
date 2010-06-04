685c5cb96689e225ae9e13cd6c3a17c5c9f61259
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-06-04 10:51:17 +0000

    TAChart: Add multiple series to line demo for performance testing
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25892 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/line/Main.pas
+++ components/tachart/demo/line/Main.pas
@@ -13,6 +13,7 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    btnAddSeries: TButton;
     cbRotated: TCheckBox;
     Chart1: TChart;
     Chart1LineSeries1: TLineSeries;
@@ -20,6 +21,7 @@ type
     cb3D: TCheckBox;
     Panel1: TPanel;
     RandomChartSource1: TRandomChartSource;
+    procedure btnAddSeriesClick(Sender: TObject);
     procedure cb3DChange(Sender: TObject);
     procedure cbLineTypeChange(Sender: TObject);
     procedure cbRotatedChange(Sender: TObject);
@@ -33,6 +35,20 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+
+procedure TForm1.btnAddSeriesClick(Sender: TObject);
+var
+  s: TLineSeries;
+  i, j: Integer;
+begin
+  for i := 1 to 10 do begin
+    s := TLineSeries.Create(Chart1);
+    s.SeriesColor := clRed;
+    for j := 1 to 1000 do
+      s.AddXY(j, Random * 5 + Chart1.SeriesCount * 10);
+    Chart1.AddSeries(s);
+  end;
+end;
 
 procedure TForm1.cb3DChange(Sender: TObject);
 begin
