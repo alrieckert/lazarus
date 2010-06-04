5f9f2c6fa8f1870a089949e0efb4e6b23c68e67b
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-06-04 15:54:08 +0000

    TAChart: Add simple benchmarking to linedemo.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@25896 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/line/Main.pas
+++ components/tachart/demo/line/Main.pas
@@ -14,14 +14,17 @@ type
 
   TForm1 = class(TForm)
     btnAddSeries: TButton;
+    btnRefresh: TButton;
+    cb3D: TCheckBox;
+    cbLineType: TComboBox;
     cbRotated: TCheckBox;
     Chart1: TChart;
     Chart1LineSeries1: TLineSeries;
-    cbLineType: TComboBox;
-    cb3D: TCheckBox;
+    edTime: TEdit;
     Panel1: TPanel;
     RandomChartSource1: TRandomChartSource;
     procedure btnAddSeriesClick(Sender: TObject);
+    procedure btnRefreshClick(Sender: TObject);
     procedure cb3DChange(Sender: TObject);
     procedure cbLineTypeChange(Sender: TObject);
     procedure cbRotatedChange(Sender: TObject);
@@ -34,6 +37,9 @@ implementation
 
 {$R *.lfm}
 
+uses
+  LCLIntf;
+
 { TForm1 }
 
 procedure TForm1.btnAddSeriesClick(Sender: TObject);
@@ -44,29 +50,47 @@ begin
   for i := 1 to 10 do begin
     s := TLineSeries.Create(Chart1);
     s.SeriesColor := clRed;
-    for j := 1 to 1000 do
+    for j := 1 to 10000 do
       s.AddXY(j, Random * 5 + Chart1.SeriesCount * 10);
     Chart1.AddSeries(s);
   end;
 end;
 
-procedure TForm1.cb3DChange(Sender: TObject);
+procedure TForm1.btnRefreshClick(Sender: TObject);
+var
+  t: TDateTime;
 begin
-  with Chart1LineSeries1 do
-    Depth := 15 - Depth;
+  t := Now;
+  Chart1.Refresh;
+  edTime.Text := FormatDateTime('s.zzz', Now - t) + ' s';
+end;
+
+procedure TForm1.cb3DChange(Sender: TObject);
+var
+  i: Integer;
+begin
+  for i := 0 to Chart1.SeriesCount - 1 do
+    with Chart1.Series[i] as TLineSeries do
+      Depth := 15 - Depth;
 end;
 
 procedure TForm1.cbLineTypeChange(Sender: TObject);
+var
+  i: Integer;
 begin
-  Chart1LineSeries1.LineType := TLineType(cbLineType.ItemIndex);
+  for i := 0 to Chart1.SeriesCount - 1 do
+    (Chart1.Series[i] as TLineSeries).LineType := TLineType(cbLineType.ItemIndex);
 end;
 
 procedure TForm1.cbRotatedChange(Sender: TObject);
+var
+  i: Integer;
 begin
-  with Chart1LineSeries1 do begin
-    AxisIndexY := Ord(cbRotated.Checked);
-    AxisIndexX := 1 - AxisIndexY;
-  end;
+  for i := 0 to Chart1.SeriesCount - 1 do
+    with Chart1.Series[i] as TLineSeries do begin
+      AxisIndexY := Ord(cbRotated.Checked);
+      AxisIndexX := 1 - AxisIndexY;
+    end;
 end;
 
 end.
