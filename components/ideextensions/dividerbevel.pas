d35c2bec7086abb931ff8d7ea8276cdd4d166d07
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-06-20 20:23:20 +0000

    License
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@26217 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/ideextensions/dividerbevel.pas
+++ components/ideextensions/dividerbevel.pas
@@ -1,20 +1,13 @@
-{ IdeGroupBox - A specially themed groupbox used inside the IDE
+{ DividerBevel
 
   Copyright (C) 2010 <name of author> <contact>
 
   This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Library General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
+  under the same terms as the Lazarus Component Library (LCL)
 
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-  for more details.
+  See the file COPYING.modifiedLGPL.txt, included in this distribution,
+  for details about the copyright.
 
-  You should have received a copy of the GNU Library General Public License
-  along with this library; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 }
 unit DividerBevel;
 
