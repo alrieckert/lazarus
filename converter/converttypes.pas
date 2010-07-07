84fa26dc652c56c70d6d0bc4cc4f8a46280b3594
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-07-07 11:43:44 +0000

    Converter: Added a unit for common types.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@26496 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ converter/converttypes.pas
@@ -0,0 +1,32 @@
+unit ConvertTypes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils; 
+
+type
+
+  // types for errors
+
+  { EConverterError }
+
+  EConverterError = class(Exception)
+    constructor Create(const AMessage: string);
+  end;
+
+
+implementation
+
+{ EConverterError }
+
+constructor EConverterError.Create(const AMessage: string);
+begin
+  inherited Create('Converter: '+AMessage);
+end;
+
+
+end.
+
