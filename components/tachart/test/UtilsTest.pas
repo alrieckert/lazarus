bc9bfdf95c1c9e2c9118b01459deff4c0204a88c
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-07-16 08:29:11 +0000

    TAChart: Add copyright headers.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@26678 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/test/UtilsTest.pas
+++ components/tachart/test/UtilsTest.pas
@@ -1,3 +1,19 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+
+Authors: Alexander Klenin
+
+}
+
 unit UtilsTest;
 
 {$mode objfpc}{$H+}
