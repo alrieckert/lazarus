6f164ea0444eab280d64cd5256dd8c30725327fa
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-07-24 10:11:17 +0000

    Qt: fixed TQtComboBox ButtonRect calculation with some themes. fixes #17018
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@26800 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/qt/qtwidgets.pas
+++ lcl/interfaces/qt/qtwidgets.pas
@@ -7528,15 +7528,16 @@ begin
           opt, QStyleSC_ComboBoxArrow, QComboBoxH(Widget));
         QStyleOptionComboBox_destroy(opt);
 
+
         if R1.Left < 0 then
           ButtonRect.Left := R.Right - R.Left - (-R1.Left)
         else
           ButtonRect.Left := R1.Left;
 
         if R1.Top < 0 then
-          ButtonRect.Top := R.Bottom - R.Top - (-R1.Top)
+          ButtonRect.Top := R.Top + (-R1.Top)
         else
-          ButtonRect.Top := R1.Top;
+          ButtonRect.Top := R.Top + R1.Top;
 
         if R1.Right < 0 then
           ButtonRect.Right := R.Right - R.Left - (-R1.Right)
@@ -7544,9 +7545,9 @@ begin
           ButtonRect.Right := R1.Right;
 
         if R1.Bottom < 0 then
-          ButtonRect.Bottom := R.Bottom - R.Top - (-R1.Bottom)
+          ButtonRect.Bottom := R.Bottom - (-R1.Bottom)
         else
-          ButtonRect.Bottom := R1.Bottom;
+          ButtonRect.Bottom := R.Bottom - R1.Bottom;
 
         if ButtonRect.Top = 0 then
           ButtonRect.Top := R.Top;
