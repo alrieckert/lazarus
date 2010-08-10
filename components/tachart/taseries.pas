5ed5741e7c22a370b5fe7f1037f00c9837b808ff
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-08-10 17:01:00 +0000

    TAChart: Initial implementation of TBubbleSeries
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@27052 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/taseries.pas
+++ components/tachart/taseries.pas
@@ -207,6 +207,33 @@ type
   // Use TLineSeries instead.
   TSerie = TLineSeries deprecated;
 
+  TBubbleRadiusTransform = (brtNone, brtX, brtY);
+
+  { TBubbleSeries }
+
+  TBubbleSeries = class(TBasicPointSeries)
+  private
+    FBubbleBrush: TBrush;
+    FBubblePen: TPen;
+    procedure SetBubbleBrush(const AValue: TBrush);
+    procedure SetBubblePen(AValue: TPen);
+  protected
+    procedure GetLegendItems(AItems: TChartLegendItems); override;
+    function GetSeriesColor: TColor; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor  Destroy; override;
+
+    procedure Draw(ACanvas: TCanvas); override;
+    function Extent: TDoubleRect; override;
+  published
+    property AxisIndexX;
+    property AxisIndexY;
+    property BubbleBrush: TBrush read FBubbleBrush write SetBubbleBrush;
+    property BubblePen: TPen read FBubblePen write SetBubblePen;
+    property Source;
+  end;
+
   TLineStyle = (lsVertical, lsHorizontal);
 
   { TConstantLine }
@@ -525,6 +552,84 @@ end;
 procedure TLineSeries.SetShowPoints(Value: Boolean);
 begin
   FShowPoints := Value;
+  UpdateParentChart;
+end;
+
+{ TBubbleSeries }
+
+constructor TBubbleSeries.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FBubblePen := TPen.Create;
+  FBubblePen.OnChange := @StyleChanged;
+  FBubbleBrush := TBrush.Create;
+  FBubbleBrush.OnChange := @StyleChanged;
+end;
+
+destructor TBubbleSeries.Destroy;
+begin
+  FreeAndNil(FBubbleBrush);
+  FreeAndNil(FBubblePen);
+  inherited Destroy;
+end;
+
+procedure TBubbleSeries.Draw(ACanvas: TCanvas);
+var
+  i: Integer;
+  pt, r: TPoint;
+  y: Double;
+begin
+  if Source.YCount < 2 then exit;
+  PrepareGraphPoints(ParentChart.CurrentExtent, true);
+  ACanvas.Pen.Assign(BubblePen);
+  ACanvas.Brush.Assign(BubbleBrush);
+  for i := 0 to High(FGraphPoints) do begin
+    pt := ParentChart.GraphToImage(FGraphPoints[i]);
+    y := Source[i + FLoBound]^.YList[0];
+    r.X := ParentChart.XGraphToImage(y) - ParentChart.XGraphToImage(0);
+    r.Y := ParentChart.YGraphToImage(y) - ParentChart.YGraphToImage(0);
+    ACanvas.EllipseC(pt.X, pt.Y, r.X, r.Y);
+  end;
+end;
+
+function TBubbleSeries.Extent: TDoubleRect;
+var
+  i: Integer;
+  r: Double;
+begin
+  Result := EmptyExtent;
+  if Source.YCount < 2 then exit;
+  for i := 0 to Count - 1 do
+    with Source[i]^ do begin
+      r := YList[0];
+      Result.a.X := Min(Result.a.X, X - r);
+      Result.b.X := Max(Result.b.X, X + r);
+      Result.a.Y := Min(Result.a.Y, Y - r);
+      Result.b.Y := Max(Result.b.Y, Y + r);
+    end;
+end;
+
+procedure TBubbleSeries.GetLegendItems(AItems: TChartLegendItems);
+begin
+  AItems.Add(TLegendItemBrushRect.Create(BubbleBrush, Title));
+end;
+
+function TBubbleSeries.GetSeriesColor: TColor;
+begin
+  Result := FBubbleBrush.Color;
+end;
+
+procedure TBubbleSeries.SetBubbleBrush(const AValue: TBrush);
+begin
+  if FBubbleBrush = AValue then exit;
+  FBubbleBrush := AValue;
+  UpdateParentChart;
+end;
+
+procedure TBubbleSeries.SetBubblePen(AValue: TPen);
+begin
+  if FBubblePen = AValue then exit;
+  FBubblePen := AValue;
   UpdateParentChart;
 end;
 
@@ -1282,6 +1387,7 @@ initialization
   RegisterSeriesClass(TAreaSeries, 'Area series');
   RegisterSeriesClass(TBarSeries, 'Bar series');
   RegisterSeriesClass(TPieSeries, 'Pie series');
+  RegisterSeriesClass(TBubbleSeries, 'Bubble series');
   RegisterSeriesClass(TFuncSeries, 'Function series');
   RegisterSeriesClass(TUserDrawnSeries, 'User-drawn series');
   RegisterSeriesClass(TConstantLine, 'Constant line');
