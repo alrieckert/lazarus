57447c57491592aa3202cbf66f669800ec8cf3d7
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-08-17 10:11:43 +0000

    TAChart: Add box-and-whiskers series demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@27121 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/multi/Main.pas
+++ components/tachart/demo/multi/Main.pas
@@ -13,15 +13,19 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    chWhiskers: TChart;
     chStackedBars: TChart;
     chBubble: TChart;
     Chart1BubbleSeries1: TBubbleSeries;
     chStackedBarsBarSeries1: TBarSeries;
+    chWhiskersBoxAndWhiskerSeries1: TBoxAndWhiskerSeries;
     lcsBubble: TListChartSource;
     PageControl1: TPageControl;
     rcsStacked: TRandomChartSource;
+    tsWhiskers: TTabSheet;
     tsStackedBar: TTabSheet;
     tsBubble: TTabSheet;
+    procedure FormCreate(Sender: TObject);
   end;
 
 var
@@ -30,6 +34,25 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  ylist: array [1..4] of Double;
+  i, j, y: Integer;
+begin
+  chWhiskersBoxAndWhiskerSeries1.ListSource.YCount := 5;
+  for i := 1 to 6 do begin
+    y := Random(80) + 10;
+    chWhiskersBoxAndWhiskerSeries1.AddXY(i, y);
+    for j := 1 to 4 do begin
+      y += Random(20) + 5;
+      ylist[j] := y;
+    end;
+    chWhiskersBoxAndWhiskerSeries1.ListSource.SetYList(i - 1, ylist);
+  end;
+end;
 
 end.
 
