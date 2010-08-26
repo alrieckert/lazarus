00c0552677c65ae8e41a6e33f7fc7167576b0915
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-08-26 14:47:15 +0000

    Added License
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@27196 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/synguttercodefolding.pp
+++ components/synedit/synguttercodefolding.pp
@@ -1,3 +1,25 @@
+{-------------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+Alternatively, the contents of this file may be used under the terms of the
+GNU General Public License Version 2 or later (the "GPL"), in which case
+the provisions of the GPL are applicable instead of those above.
+If you wish to allow use of your version of this file only under the terms
+of the GPL and not to allow others to use your version of this file
+under the MPL, indicate your decision by deleting the provisions above and
+replace them with the notice and other provisions required by the GPL.
+If you do not delete the provisions above, a recipient may use your version
+of this file under either the MPL or the GPL.
+
+-------------------------------------------------------------------------------}
+
 unit SynGutterCodeFolding;
 
 {$I synedit.inc}
@@ -419,7 +441,10 @@ procedure TSynGutterCodeFolding.DrawNodeSymbol(Canvas: TCanvas; Rect: TRect;
 var
   Points: Array [0..3] of TPoint;
   X, Y: Integer;
+  AliasMode: TAntialiasingMode;
 begin
+  AliasMode := Canvas.AntialiasingMode;
+  Canvas.AntialiasingMode:=amOff;
   Canvas.Rectangle(Rect);
   X := (Rect.Left - 1 + Rect.Right) div 2;
   Y := (Rect.Top - 1 + Rect.Bottom) div 2;
@@ -474,6 +499,7 @@ begin
         Canvas.Polygon(Points);
       end;
   end;
+  Canvas.AntialiasingMode := AliasMode;
 end;
 
 function TSynGutterCodeFolding.PreferedWidth: Integer;
