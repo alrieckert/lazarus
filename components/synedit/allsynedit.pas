d02f09153acbe6dd79a451245caea6392c83c68d
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-09-28 08:27:25 +0000

    started synedit lpk
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@27496 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/synedit/allsynedit.pas
@@ -0,0 +1,43 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit allsynedit; 
+
+interface
+
+uses
+    SynBeautifier, SynCompletion, SynDesignStringConstants, SynEdit, 
+  SynEditAutoComplete, SynEditExport, SynEditFoldedView, SynEditHighlighter, 
+  SynEditHighlighterFoldBase, SynEditHighlighterXMLBase, SynEditKeyCmds, 
+  SynEditLazDsgn, SynEditLines, SynEditMarks, SynEditMarkup, 
+  SynEditMarkupBracket, SynEditMarkupCtrlMouseLink, SynEditMarkupHighAll, 
+  SynEditMarkupSelection, SynEditMarkupSpecialLine, SynEditMarkupWordGroup, 
+  SynEditMiscClasses, SynEditMiscProcs, SynEditMouseCmds, SynEditPlugins, 
+  SynEditPointClasses, SynEditRegexSearch, SynEditSearch, SynEditStrConst, 
+  SynEditTextBase, SynEditTextBuffer, SynEditTextDoubleWidthChars, 
+  SynEditTextTabExpander, SynEditTextTrimmer, SynEditTypes, SynExportHTML, 
+  SynGutter, SynGutterBase, SynGutterChanges, SynGutterCodeFolding, 
+  SynGutterLineNumber, SynGutterLineOverview, SynGutterMarks, 
+  SynHighlighterAny, SynHighlighterCpp, SynHighlighterCss, SynHighlighterDiff, 
+  SynHighlighterHashEntries, SynHighlighterHTML, SynHighlighterJava, 
+  SynHighlighterJScript, SynHighlighterLFM, SynHighlighterMulti, 
+  SynHighlighterPas, SynHighlighterPerl, SynHighlighterPHP, 
+  SynHighlighterPosition, SynHighlighterPython, SynHighlighterSQL, 
+  SynHighlighterTeX, synhighlighterunixshellscript, SynHighlighterVB, 
+  SynHighlighterXML, SynMacroRecorder, SynMemo, SynPluginSyncroEdit, 
+  SynPluginSyncronizedEditBase, SynPluginTemplateEdit, 
+  SynPropertyEditObjectList, SynRegExpr, SynTextDrawer, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('SynEdit', @SynEdit.Register); 
+  RegisterUnit('synhighlighterunixshellscript', @synhighlighterunixshellscript.
+    Register); 
+end; 
+
+initialization
+  RegisterPackage('synedit1', @Register); 
+end.
