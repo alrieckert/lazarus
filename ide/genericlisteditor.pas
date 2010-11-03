48717903f322913ca9853832ff8006e4ed061c85
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-11-03 01:05:24 +0000

    A new form for editing lines in a memo.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@28047 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ ide/genericlisteditor.pas
@@ -0,0 +1,32 @@
+unit GenericListEditor;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
+  StdCtrls;
+
+type
+
+  { TGenericListEditForm }
+
+  TGenericListEditForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    Memo1: TMemo;
+  private
+
+  public
+
+  end; 
+
+var
+  GenericListEditForm: TGenericListEditForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
