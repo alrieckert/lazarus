e726c207a97dd458ccc296ad8510da5ec5ea3c75
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-11-08 16:18:37 +0000

    Debugger: do not report "thread-group-added" as initialization output
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@28156 4005530d-fff6-0310-9dd1-cebe43e6787f

--- debugger/gdbmidebugger.pp
+++ debugger/gdbmidebugger.pp
@@ -3066,7 +3066,19 @@ begin
   Line := ReadLine;
   while DebugProcessRunning and (Line <> '(gdb) ') do
   begin
-    S := S + Line + LineEnding;
+    case Line[1] of
+      '=': begin
+        case StringCase(GetPart(['='], [','], Line, False, False),
+          ['thread-group-added'])
+        of
+          0: {ignore};
+        else
+          S := S + Line + LineEnding;
+        end;
+      end;
+    else
+      S := S + Line + LineEnding;
+    end;
     Line := ReadLine;
   end;
   if S <> ''
