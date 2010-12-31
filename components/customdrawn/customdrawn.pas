abd2a528578f7127daad3ffad1919a59376d415f
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2010-12-31 15:12:44 +0000

    Adds a missing file to the custom drawn package
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@28849 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/customdrawn/customdrawn.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit customdrawn; 
+
+interface
+
+uses
+  customdrawnextras, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('customdrawnextras', @customdrawnextras.Register); 
+end; 
+
+initialization
+  RegisterPackage('customdrawn', @Register); 
+end.
