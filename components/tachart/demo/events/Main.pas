175900b9ac59d09d38f9278dba7b0e2f1f427504
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-01-17 02:54:58 +0000

    TAChart: Add an example of OnBeforeDrawBar event and a workaround for issue #18418 to events demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29060 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/events/Main.pas
+++ components/tachart/demo/events/Main.pas
@@ -16,10 +16,14 @@ type
     Chart1: TChart;
     Chart1BarSeries1: TBarSeries;
     RandomChartSource1: TRandomChartSource;
+    procedure Chart1BarSeries1BeforeDrawBar(ASender: TBarSeries;
+      ACanvas: TCanvas; const ARect: TRect; APointIndex, AStackIndex: Integer;
+      var ADoDefaultDrawing: Boolean);
     procedure Chart1BeforeDrawBackground(ASender: TChart; ACanvas: TCanvas;
       const ARect: TRect; var ADoDefaultDrawing: Boolean);
     procedure Chart1BeforeDrawBackWall(ASender: TChart; ACanvas: TCanvas;
       const ARect: TRect; var ADoDefaultDrawing: Boolean);
+    procedure FormCreate(Sender: TObject);
   end;
 
 var
@@ -30,6 +34,15 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+
+procedure TForm1.Chart1BarSeries1BeforeDrawBar(ASender: TBarSeries;
+  ACanvas: TCanvas; const ARect: TRect; APointIndex, AStackIndex: Integer;
+  var ADoDefaultDrawing: Boolean);
+begin
+  if APointIndex mod 2 = 0 then
+    ACanvas.Brush.Style := bsDiagCross;
+  ADoDefaultDrawing := APointIndex <> 4;
+end;
 
 procedure TForm1.Chart1BeforeDrawBackground(ASender: TChart; ACanvas: TCanvas;
   const ARect: TRect; var ADoDefaultDrawing: Boolean);
@@ -43,6 +56,14 @@ procedure TForm1.Chart1BeforeDrawBackWall(ASender: TChart; ACanvas: TCanvas;
 begin
   ACanvas.GradientFill(ARect, $FFFFFF, $80FF80, gdVertical);
   ADoDefaultDrawing := false;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  // Workaround for issue #18418
+  Chart1.OnBeforeDrawBackground := @Chart1BeforeDrawBackground;
+  Chart1.OnBeforeDrawBackWall := @Chart1BeforeDrawBackWall;
+  Chart1BarSeries1.OnBeforeDrawBar := @Chart1BarSeries1BeforeDrawBar;
 end;
 
 end.
