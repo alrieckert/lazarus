4d85217a0858c0a3f46ab2467106afd9555b5947
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-01-28 04:14:53 +0000

    wince: add alllclintfunits
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29227 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ lcl/interfaces/wince/alllclintfunits.pas
@@ -0,0 +1,28 @@
+{ This unit was automatically created by update_allunits }
+unit AllLCLIntfUnits;
+interface
+uses
+  interfaces,
+  win32compat,
+  wincedef,
+  winceextra,
+  winceint,
+  winceproc,
+  wincewsarrow,
+  wincewsbuttons,
+  wincewscalendar,
+  wincewschecklst,
+  wincewscomctrls,
+  wincewscontrols,
+  wincewsdialogs,
+  wincewsextctrls,
+  wincewsfactory,
+  wincewsforms,
+  wincewsgrids,
+  wincewsimglist,
+  wincewsmenus,
+  wincewsspin,
+  wincewsstdctrls,
+  winext;
+implementation
+end.
