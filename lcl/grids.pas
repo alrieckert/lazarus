alrieckert/lazarus

commit 09f284cbacaa588c5e79528ebb637e8050de4420

LCL, grids, do not invalidate cells which are not visible (or do not exist as in this case), fix issue 18498
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29577 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/grids.pas
@@ -6799,9 +6799,10 @@ begin
     DebugLn(['InvalidateCell  Col=',aCol,
       ' Row=',aRow,' Redraw=', Redraw]);
   {$Endif}
-  if not HandleAllocated then Exit;
-  R:=CellRect(aCol, aRow);
-  InvalidateRect(Handle, @R, Redraw);
+  if HandleAllocated and IsCellVisible(aCol,aRow) then begin
+    R:=CellRect(aCol, aRow);
+    InvalidateRect(Handle, @R, Redraw);
+  end;
 end;
 
 procedure TCustomGrid.InvalidateRange(const aRange: TRect);

commit 06769da56373a3a113d6c994d9b337d5c246a370

LCL, fix dbgrid indicator appearing multiple times, problem introduced in r29577
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29655 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/grids.pas
@@ -1094,6 +1094,7 @@ type
     procedure InvalidateRange(const aRange: TRect);
     procedure InvalidateRow(ARow: Integer);
     function  IscellVisible(aCol, aRow: Integer): Boolean;
+    function  IsFixedCellVisible(aCol, aRow: Integer): boolean;
     procedure LoadFromFile(FileName: string);
     function  MouseCoord(X,Y: Integer): TGridCoord;
     function  MouseToCell(const Mouse: TPoint): TPoint; overload;
@@ -6379,6 +6380,13 @@ begin
     Result:= (Left<=ACol)and(aCol<=Right)and(Top<=aRow)and(aRow<=Bottom);
 end;
 
+function TCustomGrid.IsFixedCellVisible(aCol, aRow: Integer): boolean;
+begin
+  with FGCache.VisibleGrid do
+    result := ((aCol<FixedCols) and ((aRow<FixedRows) or ((aRow>=Top)and(aRow<=Bottom)))) or
+              ((aRow<FixedRows) and ((aCol<FixedCols) or ((aCol>=Left)and(aCol<=Right))));
+end;
+
 procedure TCustomGrid.InvalidateCol(ACol: Integer);
 var
   R: TRect;
@@ -6799,7 +6807,7 @@ begin
     DebugLn(['InvalidateCell  Col=',aCol,
       ' Row=',aRow,' Redraw=', Redraw]);
   {$Endif}
-  if HandleAllocated and IsCellVisible(aCol,aRow) then begin
+  if HandleAllocated and (IsCellVisible(aCol, aRow) or IsFixedCellVisible(aCol, aRow)) then begin
     R:=CellRect(aCol, aRow);
     InvalidateRect(Handle, @R, Redraw);
   end;