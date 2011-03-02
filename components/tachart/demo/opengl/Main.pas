885de5bc33084266f78b91ef87fb84e704936e90
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-03-02 14:04:52 +0000

    TAChart: Add OpenGL demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29707 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/tachart/demo/opengl/Main.pas
@@ -0,0 +1,67 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, OpenGLContext, SysUtils, FileUtil, Forms, Controls, Graphics,
+  Dialogs, TAGraph, TASeries, TASources, GL, GLU, TADrawerOpenGL, TADrawUtils;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Chart1: TChart;
+    Chart1BarSeries1: TBarSeries;
+    Chart1LineSeries1: TLineSeries;
+    OpenGLControl1: TOpenGLControl;
+    RandomChartSource1: TRandomChartSource;
+    procedure FormCreate(Sender: TObject);
+    procedure OpenGLControl1Paint(Sender: TObject);
+  private
+    procedure OnAppIdle(Sender: TObject; var Done: Boolean);
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Application.AddOnIdleHandler(@OnAppIdle);
+end;
+
+procedure TForm1.OnAppIdle(Sender: TObject; var Done: Boolean);
+begin
+  Done:=false;
+  OpenGLControl1.Invalidate;
+end;
+
+procedure TForm1.OpenGLControl1Paint(Sender: TObject);
+var
+  d: IChartDrawer;
+begin
+  glClearColor(1.0, 1.0, 1.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  with OpenGLControl1 do
+    gluOrtho2D(0, Width, Height, 0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  d := TOpenGLDrawer.Create(OpenGLControl1);
+  Chart1.Title.Text.Text := 'OpenGL';
+  Chart1.Draw(d, Rect(0, 0, OpenGLControl1.Width, OpenGLControl1.Height));
+  Chart1.Title.Text.Text := 'Standard';
+
+  OpenGLControl1.SwapBuffers;
+end;
+
+end.
+
---
+++ components/tachart/demo/opengl/opengldemo.lpr
@@ -0,0 +1,20 @@
+program opengldemo;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, lazopenglcontext, Main, tachartlazaruspkg;
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
