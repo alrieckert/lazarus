f5cd0aa7b93e6e81b9bdb5454d987d0a23b98608
maxim <maxim@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-03-10 22:08:51 +0000

    IDE: improved 'Unused units' dialog localization
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29785 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/unusedunitsdlg.pas
+++ ide/unusedunitsdlg.pas
@@ -138,6 +138,7 @@ begin
 
   RemoveSelectedBitBtn.Caption:=lisRemoveSelectedUnits;
   RemoveAllBitBtn.Caption:=lisRemoveAllUnits;
+  CancelBitBtn.Caption:=dlgCancel;
 
   UnitsTreeView.StateImages := IDEImages.Images_16;
   ImgIDInterface := IDEImages.LoadImage(16, 'ce_interface');
