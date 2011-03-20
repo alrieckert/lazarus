78d318e3e92ef356155638982cbd0177fae26d0e
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-03-20 18:32:59 +0000

    TAChart: Update navigation demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29941 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/navigate/Main.pas
+++ components/tachart/demo/navigate/Main.pas
@@ -5,8 +5,8 @@ unit Main;
 interface
 
 uses
-  ComCtrls, SysUtils, Forms, TAGraph, TASeries, TASources, TANavigation,
-  TATools;
+  ComCtrls, ExtCtrls, StdCtrls, SysUtils, Forms, TAGraph, TASeries, TASources,
+  TANavigation, TATools, Classes;
 
 type
 
@@ -15,14 +15,20 @@ type
   TForm1 = class(TForm)
     Chart1: TChart;
     Chart1LineSeries1: TLineSeries;
+    ChartNavPanel1: TChartNavPanel;
     ChartToolset1: TChartToolset;
     ChartToolset1PanDragTool1: TPanDragTool;
     ChartToolset1ZoomDragTool1: TZoomDragTool;
+    cbMiniMap: TCheckBox;
+    Panel1: TPanel;
     RandomChartSource1: TRandomChartSource;
     sbChartHor: TChartNavScrollBar;
     sbChartVert: TChartNavScrollBar;
+    StaticText1: TStaticText;
     StatusBar1: TStatusBar;
+    procedure cbMiniMapChange(Sender: TObject);
     procedure Chart1ExtentChanged(ASender: TChart);
+    procedure FormCreate(Sender: TObject);
   end;
 
 var
@@ -34,11 +40,21 @@ implementation
 
 { TForm1 }
 
+procedure TForm1.cbMiniMapChange(Sender: TObject);
+begin
+  ChartNavPanel1.MiniMap := cbMiniMap.Checked;
+end;
+
 procedure TForm1.Chart1ExtentChanged(ASender: TChart);
 begin
   with ASender.LogicalExtent do
     StatusBar1.Panels[0].Text :=
       Format('(%.3g;%.3g) - (%.3g;%.3g)', [a.X, a.Y, b.X, b.Y]);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Chart1.OnExtentChanged := @Chart1ExtentChanged;
 end;
 
 end.
