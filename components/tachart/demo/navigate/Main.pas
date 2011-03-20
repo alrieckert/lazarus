0f34ba67db3abdaea3d2aeff821162f3da7434f7
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-03-20 13:08:28 +0000

    TAChart: Use TChartNavScrollBar in the navigation demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29926 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/navigate/Main.pas
+++ components/tachart/demo/navigate/Main.pas
@@ -5,8 +5,8 @@ unit Main;
 interface
 
 uses
-  Classes, ExtCtrls, StdCtrls, SysUtils, FileUtil, Forms, Controls, Graphics,
-  Dialogs, TAGraph, TASeries, TASources, TAChartUtils, TATools;
+  ComCtrls, SysUtils, Forms, TAGraph, TASeries, TASources, TANavigation,
+  TATools;
 
 type
 
@@ -19,11 +19,10 @@ type
     ChartToolset1PanDragTool1: TPanDragTool;
     ChartToolset1ZoomDragTool1: TZoomDragTool;
     RandomChartSource1: TRandomChartSource;
-    sbChartHor: TScrollBar;
-    sbChartVert: TScrollBar;
+    sbChartHor: TChartNavScrollBar;
+    sbChartVert: TChartNavScrollBar;
+    StatusBar1: TStatusBar;
     procedure Chart1ExtentChanged(ASender: TChart);
-    procedure sbChartHorChange(Sender: TObject);
-    procedure sbChartVertChange(Sender: TObject);
   end;
 
 var
@@ -36,56 +35,10 @@ implementation
 { TForm1 }
 
 procedure TForm1.Chart1ExtentChanged(ASender: TChart);
-var
-  fe, le: TDoubleRect;
-  w: Double;
 begin
-  fe := ASender.GetFullExtent;
-  le := ASender.LogicalExtent;
-  w := fe.b.X - fe.a.X;
-  with sbChartHor do begin
-    if w <= 0 then
-      Position := 0
-    else
-      Position := Round(WeightedAverage(Min, Max, (le.a.X - fe.a.X) / w));
-  end;
-  w := fe.b.Y - fe.a.Y;
-  with sbChartVert do begin
-    if w <= 0 then
-      Position := 0
-    else
-      Position := Round(WeightedAverage(Max, Min, (le.a.Y - fe.a.Y) / w));
-  end;
-end;
-
-procedure TForm1.sbChartHorChange(Sender: TObject);
-var
-  fe, le: TDoubleRect;
-  d, w: Double;
-begin
-  w := sbChartHor.Max - sbChartHor.Min;
-  if w = 0 then exit;
-  fe := Chart1.GetFullExtent;
-  le := Chart1.LogicalExtent;
-  d := WeightedAverage(fe.a.X, fe.b.X, sbChartHor.Position / w);
-  le.b.X += d - le.a.X;
-  le.a.X := d;
-  Chart1.LogicalExtent := le;
-end;
-
-procedure TForm1.sbChartVertChange(Sender: TObject);
-var
-  fe, le: TDoubleRect;
-  d, w: Double;
-begin
-  w := sbChartVert.Max - sbChartVert.Min;
-  if w = 0 then exit;
-  fe := Chart1.GetFullExtent;
-  le := Chart1.LogicalExtent;
-  d := WeightedAverage(fe.b.Y, fe.a.Y, sbChartVert.Position / w);
-  le.b.Y += d - le.a.Y;
-  le.a.Y := d;
-  Chart1.LogicalExtent := le;
+  with ASender.LogicalExtent do
+    StatusBar1.Panels[0].Text :=
+      Format('(%.3g;%.3g) - (%.3g;%.3g)', [a.X, a.Y, b.X, b.Y]);
 end;
 
 end.
