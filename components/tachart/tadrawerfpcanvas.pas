7d3799e1c74f705220c39f24dfe5999cf5d78afc
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-03-21 13:42:43 +0000

    TAChart: Add TFPCanvasDrawer
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@29965 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/tachart/tadrawerfpcanvas.pas
@@ -0,0 +1,253 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+
+Authors: Alexander Klenin
+
+}
+unit TADrawerFPCanvas;
+
+{$H+}
+
+interface
+
+uses
+  Classes, FPCanvas, TAChartUtils, TADrawUtils;
+
+type
+
+  { TFPCanvasDrawer }
+
+  TFPCanvasDrawer = class(TBasicDrawer, IChartDrawer)
+  strict private
+    FCanvas: TFPCustomCanvas;
+    procedure SetBrush(ABrush: TFPCustomBrush);
+    procedure SetFont(AFont: TFPCustomFont);
+    procedure SetPen(APen: TFPCustomPen);
+  strict protected
+    function GetFontAngle: Double; override;
+    function SimpleTextExtent(const AText: String): TPoint; override;
+    procedure SimpleTextOut(AX, AY: Integer; const AText: String); override;
+  public
+    constructor Create(ACanvas: TFPCustomCanvas);
+  public
+    procedure AddToFontOrientation(ADelta: Integer);
+    procedure ClippingStart;
+    procedure ClippingStart(const AClipRect: TRect);
+    procedure ClippingStop;
+    procedure Ellipse(AX1, AY1, AX2, AY2: Integer);
+    procedure FillRect(AX1, AY1, AX2, AY2: Integer);
+    function GetBrushColor: TChartColor;
+    procedure Line(AX1, AY1, AX2, AY2: Integer);
+    procedure Line(const AP1, AP2: TPoint);
+    procedure LineTo(AX, AY: Integer); override;
+    procedure MoveTo(AX, AY: Integer); override;
+    procedure Polygon(
+      const APoints: array of TPoint;
+      AStartIndex: Integer = 0; ANumPts: Integer = -1); override;
+    procedure Polyline(
+      const APoints: array of TPoint; AStartIndex: Integer = 0;
+      ANumPts: Integer = -1; AEndPoint: Boolean = false);
+    procedure PrepareSimplePen(AColor: TChartColor);
+    procedure RadialPie(
+      AX1, AY1, AX2, AY2: Integer;
+      AStartAngle16Deg, AAngleLength16Deg: Integer);
+    procedure Rectangle(const ARect: TRect);
+    procedure Rectangle(AX1, AY1, AX2, AY2: Integer);
+    procedure SetBrushColor(AColor: TChartColor);
+    procedure SetBrushParams(AStyle: TFPBrushStyle; AColor: TChartColor);
+    procedure SetPenParams(AStyle: TFPPenStyle; AColor: TChartColor);
+  end;
+
+implementation
+
+type
+  TFPCanvasHelperCrack = class(TFPCanvasHelper);
+
+procedure AssignFPCanvasHelper(ADest, ASrc: TFPCanvasHelper);
+var
+  d: TFPCanvasHelperCrack absolute ADest;
+begin
+  d.DoCopyProps(ASrc);
+end;
+
+function CopyArray(
+  const A: array of TPoint; AStart, ACount: Integer): TPointArray;
+var
+  i: Integer;
+begin
+  SetLength(Result, ACount);
+  for i := 0 to ACount - 1 do
+    Result[i] := A[i + AStart];
+end;
+
+{ TFPCanvasDrawer }
+
+procedure TFPCanvasDrawer.AddToFontOrientation(ADelta: Integer);
+begin
+  Unused(ADelta);
+end;
+
+procedure TFPCanvasDrawer.ClippingStart(const AClipRect: TRect);
+begin
+  Unused(AClipRect);
+  FCanvas.ClipRect := AClipRect;
+  ClippingStart;
+end;
+
+procedure TFPCanvasDrawer.ClippingStart;
+begin
+  // FIXME: FPCanvas.Clipping is broken
+  // FCanvas.Clipping := true;
+end;
+
+procedure TFPCanvasDrawer.ClippingStop;
+begin
+  FCanvas.Clipping := false;
+end;
+
+constructor TFPCanvasDrawer.Create(ACanvas: TFPCustomCanvas);
+begin
+  FCanvas := ACanvas;
+end;
+
+procedure TFPCanvasDrawer.Ellipse(AX1, AY1, AX2, AY2: Integer);
+begin
+  FCanvas.Ellipse(AX1, AY1, AX2, AY2);
+end;
+
+procedure TFPCanvasDrawer.FillRect(AX1, AY1, AX2, AY2: Integer);
+begin
+  // FIXME
+  FCanvas.Rectangle(AX1, AY1, AX2, AY2);
+end;
+
+function TFPCanvasDrawer.GetBrushColor: TChartColor;
+begin
+  Result := FPColorToChartColor(FCanvas.Brush.FPColor);
+end;
+
+function TFPCanvasDrawer.GetFontAngle: Double;
+begin
+  Result := 0.0;
+end;
+
+procedure TFPCanvasDrawer.Line(AX1, AY1, AX2, AY2: Integer);
+begin
+  FCanvas.Line(AX1, AY1, AX2, AY2);
+end;
+
+procedure TFPCanvasDrawer.Line(const AP1, AP2: TPoint);
+begin
+  FCanvas.Line(AP1, AP2);
+end;
+
+procedure TFPCanvasDrawer.LineTo(AX, AY: Integer);
+begin
+  FCanvas.LineTo(AX, AY);
+end;
+
+procedure TFPCanvasDrawer.MoveTo(AX, AY: Integer);
+begin
+  FCanvas.MoveTo(AX, AY);
+end;
+
+procedure TFPCanvasDrawer.Polygon(
+  const APoints: array of TPoint; AStartIndex: Integer; ANumPts: Integer);
+begin
+  if (ANumPts < 0) and (AStartIndex = 0) then
+    FCanvas.Polygon(APoints)
+  else
+    FCanvas.Polygon(CopyArray(APoints, AStartIndex, ANumPts));
+end;
+
+procedure TFPCanvasDrawer.Polyline(
+  const APoints: array of TPoint;
+  AStartIndex: Integer; ANumPts: Integer; AEndPoint: Boolean);
+begin
+  Unused(AEndPoint);
+  if (ANumPts < 0) and (AStartIndex = 0) then
+    FCanvas.Polyline(APoints)
+  else
+    FCanvas.Polyline(CopyArray(APoints, AStartIndex, ANumPts));
+end;
+
+procedure TFPCanvasDrawer.PrepareSimplePen(AColor: TChartColor);
+begin
+  FCanvas.Pen.FPColor := ChartColorToFPColor(AColor);
+  FCanvas.Pen.Width := 1;
+  FCanvas.Pen.Style := psSolid;
+end;
+
+procedure TFPCanvasDrawer.RadialPie(
+  AX1, AY1, AX2, AY2: Integer; AStartAngle16Deg, AAngleLength16Deg: Integer);
+begin
+  Unused(AX1, AY1);
+  Unused(AX2, AY2);
+  Unused(AStartAngle16Deg, AAngleLength16Deg);
+  raise EChartError.Create('TOpenGLDrawer.RadialPie not implemented');
+end;
+
+procedure TFPCanvasDrawer.Rectangle(AX1, AY1, AX2, AY2: Integer);
+begin
+  FCanvas.Rectangle(AX1, AY1, AX2, AY2);
+end;
+
+procedure TFPCanvasDrawer.Rectangle(const ARect: TRect);
+begin
+  FCanvas.Rectangle(ARect);
+end;
+
+procedure TFPCanvasDrawer.SetBrush(ABrush: TFPCustomBrush);
+begin
+  AssignFPCanvasHelper(FCanvas.Brush, ABrush);
+end;
+
+procedure TFPCanvasDrawer.SetBrushColor(AColor: TChartColor);
+begin
+  FCanvas.Brush.FPColor := ChartColorToFPColor(AColor);
+end;
+
+procedure TFPCanvasDrawer.SetBrushParams(
+  AStyle: TFPBrushStyle; AColor: TChartColor);
+begin
+  FCanvas.Brush.Style := AStyle;
+  FCanvas.Brush.FPColor := ChartColorToFPColor(AColor);
+end;
+
+procedure TFPCanvasDrawer.SetFont(AFont: TFPCustomFont);
+begin
+  AssignFPCanvasHelper(FCanvas.Font, AFont);
+end;
+
+procedure TFPCanvasDrawer.SetPen(APen: TFPCustomPen);
+begin
+  AssignFPCanvasHelper(FCanvas.Pen, APen);
+end;
+
+procedure TFPCanvasDrawer.SetPenParams(AStyle: TFPPenStyle; AColor: TChartColor);
+begin
+  FCanvas.Pen.Style := AStyle;
+  FCanvas.Pen.FPColor := ChartColorToFPColor(AColor);
+end;
+
+function TFPCanvasDrawer.SimpleTextExtent(const AText: String): TPoint;
+begin
+  FCanvas.GetTextSize(AText, Result.X, Result.Y);
+end;
+
+procedure TFPCanvasDrawer.SimpleTextOut(AX, AY: Integer; const AText: String);
+begin
+  FCanvas.TextOut(AX, AY, AText);
+end;
+
+end.
+
