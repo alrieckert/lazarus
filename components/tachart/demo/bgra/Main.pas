fce5fe905524b92d68418c96f3e6bf56feeeec10
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-04-05 17:16:16 +0000

    TAChart: Synchronize extents of standard and BGRA view
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30200 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/bgra/Main.pas
+++ components/tachart/demo/bgra/Main.pas
@@ -19,6 +19,7 @@ type
     PaintBox1: TPaintBox;
     RandomChartSource1: TRandomChartSource;
     Splitter1: TSplitter;
+    procedure Chart1AfterPaint(ASender: TChart);
     procedure PaintBox1Paint(Sender: TObject);
   end;
 
@@ -34,10 +35,16 @@ uses
 
 { TForm1 }
 
+procedure TForm1.Chart1AfterPaint(ASender: TChart);
+begin
+  PaintBox1.Invalidate;
+end;
+
 procedure TForm1.PaintBox1Paint(Sender: TObject);
 var
   bmp: TBGRABitmap;
   id: IChartDrawer;
+  rp: TChartRenderingParams;
 begin
   bmp := TBGRABitmap.Create(PaintBox1.Width, PaintBox1.Height);
   Chart1.DisableRedrawing;
@@ -45,7 +52,9 @@ begin
     Chart1.Title.Text.Text := 'BGRABitmap';
     id := TBGRABitmapDrawer.Create(bmp);
     id.DoGetFontOrientation := @CanvasGetFontOrientationFunc;
+    rp := Chart1.RenderingParams;
     Chart1.Draw(id, Rect(0, 0, PaintBox1.Width, PaintBox1.Height));
+    Chart1.RenderingParams := rp;
     bmp.Draw(PaintBox1.Canvas, 0, 0);
     Chart1.Title.Text.Text := 'Standard';
   finally
