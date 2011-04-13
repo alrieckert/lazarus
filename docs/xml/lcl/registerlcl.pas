f0f31ab4145a64e0fccbcc564dbe4615b0c4e977
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-04-13 08:45:51 +0000

    added documentation template for registerlcl
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30280 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ docs/xml/lcl/registerlcl.pas
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<fpdoc-descriptions>
+<package name="lcl">
+
+<!--
+  ====================================================================
+    RegisterLCL
+  ====================================================================
+-->
+
+<module name="RegisterLCL">
+<short></short>
+<descr>
+</descr>
+
+<!-- procedure Visibility: default -->
+<element name="Register">
+<short></short>
+<descr>
+</descr>
+<errors>
+</errors>
+<seealso>
+</seealso>
+</element>
+
+</module> <!-- RegisterLCL -->
+
+</package>
+</fpdoc-descriptions>
