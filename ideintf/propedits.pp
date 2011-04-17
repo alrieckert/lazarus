3099dd8b5fed7f56befdd8896f43f77b99f0ae68
paul <paul@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-04-17 04:59:32 +0000

    ideintf: fix compilation with new tkHelper element
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30332 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ideintf/propedits.pp
+++ ideintf/propedits.pp
@@ -1741,11 +1741,12 @@ end;
 
 const
 { TypeKinds  see typinfo.pp
-       TTypeKind = (tkUnknown,tkInteger,tkChar,tkEnumeration,
-                   tkFloat,tkSet,tkMethod,tkSString,tkLString,tkAString,
+       TTypeKind = (tkUnknown,tkInteger,tkChar,tkEnumeration,tkFloat,
+                   tkSet,tkMethod,tkSString,tkLString,tkAString,
                    tkWString,tkVariant,tkArray,tkRecord,tkInterface,
                    tkClass,tkObject,tkWChar,tkBool,tkInt64,tkQWord,
-                   tkDynArray,tkInterfaceRaw);
+                   tkDynArray,tkInterfaceRaw,tkProcVar,tkUString,tkUChar,
+                   tkHelper);
 }
 
   PropClassMap:array[TypInfo.TTypeKind] of TPropertyEditorClass=(
@@ -1777,6 +1778,9 @@ const
     ,nil,                      // tkProcVar
     nil,                       // tkUString
     nil                        // tkUChar
+{$ENDIF}
+{$IF declared(tkHelper)}
+    ,nil                       // tkHelper
 {$ENDIF}
     );
 
