9ee5813c3d660e4faf90aa4c855204f15154a2b0
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-05-02 09:16:59 +0000

    TAChart: Add TAChartPrint package
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30524 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/tachart/tachartprint.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit TAChartPrint; 
+
+interface
+
+uses
+  TAPrint, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('TAChartPrint', @Register); 
+end.
