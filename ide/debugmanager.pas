6940617b7e65fc42e61e1f2ad23d1f1a9fe2056c
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-05-06 11:11:55 +0000

    DBG: Handle error state as stopped.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30579 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/debugmanager.pas
+++ ide/debugmanager.pas
@@ -781,8 +781,8 @@ procedure TDebugManager.DebuggerChangeState(ADebugger: TDebugger;
 const
   // dsNone, dsIdle, dsStop, dsPause, dsInit, dsRun, dsError
   TOOLSTATEMAP: array[TDBGState] of TIDEToolStatus = (
-    // dsNone, dsIdle, dsStop, dsPause, dsInit,     dsRun,      dsError,    dsDestroying
-    itNone, itNone, itNone, itDebugger, itDebugger, itDebugger, itDebugger, itNone
+  //dsNone, dsIdle, dsStop, dsPause,    dsInit,     dsRun,      dsError,    dsDestroying
+    itNone, itNone, itNone, itDebugger, itDebugger, itDebugger, itNone, itNone
   );
   //STATENAME: array[TDBGState] of string = (
   //  'dsNone', 'dsIdle', 'dsStop', 'dsPause', 'dsInit', 'dsRun', 'dsError'
