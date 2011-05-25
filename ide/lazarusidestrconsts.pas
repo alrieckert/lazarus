13aca241fe0abdab7eadcd5f72155c5124d49e01
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-05-25 20:01:00 +0000

    IDE: Minor change in resource strings
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@30905 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -434,7 +434,7 @@ resourcestring
   lisMenuCodeToolsDefinesEditor = 'CodeTools defines editor ...';
   
   lisMenuOnlineHelp = 'Online Help';
-  lisMenuReportingBug = 'Reporting a bug ...';
+  lisMenuReportingBug = 'Reporting a bug';
   lisReportingBugURL = 'http://wiki.lazarus.freepascal.org/How_do_I_create_a_bug_report';
   lisKMConfigureHelp = 'Configure Help';
   lisKMContextSensitiveHelp = 'Context sensitive help';
