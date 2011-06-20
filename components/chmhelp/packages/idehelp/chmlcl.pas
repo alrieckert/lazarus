8fab605028bcf4c2cb873950f9eb067099a7d028
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-06-20 19:42:25 +0000

    chmhelp: fixed mem leak
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@31314 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/chmhelp/packages/idehelp/chmlcl.pas
+++ components/chmhelp/packages/idehelp/chmlcl.pas
@@ -5,31 +5,38 @@ unit ChmLcl;
 interface
 
 uses
-  LazHelpIntf, HelpFPDoc;
+  Classes, LazHelpIntf, HelpFPDoc;
 
 const
   sLclUnits = 'LCL - Lazarus component library';
 
+type
+
+  { TLclChmHelpDatabase }
+
+  TLclChmHelpDatabase = class(TFPDocHTMLHelpDatabase)
+  private
+    FBaseURL: THelpBaseURLObject;
+  public
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
 procedure RegisterLclHelpDatabase;
 
 var
-  LCLHelpDatabase: TFPDocHTMLHelpDatabase = nil;
+  LCLHelpDatabase: TLclChmHelpDatabase = nil;
 
 implementation
 
 procedure RegisterLclHelpDatabase;
 var
   FPDocNode: THelpNode;
-  BaseURL: THelpBaseURLObject;
   DirItem: THelpDBISourceDirectory;
 begin
   if Assigned(LCLHelpDatabase) then Exit;
-  LCLHelpDatabase := TFPDocHTMLHelpDatabase(
-    HelpDatabases.CreateHelpDatabase(sLclUnits, TFPDocHTMLHelpDatabase, True));
-
-  BaseURL := THelpBaseURLObject.Create;
-  BaseURL.BaseURL := 'lcl.chm://';
-  LCLHelpDatabase.BasePathObject := BaseURL;
+  LCLHelpDatabase := TLclChmHelpDatabase(
+    HelpDatabases.CreateHelpDatabase(sLclUnits, TLclChmHelpDatabase, True));
 
   // FPDoc nodes for units in the LCL
   FPDocNode := THelpNode.CreateURL(LCLHelpDatabase,
@@ -39,6 +46,22 @@ begin
   DirItem := THelpDBISourceDirectory.Create(FPDocNode, '$(LazarusDir)/lcl',
                             '*.pp;*.pas;*.inc', True);// and once as normal page
   LCLHelpDatabase.RegisterItem(DirItem);
+end;
+
+{ TLclChmHelpDatabase }
+
+constructor TLclChmHelpDatabase.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  FBaseURL := THelpBaseURLObject.Create;
+  FBaseURL.BaseURL := 'lcl.chm://';
+  BasePathObject := FBaseURL;
+end;
+
+destructor TLclChmHelpDatabase.Destroy;
+begin
+  FBaseURL.Free;
+  inherited Destroy;
 end;
 
 end.
