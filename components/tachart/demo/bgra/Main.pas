3306737abfae8cea1324edda3db4230089cfc7c8
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-07-10 09:57:55 +0000

    TAChart: Update bgra demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@31653 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/bgra/Main.pas
+++ components/tachart/demo/bgra/Main.pas
@@ -5,7 +5,7 @@ unit Main;
 interface
 
 uses
-  Classes, ExtCtrls, SysUtils, FileUtil, Forms, Controls,
+  Classes, ExtCtrls, StdCtrls, SysUtils, FileUtil, Forms, Controls,
   Graphics, Dialogs, TAGraph, TASeries, TASources;
 
 type
@@ -14,11 +14,18 @@ type
 
   TForm1 = class(TForm)
     Chart1: TChart;
+    Chart1AreaSeries1: TAreaSeries;
     Chart1BarSeries1: TBarSeries;
     Chart1LineSeries1: TLineSeries;
+    Chart1PieSeries1: TPieSeries;
+    cbAntialiasing: TCheckBox;
+    cbPie: TCheckBox;
     PaintBox1: TPaintBox;
+    Panel1: TPanel;
     RandomChartSource1: TRandomChartSource;
     Splitter1: TSplitter;
+    procedure cbAntialiasingChange(Sender: TObject);
+    procedure cbPieChange(Sender: TObject);
     procedure Chart1AfterPaint(ASender: TChart);
     procedure PaintBox1Paint(Sender: TObject);
   end;
@@ -34,6 +41,19 @@ uses
   BGRABitmap, TADrawerBGRA, TADrawerCanvas, TADrawUtils;
 
 { TForm1 }
+
+procedure TForm1.cbAntialiasingChange(Sender: TObject);
+begin
+  if cbAntialiasing.Checked then
+    Chart1.AntialiasingMode := amOn
+  else
+    Chart1.AntialiasingMode := amOff;
+end;
+
+procedure TForm1.cbPieChange(Sender: TObject);
+begin
+  Chart1PieSeries1.Active := cbPie.Checked;
+end;
 
 procedure TForm1.Chart1AfterPaint(ASender: TChart);
 begin
