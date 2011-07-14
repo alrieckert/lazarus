19b00e0404b27a609bfb6c2227312721cc9852a8
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-07-14 08:37:15 +0000

    android: Advances the basic widgetset structure, linking works now
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@31694 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ lcl/interfaces/android/alllclintfunits.pas
@@ -0,0 +1,11 @@
+{ This unit was automatically created by update_allunits }
+unit alllclintfunits;
+interface
+uses
+  androidint,
+  androidwsfactory,
+  androidwsforms,
+  androidprivate,
+  interfaces;
+implementation
+end.
---
+++ lcl/interfaces/android/androidprivate.pas
@@ -0,0 +1,68 @@
+{
+                    -----------------------------------------
+                    androidprivate.pas
+                    -----------------------------------------
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit androidprivate;
+
+{$mode objfpc}{$H+}
+
+interface
+
+// defines
+//{$I carbondefines.inc}
+
+uses
+  // libs
+//  MacOSAll,
+  // wdgetset
+  WSLCLClasses, LCLClasses,
+  // LCL + RTL
+  Types, Classes, SysUtils, Controls, LCLType, LCLProc, Graphics, Math, Contnrs,
+  AVL_Tree, LMessages, LCLMessageGlue;
+
+type
+
+  { TAndroidWindow }
+  
+  TAndroidWindow = class
+  public
+    constructor Create(const AObject: TWinControl; const AParams: TCreateParams);
+    destructor Destroy; override;
+  end;
+
+//function CheckHandle(const AWinControl: TWinControl; const AClass: TClass; const DbgText: String): Boolean;
+//function CheckWidget(const Handle: HWND; const AMethodName: String; AParamName: String = ''): Boolean;
+//function CheckWidget(const Handle: HWND; const AMethodName: String; AClass: TClass): Boolean;
+
+implementation
+
+
+
+{ TCarbonWidget }
+
+constructor TAndroidWindow.Create(const AObject: TWinControl;
+  const AParams: TCreateParams);
+begin
+
+end;
+
+destructor TAndroidWindow.Destroy;
+begin
+  inherited Destroy;
+end;
+
+end.
