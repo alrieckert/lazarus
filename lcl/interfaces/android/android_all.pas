4d38e5e3a7934219429fb4c972d0c38408b680a1
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-07-18 13:32:32 +0000

    android: Adds the latest bindings and is now compilable
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@31734 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/android/android_all.pas
+++ lcl/interfaces/android/android_all.pas
@@ -1,8 +1,133 @@
-unit ;
+unit android_all;
 
 interface
 
+uses javalang, androidpipescomm;
+
 type
+
+  { Forward declaration of classes }
+
+  TDisplayMetrics = class;
+  TDisplay = class;
+  TWindowManager = class;
+  TLayoutParams = class;
+  TView = class;
+  TViewGroup = class;
+  TLinearLayout = class;
+  TAbsoluteLayout = class;
+  TAbsoluteLayout_LayoutParams = class;
+  TTextView = class;
+  TEditText = class;
+  TButton = class;
+  TFrameLayout = class;
+  TTimePicker = class;
+  TScrollView = class;
+  TCompoundButton = class;
+  TCheckBox = class;
+
+  { Types }
+
+  TOnClickListener = procedure (v: TView) of object;
+
+  { Classes }
+
+  TDisplayMetrics = class(TJavaObject)
+  public
+    constructor Create();
+    function density(): Single;
+    function densityDpi(): Integer;
+    function heightPixels(): Integer;
+    function scaledDensity(): Single;
+    function widthPixels(): Integer;
+    function xdpi(): Single;
+    function ydpi(): Single;
+  end;
+
+  TDisplay = class(TJavaObject)
+  public
+    procedure getMetrics(var outMetrics: TDisplayMetrics);
+  end;
+
+  TWindowManager = class(TJavaObject)
+  public
+    function getDefaultDisplay(): TDisplay;
+  end;
+
+  TLayoutParams = class(TJavaObject)
+  public
+    constructor Create();
+  end;
+
+  TView = class(TJavaObject)
+  public
+    procedure setLayoutParams(var params: TLayoutParams);
+    procedure setVisibility(visibility: Integer);
+  end;
+
+  TViewGroup = class(TView)
+  public
+  end;
+
+  TLinearLayout = class(TViewGroup)
+  public
+    constructor Create();
+  end;
+
+  TAbsoluteLayout = class(TViewGroup)
+  public
+    constructor Create();
+  end;
+
+  TAbsoluteLayout_LayoutParams = class(TLayoutParams)
+  public
+    constructor Create();
+  end;
+
+  TTextView = class(TView)
+  public
+    constructor Create();
+    procedure setText(var AText: string);
+  public
+    OnClickListener: TOnClickListener;
+    procedure setOnClickListener(ACallback: TOnClickListener);
+    procedure callOnClickListener();
+  public
+    procedure setTextSize(unit_: Integer; size: Single);
+  end;
+
+  TEditText = class(TTextView)
+  public
+    constructor Create();
+    procedure setText(var AText: string);
+  end;
+
+  TButton = class(TTextView)
+  public
+    constructor Create();
+    procedure setText(var AText: string);
+  end;
+
+  TFrameLayout = class(TViewGroup)
+  public
+  end;
+
+  TTimePicker = class(TFrameLayout)
+  public
+    constructor Create();
+    function getCurrentHour(): Integer;
+    procedure setCurrentHour(currentHour: Integer);
+    function getCurrentMinute(): Integer;
+    procedure setCurrentMinute(currentMinute: Integer);
+    function is24HourView(): Boolean;
+    procedure setIs24HourView(AIs24HourView: Boolean);
+  end;
+
+  TScrollView = class(TFrameLayout)
+  public
+    constructor Create();
+  end;
+
   TCompoundButton = class(TButton)
   public
     function isChecked(): Boolean;
@@ -16,17 +141,351 @@ type
     constructor Create();
   end;
 
+function HandleMessage(AFirstInt: Integer): Boolean;
+
 implementation
 
 const
-  // TCompoundButton
-  amkUI_TCompoundButton_isChecked = $00101000;
-  amkUI_TCompoundButton_performClick = $00101001;
-  amkUI_TCompoundButton_setChecked = $00101002;
-  amkUI_TCompoundButton_toggle = $00101003;
-  // TCheckBox
-  amkUI_TCheckBox_Create = $00102000;
+  { Constants }
 
+  { TDisplayMetrics }
+  { TDisplay }
+  { TWindowManager }
+  { TLayoutParams }
+  FILL_PARENT = $FFFFFFFF;
+  { TView }
+  VISIBLE = 0;
+  INVISIBLE = 4;
+  GONE = 8;
+  { TViewGroup }
+  { TLinearLayout }
+  HORIZONTAL = 0;
+  VERTICAL = 1;
+  { TAbsoluteLayout }
+  { TAbsoluteLayout_LayoutParams }
+  { TTextView }
+  { TEditText }
+  { TButton }
+  { TFrameLayout }
+  { TTimePicker }
+  { TScrollView }
+  { TCompoundButton }
+  { TCheckBox }
+
+  { IDs }
+
+  // TDisplayMetrics
+  amkUI_TDisplayMetrics_Create = $00101000;
+  amkUI_TDisplayMetrics_density = $00101001;
+  amkUI_TDisplayMetrics_densityDpi = $00101002;
+  amkUI_TDisplayMetrics_heightPixels = $00101003;
+  amkUI_TDisplayMetrics_scaledDensity = $00101004;
+  amkUI_TDisplayMetrics_widthPixels = $00101005;
+  amkUI_TDisplayMetrics_xdpi = $00101006;
+  amkUI_TDisplayMetrics_ydpi = $00101007;
+  // TDisplay
+  amkUI_TDisplay_getMetrics = $00102000;
+  // TWindowManager
+  amkUI_TWindowManager_getDefaultDisplay = $00103000;
+  // TLayoutParams
+  amkUI_TLayoutParams_Create = $00104000;
+  // TView
+  amkUI_TView_setLayoutParams = $00105000;
+  amkUI_TView_setVisibility = $00105001;
+  // TViewGroup
+  // TLinearLayout
+  amkUI_TLinearLayout_Create = $00107000;
+  // TAbsoluteLayout
+  amkUI_TAbsoluteLayout_Create = $00108000;
+  // TAbsoluteLayout_LayoutParams
+  amkUI_TAbsoluteLayout_LayoutParams_Create = $00109000;
+  // TTextView
+  amkUI_TTextView_Create = $0010A000;
+  amkUI_TTextView_setText = $0010A001;
+  amkUI_TTextView_setOnClickListener = $0010A002;
+  amkUI_TTextView_OnClickListener_Start = $0010A002;
+  amkUI_TTextView_OnClickListener_Finished = $0010A002;
+  amkUI_TTextView_setTextSize = $0010A002;
+  // TEditText
+  amkUI_TEditText_Create = $0010B000;
+  amkUI_TEditText_setText = $0010B001;
+  // TButton
+  amkUI_TButton_Create = $0010C000;
+  amkUI_TButton_setText = $0010C001;
+  // TFrameLayout
+  // TTimePicker
+  amkUI_TTimePicker_Create = $0010E000;
+  amkUI_TTimePicker_getCurrentHour = $0010E001;
+  amkUI_TTimePicker_setCurrentHour = $0010E002;
+  amkUI_TTimePicker_getCurrentMinute = $0010E003;
+  amkUI_TTimePicker_setCurrentMinute = $0010E004;
+  amkUI_TTimePicker_is24HourView = $0010E005;
+  amkUI_TTimePicker_setIs24HourView = $0010E006;
+  // TScrollView
+  amkUI_TScrollView_Create = $0010F000;
+  // TCompoundButton
+  amkUI_TCompoundButton_isChecked = $00110000;
+  amkUI_TCompoundButton_performClick = $00110001;
+  amkUI_TCompoundButton_setChecked = $00110002;
+  amkUI_TCompoundButton_toggle = $00110003;
+  // TCheckBox
+  amkUI_TCheckBox_Create = $00111000;
+
+{ Implementation of Classes }
+
+constructor TDisplayMetrics.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+function TDisplayMetrics.density(): Single;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_density);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Single(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.densityDpi(): Integer;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_densityDpi);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Integer(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.heightPixels(): Integer;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_heightPixels);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Integer(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.scaledDensity(): Single;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_scaledDensity);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Single(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.widthPixels(): Integer;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_widthPixels);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Integer(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.xdpi(): Single;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_xdpi);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Single(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+function TDisplayMetrics.ydpi(): Single;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplayMetrics_ydpi);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Single(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+procedure TDisplay.getMetrics(var outMetrics: TDisplayMetrics);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TDisplay_getMetrics);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(outMetrics));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+function TWindowManager.getDefaultDisplay(): TDisplay;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TWindowManager_getDefaultDisplay);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := TDisplay(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+constructor TLayoutParams.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TLayoutParams_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+procedure TView.setLayoutParams(var params: TLayoutParams);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TView_setLayoutParams);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(params));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+procedure TView.setVisibility(visibility: Integer);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TView_setVisibility);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(visibility));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+constructor TLinearLayout.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TLinearLayout_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+constructor TAbsoluteLayout.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TAbsoluteLayout_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+constructor TAbsoluteLayout_LayoutParams.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TAbsoluteLayout_LayoutParams_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+constructor TTextView.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTextView_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+procedure TTextView.setText(var AText: string);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTextView_setText);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(AText));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+procedure TTextView.setOnClickListener(ACallback: TOnClickListener);
+begin
+  OnClickListener := ACallback;
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTextView_setOnClickListener);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Index
+  vAndroidPipesComm.SendInt(PtrInt(Self)); // Self, Pascal pointer
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+procedure TTextView.callOnClickListener();
+begin
+  if Assigned(OnClickListener) then OnClickListener(Self);
+end;
+procedure TTextView.setTextSize(unit_: Integer; size: Single);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTextView_setTextSize);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(unit_));
+  vAndroidPipesComm.SendInt(Integer(size));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+constructor TEditText.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TEditText_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+procedure TEditText.setText(var AText: string);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TEditText_setText);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(AText));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+constructor TButton.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TButton_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+procedure TButton.setText(var AText: string);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TButton_setText);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(AText));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+constructor TTimePicker.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+function TTimePicker.getCurrentHour(): Integer;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_getCurrentHour);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Integer(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+procedure TTimePicker.setCurrentHour(currentHour: Integer);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_setCurrentHour);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(currentHour));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+function TTimePicker.getCurrentMinute(): Integer;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_getCurrentMinute);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Integer(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+procedure TTimePicker.setCurrentMinute(currentMinute: Integer);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_setCurrentMinute);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(currentMinute));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+function TTimePicker.is24HourView(): Boolean;
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_is24HourView);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  Result := Boolean(vAndroidPipesComm.WaitForIntReturn());
+end;
+
+procedure TTimePicker.setIs24HourView(AIs24HourView: Boolean);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TTimePicker_setIs24HourView);
+  vAndroidPipesComm.SendInt(Index); // Self, Java Pointer
+  vAndroidPipesComm.SendInt(Integer(AIs24HourView));
+  vAndroidPipesComm.WaitForReturn();
+end;
+
+constructor TScrollView.Create();
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
+  vAndroidPipesComm.SendInt(amkUI_TScrollView_Create);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
 function TCompoundButton.isChecked(): Boolean;
 begin
   vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
@@ -65,6 +524,23 @@ begin
   vAndroidPipesComm.SendByte(ShortInt(amkUICommand));
   vAndroidPipesComm.SendInt(amkUI_TCheckBox_Create);
   Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+
+{ Message Handling }
+
+function HandleMessage(AFirstInt: Integer): Boolean;
+var
+  lInt: Integer;
+  lPascalPointer: PtrInt = -1;
+begin
+  case AFirstInt of
+  amkUI_TTextView_OnClickListener_Start:
+  begin
+    lPascalPointer := vAndroidPipesComm.ReadInt();
+    TTextView(lPascalPointer).callOnClickListener();
+    vAndroidPipesComm.SendMessage(amkUICommand, amkUI_TTextView_OnClickListener_Finished);
+  end;
+  end;
 end;
 
 end.
---
+++ lcl/interfaces/android/javalang.pas
@@ -0,0 +1,72 @@
+unit javalang;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fgl;
+
+type
+
+  { TJavaObject }
+
+  TJavaObject = class
+  public
+    Index: Integer;
+    constructor Create(AIndex: Integer); virtual;
+  end;
+
+  TCharSequence = class(TJavaObject)
+  public
+  end;
+
+  { TString }
+
+  TString = class(TCharSequence)
+  public
+    constructor Create(AStr: string);
+  end;
+
+  TJavaObjectList = specialize TFPGList<TJavaObject>;
+
+function FindIndexInList(AList: TJavaObjectList; AIndex: Integer): TJavaObject;
+
+implementation
+
+uses androidpipescomm;
+
+const
+  amkJavaLang_New_String = $0000;
+
+function FindIndexInList(AList: TJavaObjectList; AIndex: Integer): TJavaObject;
+var
+  i: Integer;
+begin
+  Result := nil;
+
+  for i := 0 to AList.Count - 1 do
+  begin
+    if AList.Items[i].Index = AIndex then Exit(AList.Items[i]);
+  end;
+end;
+
+{ TJavaObject }
+
+constructor TJavaObject.Create(AIndex: Integer);
+begin
+  Index := AIndex;
+end;
+
+{ TString }
+
+constructor TString.Create(AStr: string);
+begin
+  vAndroidPipesComm.SendByte(ShortInt(amkJavaLangCall));
+  vAndroidPipesComm.SendInt(amkJavaLang_New_String);
+  vAndroidPipesComm.SendString(AStr);
+  Index := vAndroidPipesComm.WaitForIntReturn();
+end;
+
+end.
+
