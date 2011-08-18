2b0380580d7d3c4164c495240fcf7457a9119d6c
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-08-18 16:51:12 +0000

    Revert r31957, it broke canceling project save.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32005 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/main.pp
+++ ide/main.pp
@@ -9055,14 +9055,15 @@ begin
           AText:=Format(lisSourceOfPageHasChangedSave, ['"',
             TSourceEditor(AEditor).PageName, '"']);
         ACaption:=lisSourceModified;
-        Result:=QuestionDlg(ACaption, AText, mtConfirmation,
-                            [mrYes, lisMenuSave, mrNo, lisDiscardChanges, mrCancel], 0);
+        Result:=QuestionDlg(ACaption, AText,
+            mtConfirmation, [mrYes, lisMenuSave, mrNo, lisDiscardChanges, mrAbort
+              ], 0);
       end else
         Result:=mrYes;
       if Result=mrYes then begin
         Result:=DoSaveEditorFile(AnEditorInfo.EditorComponent,[sfCheckAmbiguousFiles]);
       end;
-      if Result=mrCancel then exit;
+      if Result=mrAbort then exit;
       Result:=mrOk;
     end;
 
