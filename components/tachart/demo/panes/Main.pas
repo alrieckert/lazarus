38d26e9f0ed337ab931ec77c60be5872453ab739
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-08-29 14:43:56 +0000

    TAChart: Add "Transform" page to the panes demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32097 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/panes/Main.pas
+++ components/tachart/demo/panes/Main.pas
@@ -6,10 +6,17 @@ interface
 
 uses
   Classes, ComCtrls, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  TAChartExtentLink, TAGraph, TASeries, TASources;
+  TAChartExtentLink, TAGraph, TASeries, TASources, TATransformations;
 
 type
   TForm1 = class(TForm)
+    catAuto1: TChartAxisTransformations;
+    catAuto1AutoScaleAxisTransform1: TAutoScaleAxisTransform;
+    catAuto1AutoScaleAxisTransform2: TAutoScaleAxisTransform;
+    catAuto1AutoScaleAxisTransform3: TAutoScaleAxisTransform;
+    catAuto2: TChartAxisTransformations;
+    catAuto3: TChartAxisTransformations;
+    chTransform: TChart;
     ChartExtentLink1: TChartExtentLink;
     chLink1: TChart;
     chLink1LineSeries1: TLineSeries;
@@ -17,10 +24,14 @@ type
     chLink3LineSeries1: TLineSeries;
     chLink2: TChart;
     chLink3: TChart;
+    chTransformLineSeries1: TLineSeries;
+    chTransformLineSeries2: TLineSeries;
+    chTransformLineSeries3: TLineSeries;
     PageControl1: TPageControl;
     rcsLink1: TRandomChartSource;
     rcsLink2: TRandomChartSource;
     rcsLink3: TRandomChartSource;
+    tsTransform: TTabSheet;
     tsLink: TTabSheet;
   end;
 
