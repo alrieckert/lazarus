0a851c3719e81e7ae9987580888dd2f14e1ce444
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-08-31 09:00:41 +0000

    TAChart: Add license header
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32134 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/editors/tadatapointseditor.pas
+++ components/tachart/editors/tadatapointseditor.pas
@@ -1,3 +1,18 @@
+{
+ *****************************************************************************
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+
+Author: Alexander Klenin
+
+}
 unit TADataPointsEditor;
 
 {$H+}
