6aa08f99c80745b61ee5570c3a32398e0755abf7
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-09-13 14:13:57 +0000

    LCL: raise an error if compiled with old fpc < 2.4.2
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32319 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -22,14 +22,14 @@
 }
 unit LCLVersion;
 
-{ At least 2.4.0 is required, except for wince which supports fpc 2.2.0+ too }
+{ At least 2.4.2 is required, except for wince which supports fpc 2.2.0+ too }
 {$ifdef Wince}
-  {$if defined(ver1) or (defined(ver2) and (fpc_release<2))}
+  {$if defined(ver1) or (FPC_FULLVERSION<20200)}
     {$fatal Lazarus for WinCE requires at least FPC 2.2.0}
   {$endif}
 {$else}
-  {$if defined(ver1) or (defined(ver2) and (fpc_release<4))}
-    {$fatal Lazarus requires at least FPC 2.4.0}
+  {$if defined(ver1) or (FPC_FULLVERSION<20402) }
+    {$fatal Lazarus requires at least FPC 2.4.2}
   {$endif}
 {$endif}
 
