ce2d91c410bb3cb6b3f5c8114fa0bd76fec61bde
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-09-13 12:43:04 +0000

    Fixes the header of paswstring.pas
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32314 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/paswstring.pas
+++ lcl/paswstring.pas
@@ -1,21 +1,21 @@
 {
-    This file is part of the Free Pascal run time library.
-    Copyright (c) 2011 by Felipe Monteiro de Carvalho
-
-    Pascal wide string support
-
-    See the file COPYING.FPC, included in this distribution,
-    for details about the copyright.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- **********************************************************************}
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+unit paswstring;
 
 {$mode objfpc}
 {$inline on}
-
-unit paswstring;
 
 interface
 
