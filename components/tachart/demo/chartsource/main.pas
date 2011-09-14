a7b7cb7eb050de4c5037d96619e2314f1c4f759f
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-09-14 11:44:38 +0000

    TAChart: Add "Derivative" page to the source demo. Based on code by Werner Pamler
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32333 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/chartsource/main.pas
+++ components/tachart/demo/chartsource/main.pas
@@ -5,36 +5,53 @@ unit Main;
 interface
 
 uses
-  ComCtrls, ExtCtrls, StdCtrls, Forms, TAGraph, TASeries, TASources, Classes;
+  ComCtrls, ExtCtrls, Spin, StdCtrls, Forms, TAGraph, TASeries, TASources,
+  Classes;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    ccsDerivative: TCalculatedChartSource;
     cbCumulative: TCheckBox;
     ccsAvg: TCalculatedChartSource;
     ccsSum: TCalculatedChartSource;
     Chart1: TChart;
     Chart1BarSeries1: TBarSeries;
     Chart1LineSeries1: TLineSeries;
+    chDerivativeLineOrig: TLineSeries;
+    chDerivativeLineDeriv: TLineSeries;
+    Chart1LineSeries4: TLineSeries;
+    Chart1LineSeries5: TLineSeries;
     Chart2: TChart;
     Chart2AreaSeries1: TAreaSeries;
     Chart2LineSeries1: TLineSeries;
+    chDerivative: TChart;
     chCalc: TChart;
     chCalcLineSeries1: TLineSeries;
     chCalcLineSeriesAvg: TLineSeries;
     chCalcLineSeriesSum: TLineSeries;
+    seAccumulationRange: TSpinEdit;
+    lblAccumulationRange: TLabel;
     ListChartSource1: TListChartSource;
+    lcsDerivative: TListChartSource;
     Memo1: TMemo;
     PageControl1: TPageControl;
+    Panel1: TPanel;
     Panel2: TPanel;
+    rgDataShape: TRadioGroup;
     RandomChartSource1: TRandomChartSource;
     RandomChartSource2: TRandomChartSource;
     Splitter1: TSplitter;
+    tsDerivative: TTabSheet;
     tsStatistics: TTabSheet;
     tsBasic: TTabSheet;
     procedure cbCumulativeChange(Sender: TObject);
+    procedure CreateData;
+    procedure seAccumulationRangeChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure rgDataShapeClick(Sender: TObject);
   end;
 
 var
@@ -44,11 +61,60 @@ implementation
 
 {$R *.lfm}
 
+uses
+  Math;
+
 { TForm1 }
 
 procedure TForm1.cbCumulativeChange(Sender: TObject);
 begin
   chCalcLineSeriesSum.Active := cbCumulative.Checked;
+end;
+
+procedure TForm1.CreateData;
+const
+  N = 100;
+  MIN_X = -10;
+  MAX_X = 10;
+  EPS = 1e-6;
+var
+  i: Integer;
+  x, y: Double;
+begin
+  lcsDerivative.Clear;
+  if rgDataShape.ItemIndex = 6 then
+    for i := 0 to 9 do
+      lcsDerivative.Add(i - IfThen(i > 6, 1, 0), i)
+  else
+    for i := 0 to N - 1 do begin
+      x := MIN_X + (MAX_X - MIN_X) / (N - 1) * i;
+      if SameValue(x, 0.0, EPS) then x := 0;
+      case rgDataShape.ItemIndex of
+        0: y := x;
+        1: y := Sin(x);
+        2: if (x = 0) then y := 1 else y := Sin(x) / x;
+        3: y := Exp(-x / 3);
+        4: y := Exp(-Sqr((x - 2.5) / 2.5));
+        5: y := Exp(-Sqr((x - 2.5) / 2.5)) + 0.05 * (Random - 0.5);
+      end;
+      lcsDerivative.Add(x, y);
+    end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Randomize;
+  CreateData;
+end;
+
+procedure TForm1.rgDataShapeClick(Sender: TObject);
+begin
+  CreateData;
+end;
+
+procedure TForm1.seAccumulationRangeChange(Sender: TObject);
+begin
+  ccsDerivative.AccumulationRange := seAccumulationRange.Value;
 end;
 
 end.
