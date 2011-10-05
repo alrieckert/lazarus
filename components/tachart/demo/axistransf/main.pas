6fb9ac4ccbd2da0e7d9b4088071c1d65bd0d1ee7
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-10-05 07:49:52 +0000

    TAChart: Use TCumulNormDistrAxisTransform in the axis transform demo. Based on code by Werner Pamler.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@32693 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/axistransf/main.pas
+++ components/tachart/demo/axistransf/main.pas
@@ -13,6 +13,9 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    AxLabelSource: TListChartSource;
+    catCumulNormDistrCumulNormDistrAxisTransform1: TCumulNormDistrAxisTransform;
+    catCumulNormDistrLinearAxisTransform1: TLinearAxisTransform;
     catIndependent1Zoom: TLinearAxisTransform;
     catIndependent2Zoom: TLinearAxisTransform;
     catIndependent2: TChartAxisTransformations;
@@ -23,9 +26,14 @@ type
     catUser: TChartAxisTransformations;
     catUserUserDefinedAxisTransform1: TUserDefinedAxisTransform;
     catIndependent1: TChartAxisTransformations;
+    cbPercent: TCheckBox;
+    cbUseAxisTransform: TCheckBox;
+    ChartCumulNormDistr: TChart;
+    catCumulNormDistr: TChartAxisTransformations;
     ChartIndependent: TChart;
     ChartIndependentLineSeries1: TLineSeries;
     ChartIndependentLineSeries2: TLineSeries;
+    ChartCumulNormDistrLineSeries1: TLineSeries;
     ChartToolset1: TChartToolset;
     ChartToolset1DataPointDragTool1: TDataPointDragTool;
     ChartUser: TChart;
@@ -45,18 +53,23 @@ type
     catTFahrToCel: TLinearAxisTransform;
     ChartTSummer: TLineSeries;
     ChartTWinterLine: TLineSeries;
+    edDataCount: TSpinEdit;
     fseIndependent1: TFloatSpinEdit;
     fseIndependent2: TFloatSpinEdit;
+    lblDataCount: TLabel;
     lblIndependentScale1: TLabel;
     lblIndependentScale2: TLabel;
     PageControl1: TPageControl;
+    pnCumulNormDistr: TPanel;
     pnlIndependentControls: TPanel;
     pnlLogControls: TPanel;
     pnlAutoControls: TPanel;
     rcsUser: TRandomChartSource;
     rcsTSummer: TRandomChartSource;
     rcsTWinter: TRandomChartSource;
-    lsLinear: TTabSheet;
+    rgRandDistr: TRadioGroup;
+    tsLinear: TTabSheet;
+    tsCumulNormDistr: TTabSheet;
     tsIndependent: TTabSheet;
     tsUser: TTabSheet;
     tsLog: TTabSheet;
@@ -64,12 +77,17 @@ type
     procedure cbLogChange(Sender: TObject);
     procedure catUserUserDefinedAxisTransform1AxisToGraph(
       AX: Double; out AT: Double);
+    procedure cbPercentChange(Sender: TObject);
+    procedure cbUseAxisTransformChange(Sender: TObject);
     procedure ChartLogFuncSeries1Calculate(const AX: Double; out AY: Double);
+    procedure edDataCountChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure fseIndependent1Change(Sender: TObject);
     procedure fseIndependent2Change(Sender: TObject);
+    procedure rgRandDistrClick(Sender: TObject);
   private
     procedure FillIndependentSource;
+    procedure FillCumulNormDistrSource;
   end;
 
 var
@@ -78,9 +96,34 @@ var
 implementation
 
 uses
-  Math, SysUtils, TAChartAxis, TAChartUtils;
+  Math, StrUtils, SysUtils, TAChartAxis, TAChartUtils;
 
 {$R *.lfm}
+
+var
+  GaussDevAvail: Boolean = false;
+  GaussDev: Double = 0.0;
+
+// Create a random number with normal distribution, mean value 0, standard
+// deviation 1. See Numerical Recipes.
+function RndNormal: Double;
+var
+  fac, r, v1, v2: Double;
+begin
+  if GaussDevAvail then
+    Result := GaussDev
+  else begin
+    repeat
+      v1 := 2.0 * Random - 1.0;
+      v2 := 2.0 * Random - 1.0;
+      r := Sqr(v1) + Sqr(v2);
+    until (r > 0.0) and (r < 1.0);
+    fac := Sqrt(-2.0 * Ln(r) / r);
+    GaussDev := v1 * fac;
+    Result := v2 * fac;
+  end;
+  GaussDevAvail := not GaussDevAvail;
+end;
 
 function MyFunc(AX: Double): Double;
 begin
@@ -98,6 +141,20 @@ end;
 procedure TForm1.cbLogChange(Sender: TObject);
 begin
   ChartAxisTransformations1LogarithmAxisTransform1.Enabled := cbLog.Checked;
+end;
+
+procedure TForm1.cbPercentChange(Sender: TObject);
+begin
+  catCumulNormDistrLinearAxisTransform1.Enabled := cbPercent.Checked;
+  FillCumulNormDistrSource;
+  ChartCumulNormDistr.LeftAxis.Title.Caption :=
+    'Cumulative probability' + IfThen(cbPercent.Checked, ' (%)', '');
+end;
+
+procedure TForm1.cbUseAxisTransformChange(Sender: TObject);
+begin
+  catCumulNormDistrCumulNormDistrAxisTransform1.Enabled :=
+    cbUseAxisTransform.Checked;
 end;
 
 procedure TForm1.catUserUserDefinedAxisTransform1AxisToGraph(
@@ -119,6 +176,41 @@ end;
 procedure TForm1.ChartLogFuncSeries1Calculate(const AX: Double; out AY: Double);
 begin
   AY := MyFunc(AX);
+end;
+
+procedure TForm1.edDataCountChange(Sender: TObject);
+begin
+  FillCumulNormDistrSource;
+end;
+
+procedure TForm1.FillCumulNormDistrSource;
+var
+  i: Integer;
+  y: Double;
+  s: TListChartSource;
+begin
+  RandSeed := 976896;
+  s := ChartCumulNormDistrLineSeries1.ListSource;
+  s.BeginUpdate;
+  try
+    s.Clear;
+    // Add random test data as x values --> random values will
+    // get sorted in ascending direction automatically.
+    s.Sorted := false;
+    for i := 1 to edDataCount.Value do
+      case rgRandDistr.ItemIndex of
+        0: s.Add(Random, 0);
+        1: s.Add(RndNormal, 0);
+      end;
+    s.Sorted := true;
+    // Calculate cumulative probability from index in sorted list.
+    for i := 0 to s.Count - 1 do begin
+      y := (i + 1) / (s.Count + 1); // Add 1 since y=0 and y=1 are not valid.
+      s.Item[i]^.Y := IfThen(CbPercent.Checked, y * 100, y);
+    end;
+  finally
+    s.EndUpdate;
+  end;
 end;
 
 procedure TForm1.FillIndependentSource;
@@ -149,6 +241,7 @@ begin
     clsLogPoints.AddXY(x + Random - 0.5, MyFunc(x) + Random - 0.5);
   end;
   FillIndependentSource;
+  FillCumulNormDistrSource;
 end;
 
 procedure TForm1.fseIndependent1Change(Sender: TObject);
@@ -159,6 +252,11 @@ end;
 procedure TForm1.fseIndependent2Change(Sender: TObject);
 begin
   catIndependent2Zoom.Scale := fseIndependent2.Value;
+end;
+
+procedure TForm1.rgRandDistrClick(Sender: TObject);
+begin
+  FillCumulNormDistrSource;
 end;
 
 end.
