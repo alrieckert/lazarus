89e081f9c9629e67359a16039b6e221ce688de1f
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-11-01 06:11:43 +0000

    LazReport, added barcode.pas file to package
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@33197 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lazreport.pas
+++ components/lazreport/source/lazreport.pas
@@ -12,7 +12,7 @@ uses
   lr_expres, LR_Flds, LR_fmted, lr_funct_editor_unit, lr_funct_editor_unit1, 
   LR_GEdit, LR_GrpEd, LR_IFlds, LR_Pars, LR_pgopt, LR_PGrid, LR_PrDlg, 
   LR_Prntr, LR_progr, lr_propedit, LR_Register, LR_RRect, LR_Shape, LR_Utils, 
-  LR_Var, LR_Vared, LR_View, LR_Newrp, LazarusPackageIntf;
+  LR_Var, LR_Vared, LR_View, LR_Newrp, Barcode, LazarusPackageIntf;
 
 implementation
 
