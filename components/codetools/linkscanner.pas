1dbae3080c54a64fbe4e04b7e2c90e8db7694431
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-11-02 23:17:47 +0000

    Codetools: remove obsolete LinkScannerRangeNames, use WriteStr
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@33240 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/linkscanner.pas
+++ components/codetools/linkscanner.pas
@@ -583,23 +583,6 @@ var
   PSourceLinkMemManager: TPSourceLinkMemManager;
   PSourceChangeStepMemManager: TPSourceChangeStepMemManager;
 
-const
-  LinkScannerRangeNames: array[TLinkScannerRange] of string = (
-    'lsrNone',
-    'lsrInit',
-    'lsrSourceType',
-    'lsrSourceName',
-    'lsrInterfaceStart',
-    'lsrMainUsesSectionStart',
-    'lsrMainUsesSectionEnd',
-    'lsrImplementationStart',
-    'lsrImplementationUsesSectionStart',
-    'lsrImplementationUsesSectionEnd',
-    'lsrInitializationStart',
-    'lsrFinalizationStart',
-    'lsrEnd'
-    );
-
 procedure AddCodeToUniqueList(ACode: Pointer; UniqueSortedCodeList: TFPList);
 function IndexOfCodeInUniqueList(ACode: Pointer;
                                  UniqueSortedCodeList: TList): integer;
@@ -656,7 +639,7 @@ end;
 
 function dbgs(r: TLinkScannerRange): string; overload;
 begin
-  Result:=LinkScannerRangeNames[r];
+  WriteStr(Result, r);
 end;
 
 procedure AddCodeToUniqueList(ACode: Pointer; UniqueSortedCodeList: TFPList);
