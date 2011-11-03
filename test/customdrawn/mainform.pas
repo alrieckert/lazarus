7c1211e0b09a4d26bf796f2e5daa8e40b78cb903
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-11-03 13:06:31 +0000

    customdrawn: More tests
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@33261 4005530d-fff6-0310-9dd1-cebe43e6787f

--- test/customdrawn/mainform.pas
+++ test/customdrawn/mainform.pas
@@ -14,14 +14,29 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     CDButton1: TCDButton;
     CDButton2: TCDButton;
+    CDButton3: TCDButton;
+    CDButton4: TCDButton;
     CDEdit1: TCDEdit;
+    CDGroupBox1: TCDGroupBox;
+    CDGroupBox2: TCDGroupBox;
+    CDPageControl1: TCDPageControl;
+    CDTabSheet1: TCDTabSheet;
+    CDTabSheet2: TCDTabSheet;
+    CDTabSheet3: TCDTabSheet;
+    CheckBox1: TCheckBox;
     comboControls: TComboBox;
     Edit1: TEdit;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
     Memo1: TMemo;
     notebookControls: TNotebook;
     pageButtons: TPage;
+    PageControl1: TPageControl;
     pageEdits: TPage;
     pageCheckboxes: TPage;
     pageGroupBoxes: TPage;
@@ -30,6 +45,11 @@ type
     Page7: TPage;
     StaticText1: TStaticText;
     StaticText2: TStaticText;
+    StaticText3: TStaticText;
+    StaticText4: TStaticText;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
     TrackBar1: TTrackBar;
     CDTrackBar1: TCDTrackBar;
     procedure comboControlsChange(Sender: TObject);
