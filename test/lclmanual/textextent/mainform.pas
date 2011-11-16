b03e532d57f458133014452fe884cb67cfdb9f1f
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-11-16 12:52:23 +0000

    textextent test: Fixes rendering for widgetsets which correctly apply the Brush under TextOut
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@33564 4005530d-fff6-0310-9dd1-cebe43e6787f

--- test/lclmanual/textextent/mainform.pas
+++ test/lclmanual/textextent/mainform.pas
@@ -41,10 +41,13 @@ var
   lSize: TSize;
   i: Integer;
 begin
+  for i := 0 to 23 do lPartialWidths[i] := 0;
+
   // Basic initialization
   Image.Canvas.Font.Size := 12;
   Image.Canvas.Pen.Color := clWhite;
   Image.Canvas.Brush.Color := clWhite;
+  Image.Canvas.Brush.Style := bsSolid;
   Image.Canvas.Rectangle(0, 0, Image.Width, Image.Height);
 
   // TextFitInfo test 1 (calls GetTextExtentExPoint with partial widths = nil)
@@ -52,7 +55,9 @@ begin
   lRes := Image.Canvas.TextFitInfo(lStr, 80);
   Image.Canvas.TextOut(20, 20, Format('TextFitInfo Test 1. Returned %d should be aprox. 7 to 10', [lRes]));
   Image.Canvas.Brush.Color := clBlue;
+  Image.Canvas.Brush.Style := bsSolid;
   Image.Canvas.Rectangle(20, 35, 100, 50);
+  Image.Canvas.Brush.Style := bsClear;
   Image.Canvas.TextOut(20, 35, lStr);
 
   // GetTextExtentExPoint test 1 with partial widths and MaxCount
@@ -60,11 +65,14 @@ begin
   LCLIntf.GetTextExtentExPoint(Image.Canvas.Handle, PChar(lStr),
     Length(lStr), 50, @lMaxCount, @lPartialWidths[0], lSize);
   lRes := Image.Canvas.TextFitInfo(lStr, 50);
+  Image.Canvas.Brush.Style := bsClear;
   Image.Canvas.TextOut(20, 60, Format('GetTextExtentExPoint Test 1. Returned MaxCount=%d should be aprox. 4 to 6', [lMaxCount]));
   Image.Canvas.Brush.Color := clBlue;
   for i := 0 to 6 do
   begin
+    Image.Canvas.Brush.Style := bsSolid;
     Image.Canvas.Rectangle(20, 75 + i * 15, 20+lPartialWidths[i], 90 + i * 15);
+    Image.Canvas.Brush.Style := bsClear;
     Image.Canvas.TextOut(20, 75 + i *15, lStr);
   end;
 end;
