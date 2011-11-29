8dcd130739bc542a53be42f6d859887e435e3724
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-11-29 09:51:36 +0000

    lazcanvas: Implements polygon filling
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@33848 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lazcanvas.pas
+++ lcl/lazcanvas.pas
@@ -32,7 +32,7 @@ interface
 
 uses
   // RTL
-  Classes, SysUtils,
+  Classes, SysUtils, contnrs, Math,
   // FCL-Image
   fpimgcanv, fpcanvas, fpimage, clipping,
   // regions
@@ -49,6 +49,17 @@ type
     procedure Execute (x,y,w,h : integer); override;
   end;
 
+  { TLazCanvasState }
+
+  TLazCanvasState = class
+  public
+    Brush: TFPCustomBrush;
+    Pen: TFPCustomPen;
+    BaseWindowOrg: TPoint;
+    WindowOrg: TPoint;
+    destructor Destroy; override;
+  end;
+
   { TLazCanvas }
 
   TLazCanvas = class(TFPImageCanvas)
@@ -59,29 +70,44 @@ type
     FUseRegionClipping: Boolean;
     FLazClipRegion: TLazRegion;
     FWindowOrg: TPoint; // already in absolute coords with BaseWindowOrg summed up
+    GraphicStateStack: TObjectStack; // TLazCanvasState
     function GetAssignedBrush: TFPCustomBrush;
     function GetAssignedPen: TFPCustomPen;
     function GetWindowOrg: TPoint;
     procedure SetWindowOrg(AValue: TPoint);
   protected
     procedure SetColor (x,y:integer; const AValue:TFPColor); override;
+    // Routines broken in FPC
+    function IsPointInPolygon(AX, AY: Integer; const APolygon: array of TPoint): Boolean;
+    procedure DoPolygonFill (const points:array of TPoint); override;
   public
     constructor create (AnImage : TFPCustomImage);
     destructor destroy; override;
     procedure SetLazClipRegion(ARegion: TLazRegion);
+    // Canvas states stack
+    procedure SaveState;
+    procedure RestoreState;
+    // A simple operation to bring the Canvas in the default LCL TCanvas state
+    procedure ResetCanvasState;
     // Compatibility with older FPC versions
     {$if defined(ver2_4) or defined(ver2_5)}
     procedure FillRect(const ARect: TRect);
     procedure FillRect(X1,Y1,X2,Y2: Integer);
     {$endif}
-    // Utilized by LCLIntf.SelectObject
+    // Utilized by LCLIntf.SelectObject and by RestoreState
+    // This needed to be added because Pen/Brush.Assign raises exceptions
     procedure AssignPenData(APen: TFPCustomPen);
     procedure AssignBrushData(ABrush: TFPCustomBrush);
     // These properties are utilized to implement LCLIntf.SelectObject
+    // to keep track of which brush handle was assigned to this canvas
+    // They are not utilized by TLazCanvas itself
     property AssignedPen: TFPCustomPen read GetAssignedPen write FAssignedPen;
     property AssignedBrush: TFPCustomBrush read GetAssignedBrush write FAssignedBrush;
     //
-    // SetWindowOrg operations will be relative to BaseWindowOrg, useful for customdrawn wincontrols
+    // SetWindowOrg operations will be relative to BaseWindowOrg,
+    // This is very useful for implementing the non-native wincontrol,
+    // because operations of SetWindowOrg inside a non-native wincontrol will be
+    // based upon the BaseWindowOrg which is set relative to the Form canvas
     property BaseWindowOrg: TPoint read FBaseWindowOrg write FBaseWindowOrg;
     property ClipRegion: TLazRegion read FLazClipRegion write FLazClipRegion;
     property UseRegionClipping: Boolean read FUseRegionClipping write FUseRegionClipping; // when false ClipRect is used
@@ -89,6 +115,15 @@ type
   end;
 
 implementation
+
+{ TLazCanvasState }
+
+destructor TLazCanvasState.Destroy;
+begin
+  if Brush <> nil then Brush.Free;
+  if Pen <> nil then Pen.Free;
+  inherited Destroy;
+end;
 
 { TLazCanvas }
 
@@ -134,14 +169,91 @@ begin
   Clipping := OldClipping;
 end;
 
+//  The function will return True if the point x,y is inside the polygon, or
+//  False if it is not.
+//
+//  Original C code: http://www.visibone.com/inpoly/inpoly.c.txt
+//
+//  Translation from C by Felipe Monteiro de Carvalho
+//
+//  License: Public Domain
+function TLazCanvas.IsPointInPolygon(AX, AY: Integer; const APolygon: array of TPoint): Boolean;
+var
+  xnew, ynew: Cardinal;
+  xold,yold: Cardinal;
+  x1,y1: Cardinal;
+  x2,y2: Cardinal;
+  i, npoints: Integer;
+  inside: Integer = 0;
+begin
+  Result := False;
+  npoints := Length(APolygon);
+  if (npoints < 3) then Exit;
+  xold := APolygon[npoints-1].X;
+  yold := APolygon[npoints-1].Y;
+  for i := 0 to npoints - 1 do
+  begin
+    xnew := APolygon[i].X;
+    ynew := APolygon[i].Y;
+    if (xnew > xold) then
+    begin
+      x1:=xold;
+      x2:=xnew;
+      y1:=yold;
+      y2:=ynew;
+    end
+    else
+    begin
+      x1:=xnew;
+      x2:=xold;
+      y1:=ynew;
+      y2:=yold;
+    end;
+    if (((xnew < AX) = (AX <= xold))         // edge "open" at left end
+      and ((AY-y1)*(x2-x1) < (y2-y1)*(AX-x1))) then
+    begin
+      inside := not inside;
+    end;
+    xold:=xnew;
+    yold:=ynew;
+  end;
+  Result := inside <> 0;
+end;
+
+procedure TLazCanvas.DoPolygonFill(const points: array of TPoint);
+var
+  lBoundingBox: TRect;
+  x, y, i: integer;
+begin
+  // Find the Bounding Box of the Polygon
+  lBoundingBox := Rect(0, 0, 0, 0);
+  for i := low(Points) to High(Points) do
+  begin
+    lBoundingBox.Left := Min(Points[i].X, lBoundingBox.Left);
+    lBoundingBox.Top := Min(Points[i].Y, lBoundingBox.Top);
+    lBoundingBox.Right := Max(Points[i].X, lBoundingBox.Right);
+    lBoundingBox.Bottom := Max(Points[i].Y, lBoundingBox.Bottom);
+  end;
+
+  // Now scan all points using IsPointInPolygon
+  for x := lBoundingBox.Left to lBoundingBox.Right do
+    for y := lBoundingBox.Top to lBoundingBox.Bottom do
+    begin
+      if IsPointInPolygon(X, Y, Points) then SetColor(X, Y, Brush.FPColor);
+    end;
+end;
+
 constructor TLazCanvas.create(AnImage: TFPCustomImage);
 begin
   inherited Create(AnImage);
+  GraphicStateStack := TObjectStack.Create;
 end;
 
 destructor TLazCanvas.destroy;
 begin
+  GraphicStateStack.Free;
   if FAssignedBrush <> nil then FAssignedBrush.Free;
+  if FAssignedPen <> nil then FAssignedPen.Free;
   inherited destroy;
 end;
 
@@ -153,6 +265,40 @@ begin
     ClipRect := TLazRegionRect(ARegion.Parts.Items[0]).Rect;
     FLazClipRegion := ARegion;
   end;
+end;
+
+procedure TLazCanvas.SaveState;
+var
+  lState: TLazCanvasState;
+begin
+  lState := TLazCanvasState.Create;
+
+  lState.Brush := Brush.CopyBrush;
+  lState.Pen := Pen.CopyPen;
+
+  GraphicStateStack.Push(lState);
+end;
+
+procedure TLazCanvas.RestoreState;
+var
+  lState: TLazCanvasState;
+begin
+  lState := TLazCanvasState(GraphicStateStack.Pop());
+  if lState = nil then Exit;
+
+  AssignPenData(lState.Pen);
+  AssignBrushData(lState.Brush);
+
+  lState.Free;
+end;
+
+procedure TLazCanvas.ResetCanvasState;
+begin
+  Pen.FPColor := colBlack;
+  Pen.Style := psSolid;
+
+  Brush.FPColor := colWhite;
+  Brush.Style := bsSolid;
 end;
 
 {$if defined(ver2_4) or defined(ver2_5)}
@@ -176,6 +322,7 @@ end;
 
 procedure TLazCanvas.AssignPenData(APen: TFPCustomPen);
 begin
+  if APen = nil then Exit;
   Pen.FPColor := APen.FPColor;
   Pen.Style := APen.Style;
   Pen.Width := APen.Width;
@@ -183,6 +330,7 @@ end;
 
 procedure TLazCanvas.AssignBrushData(ABrush: TFPCustomBrush);
 begin
+  if ABrush = nil then Exit;
   Brush.FPColor := ABrush.FPColor;
   Brush.Style := ABrush.Style;
 end;
