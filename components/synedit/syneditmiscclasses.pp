d5da5775c3e94c904718414e272c5c1aa2bd82b1
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2011-12-15 13:09:20 +0000

    SynEdit: refactor
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@34206 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/syneditmiscclasses.pp
+++ components/synedit/syneditmiscclasses.pp
@@ -599,7 +599,7 @@ var
   i: TSynFrameSide;
 begin
   inherited AssignFrom(Src);
-  if not (Src is TSynHighlighterAttributes) then exit;
+  if not (Src is TSynSelectedColor) then exit;
   FStartX := TSynSelectedColor(Src).FStartX;
   FEndX   := TSynSelectedColor(Src).FEndX;
   for i := low(TSynFrameSide) to high(TSynFrameSide) do begin
