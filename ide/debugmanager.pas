77b28d22644f993269539cf7662f4f9ca305608e
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-01-14 14:59:18 +0000

    Debugger, ignore exceptions - Don't modify session, unless changed
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@34733 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/debugmanager.pas
+++ ide/debugmanager.pas
@@ -294,8 +294,10 @@ type
 
 procedure TProjectExceptions.SetIgnoreAll(const AValue: Boolean);
 begin
+  // Todo: move to Chamged or Update, but they are called to often...
+  if (IgnoreAll <> AValue) and (Project1 <> nil) then
+    Project1.Modified := True;
   inherited SetIgnoreAll(AValue);
-  Project1.Modified := True;
 end;
 
 procedure TProjectExceptions.Notify(Item: TCollectionItem; Action: TCollectionNotification);
