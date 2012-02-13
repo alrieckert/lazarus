710f30e697e5ed847085824ce62d7ae7805c14e4
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-02-13 13:45:30 +0000

    LCL: added lcEmulatedMDI TLCLCapability needed for mdi lcl emulator.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@35356 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfacebase.pp
+++ lcl/interfacebase.pp
@@ -84,7 +84,8 @@ type
     lcLMHelpSupport,            // support for LM_HELP command
     lcReceivesLMClearCutCopyPasteReliably, // In Carbon we do not receive LM_CLEAR, CUT, COPY, PASTE, etc reliably, and this affects DB controls. See bug http://bugs.freepascal.org/view.php?id=20394
     lcSendsUTF8KeyPress,        // If the interface does not yet send UTF8KeyPress directly, then it will be emulated in TWinControl.CNChar
-    lcAllowChildControlsInNativeControls // Utilized by LCL-CustomDrawn so that it can inject child controls in native ones
+    lcAllowChildControlsInNativeControls, // Utilized by LCL-CustomDrawn so that it can inject child controls in native ones
+    lcEmulatedMDI // used for emulating MDI on widgetsets which does not provide native MDI handling
   );
 
   { TDialogButton }
