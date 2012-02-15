ab4c83cac668adae4e6136040b7bf9781c909a8a
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-02-15 18:21:16 +0000

    LCL: vtUnicodeString is introduced in FPC 2.7.x. Add conditional compilation for it.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@35381 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/lclproc.pas
+++ lcl/lclproc.pas
@@ -2534,7 +2534,9 @@ begin
     vtPWideChar: s:=AnsiString(WideString(s)+Args[i].VPWideChar);
     vtWideChar: s:=AnsiString(WideString(s)+Args[i].VWideChar);
     vtWidestring: s:=AnsiString(WideString(s)+WideString(Args[i].VWideString));
+{$IF FPC_FULLVERSION>=20701}
     vtUnicodeString: s:=AnsiString(UnicodeString(s)+UnicodeString(Args[i].VUnicodeString));
+{$endif}
     vtObject: s:=s+DbgSName(Args[i].VObject);
     vtClass: s:=s+DbgSName(Args[i].VClass);
     vtPointer: s:=s+Dbgs(Args[i].VPointer);
