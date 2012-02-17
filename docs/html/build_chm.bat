aeda7ef557a81f27adf86e4385c3ae6c8d647cf7
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-02-17 17:51:54 +0000

    docs: Improve build_chm path handling, issue #21311, patch from BigChimp
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@35431 4005530d-fff6-0310-9dd1-cebe43e6787f

--- docs/html/build_chm.bat
+++ docs/html/build_chm.bat
@@ -3,9 +3,9 @@ REM Builds all Lazarus docs and pack them in one .chm file
 REM
 REM Notes:
 REM
-REM 1 - Please fix the path in this file for your installation
+REM 1 - If necessary, please fix the path so it finds fpdoc.exe (normally in your fpc compiler dir)
 REM 2 - Before running this file, first compile the project build_lcl_docs.lpi
 REM
-PATH=D:\programming\fpc\bin\i386-win32\
+PATH=D:\programming\fpc\bin\i386-win32\;%PATH%
 build_lcl_docs.exe --outfmt chm
 pause
--- docs/html/build_html.bat
+++ docs/html/build_html.bat
@@ -3,9 +3,9 @@ REM Builds all Lazarus docs
 REM
 REM Notes:
 REM
-REM 1 - Please fix the path in this file for your installation
+REM 1 - If necessary, please fix the path so it finds fpdoc.exe (normally in your fpc compiler dir)
 REM 2 - Before running this file, first compile the project build_lcl_docs.lpi
 REM
-PATH=C:\Programas\lazarus22\fpc\2.2.0\bin\i386-win32
+PATH=C:\Programas\lazarus22\fpc\2.2.0\bin\i386-win32;%PATH%
 build_lcl_docs.exe
 pause
