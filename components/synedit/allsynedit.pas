10d65181b46a06295a647d5ed2c12ae80fb7899e
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-03-12 20:35:03 +0000

    SynEdit: w32 IME improvements / exclude win only file
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@35904 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/allsynedit.pas
+++ components/synedit/allsynedit.pas
@@ -25,7 +25,7 @@ uses
   SynMacroRecorder, SynMemo, SynPluginSyncroEdit, SynPluginSyncronizedEditBase, 
   SynPluginTemplateEdit, SynPropertyEditObjectList, SynRegExpr, SynTextDrawer, 
   SynEditMarkupGutterMark, SynHighlighterBat, SynHighlighterIni, SynEditMarkupSpecialChar, 
-  LazSynEditText, LazSynTextArea, LazSynEditMouseCmdsTypes, SynHighlighterPo, LazSynIMM, 
+  LazSynEditText, LazSynTextArea, LazSynEditMouseCmdsTypes, SynHighlighterPo, 
   LazarusPackageIntf;
 
 implementation
