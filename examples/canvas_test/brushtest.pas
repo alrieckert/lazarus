ee918cdb93eb263bccb2bce15fbda2daa91403d2
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-04-06 14:34:36 +0000

    Expands the brush test
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36612 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/canvas_test/brushtest.pas
+++ examples/canvas_test/brushtest.pas
@@ -43,41 +43,8 @@ begin
     MyBitmap.Canvas.Brush.Color := clWhite;
     MyBitmap.Canvas.Pen.Color := clWhite;
     MyBitmap.Canvas.Rectangle(0, 0, Width, Height);
-{    // Explaning text
+    // Explaning text
     MyBitmap.Canvas.TextOut(100,  5, 'Brush: Blue  Pen: Black');
-
-    // Brush styles
-
-    MyBitmap.Canvas.TextOut( 25,  30, 'Brush styles:');
-
-    MyBitmap.Canvas.TextOut( 25,  60, 'bsSolid');
-    MyBitmap.Canvas.TextOut(125,  60, 'bsClear');
-    MyBitmap.Canvas.TextOut(225,  60, 'bsHorizontal');
-    MyBitmap.Canvas.TextOut(325,  60, 'bsVertical');
-    MyBitmap.Canvas.TextOut( 25, 160, 'bsFDiagonal');
-    MyBitmap.Canvas.TextOut(125, 160, 'bsBDiagonal');
-    MyBitmap.Canvas.TextOut(225, 160, 'bsCross');
-    MyBitmap.Canvas.TextOut(325, 160, 'bsDiagCross');
-
-    MyBitmap.Canvas.Brush.Color := clBlue;
-    MyBitmap.Canvas.Pen.Color := clBlack;
-
-    MyBitmap.Canvas.Brush.Style := bsSolid;
-    MyBitmap.Canvas.Rectangle(Bounds( 25,  75, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsClear;
-    MyBitmap.Canvas.Rectangle(Bounds(125,  75, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsHorizontal;
-    MyBitmap.Canvas.Rectangle(Bounds(225,  75, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsVertical;
-    MyBitmap.Canvas.Rectangle(Bounds(325,  75, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsFDiagonal;
-    MyBitmap.Canvas.Rectangle(Bounds( 25, 175, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsBDiagonal;
-    MyBitmap.Canvas.Rectangle(Bounds(125, 175, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsCross;
-    MyBitmap.Canvas.Rectangle(Bounds(225, 175, 50, 50));
-    MyBitmap.Canvas.Brush.Style := bsDiagCross;
-    MyBitmap.Canvas.Rectangle(Bounds(325, 175, 50, 50));}
 
     // Image brushes
     MyBitmap.Canvas.TextOut( 25,  25, 'Canvas-aligned bitmapped Brush:');
@@ -93,6 +60,45 @@ begin
     MyBitmap.Canvas.Rectangle(Bounds(100, 50, 50, 50));
     MyBitmap.Canvas.Brush.Bitmap.Free;
     MyBitmap.Canvas.Brush.Bitmap := nil;
+
+    // Brush styles
+
+    MyBitmap.Canvas.TextOut( 25, 110, 'Brush styles:');
+
+    MyBitmap.Canvas.TextOut( 25, 130, 'bsSolid');
+    MyBitmap.Canvas.TextOut(125, 130, 'bsClear');
+    MyBitmap.Canvas.TextOut(225, 130, 'bsHorizontal');
+    MyBitmap.Canvas.TextOut(325, 130, 'bsVertical');
+    MyBitmap.Canvas.TextOut( 25, 200, 'bsFDiagonal');
+    MyBitmap.Canvas.TextOut(125, 200, 'bsBDiagonal');
+    MyBitmap.Canvas.TextOut(225, 200, 'bsCross');
+    MyBitmap.Canvas.TextOut(325, 200, 'bsDiagCross');
+
+    MyBitmap.Canvas.Brush.Color := clBlue;
+    MyBitmap.Canvas.Pen.Color := clBlack;
+
+    MyBitmap.Canvas.Brush.Style := bsSolid;
+    MyBitmap.Canvas.Rectangle(Bounds( 25, 145, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsClear;
+    MyBitmap.Canvas.Rectangle(Bounds(125, 145, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsHorizontal;
+    MyBitmap.Canvas.Rectangle(Bounds(225, 145, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsVertical;
+    MyBitmap.Canvas.Rectangle(Bounds(325, 145, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsFDiagonal;
+    MyBitmap.Canvas.Rectangle(Bounds( 25, 215, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsBDiagonal;
+    MyBitmap.Canvas.Rectangle(Bounds(125, 215, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsCross;
+    MyBitmap.Canvas.Rectangle(Bounds(225, 215, 50, 50));
+    MyBitmap.Canvas.Brush.Style := bsDiagCross;
+    MyBitmap.Canvas.Rectangle(Bounds(325, 215, 50, 50));
+
+    // FillRect
+    MyBitmap.Canvas.TextOut( 25, 280, 'FillRect');
+    MyBitmap.Canvas.Brush.Color := clGray;
+    MyBitmap.Canvas.Brush.Style := bsSolid;
+    MyBitmap.Canvas.FillRect(Bounds( 25, 300, 50, 50));
 
     { Draw the bitmap to the form }
     Canvas.Draw(0, 0, MyBitmap);
