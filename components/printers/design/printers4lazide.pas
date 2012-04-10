93bb9103d9e252d7c7efe2c29397c8e3c5cc8612
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-04-10 12:08:24 +0000

    printers4lazide: added sourceprinter.pas
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36710 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/printers/design/printers4lazide.pas
+++ components/printers/design/printers4lazide.pas
@@ -1,21 +1,21 @@
 { This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+  This source is only used to compile and install the package.
  }
 
-unit printers4lazide; 
+unit printers4lazide;
 
 interface
 
 uses
-  ideprinting, LazarusPackageIntf; 
+  ideprinting, SourcePrinter, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('ideprinting', @ideprinting.Register); 
-end; 
+  RegisterUnit('ideprinting', @ideprinting.Register);
+end;
 
 initialization
-  RegisterPackage('printers4lazide', @Register); 
+  RegisterPackage('printers4lazide', @Register);
 end.
