9b8b0d14a945aa027c894c2404945d1323d8bdd0
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-04-13 12:45:54 +0000

    Adds a OpenURL/OpenDocument test
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36744 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ examples/openurltest/mainform.pas
@@ -0,0 +1,67 @@
+unit mainform;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  EditBtn, LCLIntf, LCLProc;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btnOpenURLHTTP: TButton;
+    btnOpenURLFILE: TButton;
+    btnOpenDocument: TButton;
+    btnFindBrowser: TButton;
+    editResult: TEdit;
+    editFileName: TFileNameEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure btnFindBrowserClick(Sender: TObject);
+    procedure btnOpenDocumentClick(Sender: TObject);
+    procedure btnOpenURLHTTPClick(Sender: TObject);
+    procedure btnOpenURLFILEClick(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btnOpenURLHTTPClick(Sender: TObject);
+begin
+  editResult.Text := BoolToStr(OpenURL('www.google.com'));
+end;
+
+procedure TForm1.btnOpenDocumentClick(Sender: TObject);
+begin
+  editResult.Text := BoolToStr(OpenDocument(editFilename.Text));
+end;
+
+procedure TForm1.btnFindBrowserClick(Sender: TObject);
+var
+  lStr, lParams: String;
+begin
+  FindDefaultBrowser(lStr, lParams);
+  editResult.Text := lStr + ' ' + lParams;
+end;
+
+procedure TForm1.btnOpenURLFILEClick(Sender: TObject);
+begin
+  editResult.Text := BoolToStr(OpenURL('file://'+editFilename.Text));
+end;
+
+end.
+
