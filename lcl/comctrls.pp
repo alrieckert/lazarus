003959a99ee0dea3c99653d82c644bd2dd72dcf7
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-04-21 20:56:27 +0000

    IDEIntf: fixed typo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36963 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/comctrls.pp
+++ lcl/comctrls.pp
@@ -2675,6 +2675,7 @@ type
     tvsManualNotify,
     tvsUpdating,
     tvsPainting,
+    tvoFocusedPainting,
     tvsDblClicked,
     tvsTripleClicked,
     tvsQuadClicked,
@@ -3065,6 +3066,7 @@ type
     property DragCursor;
     property DragMode;
     property Enabled;
+    property ExpandSignColor;
     property ExpandSignType;
     property Font;
     property HideSelection;
@@ -3140,7 +3142,6 @@ type
     property Items;
     property TreeLineColor;
     property TreeLinePenStyle;
-    property ExpandSignColor;
   end;
 
 
