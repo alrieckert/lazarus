a3ccb21ff2f3a3921d8001d7b1a602eb7d696926
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-05-28 14:57:33 +0000

    rx: clean up
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@37442 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/rx/rx.pas
+++ components/rx/rx.pas
@@ -1,22 +1,22 @@
 { This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+  This source is only used to compile and install the package.
  }
 
-unit rx; 
+unit rx;
 
 interface
 
 uses
-  AppUtils, MRUList, Placement, StrHolder, LazarusPackageIntf; 
+  AppUtils, MRUList, Placement, StrHolder, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('MRUList', @MRUList.Register); 
-  RegisterUnit('StrHolder', @StrHolder.Register); 
-end; 
+  RegisterUnit('MRUList', @MRUList.Register);
+  RegisterUnit('StrHolder', @StrHolder.Register);
+end;
 
 initialization
-  RegisterPackage('rx', @Register); 
+  RegisterPackage('rx', @Register);
 end.
