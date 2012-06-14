85f10a1bf852b51398da656fb65c40616b2a592b
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-06-14 20:50:11 +0000

    Add a missing file for #22232
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@37649 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/sqldb/pqteventmonitor.pas
@@ -0,0 +1,79 @@
+unit PQTEventMonitor;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,PQEventMonitor,ExtCtrls;
+
+
+type
+
+  { TPQTEventMonitor }
+
+  TPQTEventMonitor=class(TPQEventMonitor)
+  private
+    Fpoll: integer;
+    Timer:TTimer;
+    function GetPollInterval: integer;
+    procedure OnTimer(Sender: TObject);
+    procedure SetPollInterval(AValue: integer);
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure RegisterEvents; override;
+    procedure UnRegisterEvents; override;
+  published
+    property PollInterval:integer read GetPollInterval write SetPollInterval default 500;
+  end;
+
+implementation
+
+
+{ TPQTEventMonitor }
+
+procedure TPQTEventMonitor.SetPollInterval(AValue: integer);
+begin
+  if Timer.Interval=AValue then Exit;
+  Timer.Interval:=AValue;
+end;
+
+function TPQTEventMonitor.GetPollInterval: integer;
+begin
+  result:=Timer.Interval;
+end;
+
+procedure TPQTEventMonitor.OnTimer(Sender: TObject);
+begin
+  Poll;
+end;
+
+constructor TPQTEventMonitor.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  Timer:=TTImer.Create(self);
+  Timer.Interval:=500;
+  Timer.Enabled:=false;
+  Timer.OnTimer:=@OnTimer;
+end;
+
+destructor TPQTEventMonitor.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TPQTEventMonitor.RegisterEvents;
+begin
+  inherited RegisterEvents;
+  Timer.Enabled:=true;
+end;
+
+procedure TPQTEventMonitor.UnRegisterEvents;
+begin
+  Timer.Enabled:=false;
+  inherited UnRegisterEvents;
+end;
+
+end.
+
