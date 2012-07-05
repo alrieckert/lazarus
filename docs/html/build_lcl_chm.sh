fbc27855cc604e4e9bb2012b1bbbca4bb569ba0d
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-07-05 10:26:24 +0000

    docs: building lazutils+lcl chm
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@37869 4005530d-fff6-0310-9dd1-cebe43e6787f

--- docs/html/build_lcl_chm.sh
+++ docs/html/build_lcl_chm.sh
@@ -5,17 +5,22 @@
 # on 1.7GHz Core2 laptop single thread 6:26 minutes. Don't be impatient :-)
 
 # set to path to FPC docs dir. Default assume it is on the same level as the lazarus checkout
-FPCDIR=../../../fpcdocs
-FPCDIREXP=`cd $FPCDIR ; pwd`
+if [ -z "$FPCDocDir" ]; then
+  FPCDocDir=../../../fpcdocs
+fi
+FPCDocDirEXP=$(cd "$FPCDocDir" ; pwd)
 
-if [ -d "$FPCDIREXP" ]
+if [ -d "$FPCDocDirEXP" ]
 then
-echo FPCDOCS dir is $FPCDIREXP
+echo FPCDOCS dir is $FPCDocDirEXP
 else
-echo FPCDOCS dir $FPCDIR expanded to $FPCDIREXP and was not found.
+echo FPCDOCS dir $FPCDocDir expanded to $FPCDocDirEXP and was not found.
 exit 1
 fi
 
 export HTMLFMT=chm
-sh build_lazutils_html.sh fpdoc `pwd`/locallclfooter.xml $FPCDIREXP 1>lazutilsoutput.log 2>lazutilserror.log
-sh build_lcl_html.sh fpdoc `pwd`/locallclfooter.xml $FPCDIREXP 1>lcloutput.log 2>lclerror.log
+sh build_lazutils_html.sh fpdoc `pwd`/locallclfooter.xml $FPCDocDirEXP 1>lazutilsoutput.log 2>lazutilserror.log
+sh build_lcl_html.sh fpdoc `pwd`/locallclfooter.xml $FPCDocDirEXP 1>lcloutput.log 2>lclerror.log
+
+# end.
+
