74002e4793dbb828fd638092537210e88571afe8
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-07-24 22:52:28 +0000

    Win Installer: simplified build_lazarus
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@38037 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/win/build-lazarus.bat
+++ tools/install/win/build-lazarus.bat
@@ -4,9 +4,9 @@ SET OLDCURDIR=%CD%
 %BUILDDRIVE%
 cd %BUILDDIR%
 %MAKEEXE% clean PP=%COMPILER% >> %LOGFILE%
-%MAKEEXE% registration OPT="-Xs -XX" PP=%COMPILER% >> %LOGFILE% 
-%MAKEEXE% lazutils OPT="-gl -Ur" PP=%COMPILER% >> %LOGFILE% 
-%MAKEEXE% lcl OPT="-gl -Ur" PP=%COMPILER% >> %LOGFILE%
+rem %MAKEEXE% registration OPT="-Xs -XX" PP=%COMPILER% >> %LOGFILE% 
+rem %MAKEEXE% lazutils OPT="-gl -Ur" PP=%COMPILER% >> %LOGFILE% 
+rem %MAKEEXE% lcl OPT="-gl -Ur" PP=%COMPILER% >> %LOGFILE%
 
 IF NOT "%FPCTARGETOS%"=="win32" GOTO BUILDIDE
 rem %MAKEEXE% lcl OPT="-gl -Ur" PP=%COMPILER% LCL_PLATFORM=gtk2 >> %LOGFILE%
@@ -14,12 +14,16 @@ rem %MAKEEXE% lcl OPT="-gl -Ur" PP=%COMPILER% LCL_PLATFORM=qt >> %LOGFILE%
 
 :BUILDIDE
 IF "%IDE_WIDGETSET%"=="" SET IDE_WIDGETSET=win32
-%MAKEEXE% bigide OPT="-Xs -XX" PP=%COMPILER% LCL_PLATFORM=%IDE_WIDGETSET% >> %LOGFILE%
-%MAKEEXE% lazbuild OPT="-Xs -XX" PP=%COMPILER% >> %LOGFILE%
+%MAKEEXE% bigide OPT="-Xs -XX -dUseCHMHelp" PP=%COMPILER% LCL_PLATFORM=%IDE_WIDGETSET% >> %LOGFILE%
+rem %MAKEEXE% lazbuild OPT="-Xs -XX" PP=%COMPILER% >> %LOGFILE%
 
 %FPCBINDIR%\strip.exe lazarus.exe
-%FPCBINDIR%\strip.exe lazbuild.exe
 %FPCBINDIR%\strip.exe startlazarus.exe
+%FPCBINDIR%\strip.exe lazbuild.exe
+%FPCBINDIR%\strip.exe tools\lazres.exe
+%FPCBINDIR%\strip.exe tools\updatepofiles.exe
+%FPCBINDIR%\strip.exe tools\lrstolfm.exe
+%FPCBINDIR%\strip.exe tools\svn2revisioninc.exe
 %FPCBINDIR%\strip.exe components\chmhelp\lhelp\lhelp.exe
 
 %OLDCURDRIVE%
