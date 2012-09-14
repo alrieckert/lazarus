4cb0d0b28e35a0d323aeb77489a56f7e68e036bf
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-09-14 12:34:35 +0000

    TAChart: Use TChartExtentHistory in the "extent" demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@38656 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/extent/main.pas
+++ components/tachart/demo/extent/main.pas
@@ -5,13 +5,15 @@ unit main;
 interface
 
 uses
-  ExtCtrls, Spin, StdCtrls, Forms, TAGraph, TASeries, TASources, TATools;
+  ExtCtrls, Spin, StdCtrls, Forms, TAGraph, TASeries, TASources, TATools,
+  TATypes;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    btnPrevExtent: TButton;
     cgUseBounds: TCheckGroup;
     Chart1: TChart;
     ChartToolset1: TChartToolset;
@@ -23,12 +25,18 @@ type
     clBottom: TConstantLine;
     Chart1LineSeries: TLineSeries;
     fseBounds: TFloatSpinEdit;
+    lblHistory: TLabel;
     lblBoundValue: TLabel;
     Panel1: TPanel;
     RandomChartSource1: TRandomChartSource;
-    procedure cgUseBoundsItemClick(Sender: TObject; Index: integer);
+    procedure btnPrevExtentClick(Sender: TObject);
+    procedure cgUseBoundsItemClick(Sender: TObject; AIndex: integer);
+    procedure Chart1ExtentChanged(ASender: TChart);
     procedure FormCreate(Sender: TObject);
     procedure fseBoundsChange(Sender: TObject);
+  private
+    FHistory: TChartExtentHistory;
+    FIsRemembering: Boolean;
   end;
 
 var
@@ -38,10 +46,21 @@ implementation
 
 {$R *.lfm}
 
+uses
+  SysUtils, TAChartUtils;
+
 { TForm1 }
 
-procedure TForm1.cgUseBoundsItemClick(Sender: TObject; Index: integer);
+procedure TForm1.btnPrevExtentClick(Sender: TObject);
 begin
+  FIsRemembering := FHistory.Count > 0;
+  if FIsRemembering then
+    Chart1.LogicalExtent := FHistory.Pop;
+end;
+
+procedure TForm1.cgUseBoundsItemClick(Sender: TObject; AIndex: integer);
+begin
+  Unused(AIndex);
   with Chart1.Extent do begin
     UseXMin := cgUseBounds.Checked[0];
     UseXMax := cgUseBounds.Checked[1];
@@ -50,9 +69,23 @@ begin
   end;
 end;
 
+procedure TForm1.Chart1ExtentChanged(ASender: TChart);
+begin
+  Unused(ASender);
+  if not FIsRemembering then
+    FHistory.Add(Chart1.PrevLogicalExtent);
+  FIsRemembering := false;
+  btnPrevExtent.Enabled := FHistory.Count > 0;
+  lblHistory.Caption := Format(
+    'History: %d of %d', [FHistory.Count, FHistory.Capacity]);
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   fseBoundsChange(nil);
+  FHistory := TChartExtentHistory.Create;
+  FHistory.Capacity := 8;
+  FIsRemembering := true;
 end;
 
 procedure TForm1.fseBoundsChange(Sender: TObject);
