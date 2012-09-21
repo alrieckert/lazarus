2d043e9f6382e657065f0384ebfa2137bdc206aa
ask <ask@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-09-21 15:48:34 +0000

    TAChart: Add "Bars" page to the "dragdrop" demo
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@38791 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/tachart/demo/dragdrop/main.pas
+++ components/tachart/demo/dragdrop/main.pas
@@ -5,30 +5,46 @@ unit Main;
 interface
 
 uses
-  Classes, ExtCtrls, StdCtrls, SysUtils, FileUtil, LResources, Forms, Controls,
-  Graphics, Dialogs, TAGraph, TASeries, TATools, Types;
+  Classes, ComCtrls, ExtCtrls, StdCtrls, SysUtils, FileUtil, LResources, Forms,
+  Controls, Graphics, Dialogs, TAGraph, TASeries, TASources, TATools, Types, TAChartUtils;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Chart1: TChart;
-    Chart1LineSeries1: TLineSeries;
-    ChartToolset1: TChartToolset;
-    ChartToolset1DataPointClickTool1: TDataPointClickTool;
-    ChartToolset1DataPointDragTool1: TDataPointDragTool;
     cbSorted: TCheckBox;
-    ChartToolset1DataPointHintTool1: TDataPointHintTool;
+    chPoints: TChart;
+    chBarsBarSeries1: TBarSeries;
+    chPointsLineSeries1: TLineSeries;
+    chBars: TChart;
+    ctBars: TChartToolset;
+    ctBarsDataPointDragTool1: TDataPointDragTool;
+    ctPoints: TChartToolset;
+    ctPointsDataPointClickTool1: TDataPointClickTool;
+    ctPointsDataPointDragTool1: TDataPointDragTool;
+    ctPointsDataPointHintTool1: TDataPointHintTool;
+    PageControl1: TPageControl;
     Panel1: TPanel;
+    RandomChartSource1: TRandomChartSource;
+    tsPoints: TTabSheet;
+    tsBars: TTabSheet;
     procedure cbSortedChange(Sender: TObject);
-    procedure Chart1LineSeries1GetMark(out AFormattedMark: String;
+    procedure chPointsLineSeries1GetMark(out AFormattedMark: String;
       AIndex: Integer);
-    procedure ChartToolset1DataPointClickTool1PointClick(ATool: TChartTool;
+    procedure ctBarsDataPointDragTool1BeforeKeyDown(ATool: TChartTool;
       APoint: TPoint);
-    procedure ChartToolset1DataPointHintTool1Hint(ATool: TDataPointHintTool;
+    procedure ctBarsDataPointDragTool1Drag(ASender: TDataPointDragTool;
+      var AGraphPoint: TDoublePoint);
+    procedure ctBarsDataPointDragTool1DragStart(ASender: TDataPointDragTool;
+      var AGraphPoint: TDoublePoint);
+    procedure ctPointsDataPointClickTool1PointClick(ATool: TChartTool;
+      APoint: TPoint);
+    procedure ctPointsDataPointHintTool1Hint(ATool: TDataPointHintTool;
       const APoint: TPoint; var AHint: String);
+    procedure FormActivate(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure PageControl1Change(Sender: TObject);
   end;
 
 var
@@ -39,42 +55,73 @@ implementation
 {$R *.lfm}
 
 uses
-  Math, TAChartUtils;
+  Math;
 
 { TForm1 }
 
 procedure TForm1.cbSortedChange(Sender: TObject);
 begin
-  Chart1LineSeries1.ListSource.Sorted := cbSorted.Checked;
-  Chart1.SetFocus;
+  chPointsLineSeries1.ListSource.Sorted := cbSorted.Checked;
+  chPoints.SetFocus;
 end;
 
-procedure TForm1.Chart1LineSeries1GetMark(
+procedure TForm1.chPointsLineSeries1GetMark(
   out AFormattedMark: String; AIndex: Integer);
 begin
-  if AIndex = ChartToolset1DataPointDragTool1.PointIndex then
-    with Chart1LineSeries1 do
+  // Show item label for last dragged point.
+  if AIndex = ctPointsDataPointDragTool1.PointIndex then
+    with chPointsLineSeries1 do
       AFormattedMark := Source.FormatItem(Marks.Format, AIndex, 0)
   else
     AFormattedMark := '';
 end;
 
-procedure TForm1.ChartToolset1DataPointClickTool1PointClick(
+procedure TForm1.ctBarsDataPointDragTool1BeforeKeyDown(ATool: TChartTool;
+  APoint: TPoint);
+begin
+  TDataPointDragTool(ATool).GrabRadius := chBarsBarSeries1.GetBarWidth(0);
+end;
+
+procedure TForm1.ctBarsDataPointDragTool1Drag(ASender: TDataPointDragTool;
+  var AGraphPoint: TDoublePoint);
+begin
+  // Only allow vertical dragging.
+  AGraphPoint.X := ASender.Origin.X;
+end;
+
+procedure TForm1.ctBarsDataPointDragTool1DragStart(ASender: TDataPointDragTool;
+  var AGraphPoint: TDoublePoint);
+const
+  Y_TOLERANCE = 3;
+var
+  f: TGraphToImageFunc;
+begin
+  f := @chBars.YGraphToImage;
+  if Abs(f(ASender.Origin.Y) - f(AGraphPoint.Y)) > Y_TOLERANCE then
+    ASender.Handled;
+end;
+
+procedure TForm1.ctPointsDataPointClickTool1PointClick(
   ATool: TChartTool; APoint: TPoint);
 var
   pi: Integer;
 begin
   Unused(ATool, APoint);
-  pi := ChartToolset1DataPointClickTool1.PointIndex;
-  with Chart1LineSeries1 do
+  pi := ctPointsDataPointClickTool1.PointIndex;
+  with chPointsLineSeries1 do
     SetColor(pi, IfThen(GetColor(pi) = clRed, clTAColor, clRed));
 end;
 
-procedure TForm1.ChartToolset1DataPointHintTool1Hint(ATool: TDataPointHintTool;
+procedure TForm1.ctPointsDataPointHintTool1Hint(ATool: TDataPointHintTool;
   const APoint: TPoint; var AHint: String);
 begin
   Unused(APoint);
   AHint := 'Custom hint for point ' + IntToStr(ATool.PointIndex);
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  PageControl1Change(nil);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -83,7 +130,14 @@ var
 begin
   RandSeed := 675402;
   for i := 1 to 10 do
-    Chart1LineSeries1.AddXY(i, Random(20) - 10);
+    chPointsLineSeries1.AddXY(i, Random(20) - 10);
+  chBarsBarSeries1.ListSource.CopyFrom(RandomChartSource1);
+end;
+
+procedure TForm1.PageControl1Change(Sender: TObject);
+begin
+  if PageControl1.ActivePage = tsPoints then
+    chPoints.SetFocus;
 end;
 
 end.
