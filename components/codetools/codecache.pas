e46a618cde44d4e565a95d8a87646d6095249f2a
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-10-12 08:12:07 +0000

    Codetools: enclose a buggy line in IFnDEF in TCodeCache.LoadFile
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39055 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/codecache.pas
+++ components/codetools/codecache.pas
@@ -589,8 +589,11 @@ begin
   if FilenameIsAbsolute(AFilename) then begin
     if Result=nil then begin
       // load new buffer
-      if (not FileExistsCached(AFilename)) then
-        exit;
+      {$IFnDEF FixCodeCacheLoadFileBug}
+      // This line is wrong. The file is not in cache and we should not exit here.
+      // For some reason removing the line breaks debugger, that's why the IFnDEF
+      if (not FileExistsCached(AFilename)) then exit;
+      {$ENDIF}
       if DirectoryCachePool<>nil then
         DiskFilename:=DirectoryCachePool.FindDiskFilename(AFilename)
       else
