2da87e13d5817e8493ffc7e524d1cc03fb63f433
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-10-21 10:04:35 +0000

    Gtk2: added TGtk2DeviceContext class as deprecated. part of issue #23168
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39148 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/gtk2/gtk2def.pp
+++ lcl/interfaces/gtk2/gtk2def.pp
@@ -420,6 +420,8 @@ type
     property ROP2: Integer read GetRop2 write SetRop2;
   end;
 
+  TGtk2DeviceContext = TGtkDeviceContext deprecated;
+
   // memory system for TDeviceContext(s) ---------------------------------------------
 
   { TDeviceContextMemManager }
