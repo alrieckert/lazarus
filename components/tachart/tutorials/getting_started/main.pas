d3ef1d606b72ce8e7ca0812422887a930c562a81
wp <wp@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-10-31 14:57:51 +0000

    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39209 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/tachart/tutorials/getting_started/main.pas
@@ -0,0 +1,55 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, TAGraph, TASeries, Forms, Controls, Graphics,
+  Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Chart1: TChart;
+    SinSeries: TLineSeries;
+    CosSeries: TLineSeries;
+    SinCosSeries: TLineSeries;
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+const
+  N = 100;
+  MIN = -10;
+  MAX = 10;
+var
+  i: Integer;
+  x: Double;
+begin
+  for i:=0 to N - 1 do begin
+    x := MIN + (MAX - MIN) * i / (N - 1);
+    SinSeries.AddXY(x, sin(x));
+    CosSeries.AddXY(x, cos(x));
+    SinCosSeries.AddXY(x, sin(x)*cos(x));
+  end;
+
+end;
+
+end.
+
