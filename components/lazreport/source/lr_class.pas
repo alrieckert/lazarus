21064eac7f7382af74a7a8bc636677d6a21341b9
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-11-02 19:24:13 +0000

    LazReport, Right Justified text incorrectly aligned on printer, from Tony Whyman, issue 23135
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39232 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lr_class.pas
+++ components/lazreport/source/lr_class.pas
@@ -3102,8 +3102,8 @@ var
         {$ENDIF}
         case Alignment of
           Classes.taLeftJustify : CurX :=x+gapx;
-          Classes.taRightJustify: CurX :=x+dx-1-gapx-nw;
-          Classes.taCenter      : CurX :=x+gapx+(dx-gapx-gapx-nw) div 2;
+          Classes.taRightJustify: CurX :=x+dx-1-gapx-Canvas.TextWidth(St);
+          Classes.taCenter      : CurX :=x+gapx+(dx-gapx-gapx-Canvas.TextWidth(St)) div 2;
         end;
 
         if not Exporting then
@@ -3149,15 +3149,25 @@ var
     procedure OutLine(str: String);
     var
       cury: Integer;
+      Ts: TTextStyle;
     begin
       SetLength(str, Length(str) - 2);
       if str[Length(str)] = #1 then
         SetLength(str, Length(str) - 1);
       cury := 0;
+
+      Ts := Canvas.TextStyle;
+      Ts.Layout    :=tlTop;
+      Ts.Alignment :=self.Alignment;
+      Ts.Wordbreak :=false;
+      Ts.SingleLine:=True;
+      Ts.Clipping  :=True;
+      Canvas.TextStyle := Ts;
+
       case Alignment of
           Classes.taLeftJustify : CurY :=y + dy-gapy;
-          Classes.taRightJustify: CurY :=y - gapy + Canvas.TextWidth(str);
-          Classes.taCenter      : CurY :=y - gapy + (dy + Canvas.TextWidth(str)) div 2;
+          Classes.taRightJustify: CurY :=y + gapy + 1 + Canvas.TextWidth(str);
+          Classes.taCenter      : CurY :=y + gapy + (dy + Canvas.TextWidth(str)) div 2;
       end;
       if not Exporting then
          canvas.TextOut(curx,cury,str)
