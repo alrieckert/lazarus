3f5019f81c37515b2bcdb6f7b5c947ac89637827
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-01-28 16:42:12 +0000

    cody: started circlediagram
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@40009 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/ide/codyunitdepwnd.pas
+++ components/codetools/ide/codyunitdepwnd.pas
@@ -5,12 +5,96 @@ unit CodyUnitDepWnd;
 interface
 
 uses
-  Classes, SysUtils, AVL_Tree, FileUtil, lazutf8classes, LazLogger,
+  Classes, SysUtils, AVL_Tree, contnrs, FileUtil, lazutf8classes, LazLogger,
   TreeFilterEdit, CTUnitGraph, CodeToolManager, DefineTemplates, Forms,
   Controls, Graphics, Dialogs, ExtCtrls, Buttons, ComCtrls, LazIDEIntf,
   ProjectIntf, IDEWindowIntf;
 
+const
+  DefaultCategoryGap = 0.04;
+  DefaultFirstCategory = 0;
 type
+  TCustomCircleDiagramControl = class;
+  TCircleDiagramCategory = class;
+
+  { TCircleDiagramItem }
+
+  TCircleDiagramItem = class(TPersistent)
+  private
+    FCaption: TCaption;
+    FCategory: TCircleDiagramCategory;
+    FSize: single;
+    procedure SetCaption(AValue: TCaption);
+    procedure SetSize(AValue: single);
+    procedure UpdateLayout;
+  public
+    constructor Create(TheCategory: TCircleDiagramCategory);
+    destructor Destroy; override;
+    property Category: TCircleDiagramCategory read FCategory;
+    property Caption: TCaption read FCaption write SetCaption;
+    property Size: single read FSize write SetSize;
+  end;
+
+  { TCircleDiagramCategory }
+
+  TCircleDiagramCategory = class(TPersistent)
+  private
+    FCaption: TCaption;
+    FDiagram: TCustomCircleDiagramControl;
+    FMinSize: single;
+    fItems: TObjectList; // list of TCircleDiagramItem
+    function GetItems(Index: integer): TCircleDiagramItem;
+    procedure SetCaption(AValue: TCaption);
+    procedure SetMinSize(AValue: single);
+    procedure UpdateLayout;
+  public
+    constructor Create(TheDiagram: TCustomCircleDiagramControl);
+    destructor Destroy; override;
+    property Diagram: TCustomCircleDiagramControl read FDiagram;
+    property Caption: TCaption read FCaption write SetCaption;
+    property MinSize: single read FMinSize write SetMinSize;
+    function Count: integer;
+    property Items[Index: integer]: TCircleDiagramItem read GetItems;
+  end;
+
+  TCircleDiagramCtrlFlag = (
+    cdcNeedUpdateLayout
+    );
+  TCircleDiagramCtrlFlags = set of TCircleDiagramCtrlFlag;
+
+  { TCustomCircleDiagramControl }
+
+  TCustomCircleDiagramControl = class(TCustomControl)
+  private
+    FCategoryGap: single;
+    FCenterCaption: TCaption;
+    FFirstCategory: single;
+    fCategories: TObjectList; // list of TCircleDiagramCategory
+    fUpdateLock: integer;
+    fFlags: TCircleDiagramCtrlFlags;
+    function GetCategories(Index: integer): TCircleDiagramCategory;
+    procedure SetCategoryGap(AValue: single);
+    procedure SetCenterCaption(AValue: TCaption);
+    procedure SetFirstCategory(AValue: single);
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    property CenterCaption: TCaption read FCenterCaption write SetCenterCaption;
+    procedure BeginUpdate; virtual;
+    procedure EndUpdate; virtual;
+    procedure UpdateLayout;
+    function InsertCategory(Index: integer; aCaption: TCaption): TCircleDiagramCategory;
+    function AddCategory(aCaption: TCaption): TCircleDiagramCategory;
+    property CategoryGap: single read FCategoryGap write SetCategoryGap default DefaultCategoryGap; // in part of a full circle
+    property FirstCategory: single read FFirstCategory write SetFirstCategory default DefaultFirstCategory; // in part of a full circle starting at top
+    function CategoryCount: integer;
+    property Categories[Index: integer]: TCircleDiagramCategory read GetCategories;
+  end;
+
+  TCircleDiagramControl = class(TCustomCircleDiagramControl)
+  published
+
+  end;
 
   { TUnitDependenciesDialog }
 
@@ -64,6 +148,167 @@ begin
   finally
     Dlg.Free;
   end;
+end;
+
+{ TCustomCircleDiagramControl }
+
+procedure TCustomCircleDiagramControl.SetCategoryGap(AValue: single);
+begin
+  if AValue<0 then AValue:=0;
+  if AValue>0.3 then AValue:=0.3;
+  if FCategoryGap=AValue then Exit;
+  FCategoryGap:=AValue;
+  UpdateLayout;
+end;
+
+function TCustomCircleDiagramControl.GetCategories(Index: integer
+  ): TCircleDiagramCategory;
+begin
+  Result:=TCircleDiagramCategory(fCategories[Index]);
+end;
+
+procedure TCustomCircleDiagramControl.SetCenterCaption(AValue: TCaption);
+begin
+  if FCenterCaption=AValue then Exit;
+  FCenterCaption:=AValue;
+  UpdateLayout;
+end;
+
+procedure TCustomCircleDiagramControl.SetFirstCategory(AValue: single);
+begin
+  if FFirstCategory=AValue then Exit;
+  FFirstCategory:=AValue;
+  UpdateLayout;
+end;
+
+constructor TCustomCircleDiagramControl.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  fCategories:=TObjectList.create(true);
+end;
+
+destructor TCustomCircleDiagramControl.Destroy;
+begin
+  FreeAndNil(fCategories);
+  inherited Destroy;
+end;
+
+procedure TCustomCircleDiagramControl.BeginUpdate;
+begin
+  inc(fUpdateLock);
+end;
+
+procedure TCustomCircleDiagramControl.EndUpdate;
+begin
+  if fUpdateLock=0 then
+    raise Exception.Create('TCustomCircleDiagramControl.EndUpdate');
+  dec(fUpdateLock);
+  if fUpdateLock=0 then begin
+    if cdcNeedUpdateLayout in fFlags then
+      UpdateLayout;
+  end;
+end;
+
+procedure TCustomCircleDiagramControl.UpdateLayout;
+begin
+  if fUpdateLock>0 then begin
+    Include(fFlags,cdcNeedUpdateLayout);
+    exit;
+  end;
+  Exclude(fFlags,cdcNeedUpdateLayout);
+
+end;
+
+function TCustomCircleDiagramControl.InsertCategory(Index: integer;
+  aCaption: TCaption): TCircleDiagramCategory;
+begin
+
+end;
+
+function TCustomCircleDiagramControl.AddCategory(aCaption: TCaption
+  ): TCircleDiagramCategory;
+begin
+  Result:=InsertCategory(CategoryCount,aCaption);
+end;
+
+function TCustomCircleDiagramControl.CategoryCount: integer;
+begin
+  Result:=fCategories.Count;
+end;
+
+{ TCircleDiagramCategory }
+
+procedure TCircleDiagramCategory.SetCaption(AValue: TCaption);
+begin
+  if FCaption=AValue then Exit;
+  FCaption:=AValue;
+end;
+
+function TCircleDiagramCategory.GetItems(Index: integer): TCircleDiagramItem;
+begin
+  Result:=TCircleDiagramItem(fItems[Index]);
+end;
+
+procedure TCircleDiagramCategory.SetMinSize(AValue: single);
+begin
+  if FMinSize=AValue then Exit;
+  FMinSize:=AValue;
+end;
+
+procedure TCircleDiagramCategory.UpdateLayout;
+begin
+  if Diagram<>nil then
+    Diagram.UpdateLayout;
+end;
+
+constructor TCircleDiagramCategory.Create(
+  TheDiagram: TCustomCircleDiagramControl);
+begin
+  FDiagram:=TheDiagram;
+  fItems:=TObjectList.Create(true);
+end;
+
+destructor TCircleDiagramCategory.Destroy;
+begin
+  FreeAndNil(fItems);
+  inherited Destroy;
+end;
+
+function TCircleDiagramCategory.Count: integer;
+begin
+  Result:=fItems.Count;
+end;
+
+{ TCircleDiagramItem }
+
+procedure TCircleDiagramItem.SetCaption(AValue: TCaption);
+begin
+  if FCaption=AValue then Exit;
+  FCaption:=AValue;
+  UpdateLayout;
+end;
+
+procedure TCircleDiagramItem.SetSize(AValue: single);
+begin
+  if FSize=AValue then Exit;
+  FSize:=AValue;
+  UpdateLayout;
+end;
+
+procedure TCircleDiagramItem.UpdateLayout;
+begin
+  if Category<>nil then
+    Category.UpdateLayout;
+end;
+
+constructor TCircleDiagramItem.Create(TheCategory: TCircleDiagramCategory);
+begin
+  FCategory:=TheCategory;
+end;
+
+destructor TCircleDiagramItem.Destroy;
+begin
+  inherited Destroy;
 end;
 
 { TUnitDependenciesDialog }
