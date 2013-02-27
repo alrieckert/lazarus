895290ff7e0d5d1fb0fbd8821e92a97541c156e1
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-02-27 23:32:22 +0000

    LazReport, reverted band's patch in r40127
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@40441 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lr_class.pas
+++ components/lazreport/source/lr_class.pas
@@ -6461,7 +6461,6 @@ begin
       x := bt.x; dx := bt.dx;
       InitDataSet(TfrBandView(bt).DataSet);
       View := bt;
-      Name:= bt.Name; // Get Band Name from BandView Name (G. Colla)
       Flags := bt.Flags;
       Visible := bt.Visible;
       bt.Parent := Bands[TfrBandView(bt).BandType];
@@ -6482,7 +6481,6 @@ begin
            t.x := t.x - Bnd.x;
            t.Parent := Bnd;
            Bnd.Objects.Add(t);
-           Bnd.Name:= t.Name;  // Get Band Name from BandView Name (G. Colla)
          end;
       end;
     end;
@@ -6511,7 +6509,6 @@ begin
         Bnd.y := bt.y;
         Bnd.dy := bt.dy;
         Bnd.View := bt;
-        Bnd.Name:= bt.Name;  // Get Band Name from BandView Name (G. Colla)
         Bnd.Flags := bt.Flags;
         Bnd.Visible := bt.Visible;
         bt.Parent := Bnd;
