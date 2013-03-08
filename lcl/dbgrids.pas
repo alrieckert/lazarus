0291b21897ffc40c83eb92f0ec65dcbde3c7a03a
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-03-08 05:43:01 +0000

    LCL, reverted dbgrid patch to workaround qt scrolling problem, will fix qt interface instead
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@40510 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/dbgrids.pas
+++ lcl/dbgrids.pas
@@ -316,7 +316,6 @@ type
     FKeySign: Integer;
     FSavedRecord: Integer;
     FOnGetCellHint: TDbGridCellHintEvent;
-    FIntScrollDisabled: Boolean;
     procedure EmptyGrid;
     function GetColumns: TDBGridColumns;
     function GetCurrentColumn: TColumn;
@@ -1236,8 +1235,7 @@ var
   end;
 
 begin
-  if FIntScrollDisabled or not FDatalink.Active then
-    exit;
+  if not FDatalink.Active then exit;
 
   {$ifdef dbgDBGrid}
   DebugLn('VSCROLL: Code=',SbCodeToStr(Message.ScrollCode),
@@ -1519,19 +1517,11 @@ begin
   // if the scrollbar is visible or not, in windows it
   // seems to mean if the scrollbar is redrawn or not
   // to reflect the scrollbar changes made
-  //
-  // FInt(eractive)ScrollDisabled is workaround for qt
-  // that triggers a spurious scroll event when it shouldn't
-  FIntScrollDisabled := true;
-  try
-    SetScrollInfo(Handle, SB_VERT, ScrollInfo,
-      (ScrollBars in [ssBoth, ssVertical]) or
-      ((Scrollbars in [ssAutoVertical, ssAutoBoth]) and (aRange>aPAge))
-    );
-  finally
-    FIntScrollDisabled := false;
-    FOldPosition := aPos;
-  end;
+  SetScrollInfo(Handle, SB_VERT, ScrollInfo,
+    (ScrollBars in [ssBoth, ssVertical]) or
+    ((Scrollbars in [ssAutoVertical, ssAutoBoth]) and (aRange>aPAge))
+  );
+  FOldPosition := aPos;
   {$ifdef dbgDBGrid}
   DebugLn('UpdateScrollBarRange: Handle=',IntToStr(Handle),
    ' aRange=', IntToStr(aRange),
