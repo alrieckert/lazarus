7cd61c837b7af64e7a557e47261b3f9ea63e695d
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-04-27 21:27:33 +0000

    IDE: Fix ide.lpk package. Now it compiles. Issue #24160, patch from Cyrax
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@40903 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/ide.pas
+++ ide/ide.pas
@@ -7,20 +7,19 @@ unit ide;
 interface
 
 uses
-  PackageSystem, AddDirToPkgDlg, AddFileToAPackageDlg, AddToPackageDlg, 
-  BasePkgManager, BrokenDependenciesDlg, ConfirmPkgListDlg, 
-  package_description_options, package_i18n_options, 
-  package_integration_options, package_provides_options, 
-  package_usage_options, InstallPkgSetDlg, MissingPkgFilesDlg, 
-  OpenInstalledPkgDlg, PackageDefs, PackageEditor, PackageLinks, 
-  PkgGraphExplorer, PkgLinksDlg, PkgManager, PkgVirtualUnitEditor, 
-  UComponentManMain, UFrmAddComponent, AssemblerDlg, BreakPointsDlg, 
-  BreakPropertyDlg, BreakPropertyDlgGroups, CallStackDlg, CmdLineDebugger, 
-  DebugEventsForm, Debugger, DebuggerDlg, DebugOutputForm, DebugUtils, 
-  EvaluateDlg, ExceptionDlg, FeedbackDlg, GDBMIDebugger, GDBMIMiscClasses, 
-  GDBTypeInfo, HistoryDlg, InspectDlg, LocalsDlg, ProcessDebugger, 
-  ProcessList, PseudoTerminalDlg, RegistersDlg, SSHGDBMIDebugger, ThreadDlg, 
-  WatchesDlg, WatchPropertyDlg;
+  PackageSystem, AddDirToPkgDlg, AddFileToAPackageDlg, AddToPackageDlg,
+  BasePkgManager, BrokenDependenciesDlg, ConfirmPkgListDlg,
+  package_description_options, package_i18n_options,
+  package_integration_options, package_provides_options,
+  package_usage_options, InstallPkgSetDlg, MissingPkgFilesDlg,
+  OpenInstalledPkgDlg, PackageDefs, PackageEditor, PackageLinks,
+  PkgGraphExplorer, PkgLinksDlg, PkgManager, PkgVirtualUnitEditor,
+  AssemblerDlg, BreakPointsDlg, BreakPropertyDlg, BreakPropertyDlgGroups,
+  CallStackDlg, CmdLineDebugger, DebugEventsForm, Debugger, DebuggerDlg,
+  DebugOutputForm, DebugUtils, EvaluateDlg, ExceptionDlg, FeedbackDlg,
+  GDBMIDebugger, GDBMIMiscClasses, GDBTypeInfo, HistoryDlg, InspectDlg,
+  LocalsDlg, ProcessDebugger, ProcessList, PseudoTerminalDlg, RegistersDlg,
+  SSHGDBMIDebugger, ThreadDlg, WatchesDlg, WatchPropertyDlg, lfmUnitResource;
 
 implementation
 
