374863f4c17087a20a6db5a28285055591d1acef
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-05-02 00:16:38 +0000

    CairoCanvas: Implemented DrawSurface to draw a cairo surface onto canvas
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@40984 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/cairocanvas/cairocanvas.pas
+++ components/cairocanvas/cairocanvas.pas
@@ -107,6 +107,7 @@ type
     procedure SetPixel(X,Y: Integer; Value: TColor); override;
   public
     procedure MixedRoundRect(X1, Y1, X2, Y2: Integer; RX, RY: Integer; SquaredCorners: TSquaredCorners);
+    procedure DrawSurface(const SourceRect, DestRect: TRect; surface: Pcairo_surface_t);
 {  Not implemented
     procedure FloodFill(X, Y: Integer; FillColor: TColor; FillStyle: TFillStyle); override;
     procedure CopyRect(const Dest: TRect; SrcCanvas: TCanvas; const Source: TRect); override;
@@ -702,6 +703,25 @@ begin
     EllipseArcPath(X1+RX, Y1+RY, RX, RY, PI, PI*1.5, True, True);
 
   FillAndStroke;
+  Changed;
+end;
+
+procedure TCairoPrinterCanvas.DrawSurface(const SourceRect, DestRect: TRect;
+  surface: Pcairo_surface_t);
+var
+  SW, SH: Double;
+begin
+  Changing;
+  RequiredState([csHandleValid]);
+
+  cairo_save(cr);
+  cairo_translate(cr, SX(DestRect.Left), SY(DestRect.Top));
+  SW := (DestRect.Right - DestRect.Left)/(SourceRect.Right-SourceRect.Left);
+  SH := (DestRect.Bottom - DestRect.Top)/(SourceRect.Bottom-SourceRect.Top);
+  cairo_scale(cr, SX2(SW), SY2(SH));
+  cairo_set_source_surface(cr, surface, 0, 0);
+  cairo_paint(cr);
+  cairo_restore(cr);
   Changed;
 end;
 
