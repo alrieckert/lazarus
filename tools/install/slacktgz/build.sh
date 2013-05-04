5b81aea4c2f7082103ca1ee08bce381e948f5326
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-05-04 20:15:57 +0000

    slacktgz: clean up
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@41023 4005530d-fff6-0310-9dd1-cebe43e6787f

--- tools/install/slacktgz/build.sh
+++ tools/install/slacktgz/build.sh
@@ -3,14 +3,7 @@ cd $1
 if [ -n "$FPCCfg" ]; then
 	  MAKEOPTS=" -n @$FPCCfg"
   fi
-  make
   make bigide OPT="$MAKEOPTS" USESVN2REVISIONINC=0
-  make tools OPT="$MAKEOPTS"
-  make lazbuild OPT="$MAKEOPTS"
-  # build gtk2 .ppu
-  export LCL_PLATFORM=gtk2
-  make lcl ideintf packager/registration bigidecomponents OPT="$MAKEOPTS"
-  export LCL_PLATFORM=
 strip lazarus
 strip startlazarus
 strip lazbuild
