591e13a6c384d119548948b20a397cff1c98093e
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-05-16 16:02:00 +0000

    LCL: Improve the header comment in TArrow.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@41227 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/arrow.pp
+++ lcl/arrow.pp
@@ -1,6 +1,5 @@
 {
-  Copyright (C) 2013 H. Page-Clark
-
+  ***************************************************************************
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
@@ -14,6 +13,12 @@
   You should have received a copy of the GNU Library General Public License
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  ***************************************************************************
+
+  Copyright (C) 2013 H. Page-Clark
+
+  Abstract:
+    Show an arrow. Its size, direction, color and shadow can be adjusted.
 }
 
 unit Arrow;
