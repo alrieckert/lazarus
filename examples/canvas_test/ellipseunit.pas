174eea400da33617bbbb5984d4b9a2ecd7b14179
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-06-06 09:44:25 +0000

    canvas_test: Adds test to arc with start and end point
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@41562 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/canvas_test/ellipseunit.pas
+++ examples/canvas_test/ellipseunit.pas
@@ -42,6 +42,10 @@ begin
   Canvas.TextOut(150, 130, 'Arc S=-25 L=45');
   Canvas.TextOut(250, 130, 'Arc S=30 L=-45');
 
+  Canvas.TextOut(50, 215, 'Arc SX=0 SY=0 EX=300 EY=300');
+  Canvas.TextOut(150, 230, 'Arc SX=300 SY=300 EX=0 EY=0');
+  //Canvas.TextOut(250, 230, 'Arc S=30 L=-45');
+
   Canvas.Brush.Style := bsSolid;
 
   Canvas.Ellipse(50, 50, 100, 100);
@@ -51,6 +55,10 @@ begin
   Canvas.Arc(50, 150, 100, 200, 0, 90*16);
   Canvas.Arc(150, 150, 200, 200, -25*16, 45*16);
   Canvas.Arc(250, 150, 300, 200, 30*16, -45*16);
+
+  Canvas.Arc(50, 250, 100, 300, 0, 0, 300, 300);
+  Canvas.Arc(150, 250, 200, 300, 300, 300, 0, 0);
+  //Canvas.Arc(250, 150, 300, 200, 30*16, -45*16);
 end;
 
 end.
