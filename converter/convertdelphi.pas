64e2dc02f53b8c1994aebea1f56b06e93a59975e
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-07-02 13:21:00 +0000

    Converter: debug messages for the fall-bakc replacement.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@41968 4005530d-fff6-0310-9dd1-cebe43e6787f

--- converter/convertdelphi.pas
+++ converter/convertdelphi.pas
@@ -542,6 +542,7 @@ begin
   repeat
     CodeOk:=True;
     try
+      DebugLn('TDelphiUnit.CopyAndLoadFile: Creating UsedUnitsTool');
       // Create a tool for missing units.
       fUsedUnitsTool:=TUsedUnitsTool.Create(fCTLink, fOrigUnitFilename);
     except
@@ -550,6 +551,7 @@ begin
       if CodeFixed then
         Raise               // There was a second exception -> we are doomed!
       else begin
+        DebugLn('TDelphiUnit.CopyAndLoadFile exception: Replacing .type -> .&type');
         with fPascalBuffer do
           Source:=StringReplace(Source,'.type','.&type',[rfReplaceAll]);
         CodeOk := False;
