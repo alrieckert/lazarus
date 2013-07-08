a08660dcd4159f0e5e1c6a95313bca08c1c5a729
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-07-08 16:52:13 +0000

    tests: added test if CP1251-UTF8-CP1251 is lossless
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@42014 4005530d-fff6-0310-9dd1-cebe43e6787f

--- test/lazutils/testlconvencoding.pas
+++ test/lazutils/testlconvencoding.pas
@@ -48,6 +48,8 @@ procedure TTestLConvEncoding.Test_CP_UTF8_CP;
 
 begin
   Test('1250',@CP1250ToUTF8,@UTF8ToCP1250);
+  Test('1251',@CP1251ToUTF8,@UTF8ToCP1251);
+  Test('866',@CP866ToUTF8,@UTF8ToCP866);
 end;
 
 initialization
