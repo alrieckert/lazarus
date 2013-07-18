74c0de7547565e1d086ce747e46916e8081437a5
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-07-18 22:24:03 +0000

    LazControls: Generate resources for icons. Use new icons from Anton.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@42134 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazcontrols/lazctrls.bat
+++ components/lazcontrols/lazctrls.bat
@@ -1,6 +1,6 @@
-../../tools/lazres dividerbevel_icon.lrs images/dividerbevel.png
-../../tools/lazres extendednotebook_icon.lrs images/textnotebook.png
-../../tools/lazres listfilteredit_icon.lrs images/listfilteredit.png
-../../tools/lazres treefilteredit_icon.lrs images/treefilteredit.png
-#../../tools/lazres lvlgraph_icon.lrs images/lvlgraph.png
-../../tools/lazres shortpathedit_icon.lrs images/shortpathedit.png
+../../tools/lazres dividerbevel_icon.lrs images/dividerbevel.png=tdividerbevel
+../../tools/lazres extendednotebook_icon.lrs images/textnotebook.png=textendednotebook
+../../tools/lazres listfilteredit_icon.lrs images/listfilteredit.png=tlistfilteredit
+../../tools/lazres treefilteredit_icon.lrs images/treefilteredit.png=ttreefilteredit
+#../../tools/lazres lvlgraph_icon.lrs images/lvlgraph.png=tlvlgraphcontrol
+../../tools/lazres shortpathedit_icon.lrs images/shortpathedit.png=tshortpathedit
