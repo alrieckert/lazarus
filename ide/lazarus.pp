ca0290036ca998215e60d043fbf13f1e62ff92a4
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-07-18 10:03:23 +0000

    IDE: less warnings
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@42129 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarus.pp
+++ ide/lazarus.pp
@@ -89,7 +89,7 @@ begin
   LazarusRevisionStr:=RevisionStr;
   Application.Title:='Lazarus';
   OnGetApplicationName:=@GetLazarusApplicationName;
-  Application.MainFormOnTaskBar := True;
+  Application.{%H-}MainFormOnTaskBar := True;
   Application.Initialize;
   TMainIDE.ParseCmdLineOptions;
   if not SetupMainIDEInstance then exit;
