4a1d60cfcaac2b9477b4fb607dd0017f97ea8af5
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-07-18 21:46:27 +0000

    Add a script for updating icons for LazControls.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@42133 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/lazcontrols/lazctrls.bat
@@ -0,0 +1,6 @@
+../../tools/lazres dividerbevel_icon.lrs images/dividerbevel.png
+../../tools/lazres extendednotebook_icon.lrs images/textnotebook.png
+../../tools/lazres listfilteredit_icon.lrs images/listfilteredit.png
+../../tools/lazres treefilteredit_icon.lrs images/treefilteredit.png
+#../../tools/lazres lvlgraph_icon.lrs images/lvlgraph.png
+../../tools/lazres shortpathedit_icon.lrs images/shortpathedit.png
