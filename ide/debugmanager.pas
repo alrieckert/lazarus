70d0d9d3c640d5f5422a9dc6ee952685f339a20e
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2013-09-18 11:17:12 +0000

    Debugger: Disable logging of stackdump during unit lookup
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@42856 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/debugmanager.pas
+++ ide/debugmanager.pas
@@ -602,7 +602,7 @@ function TDebugManager.GetFullFilename(const AUnitinfo: TDebuggerUnitInfo;
       exit;
     debugln(['TDebugManager.GetFullFilename found ',CodePos.Code.Filename,' Line=',CodePos.Y,' Col=',CodePos.X]);
     AUnitinfo.LocationFullFile := CodePos.Code.Filename;
-    DumpStack;
+    //DumpStack;
     Result:=true;
   end;
 
