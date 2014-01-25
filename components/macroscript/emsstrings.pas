3bc4c139432b82cc5967cee0a1f689fd3cc32e17
maxim <maxim@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-01-25 16:45:16 +0000

    Components, MacroScript: improved resourcestrings
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@43795 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/macroscript/emsstrings.pas
+++ components/macroscript/emsstrings.pas
@@ -7,19 +7,19 @@ interface
 resourcestring
   EmsSelfTestErrCaption = 'Error in EditorMacroScript';
   EmsSelfTestFailed = 'The package EditorMacroScript (pascalscript macros)' +
-    ' has detected a problem and was de-activated. %0:s' +
-    'The package failed its selftest with the message:%0:s "%1:s"';
+    ' has detected a problem and was deactivated.%0:s' +
+    'The package failed its selftest with the message:%0:s"%1:s"';
   EmsSelfTestFailedLastTime = 'The package EditorMacroScript (pascalscript macros)' +
-    ' has detected a problem and was de-activated. %0:s' +
-    'The package selftest was not completed during the last start of the IDE"';
+    ' has detected a problem and was deactivated.%0:s' +
+    'The package selftest was not completed during the last start of the IDE.';
   EMSStatusTitle = 'Status';
   EMSEditorMacroTitle = 'Editor Macro Script';
   EMSBtnTestAgain = 'Test again';
-  EMSNotSupported = 'Scripting not active. Not supported on this platform or CPU';
-  EMSNotActive = 'Scripting not active. Selftest failed';
-  EMSActive = 'Scripting active';
+  EMSNotSupported = 'Scripting not active. Not supported on this platform or CPU.';
+  EMSNotActive = 'Scripting not active. Selftest failed.';
+  EMSActive = 'Scripting active.';
   EMSPending = 'Scripting not active. Selftest will run next time the IDE is '
-    +'started';
+    +'started.';
 
 implementation
 
