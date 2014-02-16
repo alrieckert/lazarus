439382742a71befba4dc597a7e6755d5760a11cd
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-02-16 21:35:24 +0000

    GDBMIDebugger: prepare new package
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44101 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/lazdebuggergdbmi/lazdebuggergdbmi.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit LazDebuggerGdbmi;
+
+interface
+
+uses
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('LazDebuggerGdbmi', @Register);
+end.
