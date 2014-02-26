9bd3aab44440950cadacbae10faf8e0ea054d058
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-02-26 18:38:53 +0000

    codetools: parse asm: allow @keyword
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44252 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/pascalparsertool.pas
+++ components/codetools/pascalparsertool.pas
@@ -2781,8 +2781,7 @@ begin
       
     if (CurPos.Flag=cafEND) then begin
       // end
-      if (BlockType<>ebtAsm) or (CurPos.StartPos=1) or (Src[CurPos.StartPos-1]<>'@')
-      then begin
+      if (BlockType<>ebtAsm) or (Src[CurPos.StartPos-1]<>'@') then begin
         if AutomaticallyEnded then break;
         if BlockType=ebtRepeat then
           RaiseStrExpectedWithBlockStartHint('"until"');
@@ -2804,11 +2803,13 @@ begin
       end;
     end else if CurPos.Flag<>cafWord then begin
       continue;
+    end else if (BlockType=ebtAsm) and (Src[CurPos.StartPos-1]='@') then begin
+      // allow anything behind @
     end else if BlockStatementStartKeyWordFuncList.DoIdentifier(@Src[CurPos.StartPos])
     then begin
-      if BlockType=ebtAsm then
+      if BlockType=ebtAsm then begin
         SaveRaiseUnexpectedKeyWordInAsmBlock;
-      if (BlockType<>ebtRecord) then begin
+      end else if (BlockType<>ebtRecord) then begin
         ReadTilBlockEnd(false,CreateNodes);
         if (BlockType=ebtIf) and (CurPos.Flag in [cafSemicolon]) then
           break;
@@ -3454,6 +3455,7 @@ function TPascalParserTool.KeyWordFuncBeginEnd: boolean;
 
 var
   ChildNodeCreated: boolean;
+  IsAsm, IsBegin: Boolean;
 begin
   //DebugLn('TPascalParserTool.KeyWordFuncBeginEnd CurNode=',CurNode.DescAsString);
   if (CurNode<>nil)
@@ -3461,10 +3463,12 @@ begin
     [ctnProcedure,ctnProgram,ctnLibrary,ctnImplementation]))
   then
     SaveRaiseStringExpectedButAtomFound('end');
-  ChildNodeCreated:=UpAtomIs('BEGIN') or UpAtomIs('ASM');
+  IsAsm:=UpAtomIs('ASM');
+  IsBegin:=UpAtomIs('BEGIN');
+  ChildNodeCreated:=IsBegin or IsAsm;
   if ChildNodeCreated then begin
     CreateChildNode;
-    if UpAtomIs('BEGIN') then
+    if IsBegin then
       CurNode.Desc:=ctnBeginBlock
     else
       CurNode.Desc:=ctnAsmBlock;
