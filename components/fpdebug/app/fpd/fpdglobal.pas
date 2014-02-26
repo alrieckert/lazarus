41cc76190152d74f53d5f1cf76500bf9471370d0
joost <joost@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-02-26 07:43:02 +0000

    * Removed now unused windows-unit from uses section
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44241 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/fpdebug/app/fpd/fpdglobal.pas
+++ components/fpdebug/app/fpd/fpdglobal.pas
@@ -36,7 +36,7 @@ unit FPDGlobal;
 interface
 
 uses
-  SysUtils, Windows, FPDType, Maps, FpDbgUtil, FpDbgClasses, FpDbgWinExtra;
+  SysUtils, FPDType, Maps, FpDbgUtil, FpDbgClasses, FpDbgWinExtra;
 
 type
   TFPDState = (dsStop, dsRun, dsPause, dsQuit, dsEvent);
