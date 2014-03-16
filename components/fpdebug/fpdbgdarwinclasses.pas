26d7381f1fbf91b374b09e9cd84d109e7a8b8e96
joost <joost@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-03-16 11:24:51 +0000

    FpDbg: Read register-information on Darwin
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44442 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/fpdebug/fpdbgdarwinclasses.pas
+++ components/fpdebug/fpdbgdarwinclasses.pas
@@ -19,6 +19,26 @@ uses
   LazLoggerBase;
 
 type
+  x86_thread_state32_t = record
+    __eax: cuint;
+    __ebx: cuint;
+    __ecx: cuint;
+    __edx: cuint;
+    __edi: cuint;
+    __esi: cuint;
+    __ebp: cuint;
+    __esp: cuint;
+    __ss: cuint;
+    __eflags: cuint;
+    __eip: cuint;
+    __cs: cuint;
+    __ds: cuint;
+    __es: cuint;
+    __fs: cuint;
+    __gs: cuint;
+  end;
+
+type
 
   { TDbgDarwinProcess }
 
@@ -27,6 +47,7 @@ type
     FStatus: cint;
     FProcessStarted: boolean;
     FTaskPort: mach_port_name_t;
+    FThreadState: x86_thread_state32_t;
     function GetDebugAccessRights: boolean;
   protected
     function InitializeLoader: TDbgImageLoader; override;
@@ -36,6 +57,9 @@ type
 
     function ReadData(const AAdress: TDbgPtr; const ASize: Cardinal; out AData): Boolean; override;
     function WriteData(const AAdress: TDbgPtr; const ASize: Cardinal; const AData): Boolean; override;
+
+    function GetInstructionPointerRegisterValue: TDbgPtr; override;
+    function GetStackBasePointerRegisterValue: TDbgPtr; override;
 
     function Continue(AProcess: TDbgProcess; AThread: TDbgThread; AState: TFPDState): boolean; override;
     function WaitForDebugEvent(out ProcessIdentifier: THandle): boolean; override;
@@ -55,6 +79,17 @@ type
   mach_vm_address_t = uint64;
   mach_msg_Type_number_t = natural_t;
   mach_vm_size_t = uint64;
+  task_t = mach_port_t;
+  thread_act_t = mach_port_t;
+  thread_act_array = array[0..255] of thread_act_t;
+  thread_act_array_t = ^thread_act_array;
+  thread_state_flavor_t = cint;
+  thread_state_t = ^natural_t;
+
+const
+  x86_THREAD_STATE32    = 1;
+
+  x86_THREAD_STATE32_COUNT: mach_msg_Type_number_t = sizeof(x86_thread_state32_t) div sizeof(cint);
 
 function task_for_pid(target_tport: mach_port_name_t; pid: integer; var t: mach_port_name_t): kern_return_t; cdecl external name 'task_for_pid';
 function mach_task_self: mach_port_name_t; cdecl external name 'mach_task_self';
@@ -62,6 +97,9 @@ function mach_error_string(error_value: mach_error_t): pchar; cdecl; external na
 function vm_protect(target_task: vm_map_t; adress: vm_address_t; size: vm_size_t; set_maximum: boolean_t; new_protection: vm_prot_t): kern_return_t; cdecl external name 'vm_protect';
 function mach_vm_write(target_task: vm_map_t; address: mach_vm_address_t; data: vm_offset_t; dataCnt: mach_msg_Type_number_t): kern_return_t; cdecl external name 'mach_vm_write';
 function mach_vm_read(target_task: vm_map_t; address: mach_vm_address_t; size: mach_vm_size_t; var data: vm_offset_t; var dataCnt: mach_msg_Type_number_t): kern_return_t; cdecl external name 'mach_vm_read';
+
+function task_threads(target_task: task_t; var act_list: thread_act_array_t; var act_listCnt: mach_msg_type_number_t): kern_return_t; cdecl external name 'task_threads';
+function thread_get_state(target_act: thread_act_t; flavor: thread_state_flavor_t; old_state: thread_state_t; var old_stateCnt: mach_msg_Type_number_t): kern_return_t; cdecl external name 'thread_get_state';
 
 procedure RegisterDbgClasses;
 begin
@@ -190,6 +228,17 @@ begin
   result := true;
 end;
 
+function TDbgDarwinProcess.GetInstructionPointerRegisterValue: TDbgPtr;
+begin
+  result := FThreadState.__eip;
+end;
+
+function TDbgDarwinProcess.GetStackBasePointerRegisterValue: TDbgPtr;
+begin
+  result := FThreadState.__ebp;
+
+end;
+
 function TDbgDarwinProcess.Continue(AProcess: TDbgProcess; AThread: TDbgThread;
   AState: TFPDState): boolean;
 var
@@ -214,12 +263,34 @@ begin
 end;
 
 function TDbgDarwinProcess.WaitForDebugEvent(out ProcessIdentifier: THandle): boolean;
+var
+  aKernResult: kern_return_t;
+  act_list: thread_act_array_t;
+  act_listCtn: mach_msg_type_number_t;
+  old_StateCnt: mach_msg_Type_number_t;
 begin
   ProcessIdentifier:=FpWaitPid(-1, FStatus, 0);
-  writeln('waited');
+
   result := ProcessIdentifier<>-1;
   if not result then
-    writeln('Failed to wait for debug event. Errcode: ', fpgeterrno);
+    writeln('Failed to wait for debug event. Errcode: ', fpgeterrno)
+  else
+    begin
+    // Read thread-information
+    aKernResult := task_threads(FTaskPort, act_list, act_listCtn);
+    if aKernResult <> KERN_SUCCESS then
+      begin
+      Log('Failed to call task_threads. Mach error: '+mach_error_string(aKernResult));
+      end;
+
+    old_StateCnt:=x86_THREAD_STATE32_COUNT;
+    aKernResult:=thread_get_state(act_list^[0],x86_THREAD_STATE32, @FThreadState,old_StateCnt);
+    if aKernResult <> KERN_SUCCESS then
+      begin
+      Log('Failed to call thread_get_state. Mach error: '+mach_error_string(aKernResult));
+      end;
+    writeln(Format('eip: %s, eax: %s, ebx: %s, ecx: %s, edx: %s',[FormatAddress(FThreadState.__eip), FormatAddress(FThreadState.__eax),FormatAddress(FThreadState.__ebx),FormatAddress(FThreadState.__ecx), FormatAddress(FThreadState.__edx)]));
+    end
 end;
 
 function TDbgDarwinProcess.ResolveDebugEvent(AThread: TDbgThread): TFPDEvent;
@@ -275,4 +346,3 @@ begin
 end;
 
 end.
-
