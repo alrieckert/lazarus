084c0c76c2cb06bad8c2aaf2b1a09eed06236b6b
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-06-24 15:36:50 +0000

    examples: license
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@45642 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/designnonlcl/mywidgetset.pas
+++ examples/designnonlcl/mywidgetset.pas
@@ -12,7 +12,6 @@
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
   option) any later version with the following modification:
-
   As a special exception, the copyright holders of this library give you
   permission to link this library with independent modules to produce an
   executable, regardless of the license terms of these independent modules,and
@@ -23,12 +22,10 @@
   this library, you may extend this exception to your version of the library,
   but you are not obligated to do so. If you do not wish to do so, delete this
   exception statement from your version.
-
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
   for more details.
-
   You should have received a copy of the GNU Library General Public License
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
