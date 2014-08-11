07746ef708cd24854a66a4e49ee3aaa24576fe06
reiniero <reiniero@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-08-11 14:23:42 +0000

    * Examples: database\sqlite_mushrooms: added Firebird embedded support
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@46033 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/database/sqlite_mushrooms/Readme.txt
+++ examples/database/sqlite_mushrooms/Readme.txt
@@ -1,23 +1,38 @@
-MushRoomsDatabase by Jurassic Pork  - January 2014
+MushRoomsDatabase by Jurassic Pork using SQLite - January 2014
+Updated for Firebird Embedded - August 2014
+
 Features:
 - Use SqlDb and lazreport components.
-- Sqlite3 database DeadlyMushrooms with 5 mushrooms.
+- Sqlite3 or Firebird embedded database DeadlyMushrooms with 5 mushrooms.
+  Sqlite3 will be tried first; if no Sqlite library is available, Firebird 
+	embedded will be tried.
+- It demonstrates:
+  - creating a new SQLite3 database with table if the db does not exist
+  - use of TSQLScript to run multiple SQL statements
+  - use of FBAdmin to restore Firebird backup (smaller than the live .fdb file) 
+    on first run, useful for keeping your setup file small and compatible with
+    older Firebird versions
 - The images are stored in blob field without extension at the beginning.
-  With this you can view blob images with database browser editor (ex sqlite2009pro).
+  With this you can view blob images with database browser editor 
+  (e.g. sqlite2009pro).
 - In the database there is also a field with images links (filenames).
-- You can see the linked images in a Timage.
 - The linked images are stored in the folder images of the project.
-- You can change the images in the database :
-  - for Tdbimage double click on the component and choose your image.
-  - for Timage click on the button near the image filename.(you must be in edit mode )
-- Transaction commit when you click on Tdbnavigator refresh button or on close form.
-- Small pictures of the mushrooms are in the sqlite3Database. Largest images are in files in the folder images.
+- You can see the linked images in a Timage.
+- You can change the images in the database:
+  - for Tdbimage (image in db): double click on the component and choose your 
+    image.
+  - for Timage (linked image): click on the button near the image filename 
+    (you must be in edit mode).
+- Transaction commits when you click on Tdbnavigator refresh button or on close 
+  form.
+- Small pictures of the mushrooms are in the sqlite3Database. Largest images are
+  in files in the folder images.
 - Print button to print all the mushrooms (lazreport).
-  On each page you have: 
+  On each page you have:
    - a title. 
    - the field common_name of the mushroom database.
    - the field notes of the mushroom database.
    - the field picture of the mushroom database (picture picture1).
    - the picture of the field image_link (picture picture2).
 
-The report  name is Mushroom_Report.lrf
+The report name is Mushroom_Report.lrf
--- examples/database/sqlite_mushrooms/unit1.pas
+++ examples/database/sqlite_mushrooms/unit1.pas
@@ -1,5 +1,6 @@
 unit Unit1;
-// J.P  August 2013
+// J.P. August 2013
+// Firebird embedded support added 2014
 {$mode objfpc}{$H+}
 
 interface
@@ -7,7 +8,7 @@ interface
 uses
   Classes, SysUtils, FileUtil, LR_Class, LR_DBSet, Forms, Controls, Graphics,
   Dialogs, DbCtrls, Buttons, ExtCtrls, StdCtrls, DBGrids,
-  db, sqldb, sqlite3conn, Grids;
+  db, sqldb, sqlite3conn, ibconnection, fbadmin, Grids;
 
 type
 
@@ -26,6 +27,7 @@ type
     DBText1: TDBText;
     frDBDataSet1: TfrDBDataSet;
     frReport1: TfrReport;
+    IBConnection1: TIBConnection;
     Image1: TImage;
     Label1: TLabel;
     Label2: TLabel;
@@ -49,9 +51,16 @@ type
     procedure frReport1EnterRect(Memo: TStringList; View: TfrView);
   private
     { private declarations }
+    FUsingFirebird: boolean; //indicates whether we're using Firebird (true) or SQLite3 (false)
+    // Updates linked image field with image named in DBEdit1.Text
+    // Shows image to user
     procedure UpdateImageLink;
+    // Lets user change image stored in database blob field
     procedure ChangeImage;
+    // Load image from file into database blob
     procedure LoadDbImage(aFileName: string);
+    // Recreates mushroom database
+    // for either SQLite3 (empty) or Firebird (filled)
     function RecreateDB: boolean;
   public
     { public declarations }
@@ -63,6 +72,11 @@ var
 implementation
 
 {$R *.lfm}
+
+const
+  FirebirdDB='ImageTest.fdb'; //database file for Firebird
+  SQLiteDB='ImageTest.db3'; //database file for SQLite3
+
 
 { TForm1 }
 
@@ -106,8 +120,8 @@ procedure TForm1.DBNavigator1BeforeAction(Sender: TObject;
 begin
   if Button = nbRefresh then
   begin
-   SQLQuery1.ApplyUpdates;
-   SQLTransaction1.CommitRetaining;
+    SQLQuery1.ApplyUpdates;
+    SQLTransaction1.CommitRetaining;
   end;
 end;
 
@@ -120,36 +134,113 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   SQLQuery1.Close;
-  SQLite3Connection1.DatabaseName:= ExtractFilePath(ParamStr(0)) + 'ImageTest.db3';
-  try
-    SQLQuery1.Active := true;
-  except
-    on E:Exception do begin
-      //WriteLn(E.ClassName,': '+E.Message);
-      if RecreateDb then begin
-        SQLQuery1.Active := true;
-        ShowMessage('The database has been recreated');
+
+  // First try SQLite3 database in application directory
+  // If no sqlite3 driver is found, try Firebird
+  FUsingFirebird := false; //try sqlite first
+  if not(FUsingFirebird) then
+  begin
+    SQLite3Connection1.DatabaseName:=ExtractFilePath(ParamStr(0)) + SQLiteDB;
+    try
+      SQLQuery1.Active := true;
+    except
+      on E:Exception do
+      begin
+        //ShowMessage('Exception: '+E.ClassName+': '+E.Message);
+        if E.ClassNameIs('EInOutError') then
+        // Assume 'Can not load SQLite client library "sqlite3.dll". Check your installation.' => but this text may change
+        begin
+          FUsingFirebird:=true;
+        end
+        else
+        begin
+          if RecreateDb then
+          begin
+            SQLQuery1.Active := true;
+            ShowMessage('The SQLite3 database has been recreated');
+          end
+          else
+          begin
+            ShowMessage('Failure to create SQLite3 databse. Aborting');
+            Application.Terminate;
+          end;
+        end;
       end;
     end;
+    Caption := Caption + ' (SQLite3)';
+  end;
+
+  if FUsingFirebird then
+  begin
+    // Try Firebird. We'll need to reconnect some components as they were
+    // set up for sqlite
+    // Hostname was already empty in Object Inspector so we use embedded
+    //IBConnection1.HostName:='';
+    IBConnection1.UserName:='SYSDBA'; //for embedded, this should be an existing name
+    IBConnection1.CharSet:='UTF8';
+    IBConnection1.Params.Add('PAGE_SIZE=16384'); //enough space for indexes etc
+    IBConnection1.DatabaseName:=ExtractFilePath(ParamStr(0)) + FirebirdDB;
+    IBConnection1.Transaction:=SQLTransaction1;
+    SQLTransaction1.Database:=IBConnection1;
+    SQLQuery1.DataBase:=IBConnection1;
+    try
+      SQLQuery1.Active:=true;
+    except
+      on I: EIBDatabaseError do
+      begin
+        // Check for database file not found error message
+        if (I.GDSErrorcode=335544344) and (pos('I/O error',i.message)>0) then
+        begin
+          if RecreateDb then
+          begin
+            SQLQuery1.Active := true;
+            ShowMessage('The Firebird database has been recreated');
+          end
+          else
+          begin
+            ShowMessage('Failure to create Firebird databse. Aborting');
+            Application.Terminate;
+          end;
+        end
+        else
+        begin
+          ShowMessage('Have tried SQLite3 and Firebird dbs without success. '+LineEnding+
+            'Exception: '+I.ClassName+': '+I.Message+LineEnding+
+            '(Firebird GDS Eror code: '+inttostr(I.GDSErrorCode)+')'+LineEnding+
+            'Aborting.');
+          Application.Terminate;
+        end;
+      end;
+      on E: Exception do
+      begin
+        ShowMessage('Have tried SQLite3 and Firebird dbs without success. '+LineEnding+
+          'Exception: '+E.ClassName+': '+E.Message+LineEnding+
+          'Aborting.');
+        Application.Terminate;
+      end;
+    end;
+    Caption := Caption + ' (Firebird embedded)';
   end;
 end;
 
 procedure TForm1.frReport1EnterRect(Memo: TStringList; View: TfrView);
 begin
-   if  (View.Name = 'Picture2') AND
+   if (View.Name = 'Picture2') AND
    (frDBDataSet1.DataSet.FieldByName('Image_Link').AsString <> '') then
-    TFrPictureView(View).Picture.LoadFromFile(ExtractFilePath(ParamStr(0)) +
-  'images' +  DirectorySeparator +  frDBDataSet1.DataSet.FieldByName('Image_Link').AsString);
+     TFrPictureView(View).Picture.LoadFromFile(ExtractFilePath(ParamStr(0)) +
+    'images' + DirectorySeparator + frDBDataSet1.DataSet.FieldByName('Image_Link').AsString);
 end;
 
 procedure TForm1.UpdateImageLink;
 var
   s: String;
 begin
-  if   DBEdit1.Text <> '' then begin
-    s := ExtractFilePath(ParamStr(0)) + 'images' +  DirectorySeparator +  DBEdit1.Text;
+  if DBEdit1.Text <> '' then
+  begin
+    s := ExtractFilePath(ParamStr(0)) + 'images' + DirectorySeparator + DBEdit1.Text;
     Image1.Picture.LoadFromFile(s);
-  end else
+  end
+  else
     Image1.Picture.Clear;
 end;
 
@@ -157,15 +248,18 @@ procedure TForm1.ChangeImage;
 var
   WasEditing: Boolean;
 begin
-  if SQLQuery1.Active then begin
+  if SQLQuery1.Active then
+  begin
     OpenDialog1.Filter :=
     'All image files (*.bmp,*.jpg,*.png,*.gif)|*.bmp;*.jpg;*.png;*.gif|' +
     'BMP files (*.bmp)|*.bmp|' +
     'JPEG files (*.jpg)|*.jpg|' +
     'PNG files (*.png)|*.png|' +
-    'GIF files (*.gif)|*.gif';
+    'GIF files (*.gif)|*.gif' +
+    'TIFF files (*.tiff)|*.tiff;*.tif';
     OpenDialog1.InitialDir := ExtractFilePath(ParamStr(0)) + 'images';
-    if OpenDialog1.Execute then begin
+    if OpenDialog1.Execute then
+    begin
       WasEditing := (SQLQuery1.State = dsEdit);
       if not WasEditing then
         SQLQuery1.Edit;
@@ -179,27 +273,109 @@ end;
 
 procedure TForm1.LoadDbImage(aFileName: string);
 begin
+  // Disabling writeheader means the image is compatible with Delphi TDBImage applications
+  // and other applications that only expect raw jpg data in the database blob.
+  // However, it makes it a bit more difficult to mix various kinds of image types
+  // e.g. jpg and tiff in the same database fields.
+  // Fortunately, Lazarus can deal with that for us.
   DbImage1.WriteHeader:=false;
   DbImage1.Picture.LoadFromFile(aFileName);
 end;
 
 function TForm1.RecreateDB: boolean;
+var
+  BackupFile: string;
+  DBFile: string;
+  FBAdmin: TFBAdmin;
+  SQLInstructions: string; //file with sql instructions to create tables etc
+  Scripter: TSQLScript;
 begin
-  try
-    SQLite3Connection1.ExecuteDirect(
-    'CREATE TABLE DeadlyMushrooms '+LineEnding+
-    '-- This table created by Jurassic Pork '+LineEnding+
-    '-- for Free Pascal Lazarus '+LineEnding+
-    '-- Create date:2013-08-05 23:55:09 '+LineEnding+
-    '( '+LineEnding+
-    '       ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, '+LineEnding+
-    '       Scientific_Name VARCHAR NOT NULL, '+LineEnding+
-    '       Common_Name VARCHAR, '+LineEnding+
-    '       `Order`  VARCHAR, `Genus`  VARCHAR, `Notes`  TEXT, `Picture`  BLOB, `Image_Link`  VARCHAR)'
-    );
-    result := true;
-  except
-    result := false;
+  result:=false;
+  if not(FUsingFirebird) then //not using Firebird, so using SQLite3
+  begin
+    try
+      SQLite3Connection1.ExecuteDirect(
+      'CREATE TABLE DeadlyMushrooms '+LineEnding+
+      '-- This table created by Jurassic Pork '+LineEnding+
+      '-- for Free Pascal Lazarus '+LineEnding+
+      '-- Create date:2013-08-05 23:55:09 '+LineEnding+
+      '( '+LineEnding+
+      '       ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, '+LineEnding+
+      '       Scientific_Name VARCHAR NOT NULL, '+LineEnding+
+      '       Common_Name VARCHAR, '+LineEnding+
+      '       `Order`  VARCHAR, `Genus`  VARCHAR, `Notes`  TEXT, `Picture`  BLOB, `Image_Link`  VARCHAR)'
+      );
+      result := true;
+    except
+      result := false;
+    end;
+  end
+  else
+  begin //Using Firebird
+    try
+      DBFile:=ExtractFilePath(ParamStr(0)) + FirebirdDB;
+      BackupFile:=ChangeFileExt(DBFile,'.fbk');
+      // First try restoring backup file with full content
+      if fileexists(BackupFile) then
+      begin
+        FBAdmin:=TFBAdmin.Create(nil);
+        try
+          try
+            FBAdmin.UseExceptions:=true;
+            FBAdmin.Host:=''; //embedded
+            FBAdmin.Protocol:=IBSPLOCAL;
+            FBAdmin.User:='SYSDBA';
+            FBAdmin.Password:=''; //embedded: doesn't matter which password is used
+            FBAdmin.Connect;
+            // Now run the restore. It will not overwrite an existing file but will
+            // create a new db
+            if FBAdmin.Restore(DBFile,BackupFile,[IBResCreate]) then
+            begin
+              sleep(200); //at least needed on Windows: give the filesystem time to update so we don't get failures connecting
+              exit(true) //we're done; exit the function
+            end
+            else
+              result := false;
+          finally
+            FBAdmin.Free;
+          end;
+        except
+          result := false; //we can continue with the next fallback step
+        end;
+      end;
+
+      // If no backup was found or restore failed,
+      // create an empty .fdb file
+      if (FileExists(DBFile))=false then
+      begin
+        IBConnection1.CreateDB;
+      end;
+      if (FileExists(DBFile)=false) then
+        raise Exception.CreateFmt('Firebird error: failure to create database %s',[DBFile]);
+      // Now create needed tables etc:
+      if not(IBConnection1.Connected) then IBConnection1.Connected := true;
+      Scripter:=TSQLScript.Create(nil);
+      try
+        SQLInstructions := ExtractFilePath(ParamStr(0)) + 'mushrooms_firebird.sql';
+        if not fileexists(SQLInstructions) then
+          raise Exception.CreateFmt('Error creating db: could not load SQL definition file %s',[SQLInstructions]);
+        Scripter.Script.LoadFromFile(SQLInstructions);
+        Scripter.DataBase := IBConnection1;
+        Scripter.Transaction := SQLTransaction1;
+        Scripter.CommentsInSQL := false; //needed to circumvent bugs in TSQLScript
+        Scripter.UseSetTerm := true; //needed because we have SET TERM statements in the SQL file
+        // Now everything is loaded in, run all commands at once:
+        Scripter.Execute;
+        //... and then commit to make them stick and show them to the SQL that comes
+        // after the commit
+        SQLTransaction1.Commit;
+        result := true;
+      finally
+        Scripter.Free;
+      end;
+    except
+      result := false;
+    end;
   end;
 end;
 
