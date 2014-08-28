f9489104d452c1b301cc5678b38e04a64be1b37b
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-08-28 15:27:44 +0000

    lazreport, another couple of missing files
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@46081 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/lazreport/source/lr_e_txt_params.pas
@@ -0,0 +1,128 @@
+unit LR_E_TXT_Params;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
+  ExtCtrls, StdCtrls;
+
+type
+
+  { TlrExpTxtParamsForm }
+
+  TlrExpTxtParamsForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    Panel1: TPanel;
+    RadioGroup1: TRadioGroup;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
+unit LR_E_TXT_Params;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
+  ExtCtrls, StdCtrls;
+
+type
+
+  { TlrExpTxtParamsForm }
+
+  TlrExpTxtParamsForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    Panel1: TPanel;
+    RadioGroup1: TRadioGroup;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
+unit LR_E_TXT_Params;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
+  ExtCtrls, StdCtrls;
+
+type
+
+  { TlrExpTxtParamsForm }
+
+  TlrExpTxtParamsForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    Panel1: TPanel;
+    RadioGroup1: TRadioGroup;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
+unit LR_E_TXT_Params;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
+  ExtCtrls, StdCtrls;
+
+type
+
+  { TlrExpTxtParamsForm }
+
+  TlrExpTxtParamsForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    Panel1: TPanel;
+    RadioGroup1: TRadioGroup;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
