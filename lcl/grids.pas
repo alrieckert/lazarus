0575725873376465bc23b59dc013a3883c0bea30
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2014-10-09 21:00:07 +0000

    LCL, fix grid focus rect drawing when rowselect and writing something to a cell in current row, based on patch from Yuichiro Takahashi, issue 25371
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@46501 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/grids.pas
+++ lcl/grids.pas
@@ -863,7 +863,7 @@ type
     function  BoxRect(ALeft,ATop,ARight,ABottom: Longint): TRect;
     procedure CacheMouseDown(const X,Y:Integer);
     procedure CalcAutoSizeColumn(const Index: Integer; var AMin,AMax,APriority: Integer); virtual;
-    procedure CalcFocusRect(var ARect: TRect);
+    procedure CalcFocusRect(var ARect: TRect; adjust: boolean = true);
     function  CalcMaxTopLeft: TPoint;
     procedure CalcScrollbarsRange;
     function  CanEditShow: Boolean; virtual;
@@ -3956,11 +3956,11 @@ begin
       ColRowToOffset(True, True, aCol, R.Left, R.Right);
       if (R.Left>=R.Right) or not HorizontalIntersect(R, ClipArea) then
         continue;
-      Rs := (goRowSelect in Options);
       gds := GetGridDrawState(ACol, ARow);
       DoDrawCell;
     end;
 
+    Rs := (goRowSelect in Options);
     // Draw the focus Rect
     if FFocusRectVisible and (ARow=FRow) and
        ((Rs and (ARow>=Top) and (ARow<=Bottom)) or IsCellVisible(FCol,ARow))
@@ -3969,7 +3969,10 @@ begin
       //if EditorAlwaysShown and (FEditor<>nil) and FEditor.Visible then begin
         //DebugLn('No Draw Focus Rect');
       end else begin
-        ColRowToOffset(True, True, FCol, R.Left, R.Right);
+        if Rs then
+          CalcFocusRect(R, false) // will be adjusted when calling DrawFocusRect
+        else
+          ColRowToOffset(True, True, FCol, R.Left, R.Right);
         // is this column within the ClipRect?
         if HorizontalIntersect(R, ClipArea) then
           DrawFocusRect(FCol,FRow, R);
@@ -7201,7 +7204,7 @@ begin
   APriority := 0;
 end;
 
-procedure TCustomGrid.CalcFocusRect(var ARect: TRect);
+procedure TCustomGrid.CalcFocusRect(var ARect: TRect; adjust: boolean = true);
 {
 var
   dx,dy: integer;
@@ -7219,6 +7222,10 @@ begin
 
     FlipRect(aRect);
   end;
+
+  if not adjust then
+    exit;
+
   if goHorzLine in Options then dec(aRect.Bottom, 1);
   if goVertLine in Options then
     if UseRightToLeftAlignment then
