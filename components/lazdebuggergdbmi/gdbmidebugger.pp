e53e8e28d88f9908f0aaabf9f3edd9f60a5ec866
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-02-02 03:07:09 +0000

    Debugger: GDB more fixes for gdb 7.7 and up. Uppercase expressions / See issue 27136
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47581 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazdebuggergdbmi/gdbmidebugger.pp
+++ components/lazdebuggergdbmi/gdbmidebugger.pp
@@ -8344,7 +8344,7 @@ begin
   end;
 
   R := GDBMIExecResultDefault;
-  Result := ExecuteCommandFull('-gdb-set var %s := %s', [AExpression, S], [cfscIgnoreError], @GDBModifyDone, 0, R)
+  Result := ExecuteCommandFull('-gdb-set var %s := %s', [UpperCase(AExpression), S], [cfscIgnoreError], @GDBModifyDone, 0, R)
         and (R.State <> dsError);
 
   FTypeRequestCache.Clear;
@@ -9204,7 +9204,7 @@ begin
   Result := False;
   if ABreakID = 0 then Exit;
 
-  Result := ExecuteCommand('-break-condition %d %s', [ABreakID, AnExpression], []);
+  Result := ExecuteCommand('-break-condition %d %s', [ABreakID, UpperCase(AnExpression)], []);
 end;
 
 { TGDBMIDebuggerCommandBreakInsert }
@@ -9247,7 +9247,7 @@ begin
     bpkData:
       begin
         if (FWatchData = '') then exit;
-        WatchExpr := WatchData;
+        WatchExpr := UpperCase(WatchData);
         if FWatchScope = wpsGlobal then begin
           Result := ExecuteCommand('ptype %s', [WatchExpr], R);
           Result := Result and (R.State <> dsError);
