312e2623ac88e34f19fe6f08b9eb9f9fe8be40e3
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-02-13 23:21:14 +0000

    lazreport: use clocale only under unix
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47757 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazreport/source/lr_class.pas
+++ components/lazreport/source/lr_class.pas
@@ -15,7 +15,8 @@ interface
 {$I LR_Vers.inc}
 
 uses
-  SysUtils, Math, CLocale, Classes, MaskUtils, Controls, FileUtil, Forms,
+  SysUtils, Math, {$IFDEF UNIX}CLocale,{$ENDIF}
+  Classes, MaskUtils, Controls, FileUtil, Forms,
   Dialogs, Menus, Variants, DB, Graphics, Printers, osPrinters, LazUTF8, DOM,
   XMLWrite, XMLRead, XMLConf, LCLType, LCLIntf, TypInfo, LR_View, LR_Pars,
   LR_Intrp, LR_DSet, LR_DBSet, LR_DBRel, LR_Const, DbCtrls, LazUtf8Classes,
