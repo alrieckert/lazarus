15785e847c1206b3dcc5b7603cc875ff5dc2ee5d
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-02-16 11:54:18 +0000

    LCL: deprecated hint
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47824 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/graphics.pp
+++ lcl/graphics.pp
@@ -561,7 +561,7 @@ type
     procedure EndUpdate;
     property FontData: TFontData read GetData write SetData;
     function HandleAllocated: boolean;
-    property Handle: HFONT read GetHandle write SetHandle; deprecated;
+    property Handle: HFONT read GetHandle write SetHandle; deprecated 'use Reference.Handle instead';
     function IsDefault: boolean;
     function IsEqual(AFont: TFont): boolean; virtual;
     property IsMonoSpace: boolean read GetIsMonoSpace;
