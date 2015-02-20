d9925acfab423150728a4921fa3b05d4aeb64c9c
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-02-20 15:49:38 +0000

    example: OnShowHint
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47919 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ examples/customhint/unit1.pas
@@ -0,0 +1,68 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LazLogger, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, StdCtrls, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    HideTimeoutLabel: TLabel;
+    HideTimeoutTrackBar: TTrackBar;
+    ReshowTimeoutLabel: TLabel;
+    ReshowTimeoutTrackBar: TTrackBar;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1ShowHint(Sender: TObject; HintInfo: PHintInfo);
+  private
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Button1.OnShowHint:=@Button1ShowHint;
+end;
+
+procedure TForm1.Button1ShowHint(Sender: TObject; HintInfo: PHintInfo);
+var
+  Hour: word;
+  Minute: word;
+  Second: word;
+  MilliSecond: word;
+begin
+  DebugLn(['TForm1.Button1ShowHint',
+    ' HintControl=',DbgSName(HintInfo^.HintControl),
+    ' HintWindowClass=',DbgSName(HintInfo^.HintWindowClass),
+    ' HintPos=',dbgs(HintInfo^.HintPos),
+    ' HintMaxWidth=',HintInfo^.HintMaxWidth,
+    ' HintColor=',dbgs(HintInfo^.HintColor),
+    ' CursorRect=',dbgs(HintInfo^.CursorRect),
+    ' CursorPos=',dbgs(HintInfo^.CursorPos),
+    ' ReshowTimeout=',HintInfo^.ReshowTimeout,
+    ' HideTimeout=',HintInfo^.HideTimeout,
+    ' HintStr=',dbgstr(HintInfo^.HintStr),
+    ' HintData=',dbgs(HintInfo^.HintData)
+    ]);
+  DecodeTime(Now, Hour, Minute, Second, MilliSecond);
+  HintInfo^.HintStr:=Format('Time: %2D:%2D:%2D.%4D',[Hour, Minute, Second, MilliSecond]);
+  HintInfo^.HideTimeout:=HideTimeoutTrackBar.Position;
+  HintInfo^.ReshowTimeout:=ReshowTimeoutTrackBar.Position;
+end;
+
+end.
+
