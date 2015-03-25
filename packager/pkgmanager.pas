f9d5d227f39261e55fd1be3c03fd75a805819cdd
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-03-25 20:49:49 +0000

    Packager: a better fix for new package save dialog, issue #27726.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@48505 4005530d-fff6-0310-9dd1-cebe43e6787f

--- packager/pkgmanager.pas
+++ packager/pkgmanager.pas
@@ -1132,7 +1132,8 @@ begin
   try
     InputHistories.ApplyFileDialogSettings(SaveDialog);
     SaveDialog.Title:=Format(lisPkgMangSavePackageLpk, [APackage.IDAsString]);
-    SaveDialog.Filter:='lpk files|*.lpk';
+    SaveDialog.Filter:=lisLazarusPackage+' (*.lpk)|*.lpk'
+                     +'|'+dlgAllFiles+' ('+FileMask+')|'+FileMask;
     if APackage.HasDirectory then
       SaveDialog.InitialDir:=APackage.Directory;
 
