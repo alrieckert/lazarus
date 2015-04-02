6e856b7826c0084fdb8b0fed0f40b863fe18b3a3
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-04-02 12:12:04 +0000

    IDE/lazbuild: build ide: resolve macros in environment custom options
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@48591 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/buildlazdialog.pas
+++ ide/buildlazdialog.pas
@@ -565,6 +565,7 @@ var
   LazDir, TargetLCLPlatform: string;
   DefaultTargetFilename: String;
   IsCrossCompiling: Boolean;
+  s: String;
 begin
   Result:=mrOk;
   fOutputDirRedirected:=False;
@@ -591,10 +592,13 @@ begin
       AppendExtraOption('-dWIN9XPLATFORM');
     {$ENDIF}
 
-    AppendExtraOption(fProfile.ExtraOptions,false);
-
+    // append profile and global custom options
+    s:=fProfile.ExtraOptions;
     if OnAppendCustomOption<>nil then
-      OnAppendCustomOption(Self,fExtraOptions,[bmgtEnvironment]);
+      OnAppendCustomOption(Self,s,[bmgtEnvironment]);
+
+    GlobalMacroList.SubstituteStr(s);
+    AppendExtraOption(s,false);
   end;
 
   // set target filename and target directory:
