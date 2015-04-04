c6c65a07a1a348c7dac0036097fd58ebb1b08d2e
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-04-04 21:49:29 +0000

    LCL: re-added CalculateAutoRanges for custom controls
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@48626 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/forms.pp
+++ lcl/forms.pp
@@ -157,6 +157,7 @@ type
     class procedure WSRegisterClass; override;
     procedure AlignControls(AControl: TControl; var ARect: TRect); override;
     function AutoScrollEnabled: Boolean; virtual;
+    procedure CalculateAutoRanges; virtual;
     procedure CreateWnd; override;
     function GetClientScrollOffset: TPoint; override;
     function GetLogicalClientRect: TRect; override;// logical size of client area
