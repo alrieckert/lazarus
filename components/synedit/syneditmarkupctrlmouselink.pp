df7827433a63d223b3c1876111f50431801af343
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-05-27 20:13:33 +0000

    SynEdit: Fixed Option for MouseLink (when not using MouseActions)
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49192 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/syneditmarkupctrlmouselink.pp
+++ components/synedit/syneditmarkupctrlmouselink.pp
@@ -172,6 +172,13 @@ var
   i: Integer;
 begin
   Result := False;
+
+  if not (emUseMouseActions in TCustomSynEdit(SynEdit).MouseOptions) then begin
+    Result := (emShowCtrlMouseLinks in TCustomSynEdit(SynEdit).MouseOptions) and
+              (AShift * [ssShift, ssCtrl, ssAlt] = [ssCtrl]);
+    exit;
+  end;
+
   // todo: check FMouseSelActions if over selection?
   for i := 0 to TCustomSynEdit(SynEdit).MouseActions.Count - 1 do begin
     act := TCustomSynEdit(SynEdit).MouseActions.Items[i];
