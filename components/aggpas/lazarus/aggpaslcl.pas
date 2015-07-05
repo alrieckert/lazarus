693d2189e2cf81dafbd1c68ef646aa53763c7714
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-07-05 09:50:56 +0000

    aggpass: added missing file
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49498 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/aggpas/lazarus/aggpaslcl.pas
@@ -0,0 +1,66 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit AggPasLCL;
+
+interface
+
+uses
+  agg_2D, agg_alpha_mask_u8, agg_arc, agg_array, agg_arrowhead, agg_basics, 
+  agg_bezier_arc, agg_bitset_iterator, agg_blur, agg_bounding_rect, 
+  agg_bspline, agg_clip_liang_barsky, agg_color, agg_conv_adaptor_vcgen, 
+  agg_conv_adaptor_vpgen, agg_conv_bspline, agg_conv_clip_polygon, 
+  agg_conv_clip_polyline, agg_conv_concat, agg_conv_contour, agg_conv_curve, 
+  agg_conv_dash, agg_conv_marker, agg_conv_marker_adaptor, 
+  agg_conv_segmentator, agg_conv_shorten_path, agg_conv_smooth_poly1, 
+  agg_conv_stroke, agg_conv_transform, agg_curves, agg_dda_line, agg_ellipse, 
+  agg_ellipse_bresenham, agg_embedded_raster_fonts, agg_font_cache_manager, 
+  agg_font_engine, agg_fpimage, agg_gamma_functions, agg_gamma_lut, 
+  agg_glyph_raster_bin, agg_gradient_lut, agg_gsv_text, agg_image_filters, 
+  Agg_LCL, agg_line_aa_basics, agg_math, agg_math_stroke, agg_path_storage, 
+  agg_path_storage_integer, agg_pattern_filters_rgba, agg_pixfmt, 
+  agg_pixfmt_amask_adaptor, agg_pixfmt_gray, agg_pixfmt_rgb, 
+  agg_pixfmt_rgb_packed, agg_pixfmt_rgba, agg_pixfmt_transposer, 
+  agg_rasterizer_cells_aa, agg_rasterizer_compound_aa, agg_rasterizer_outline, 
+  agg_rasterizer_outline_aa, agg_rasterizer_scanline_aa, 
+  agg_rasterizer_sl_clip, agg_render_scanlines, agg_renderer_base, 
+  agg_renderer_markers, agg_renderer_mclip, agg_renderer_outline_aa, 
+  agg_renderer_outline_image, agg_renderer_primitives, 
+  agg_renderer_raster_text, agg_renderer_scanline, agg_rendering_buffer, 
+  agg_rendering_buffer_dynarow, agg_rounded_rect, agg_scanline, 
+  agg_scanline_bin, agg_scanline_boolean_algebra, agg_scanline_p, 
+  agg_scanline_storage_aa, agg_scanline_storage_bin, agg_scanline_u, 
+  agg_shorten_path, agg_simul_eq, agg_span_allocator, agg_span_converter, 
+  agg_span_generator, agg_span_gouraud, agg_span_gouraud_gray, 
+  agg_span_gouraud_rgba, agg_span_gradient, agg_span_gradient_alpha, 
+  agg_span_gradient_contour, agg_span_gradient_image, agg_span_image_filter, 
+  agg_span_image_filter_gray, agg_span_image_filter_rgb, 
+  agg_span_image_filter_rgba, agg_span_image_resample, 
+  agg_span_image_resample_gray, agg_span_image_resample_rgb, 
+  agg_span_image_resample_rgba, agg_span_interpolator_adaptor, 
+  agg_span_interpolator_linear, agg_span_interpolator_persp, 
+  agg_span_interpolator_trans, agg_span_pattern, agg_span_pattern_filter_gray, 
+  agg_span_pattern_filter_rgb, agg_span_pattern_filter_rgba, 
+  agg_span_pattern_resample_gray, agg_span_pattern_resample_rgb, 
+  agg_span_pattern_resample_rgba, agg_span_pattern_rgb, agg_span_pattern_rgba, 
+  agg_span_solid, agg_span_subdiv_adaptor, agg_trans_affine, 
+  agg_trans_bilinear, agg_trans_double_path, agg_trans_perspective, 
+  agg_trans_single_path, agg_trans_viewport, agg_trans_warp_magnifier, 
+  agg_vcgen_bspline, agg_vcgen_contour, agg_vcgen_dash, 
+  agg_vcgen_markers_term, agg_vcgen_smooth_poly1, agg_vcgen_stroke, 
+  agg_vcgen_vertex_sequence, agg_vertex_sequence, agg_vertex_source, 
+  agg_vpgen_clip_polygon, agg_vpgen_clip_polyline, agg_vpgen_segmentator, 
+  agg_svg_exception, agg_svg_parser_lcl, agg_svg_path_renderer, 
+  agg_svg_path_tokenizer, agg_color_conv, expat, expat_basics, expat_external, 
+  xmlrole, xmltok, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('AggPasLCL', @Register);
+end.
