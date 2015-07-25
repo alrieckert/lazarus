2d5093819ca64ede93967efcb0d171bad3acfaf0
bart <bart@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-07-25 14:52:17 +0000

    Examples: ResExplorer - implement a binary viewer as default viewer for resources that don't have a dedicated viewer yet. - move the logic of what esource to view from the TreeView's OnClick to the OnSelectionChanged event. - implement Relase and Debug build modes.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49568 4005530d-fff6-0310-9dd1-cebe43e6787f

--- examples/lazresexplorer/remainunit.pas
+++ examples/lazresexplorer/remainunit.pas
@@ -5,8 +5,9 @@ unit reMainUnit;
 interface
 
 uses
-  SysUtils, resource, LazUTF8, Forms, Controls, Dialogs, ComCtrls, ActnList,
-  Menus, ExtCtrls, Grids, bitmapresource, versionresource, groupiconresource;
+  SysUtils, resource, LazUTF8, SynEdit, Forms, Controls, Dialogs, ComCtrls,
+  ActnList, Menus, ExtCtrls, Grids, bitmapresource, versionresource,
+  groupiconresource, Classes;
 
 type
 
@@ -33,6 +34,8 @@ type
     Splitter1: TSplitter;
     StatusBar1: TStatusBar;
     StringGrid1: TStringGrid;
+    SynEdit1: TSynEdit;
+    tabBinary: TTabSheet;
     tabString: TTabSheet;
     tabImage: TTabSheet;
     ToolBar1: TToolBar;
@@ -49,13 +52,14 @@ type
       Section: THeaderSection);
     procedure hlpAboutExecute(Sender: TObject);
     procedure Splitter1ChangeBounds(Sender: TObject);
-    procedure TreeView1Click(Sender: TObject);
+    procedure TreeView1SelectionChanged(Sender: TObject);
   private
     Res:TResources;
     procedure ClearDisplay;
     procedure LoadVersionResource(V:TVersionResource);
     procedure LoadBitmapResource(B:TBitmapResource);
     procedure LoadGroupIconResource(G:TGroupIconResource);
+    procedure LoadResourceAsBinary(R: TAbstractResource);
   public
     procedure OpenFile(const AFileName:string);
   end;
@@ -103,6 +107,8 @@ begin
   OpenDialog1.Filter := sAllFilesSharedLibSoSo;
   {$ENDIF}
   Splitter1ChangeBounds(nil);
+  SynEdit1.Gutter.Visible := False;
+  ClearDisplay;
 end;
 
 procedure TreMainForm.FormDestroy(Sender: TObject);
@@ -130,13 +136,13 @@ begin
   HeaderControl1.Sections[1].Width:=Width -  TreeView1.Width;
 end;
 
-procedure TreMainForm.TreeView1Click(Sender: TObject);
+procedure TreMainForm.TreeView1SelectionChanged(Sender: TObject);
 var
   ResItem:TAbstractResource;
 begin
+  if Assigned(TreeView1.Selected) then ClearDisplay;
   if Assigned(TreeView1.Selected) and Assigned(TreeView1.Selected.Data) then
   begin
-    ClearDisplay;
     ResItem:=TAbstractResource(TreeView1.Selected.Data);
     HeaderControl1.Sections[1].Text:=ResItem.ClassName + ' : ' + ResItem.Name.Name;
     if ResItem is TVersionResource then
@@ -148,14 +154,17 @@ begin
     if ResItem is TGroupIconResource then
       LoadGroupIconResource(ResItem as TGroupIconResource)
     else
-      ;
+      LoadResourceAsBinary(ResItem);
   end;
 end;
+
 
 procedure TreMainForm.ClearDisplay;
 begin
   StringGrid1.Clean;
+  StringGrid1.Visible := False;
   Image1.Picture.Clear;
+  SynEdit1.Lines.Clear;
 end;
 
 procedure TreMainForm.LoadVersionResource(V: TVersionResource);
@@ -163,6 +172,7 @@ var
   i,j, k:integer;
 begin
   PageControl1.ActivePage:=tabString;
+  StringGrid1.Visible := True;
   k:=0;
   StringGrid1.RowCount:=0;
   StringGrid1.ColCount:=2;
@@ -188,7 +198,59 @@ end;
 
 procedure TreMainForm.LoadGroupIconResource(G: TGroupIconResource);
 begin
+  //ToDo: implement proper method to display
+  LoadResourceAsBinary(G);
+end;
 
+procedure TreMainForm.LoadResourceAsBinary(R: TAbstractResource);
+var
+  Offset, Size, BytesRead: Int64;
+  Buf: Array[0..15] of Byte;
+  Line, BinLine, AscLine: ShortString;
+  i: Integer;
+begin
+  PageControl1.ActivePage:=tabBinary;
+  //writeln('TreMainForm.LoadResourceAsBinary');
+  //writeln('  ClassName      = ',R.ClassName);
+  //writeln('  DataSize       = ',R.DataSize);
+  //writeln('  R.RawData.Size = ',R.RawData.Size);
+  //writeln('  DataOffset     = ',R.DataOffset);
+  //writeln('  DataVersion    = ',R.DataVersion);
+  SynEdit1.Lines.BeginUpdate;
+  try
+    Offset := 0;
+    R.RawData.Position := 0;
+    Size := R.RawData.Size;
+    while (Offset < Size) do
+    begin
+      FillChar(Buf{%H-}, SizeOf(Buf), #0);
+      BytesRead := R.RawData.Read(Buf[0], SizeOf(Buf));
+      //writeln('    BytesRead = ',BytesRead);
+      Line := '';
+      BinLine := '';
+      Ascline := '';
+      for i := 0 to BytesRead - 1 do
+      begin
+        BinLine := BinLine + IntToHex(Buf[i],2) + #32;
+        if (Buf[i] in [32..127]) then
+          AscLine := AscLine + Char(Buf[i])
+        else
+          AscLine := AscLine + #32;
+      end;
+      for i := (BytesRead + 1) to SizeOf(Buf) do
+      begin
+        BinLine := BinLine + '   ';
+        AscLine := AscLine + ' ';
+      end;
+      Line := IntToHex(Offset, 8) + #32;
+      Line := Line + BinLine + #32 + AscLine + LineEnding;
+      //writeln('    Line = ',Line);
+      SynEdit1.Lines.Add(Line);
+      Inc(Offset, SizeOf(Buf));
+    end;
+  finally
+    SynEdit1.Lines.EndUpdate;
+  end;
 end;
 
 procedure TreMainForm.OpenFile(const AFileName: string);
