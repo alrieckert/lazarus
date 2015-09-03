ed196877397ee21a7857e0d1725f9c561090ccf1
bart <bart@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-09-03 19:44:46 +0000

    LCL: Publish TEditButton.ParentShowHint (Issue #0028492).
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49751 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/editbtn.pas
+++ lcl/editbtn.pas
@@ -394,6 +394,7 @@ type
     property ParentBiDiMode;
     property ParentColor;
     property ParentFont;
+    property ParentShowHint;
     property PasswordChar;
     property PopupMenu;
     property ReadOnly;
