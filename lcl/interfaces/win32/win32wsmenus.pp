25f02cc3fdcf1507096b47995c61ca6784d2041b
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-09-06 18:11:37 +0000

    LCL-Win32: Don't cut menu texts when using non-default font. Issue #28622, patch from Ondrej Pokorny.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49771 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/win32/win32wsmenus.pp
+++ lcl/interfaces/win32/win32wsmenus.pp
@@ -563,6 +563,7 @@ var
   IsRightToLeft: Boolean;
   Info: tagMENUBARINFO;
   AWnd: HWND;
+  CalculatedSize: TSIZE;
 begin
   if (ItemState and ODS_SELECTED) <> 0 then
     MenuState := tmBarItemPushed
@@ -622,10 +623,13 @@ begin
   ThemeDrawElement(AHDC, Details, ARect, nil);
 
   TextRect := ARect;
-  inc(TextRect.Left, Metrics.ItemMargins.cxLeftWidth);
-  dec(TextRect.Right, Metrics.ItemMargins.cxRightWidth);
-  inc(TextRect.Top, Metrics.ItemMargins.cyTopHeight);
-  dec(TextRect.Bottom, Metrics.ItemMargins.cyBottomHeight);
+  //center the menu item
+  CalculatedSize := VistaBarMenuItemSize(AMenuItem, AHDC);
+  TextRect.Left := (TextRect.Right+TextRect.Left-CalculatedSize.cx) div 2;
+  TextRect.Right := TextRect.Left + CalculatedSize.cx;
+  TextRect.Top := (TextRect.Bottom+TextRect.Top-CalculatedSize.cy) div 2;
+  TextRect.Bottom := TextRect.Top + CalculatedSize.cy;
+
   // draw check/image
   if AMenuItem.HasIcon then
   begin
