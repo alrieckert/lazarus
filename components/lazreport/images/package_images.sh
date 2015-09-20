47815cddaf2d6207b5433e3336a86ee6a7e22bfa
jesus <jesus@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-09-20 15:54:45 +0000

    LazReport, added missing package_images.sh
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49855 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/lazreport/images/package_images.sh
@@ -0,0 +1,2 @@
+../../../tools/lazres ../source/lr_register.res @Package_images.txt
+../../../tools/lazres ../source/bullets.res bitmaps/bulletgray.png bitmaps/bulletgreen.png designer/lrd_ins_fields.bmp
