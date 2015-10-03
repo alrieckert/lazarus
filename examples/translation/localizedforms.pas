alrieckert/lazarus

commit ff22af15aec6c03fa832263859c61c4b6e9f31cb

Examples: Fix compilation when function wrappers in FileUtil are disabled (define NoLazUTF8Wrappers). Clean uses sections.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49495 4005530d-fff6-0310-9dd1-cebe43e6787f

examples/translation/localizedforms.pas
@@ -16,7 +16,7 @@ unit LocalizedForms;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, Forms;
 
 type
   TLocalizedForm = class(TForm)
@@ -48,11 +48,6 @@ implementation
 
 {$R *.lfm}
 
-uses
-{$IFDEF MSWINDOWS}
-  Windows,
-{$ENDIF}
-  ExtCtrls;
 
 
 { Local procedures }

commit 01e2102002a231f064af438dddd7cd1ae21174d6

Examples: Fix compilation of "Translation" example on Windows, broken by r49495. Issue #28761.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49920 4005530d-fff6-0310-9dd1-cebe43e6787f

examples/translation/localizedforms.lfm
@@ -4,5 +4,5 @@ object LocalizedForm: TLocalizedForm
   Top = 238
   Width = 320
   Caption = 'LocalizedForm'
-  LCLVersion = '1.3'
+  LCLVersion = '1.5'
 end

examples/translation/localizedforms.pas
@@ -16,7 +16,10 @@ unit LocalizedForms;
 interface
 
 uses
-  Classes, Forms;
+{$IFDEF MSWINDOWS}
+  Windows,
+{$ENDIF}
+  Classes, SysUtils, Forms;
 
 type
   TLocalizedForm = class(TForm)
@@ -47,8 +50,6 @@ function GetLCIDFromLangCode(ALang: String): Integer;
 implementation
 
 {$R *.lfm}
-
-
 
 { Local procedures }
 