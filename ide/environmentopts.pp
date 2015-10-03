40964a3034f57f9fc324573c92253aa51286b6cd
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-10-03 16:10:16 +0000

    lazbuild: fixed default lazarusdir
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@49927 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/environmentopts.pp
+++ ide/environmentopts.pp
@@ -2276,7 +2276,7 @@ begin
           begin
             ParsedValue:=TrimAndExpandDirectory(ParsedValue,GetPrimaryConfigPath);
             if ParsedValue='' then
-              ParsedValue:=GetPrimaryConfigPath;
+              ParsedValue:=TrimFilename(AppendPathDelim(GetCurrentDirUTF8));
           end;
         eopFPCSourceDirectory,eopTestBuildDirectory:
           // directory
