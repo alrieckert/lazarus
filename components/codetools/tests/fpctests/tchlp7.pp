d906e18f8f221e947e22e0e60d70e724cf9e781c
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-10-09 10:21:36 +0000

    codetools: added test for default property in class helper
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50019 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/codetools/tests/fpctests/tchlp7.pp
@@ -0,0 +1,42 @@
+{ helpers may override existing default properties }
+program tchlp7;
+
+{$ifdef fpc}
+  {$mode delphi}
+{$endif}
+{$apptype console}
+
+type
+  TTest = class
+  private
+    function GetTest(aIndex: Integer): String;
+  public
+    property Test[Index: Integer]: String read GetTest; default;
+  end;
+
+  TTestHelper = class helper for TTest
+    function GetTest(aIndex: Integer): Integer;
+    property Test[Index: Integer]: Integer read GetTest; default;
+  end;
+
+function TTest.GetTest(aIndex: Integer): String;
+begin
+  Result := chr(aIndex);
+end;
+
+function TTestHelper.GetTest(aIndex: Integer): Integer;
+begin
+  Result := aIndex;
+end;
+
+var
+  t: TTest;
+  res: Integer;
+begin
+  t := TTest.Create;
+  res{guesstype:integer} := t[3];
+  Writeln('value: ', res);
+  if res <> 3 then
+    Halt(1);
+  Writeln('ok');
+end.
