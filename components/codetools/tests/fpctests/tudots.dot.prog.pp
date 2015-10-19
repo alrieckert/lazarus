09a50d1f1e86c5b8f288675c9468d90dc622f3b5
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-10-19 13:25:09 +0000

    codetools: added tests for dotted units
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50121 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/tests/fpctests/tudots.dot.prog.pp
+++ components/codetools/tests/fpctests/tudots.dot.prog.pp
@@ -5,7 +5,7 @@ program tudots.dot.prog;
 {$mode delphi}
 
 uses
-  tudots{ todo declaration:tudots};
+  tudots{declaration:tudots};
 
 begin
   // this must fail because we have a namespace tudots.dot and it has no unit test
