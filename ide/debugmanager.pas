f7c2a2c1f7ef47589076d93e56bac0cd1894c7e9
ondrej <ondrej@4005530d-fff6-0310-9dd1-cebe43e6787f>
2015-10-29 11:52:52 +0000

    IDE: fixed debugger menu commands.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50198 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/debugmanager.pas
+++ ide/debugmanager.pas
@@ -885,28 +885,33 @@ begin
 end;
 
 procedure TDebugManager.mnuViewDebugDialogClick(Sender: TObject);
+var
+  xCommand: Integer;
 begin
-  if Sender is TIDEMenuItem
-  then begin
-    ViewDebugDialog(TDebugDialogType((Sender as TIDEMenuItem).Tag));
-  end;
+  if (Sender is TIDESpecialCommand) and (TIDESpecialCommand(Sender).Command<>nil) then
+    xCommand := TIDESpecialCommand(Sender).Command.Command
+  else
+  if Sender is TIDECommand then
+    xCommand := TIDECommand(Sender).Command
+  else
+    xCommand := -1;
 
-  if Sender is TIDECommand then begin
-    case TIDECommand(Sender).Command of
-      ecToggleWatches     : ViewDebugDialog(ddtWatches);
-      ecToggleBreakPoints : ViewDebugDialog(ddtBreakpoints);
-      ecToggleDebuggerOut : ViewDebugDialog(ddtOutput);
-      ecToggleLocals      : ViewDebugDialog(ddtLocals);
-      ecToggleCallStack   : ViewDebugDialog(ddtCallStack);
-      ecToggleRegisters   : ViewDebugDialog(ddtRegisters);
-      ecToggleAssembler   : ViewDebugDialog(ddtAssembler);
-      ecToggleDebugEvents : ViewDebugDialog(ddtEvents);
-      ecEvaluate          : ViewDebugDialog(ddtEvaluate);
-      ecInspect           : ViewDebugDialog(ddtInspect);
-      ecViewPseudoTerminal: ViewDebugDialog(ddtPseudoTerminal);
-      ecViewThreads       : ViewDebugDialog(ddtThreads);
-      ecViewHistory       : ViewDebugDialog(ddtHistory);
-    end;
+  case xCommand of
+    ecToggleWatches     : ViewDebugDialog(ddtWatches);
+    ecToggleBreakPoints : ViewDebugDialog(ddtBreakpoints);
+    ecToggleDebuggerOut : ViewDebugDialog(ddtOutput);
+    ecToggleLocals      : ViewDebugDialog(ddtLocals);
+    ecToggleCallStack   : ViewDebugDialog(ddtCallStack);
+    ecToggleRegisters   : ViewDebugDialog(ddtRegisters);
+    ecToggleAssembler   : ViewDebugDialog(ddtAssembler);
+    ecToggleDebugEvents : ViewDebugDialog(ddtEvents);
+    ecEvaluate          : ViewDebugDialog(ddtEvaluate);
+    ecInspect           : ViewDebugDialog(ddtInspect);
+    ecViewPseudoTerminal: ViewDebugDialog(ddtPseudoTerminal);
+    ecViewThreads       : ViewDebugDialog(ddtThreads);
+    ecViewHistory       : ViewDebugDialog(ddtHistory);
+  else
+    raise Exception.CreateFmt('IDE Internal error: TDebugManager.mnuViewDebugDialogClick, wrong command parameter %d.', [xCommand]);
   end;
 end;
 
