9edc7808dad9e7fe07ba6a36f443e30dd5cb1fb2
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-01-04 00:45:00 +0000

    Anchordocking: Don't update a tabbed page's caption with its name when saving desktops. Issue #29199.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51181 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/anchordocking/anchordocking.pas
+++ components/anchordocking/anchordocking.pas
@@ -1983,7 +1983,6 @@ var
             Site.Pages.Pages.Add(aPageName);
           aPage:=Site.Pages.Page[j];
           inc(j);
-          aPage.Caption:=aPageName;
           AControl:=Restore(Node[i],aPage);
           if AControl=nil then continue;
           AControl.Align:=alClient;
