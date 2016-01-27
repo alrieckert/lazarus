d1cc19dc9bb893bc7f1b5e781d9da32600c64be5
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-01-27 18:18:18 +0000

    New example for IPro FindNode functions. Issue #29483, from Silvio Clécio.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51432 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ components/turbopower_ipro/examples/sumab.lpr
@@ -0,0 +1,19 @@
+program sumab;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, Forms, sumabfrm;
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ components/turbopower_ipro/examples/sumabfrm.pas
@@ -0,0 +1,51 @@
+unit sumabfrm;
+
+{$mode objfpc}{$H+}
+{$MODESWITCH NESTEDPROCVARS}
+
+interface
+
+uses
+  Classes, SysUtils, Ipfilebroker, IpHtml, Forms, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    IpFileDataProvider1: TIpFileDataProvider;
+    IpHtmlPanel1: TIpHtmlPanel;
+    procedure FormShow(Sender: TObject);
+    procedure IpHtmlPanel1ControlClick2(Sender: TIpHtmlCustomPanel;
+      Frame: TIpHtmlFrame; Html: TIpHtml; Node: TIpHtmlNodeControl;
+      var cancel: boolean);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  IpHtmlPanel1.OpenURL(
+    ExpandLocalHtmlFileName(ExtractFilePath(ParamStr(0)) + 'sum.html'));
+end;
+
+procedure TForm1.IpHtmlPanel1ControlClick2(Sender: TIpHtmlCustomPanel;
+  Frame: TIpHtmlFrame; Html: TIpHtml; Node: TIpHtmlNodeControl;
+  var cancel: boolean);
+var
+  A, B: TIpHtmlNodeINPUT;
+begin
+  A := FindNodeByElemId(Html.HtmlNode, 'a') as TIpHtmlNodeINPUT;
+  B := FindNodeByElemId(Html.HtmlNode, 'b') as TIpHtmlNodeINPUT;
+  ShowMessageFmt('Sum: %d', [A.Value.ToInteger + B.Value.ToInteger]);
+end;
+
+end.
+
