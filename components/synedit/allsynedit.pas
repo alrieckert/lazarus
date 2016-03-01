5689c1add6705c1b05090a3ce2f8406e7d396c49
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-03-01 15:23:47 +0000

    components: less hints
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51780 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/allsynedit.pas
+++ components/synedit/allsynedit.pas
@@ -4,6 +4,7 @@
 
 unit allsynedit;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
--- components/synunihighlighter/synuni.pas
+++ components/synunihighlighter/synuni.pas
@@ -1,22 +1,23 @@
 { This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+  This source is only used to compile and install the package.
  }
 
-unit SynUni; 
+unit SynUni;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-  SynUniDesigner, SynUniHighlighter, SynUniReg, LazarusPackageIntf; 
+  SynUniDesigner, SynUniHighlighter, SynUniReg, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('SynUniHighlighter', @SynUniHighlighter.Register); 
-  RegisterUnit('SynUniReg', @SynUniReg.Register); 
-end; 
+  RegisterUnit('SynUniHighlighter', @SynUniHighlighter.Register);
+  RegisterUnit('SynUniReg', @SynUniReg.Register);
+end;
 
 initialization
-  RegisterPackage('SynUni', @Register); 
+  RegisterPackage('SynUni', @Register);
 end.
