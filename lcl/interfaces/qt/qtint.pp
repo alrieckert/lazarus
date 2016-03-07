fbb410878bdb699a57f8b69959fdfd9b649c3336
zeljko <zeljko@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-03-07 12:43:24 +0000

    Qt: added GetX11SupportedAtoms() helper function so we know what specific WM supports.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51844 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/qt/qtint.pp
+++ lcl/interfaces/qt/qtint.pp
@@ -265,6 +265,7 @@ type
   {force mapping}
   procedure MapX11Window(AWinID: LongWord);
   {$IFDEF QtUseAccurateFrame}
+  function GetX11SupportedAtoms(AWinID: LongWord; AList: TStrings): boolean;
   function GetX11WindowPos(AWinID: LongWord; out ALeft, ATop: integer): boolean;
   {check if wm supports request for frame extents}
   function AskX11_NET_REQUEST_FRAME_EXTENTS(AWinID: LongWord; out AMargins: TRect): boolean;
