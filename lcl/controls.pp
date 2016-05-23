1cab0cbb587b152bf52e0212175e7d8381b1b214
ondrej <ondrej@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-05-23 13:51:54 +0000

    LCL: add TControlAtPosFlag.capfOnlyWinControls
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@52355 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/controls.pp
+++ lcl/controls.pp
@@ -1862,7 +1862,8 @@ type
     capfAllowWinControls,// include TWinControls
     capfOnlyClientAreas, // use the client areas, not the whole child area
     capfRecursive,       // search recursively in grand childrens
-    capfHasScrollOffset  // do not add the scroll offset to Pos (already included)
+    capfHasScrollOffset, // do not add the scroll offset to Pos (already included)
+    capfOnlyWinControls  // include only TWinControls (ignore TControls)
     );
   TControlAtPosFlags = set of TControlAtPosFlag;
 
