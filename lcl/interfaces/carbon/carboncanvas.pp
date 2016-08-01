f96e95c751f0a6cff9a1625d7c67b0bbd92d80fe
sekelsenmat <sekelsenmat@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-08-01 19:04:24 +0000

    carbon: patch 28527, fixes LineTo 1 pixel issue
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@52766 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/carbon/carboncanvas.pp
+++ lcl/interfaces/carbon/carboncanvas.pp
@@ -1228,40 +1228,45 @@ procedure TCarbonDeviceContext.LineTo(X, Y: Integer);
 var
   deltaX, deltaY, absDeltaX, absDeltaY: Integer;
   clipDeltaX, clipDeltaY: Float32;
-  tx,ty:Float32;
+  tx, ty, bx, by: Float32;
 begin
-  deltaX := X - PenPos.x;
-  deltaY := Y - PenPos.y;
+  bx := FPenPos.x;
+  by := FPenPos.y;
+  deltaX := X - FPenPos.x;
+  deltaY := Y - FPenPos.y;
   if (deltaX=0) and (deltaY=0) then Exit;
 
   absDeltaX := Abs(deltaX);
   absDeltaY := Abs(deltaY);
+
   if (absDeltaX<=1) and (absDeltaY<=1) then
   begin
     // special case for 1-pixel lines
-    tx := FPenPos.x + 0.55;
-    ty := FPenPos.y + 0.55;
+    tx := bx + 0.05;
+    ty := by + 0.05;
   end
   else
   begin
-    // exclude the last pixel from the line
+    // correct start/end coordinates
     if absDeltaX > absDeltaY then
     begin
-      if deltaX > 0 then clipDeltaX := -1.0 else clipDeltaX := 1.0;
+      if deltaX > 0 then clipDeltaX := -0.5 else clipDeltaX := 0.5;
       clipDeltaY := clipDeltaX * deltaY / deltaX;
     end
     else
     begin
-      if deltaY > 0 then clipDeltaY := -1.0 else clipDeltaY := 1.0;
+      if deltaY > 0 then clipDeltaY := -0.5 else clipDeltaY := 0.5;
       clipDeltaX := clipDeltaY * deltaX / deltaY;
     end;
-    tx := X + clipDeltaX + 0.5;
-    ty := Y + clipDeltaY + 0.5;
+    bx := bx + clipDeltaX;
+    by := by + clipDeltaY;
+    tx := X + clipDeltaX;
+    ty := Y + clipDeltaY;
   end;
 
   CGContextBeginPath(CGContext);
-  CGContextMoveToPoint(CGContext, PenPos.x + 0.5, PenPos.y + 0.5);
-  CGContextAddLineToPoint(CGContext, tx, ty);
+  CGContextMoveToPoint(CGContext, bx + 0.5, by + 0.5);
+  CGContextAddLineToPoint(CGContext, tx + 0.5, ty + 0.5);
   CGContextStrokePath(CGContext);
 
   FPenPos.x := X;
