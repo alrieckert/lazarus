2c7625bb14153a06647d6232e9860fd9a0da3900
joost <joost@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-10-14 19:30:52 +0000

    * Added wslazdeviceapis to lclbase-package
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53120 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/alllclunits.pp
+++ lcl/alllclunits.pp
@@ -8,27 +8,23 @@ unit alllclunits;
 interface
 
 uses
-  CheckLst, Clipbrd, ColorBox, ComCtrls, Controls, CustomTimer, DBActns, 
-  DbCtrls, DBGrids, DefaultTranslator, Dialogs, DynamicArray, DynHashArray, 
-  DynQueue, ExtCtrls, ExtDlgs, ExtendedStrings, ExtGraphics, FileCtrl, Forms, 
-  Graphics, GraphMath, GraphType, GraphUtil, Grids, HelpIntfs, IcnsTypes, 
-  ImageListCache, ImgList, IniPropStorage, InterfaceBase, IntfGraphics, 
-  LazHelpHTML, LazHelpIntf, LazLinkedList, LCLClasses, LCLIntf, LCLMemManager, 
-  LCLMessageGlue, LCLProc, LCLResCache, LCLStrConsts, LCLType, Menus, 
-  LCLUnicodeData, LCLVersion, LMessages, LResources, Maps, MaskEdit, 
-  PairSplitter, PopupNotifier, PostScriptCanvas, PostScriptPrinter, 
-  PostScriptUnicode, Printers, PropertyStorage, RubberBand, ShellCtrls, Spin, 
-  StdActns, StdCtrls, StringHashList, TextStrings, Themes, TmSchema, Toolwin, 
-  Translations, UTrace, XMLPropStorage, TimePopup, Messages, WSButtons, 
-  WSCalendar, WSCheckLst, WSComCtrls, WSControls, WSDesigner, WSDialogs, 
-  WSExtCtrls, WSExtDlgs, WSFactory, WSForms, WSGrids, WSImgList, WSLCLClasses, 
-  WSMenus, WSPairSplitter, WSProc, WSReferences, WSSpin, WSStdCtrls, 
-  WSToolwin, ActnList, AsyncProcess, ButtonPanel, Buttons, Calendar, 
-  RegisterLCL, ValEdit, LazCanvas, LazDialogs, LazRegions, CustomDrawn_Common, 
-  CustomDrawnControls, CustomDrawnDrawers, LazDeviceApis, LDockTree, 
-  LazFreeTypeIntfDrawer, CustomDrawn_WinXP, CustomDrawn_Android, Arrow, 
-  EditBtn, ComboEx, DBExtCtrls, CustomDrawn_Mac, CalcForm, LCLTranslator, 
-  GroupedEdit, LCLTaskDialog, LazarusPackageIntf;
+  CheckLst, Clipbrd, ColorBox, ComCtrls, Controls, CustomTimer, DBActns, DbCtrls, DBGrids, 
+  DefaultTranslator, Dialogs, DynamicArray, DynHashArray, DynQueue, ExtCtrls, ExtDlgs, 
+  ExtendedStrings, ExtGraphics, FileCtrl, Forms, Graphics, GraphMath, GraphType, GraphUtil, Grids, 
+  HelpIntfs, IcnsTypes, ImageListCache, ImgList, IniPropStorage, InterfaceBase, IntfGraphics, 
+  LazHelpHTML, LazHelpIntf, LazLinkedList, LCLClasses, LCLIntf, LCLMemManager, LCLMessageGlue, 
+  LCLProc, LCLResCache, LCLStrConsts, LCLType, Menus, LCLUnicodeData, LCLVersion, LMessages, 
+  LResources, Maps, MaskEdit, PairSplitter, PopupNotifier, PostScriptCanvas, PostScriptPrinter, 
+  PostScriptUnicode, Printers, PropertyStorage, RubberBand, ShellCtrls, Spin, StdActns, StdCtrls, 
+  StringHashList, TextStrings, Themes, TmSchema, Toolwin, Translations, UTrace, XMLPropStorage, 
+  TimePopup, Messages, WSButtons, WSCalendar, WSCheckLst, WSComCtrls, WSControls, WSDesigner, 
+  WSDialogs, WSExtCtrls, WSExtDlgs, WSFactory, WSForms, WSGrids, WSImgList, WSLCLClasses, WSMenus, 
+  WSPairSplitter, WSProc, WSReferences, WSSpin, WSStdCtrls, WSToolwin, ActnList, AsyncProcess, 
+  ButtonPanel, Buttons, Calendar, RegisterLCL, ValEdit, LazCanvas, LazDialogs, LazRegions, 
+  CustomDrawn_Common, CustomDrawnControls, CustomDrawnDrawers, LazDeviceApis, LDockTree, 
+  LazFreeTypeIntfDrawer, CustomDrawn_WinXP, CustomDrawn_Android, Arrow, EditBtn, ComboEx, 
+  DBExtCtrls, CustomDrawn_Mac, CalcForm, LCLTranslator, GroupedEdit, LCLTaskDialog, 
+  WSLazDeviceAPIS, LazarusPackageIntf;
 
 implementation
 
