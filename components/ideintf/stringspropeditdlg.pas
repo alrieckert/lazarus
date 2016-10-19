2a246abdebd85a3a1d947bc6c2eca40aa286b03b
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-10-19 21:02:09 +0000

    IDE: Add a "Clear" button to StringsPropEditor dialog. Issue #30743, patch from Silvio Clécio.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53164 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/ideintf/stringspropeditdlg.pas
+++ components/ideintf/stringspropeditdlg.pas
@@ -25,10 +25,12 @@ type
 
   TStringsPropEditorFrm = class(TForm)
     BtnPanel: TButtonPanel;
+    ClearButton: TButton;
     StatusLabel: TLabel;
     SortButton: TButton;
     TextGroupBox: TGroupBox;
     Memo: TMemo;
+    procedure ClearButtonClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure MemoChange(Sender: TObject);
     procedure SortButtonClick(Sender: TObject);
@@ -48,8 +50,14 @@ begin
   Caption := oisStringsEditorDialog;
   StatusLabel.Caption := ois0Lines0Chars;
   SortButton.Caption := oisSort;
+  ClearButton.Caption := oisClear;
 
   AddButtons;
+end;
+
+procedure TStringsPropEditorFrm.ClearButtonClick(Sender: TObject);
+begin
+  Memo.Clear;
 end;
 
 procedure TStringsPropEditorFrm.MemoChange(Sender: TObject);
@@ -58,7 +66,8 @@ var
   I: Integer;
 begin
   NumChars := 0;
-  for I := 0 to Memo.Lines.Count - 1 do Inc(NumChars, Utf8Length(Memo.Lines[I]));
+  for I := 0 to Memo.Lines.Count - 1 do
+    Inc(NumChars, Utf8Length(Memo.Lines[I]));
 
   if Memo.Lines.Count = 1 then
     StatusLabel.Caption := Format(ois1LineDChars, [NumChars])
