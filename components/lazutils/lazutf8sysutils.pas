a4ede9d20d2778d8ef30c8d8dedebc41a7bb83b8
ondrej <ondrej@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-11-14 13:41:54 +0000

    LazUtils: lazutf8sysutils: use GetTickCount32 from FPC 3 if available. Issue 30927
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53364 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazutils/lazutf8sysutils.pas
+++ components/lazutils/lazutf8sysutils.pas
@@ -31,6 +31,13 @@ uses
   {$endif}
   Classes;
 
+{$IF FPC_FULLVERSION>=30000}
+function GetTickCount64: QWord;
+begin
+  Result := SysUtils.GetTickCount64;
+end;
+{$ENDIF}
+
 // ToDo: Move the code to 1 include file per platform
 {$IFDEF WINDOWS}
 function NowUTC: TDateTime;
@@ -41,13 +48,15 @@ begin
   result := systemTimeToDateTime(SystemTime);
 end;
 
+{$IF FPC_FULLVERSION<30000}
 function GetTickCount64: QWord;
 begin
   // GetTickCount64 is better, but we need to check the Windows version to use it
   Result := Windows.GetTickCount();
 end;
+{$ENDIF FPC_FULLVERSION}
 
-{$else}
+{$else WINDOWS}
 {$ifdef UNIX}
 Const
 {Date Translation}
@@ -104,6 +113,7 @@ begin
   result := systemTimeToDateTime(SystemTime);
 end;
 
+{$IF FPC_FULLVERSION<30000}
 {$IF defined(Linux) and not defined(GetTickCountTimeOfDay)}
 function GetTickCount64: QWord;
 var
@@ -121,20 +131,23 @@ begin
   Result := (Int64(tp.tv_sec) * 1000) + (tp.tv_usec div 1000);
 end;
 {$ENDIF}
+{$ENDIF FPC_FULLVERSION}
 
-{$else}
-// Not Windows and not UNIX, so just write the most trivial code until we have something besser:
+{$else UNIX}
+// Not Windows and not UNIX, so just write the most trivial code until we have something better:
 function NowUTC: TDateTime;
 begin
   Result := Now;
 end;
 
+{$IF FPC_FULLVERSION<30000}
 function GetTickCount64: QWord;
 begin
   Result := Trunc(Now * 24 * 60 * 60 * 1000);
 end;
-{$endif}
-{$endif}
+{$ENDIF FPC_FULLVERSION}
+{$endif UNIX}
+{$endif WINDOWS}
 
 end.
 
