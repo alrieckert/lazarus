4d7d9766b7ebac0a543dfce60a24ba5e5633292b
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-12-01 11:29:31 +0000

    synedit: fixed license of SynHighlighterPosition
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53505 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/synedit/synhighlighterposition.pas
+++ components/synedit/synhighlighterposition.pas
@@ -7,11 +7,19 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
 the specific language governing rights and limitations under the License.
 
+Alternatively, the contents of this file may be used under the terms of the
+GNU General Public License Version 2 or later (the "GPL"), in which case
+the provisions of the GPL are applicable instead of those above.
+If you wish to allow use of your version of this file only under the terms
+of the GPL and not to allow others to use your version of this file
+under the MPL, indicate your decision by deleting the provisions above and
+replace them with the notice and other provisions required by the GPL.
+If you do not delete the provisions above, a recipient may use your version
+of this file under either the MPL or the GPL.
 
-$Id$
-
-This is a basic synedit highlighter that does not parse text to create the
-attributes, but stores a list of ranges.
+Abstract:
+  This is a basic synedit highlighter that does not parse text to create the
+  attributes, but stores a list of ranges.
 
 -------------------------------------------------------------------------------}
 unit SynHighlighterPosition;
