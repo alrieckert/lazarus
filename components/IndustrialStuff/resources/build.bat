dfae67dc88b51bd2b4ca4ea8345de6d292028960
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2016-12-28 19:11:43 +0000

    Move Industrial package from Lazarus sources to CCR.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53792 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/IndustrialStuff/resources/build.bat
+++
@@ -1,3 +0,0 @@
-..\..\..\tools\lazres ..\source\sensors.res @sensors.txt
-..\..\..\tools\lazres ..\source\industrial_icons.res @industrial_icons.txt
-..\..\..\tools\lazres ..\source\ledbuttons.res @ledbuttons.txt
