ba02126a1a06d4b2f2017ffffcfaf23a6f96d821
juha <juha@4005530d-fff6-0310-9dd1-cebe43e6787f>
2017-01-11 15:49:16 +0000

    LCL: Show minimize button on form with borderstyle single also with XFCE. Issue #31207, patch from AlexeyT.
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53927 4005530d-fff6-0310-9dd1-cebe43e6787f

--- lcl/interfaces/gtk2/gtk2globals.pp
+++ lcl/interfaces/gtk2/gtk2globals.pp
@@ -78,7 +78,7 @@ const
   //(bsNone, bsSingle, bsSizeable, bsDialog, bsToolWindow, bsSizeToolWin);
   GtkWindowTypeHints: array[TFormBorderStyle] of TGdkWindowTypeHint = (
     GDK_WINDOW_TYPE_HINT_NOTIFICATION,
-    GDK_WINDOW_TYPE_HINT_DIALOG,
+    GDK_WINDOW_TYPE_HINT_NORMAL,
     GDK_WINDOW_TYPE_HINT_NORMAL,
     GDK_WINDOW_TYPE_HINT_DIALOG,
     GDK_WINDOW_TYPE_HINT_UTILITY,
