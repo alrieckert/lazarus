d24b823e006653ac94e7f8b7ab9b1cdb959b4d31
joost <joost@4005530d-fff6-0310-9dd1-cebe43e6787f>
2017-01-29 21:02:29 +0000

    ide: Updated address of the Free Software Foundation
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@54030 4005530d-fff6-0310-9dd1-cebe43e6787f

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -22,7 +22,7 @@
  *   A copy of the GNU General Public License is available on the World    *
  *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
  *   obtain it by writing to the Free Software Foundation,                 *
- *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *   Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.   *
  *                                                                         *
  ***************************************************************************
 }
@@ -1167,7 +1167,7 @@ resourcestring
    +'A copy of the GNU General Public License is available on the World '
    +'Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also '
    +'obtain it by writing to the Free Software Foundation, '
-   +'Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
+   +'Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.';
 
   lisLGPLNotice =
     '<description>' + sLineBreak + sLineBreak
@@ -1182,7 +1182,7 @@ resourcestring
    +'GNU Library General Public License for more details. ' + sLineBreak + sLineBreak
    +'You should have received a copy of the GNU Library General Public License '
    +'along with this library; if not, write to the Free Software '
-   +'Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
+   +'Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.';
 
   lisModifiedLGPLNotice =
     '<description>' + sLineBreak + sLineBreak
@@ -1207,7 +1207,7 @@ resourcestring
    +'GNU Library General Public License for more details. ' + sLineBreak + sLineBreak
    +'You should have received a copy of the GNU Library General Public License '
    +'along with this library; if not, write to the Free Software '
-   +'Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.';
+   +'Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.';
 
   // MIT license aka Expat license see: http://www.opensource.org/licenses/MIT
   lisMITNotice =
