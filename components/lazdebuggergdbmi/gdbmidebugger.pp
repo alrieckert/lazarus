67d4181e0faf8baf2cf7ce482dc1114b255ad7f7
martin <martin@4005530d-fff6-0310-9dd1-cebe43e6787f>
2017-02-21 00:44:12 +0000

    Debugger: fix for gdb 7.12 issue 0031386 / patch by Cyrax
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@54215 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/lazdebuggergdbmi/gdbmidebugger.pp
+++ components/lazdebuggergdbmi/gdbmidebugger.pp
@@ -4988,6 +4988,14 @@ function TGDBMIDebuggerCommandStartDebugging.DoExecute: Boolean;
       if TargetInfo^.TargetPID <> 0 then exit;
     end;
 
+    // gdb server
+    if ExecuteCommand('info proc', [], R, [cfCheckState]) and (R.State <> dsError)
+    then begin
+      s := GetPart(['process '], [#10,#13#10], R.Values, True);
+      TargetInfo^.TargetPID := StrToIntDef(s, 0);
+      if TargetInfo^.TargetPID <> 0 then exit;
+    end;
+
     // apple / MacPort 7.1 / 32 bit dwarf
     if ExecuteCommand('info threads', [], R, [cfCheckState]) and (R.State <> dsError)
     then begin
@@ -4996,7 +5004,7 @@ function TGDBMIDebuggerCommandStartDebugging.DoExecute: Boolean;
       if TargetInfo^.TargetPID <> 0 then exit;
 
       // returned by gdb server (maybe others)
-      s := GetPart(['Thread '], [' '], R.Values, True);
+      s := GetPart(['Thread '], [' ', '.'], R.Values, True);
       TargetInfo^.TargetPID := StrToIntDef(s, 0);
       if TargetInfo^.TargetPID <> 0 then exit;
     end;
