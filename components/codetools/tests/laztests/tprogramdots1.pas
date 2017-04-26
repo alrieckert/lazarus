737c6e2606266904b4478e87097b214a5a7d64da
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2017-04-26 18:33:51 +0000

    codetools: tests: namespace in uses section
    git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@54744 4005530d-fff6-0310-9dd1-cebe43e6787f

--- components/codetools/tests/laztests/tprogramdots1.pas
+++ components/codetools/tests/laztests/tprogramdots1.pas
@@ -3,7 +3,9 @@ program unitdots.tprogramdots1; // defines program namespace unitdots
 {$mode objfpc}{$H+}
 
 uses
-  dot{declaration:unitdots.dot};
+  dot{declaration:unitdots.dot},
+  unitdots{declaration:unitdots}.nsA{declaration:unitdots.nsA}
+    .nsAA{declaration:unitdots.nsA.nsAA}.nsAAA{declaration:unitdots.nsA.nsAA.nsAAA};
 
 begin
 end.
